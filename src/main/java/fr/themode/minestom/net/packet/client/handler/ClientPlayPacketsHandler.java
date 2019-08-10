9a18b845b38dce29e691b011af1dbe61eafebc8a
TheMode <themode@outlook.fr>
2019-08-10 04:17:45 +0200

    registered ClientPlayerLookPacket

--- src/main/java/fr/themode/minestom/net/packet/client/handler/ClientPlayPacketsHandler.java
+++ src/main/java/fr/themode/minestom/net/packet/client/handler/ClientPlayPacketsHandler.java
@@ -5,11 +5,12 @@ import fr.themode.minestom.net.packet.client.play.*;
 public class ClientPlayPacketsHandler extends ClientPacketsHandler {
 
     public ClientPlayPacketsHandler() {
+        register(0x00, ClientTeleportConfirmPacket.class);
         register(0x05, ClientSettingsPacket.class);
         register(0x0B, ClientPluginMessagePacket.class);
         register(0x11, ClientPlayerPositionPacket.class);
         register(0x12, ClientPlayerPositionAndLookPacket.class);
-        register(0x00, ClientTeleportConfirmPacket.class);
+        register(0x13, ClientPlayerLookPacket.class);
     }
 
 }
