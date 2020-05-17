4c889923f10b5ab430bbf43d08d0d61ec6ef4020
Felix Cravic <themode@outlook.fr>
2020-05-17 03:56:36 +0200

    Added more TimeUnit (HOUR/MINUTE/SECOND)

--- src/main/java/net/minestom/server/utils/time/TimeUnit.java
+++ src/main/java/net/minestom/server/utils/time/TimeUnit.java
@@ -4,16 +4,23 @@ import net.minestom.server.MinecraftServer;
 
 public enum TimeUnit {
 
-    TICK, MILLISECOND;
+    TICK, HOUR, MINUTE, SECOND, MILLISECOND;
 
     public long toMilliseconds(long value) {
         switch (this) {
             case TICK:
                 return MinecraftServer.TICK_MS * value;
+            case HOUR:
+                return value * 3_600_000;
+            case MINUTE:
+                return value * 60_000;
+            case SECOND:
+                return value * 1000;
             case MILLISECOND:
                 return value;
+            default:
+                return -1; // Unexpected
         }
-        return -1; // Unexpected
     }
 
 }
