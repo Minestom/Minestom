4fb03a8479df04b33d406141a994881401644680
jglrxavpok <jglrxavpok@gmail.com>
2020-07-01 20:36:19 +0200

    Added normals to Directions

--- src/main/java/net/minestom/server/utils/Direction.java
+++ src/main/java/net/minestom/server/utils/Direction.java
@@ -1,14 +1,36 @@
 package net.minestom.server.utils;
 
 public enum Direction {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST,
-    UP,
-    DOWN;
+    NORTH(0, 0, -1),
+    EAST(1, 0, 0),
+    SOUTH(0, 0, 1),
+    WEST(-1, 0, 0),
+    UP(0, 1, 0),
+    DOWN(0, -1, 0);
 
     public static final Direction[] HORIZONTAL = { SOUTH, WEST, NORTH, EAST };
+
+    private final int normalX;
+    private final int normalY;
+    private final int normalZ;
+
+    Direction(int normalX, int normalY, int normalZ) {
+        this.normalX = normalX;
+        this.normalY = normalY;
+        this.normalZ = normalZ;
+    }
+
+    public int normalX() {
+        return normalX;
+    }
+
+    public int normalY() {
+        return normalY;
+    }
+
+    public int normalZ() {
+        return normalZ;
+    }
 
     public Direction opposite() {
         switch (this) {
