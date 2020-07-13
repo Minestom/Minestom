cca65f8d42748517c9960edefb620f2720e2f884
Articdive <13535885+Articdive@users.noreply.github.com>
2020-07-13 19:34:32 +0200

    Update dependncies and JUnit

--- src/test/java/tags/TestTags.java
+++ src/test/java/tags/TestTags.java
@@ -3,10 +3,10 @@ package tags;
 import net.minestom.server.gamedata.tags.Tag;
 import net.minestom.server.gamedata.tags.TagManager;
 import net.minestom.server.utils.NamespaceID;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 import java.io.StringReader;
@@ -15,7 +15,7 @@ public class TestTags {
 
     private TagManager tags;
 
-    @Before
+    @BeforeEach
     public void init() {
         tags = new TagManager();
     }
@@ -36,13 +36,13 @@ public class TestTags {
                 "\t\t\"minestom:some_other_item\"\n" +
                 "\t]\n" +
                 "}";
-        Assert.assertNotEquals(Tag.EMPTY, tags.load(NamespaceID.from("minestom:test_sub"), "any", new StringReader(tag1)));
+        Assertions.assertNotEquals(Tag.EMPTY, tags.load(NamespaceID.from("minestom:test_sub"), "any", new StringReader(tag1)));
         Tag loaded = tags.load(NamespaceID.from("minestom:test"), "any", new StringReader(tag2));
         NamespaceID[] values = loaded.getValues().toArray(new NamespaceID[0]);
-        Assert.assertEquals(2, values.length);
-        Assert.assertTrue(loaded.contains(NamespaceID.from("minestom:an_item")));
-        Assert.assertTrue(loaded.contains(NamespaceID.from("minestom:some_other_item")));
-        Assert.assertFalse(loaded.contains(NamespaceID.from("minestom:some_other_item_that_is_not_in_the_tag")));
+        Assertions.assertEquals(2, values.length);
+        Assertions.assertTrue(loaded.contains(NamespaceID.from("minestom:an_item")));
+        Assertions.assertTrue(loaded.contains(NamespaceID.from("minestom:some_other_item")));
+        Assertions.assertFalse(loaded.contains(NamespaceID.from("minestom:some_other_item_that_is_not_in_the_tag")));
     }
 
     /**
@@ -63,12 +63,12 @@ public class TestTags {
                 "\t\t\"minestom:some_other_item\"\n" +
                 "\t]\n" +
                 "}";
-        Assert.assertNotEquals(Tag.EMPTY, tags.load(NamespaceID.from("minestom:test"), "any", new StringReader(tag1)));
+        Assertions.assertNotEquals(Tag.EMPTY, tags.load(NamespaceID.from("minestom:test"), "any", new StringReader(tag1)));
         Tag loaded = tags.forceLoad(NamespaceID.from("minestom:test"), "any", () -> new StringReader(tag2));
-        Assert.assertNotEquals(Tag.EMPTY, loaded);
-        Assert.assertEquals(1, loaded.getValues().size());
-        Assert.assertTrue(loaded.contains(NamespaceID.from("minestom:some_other_item")));
-        Assert.assertFalse(loaded.contains(NamespaceID.from("minestom:an_item")));
+        Assertions.assertNotEquals(Tag.EMPTY, loaded);
+        Assertions.assertEquals(1, loaded.getValues().size());
+        Assertions.assertTrue(loaded.contains(NamespaceID.from("minestom:some_other_item")));
+        Assertions.assertFalse(loaded.contains(NamespaceID.from("minestom:an_item")));
     }
 
     /**
@@ -89,15 +89,15 @@ public class TestTags {
                 "\t\t\"minestom:some_other_item\"\n" +
                 "\t]\n" +
                 "}";
-        Assert.assertNotEquals(Tag.EMPTY, tags.load(NamespaceID.from("minestom:test"), "any", new StringReader(tag1)));
+        Assertions.assertNotEquals(Tag.EMPTY, tags.load(NamespaceID.from("minestom:test"), "any", new StringReader(tag1)));
         Tag loaded = tags.forceLoad(NamespaceID.from("minestom:test"), "any", () -> new StringReader(tag2));
-        Assert.assertNotEquals(Tag.EMPTY, loaded);
-        Assert.assertEquals(2, loaded.getValues().size());
-        Assert.assertTrue(loaded.contains(NamespaceID.from("minestom:some_other_item")));
-        Assert.assertTrue(loaded.contains(NamespaceID.from("minestom:an_item")));
+        Assertions.assertNotEquals(Tag.EMPTY, loaded);
+        Assertions.assertEquals(2, loaded.getValues().size());
+        Assertions.assertTrue(loaded.contains(NamespaceID.from("minestom:some_other_item")));
+        Assertions.assertTrue(loaded.contains(NamespaceID.from("minestom:an_item")));
     }
 
-    @After
+    @AfterEach
     public void cleanup() {
         tags = null;
     }
