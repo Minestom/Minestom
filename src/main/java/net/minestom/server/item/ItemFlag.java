78f4bc0a13313a95f7e2c490365dca5b0affc241
Articdive <13535885+Articdive@users.noreply.github.com>
2020-07-29 11:17:59 +0200

    Started work on item hovering.

--- src/main/java/net/minestom/server/item/ItemFlag.java
+++ src/main/java/net/minestom/server/item/ItemFlag.java
@@ -1,10 +1,19 @@
 package net.minestom.server.item;
 
 public enum ItemFlag {
-    HIDE_ENCHANTS,
-    HIDE_ATTRIBUTES,
-    HIDE_UNBREAKABLE,
-    HIDE_DESTROYS,
-    HIDE_PLACED_ON,
-    HIDE_POTION_EFFECTS,
+    HIDE_ENCHANTS(1),
+    HIDE_ATTRIBUTES(2),
+    HIDE_UNBREAKABLE(4),
+    HIDE_DESTROYS(8),
+    HIDE_PLACED_ON(16),
+    HIDE_POTION_EFFECTS(32);
+
+    private final int bitFieldPart;
+    ItemFlag(int bit) {
+        this.bitFieldPart = bit;
+    }
+
+    public int getBitFieldPart() {
+        return bitFieldPart;
+    }
 }
