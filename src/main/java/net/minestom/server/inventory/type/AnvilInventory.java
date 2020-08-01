d980b3f56cdc6ff230d2e5e2628fda6418d788b5
Felix Cravic <themode@outlook.fr>
2020-08-01 10:33:06 +0200

    Updated inventory types

--- src/main/java/net/minestom/server/inventory/type/AnvilInventory.java
+++ src/main/java/net/minestom/server/inventory/type/AnvilInventory.java
@@ -6,11 +6,28 @@ import net.minestom.server.inventory.InventoryType;
 
 public class AnvilInventory extends Inventory {
 
+    private short repairCost;
+
     public AnvilInventory(String title) {
         super(InventoryType.ANVIL, title);
     }
 
+    /**
+     * Get the anvil repair cost
+     *
+     * @return the repair cost
+     */
+    public short getRepairCost() {
+        return repairCost;
+    }
+
+    /**
+     * Set the anvil repair cost
+     *
+     * @param cost the new anvil repair cost
+     */
     public void setRepairCost(short cost) {
+        this.repairCost = repairCost;
         sendProperty(InventoryProperty.ANVIL_REPAIR_COST, cost);
     }
 }
--- src/main/java/net/minestom/server/inventory/type/EnchantmentTableInventory.java
+++ src/main/java/net/minestom/server/inventory/type/EnchantmentTableInventory.java
@@ -1,11 +1,143 @@
 package net.minestom.server.inventory.type;
 
 import net.minestom.server.inventory.Inventory;
+import net.minestom.server.inventory.InventoryProperty;
 import net.minestom.server.inventory.InventoryType;
+import net.minestom.server.item.Enchantment;
 
 public class EnchantmentTableInventory extends Inventory {
+
+    private short[] levelRequirements = new short[EnchantmentSlot.values().length];
+    private short seed;
+    private short[] enchantmentShown = new short[EnchantmentSlot.values().length];
+    private short[] enchantmentLevel = new short[EnchantmentSlot.values().length];
 
     public EnchantmentTableInventory(String title) {
         super(InventoryType.ENCHANTMENT, title);
     }
+
+    /**
+     * Get the level requirement in a slot
+     *
+     * @param enchantmentSlot the slot to check the level requirement
+     * @return the level requirement of the slot
+     */
+    public short getLevelRequirement(EnchantmentSlot enchantmentSlot) {
+        return levelRequirements[enchantmentSlot.ordinal()];
+    }
+
+    /**
+     * Set the level requirement of a slot
+     *
+     * @param enchantmentSlot the slot
+     * @param level           the level
+     */
+    public void setLevelRequirement(EnchantmentSlot enchantmentSlot, short level) {
+        switch (enchantmentSlot) {
+            case TOP:
+                sendProperty(InventoryProperty.ENCHANTMENT_TABLE_LEVEL_REQUIREMENT_TOP, level);
+                break;
+            case MIDDLE:
+                sendProperty(InventoryProperty.ENCHANTMENT_TABLE_LEVEL_REQUIREMENT_MIDDLE, level);
+                break;
+            case BOTTOM:
+                sendProperty(InventoryProperty.ENCHANTMENT_TABLE_LEVEL_REQUIREMENT_BOTTOM, level);
+                break;
+        }
+        this.levelRequirements[enchantmentSlot.ordinal()] = level;
+    }
+
+    /**
+     * Get the enchantment seed
+     *
+     * @return the enchantment seed
+     */
+    public short getSeed() {
+        return seed;
+    }
+
+    /**
+     * Set the enchantment seed
+     *
+     * @param seed the enchantment seed
+     */
+    public void setSeed(short seed) {
+        this.seed = seed;
+        sendProperty(InventoryProperty.ENCHANTMENT_TABLE_SEED, seed);
+    }
+
+    /**
+     * Get the enchantment shown in a slot
+     *
+     * @param enchantmentSlot the enchantment slot
+     * @return the enchantment shown in the slot, null if it is hidden
+     */
+    public Enchantment getEnchantmentShown(EnchantmentSlot enchantmentSlot) {
+        final short id = enchantmentShown[enchantmentSlot.ordinal()];
+        if (id == -1)
+            return null;
+        return Enchantment.fromId(id);
+    }
+
+    /**
+     * Set the enchantment shown in a slot
+     * <p>
+     * Can be set to null to hide it
+     *
+     * @param enchantmentSlot the enchantment slot
+     * @param enchantment     the enchantment
+     */
+    public void setEnchantmentShown(EnchantmentSlot enchantmentSlot, Enchantment enchantment) {
+        final short id = enchantment == null ? -1 : (short) enchantment.getId();
+        switch (enchantmentSlot) {
+            case TOP:
+                sendProperty(InventoryProperty.ENCHANTMENT_TABLE_ENCH_ID_TOP, id);
+                break;
+            case MIDDLE:
+                sendProperty(InventoryProperty.ENCHANTMENT_TABLE_ENCH_ID_MIDDLE, id);
+                break;
+            case BOTTOM:
+                sendProperty(InventoryProperty.ENCHANTMENT_TABLE_ENCH_ID_BOTTOM, id);
+                break;
+        }
+        this.enchantmentShown[enchantmentSlot.ordinal()] = id;
+    }
+
+    /**
+     * Get the enchantment level shown on mouse hover
+     *
+     * @param enchantmentSlot the enchantment slot
+     * @return the level shown, -1 if no enchant
+     */
+    public short getEnchantmentLevel(EnchantmentSlot enchantmentSlot) {
+        return enchantmentLevel[enchantmentSlot.ordinal()];
+    }
+
+    /**
+     * Set the enchantment level shown on mouse hover
+     * <p>
+     * Can be set to -1 if no enchant
+     *
+     * @param enchantmentSlot the enchantment slot
+     * @param level           the level shown
+     */
+    public void setEnchantmentLevel(EnchantmentSlot enchantmentSlot, short level) {
+        switch (enchantmentSlot) {
+            case TOP:
+                sendProperty(InventoryProperty.ENCHANTMENT_TABLE_ENCH_LEVEL_TOP, level);
+                break;
+            case MIDDLE:
+                sendProperty(InventoryProperty.ENCHANTMENT_TABLE_ENCH_LEVEL_MIDDLE, level);
+                break;
+            case BOTTOM:
+                sendProperty(InventoryProperty.ENCHANTMENT_TABLE_ENCH_LEVEL_BOTTOM, level);
+                break;
+        }
+        this.enchantmentLevel[enchantmentSlot.ordinal()] = level;
+    }
+
+    public enum EnchantmentSlot {
+        TOP, MIDDLE, BOTTOM
+    }
+
 }
