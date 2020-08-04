98beac9b0f3e7ddcd67ce42485d8cccdd8ba2145
Articdive <13535885+Articdive@users.noreply.github.com>
2020-08-04 16:33:43 +0200

    Fix Indentation for autogenerated classes.

--- src/autogenerated/java/net/minestom/server/fluids/Fluid.java
+++ src/autogenerated/java/net/minestom/server/fluids/Fluid.java
@@ -7,31 +7,31 @@ import net.minestom.server.utils.NamespaceID;
 @SuppressWarnings({"deprecation"})
 
 public enum Fluid {
-	EMPTY("minecraft:empty"),
-	FLOWING_WATER("minecraft:flowing_water"),
-	WATER("minecraft:water"),
-	FLOWING_LAVA("minecraft:flowing_lava"),
-	LAVA("minecraft:lava"),
+    EMPTY("minecraft:empty"),
+    FLOWING_WATER("minecraft:flowing_water"),
+    WATER("minecraft:water"),
+    FLOWING_LAVA("minecraft:flowing_lava"),
+    LAVA("minecraft:lava"),
 ;
-	private String namespaceID;
+    private String namespaceID;
 
-	Fluid(String namespaceID) {
-		this.namespaceID = namespaceID;
-		Registries.fluids.put(NamespaceID.from(namespaceID), this);
-	}
+    Fluid(String namespaceID) {
+        this.namespaceID = namespaceID;
+        Registries.fluids.put(NamespaceID.from(namespaceID), this);
+    }
 
-	public int getId() {
-		return ordinal();
-	}
+    public int getId() {
+        return ordinal();
+    }
 
-	public String getNamespaceID() {
-		return namespaceID;
-	}
+    public String getNamespaceID() {
+        return namespaceID;
+    }
 
-	public static Fluid fromId(int id) {
-		if(id >= 0 && id < values().length) {
-			return values()[id];
-		}
-		return EMPTY;
-	}
+    public static Fluid fromId(int id) {
+        if(id >= 0 && id < values().length) {
+        	return values()[id];
+        }
+        return EMPTY;
+    }
 }
