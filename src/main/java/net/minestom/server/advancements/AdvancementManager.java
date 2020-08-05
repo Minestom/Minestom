87fcb83837b3f042d60b09a6af735a132dda1e8b
Felix Cravic <themode@outlook.fr>
2020-08-05 11:13:57 +0200

    You cannot create two advancement tab with the same identifier

--- src/main/java/net/minestom/server/advancements/AdvancementManager.java
+++ src/main/java/net/minestom/server/advancements/AdvancementManager.java
@@ -1,5 +1,7 @@
 package net.minestom.server.advancements;
 
+import net.minestom.server.utils.validate.Check;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +10,8 @@ public class AdvancementManager {
     private Map<String, AdvancementTab> advancementTabMap = new HashMap<>();
 
     public AdvancementTab createTab(String rootIdentifier, AdvancementRoot root) {
+        Check.stateCondition(advancementTabMap.containsKey(rootIdentifier),
+                "A tab with the identifier '" + rootIdentifier + "' already exists");
         final AdvancementTab advancementTab = new AdvancementTab(rootIdentifier, root);
         this.advancementTabMap.put(rootIdentifier, advancementTab);
         return advancementTab;
