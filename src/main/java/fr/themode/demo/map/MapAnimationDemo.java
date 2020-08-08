3144f64b06f6db33b8a90b19bdaed4e1895bacb7
jglrxavpok <jglrxavpok@gmail.com>
2020-08-08 18:35:28 +0200

    Example demonstrates multiple OpenGL contexts possible

--- src/main/java/fr/themode/demo/map/MapAnimationDemo.java
+++ src/main/java/fr/themode/demo/map/MapAnimationDemo.java
@@ -18,10 +18,12 @@ import static org.lwjgl.opengl.GL11.*;
 public class MapAnimationDemo {
 
     public static final int MAP_ID = 1;
-    public static final int EGL_MAP_ID = 2;
+    public static final int OPENGL_MAP_ID = 2;
+    public static final int OPENGL2_MAP_ID = 3;
 
     private static final Graphics2DFramebuffer framebuffer = new Graphics2DFramebuffer();
     private static final GLFWFramebuffer glfwFramebuffer = new GLFWFramebuffer();
+    private static final GLFWFramebuffer glfwFramebuffer2 = new GLFWFramebuffer();
 
     public static void init() {
         SchedulerManager scheduler = MinecraftServer.getSchedulerManager();
@@ -34,13 +36,35 @@ public class MapAnimationDemo {
                 player.getInventory().addItemStack(map);
 
                 ItemStack map2 = new ItemStack(Material.FILLED_MAP, (byte) 1);
-                map2.setItemMeta(new MapMeta(EGL_MAP_ID));
+                map2.setItemMeta(new MapMeta(OPENGL_MAP_ID));
                 player.getInventory().addItemStack(map2);
+
+                ItemStack map3 = new ItemStack(Material.FILLED_MAP, (byte) 1);
+                map3.setItemMeta(new MapMeta(OPENGL2_MAP_ID));
+                player.getInventory().addItemStack(map3);
             });
         });
 
         glfwFramebuffer.setupRenderLoop(16, TimeUnit.MILLISECOND, () -> {
             glClearColor(0f, 0f, 0f, 1f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glBegin(GL_TRIANGLES);
+
+            glVertex2f(0, -0.75f);
+            glColor3f(1f, 0f, 0f);
+
+            glVertex2f(0.75f, 0.75f);
+            glColor3f(0f, 1f, 0f);
+
+            glVertex2f(-0.75f, 0.75f);
+            glColor3f(0f, 0f, 1f);
+
+            glEnd();
+        });
+
+        glfwFramebuffer2.setupRenderLoop(16, TimeUnit.MILLISECOND, () -> {
+            glClearColor(0f, 1f, 0f, 1f);
             glClear(GL_COLOR_BUFFER_BIT);
 
             glBegin(GL_TRIANGLES);
@@ -95,8 +119,14 @@ public class MapAnimationDemo {
             p.getPlayerConnection().sendPacket(mapDataPacket);
         });
 
-        mapDataPacket.mapId = EGL_MAP_ID;
+        mapDataPacket.mapId = OPENGL_MAP_ID;
         glfwFramebuffer.preparePacket(mapDataPacket);
+        MinecraftServer.getConnectionManager().getOnlinePlayers().forEach(p -> {
+            p.getPlayerConnection().sendPacket(mapDataPacket);
+        });
+
+        mapDataPacket.mapId = OPENGL2_MAP_ID;
+        glfwFramebuffer2.preparePacket(mapDataPacket);
         MinecraftServer.getConnectionManager().getOnlinePlayers().forEach(p -> {
             p.getPlayerConnection().sendPacket(mapDataPacket);
         });
