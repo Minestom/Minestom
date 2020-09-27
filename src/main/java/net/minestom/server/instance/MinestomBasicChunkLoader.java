d43ba6a39520b06d5d4a8b2901b17ab05a9f5842
themode <themode@outlook.fr>
2020-09-27 20:45:46 +0200

    Comments for MinestomBasicChunkLoader

--- src/main/java/net/minestom/server/instance/MinestomBasicChunkLoader.java
+++ src/main/java/net/minestom/server/instance/MinestomBasicChunkLoader.java
@@ -6,6 +6,15 @@ import net.minestom.server.utils.chunk.ChunkCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * A {@link IChunkLoader} used by default by {@link InstanceContainer}
+ * which is based on the {@link StorageLocation} associated to it
+ * <p>
+ * It simply save chunk serialized data from {@link Chunk#getSerializedData()}
+ * and deserialize it later with {@link Chunk#readChunk(BinaryReader, ChunkCallback)}
+ * <p>
+ * The key used in the {@link StorageLocation} is defined by {@link #getChunkKey(int, int)} and should NOT be changed
+ */
 public class MinestomBasicChunkLoader implements IChunkLoader {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(MinestomBasicChunkLoader.class);
@@ -27,13 +36,17 @@ public class MinestomBasicChunkLoader implements IChunkLoader {
         final int chunkZ = chunk.getChunkZ();
 
         final String key = getChunkKey(chunkX, chunkZ);
+
+        // Serialize the chunk
         final byte[] data = chunk.getSerializedData();
         if (data == null) {
+            // Chunk cannot be serialized (returned null), stop here
             if (callback != null)
                 callback.run();
             return;
         }
 
+        // Save the serialized data to the storage location
         storageLocation.set(key, data);
 
         if (callback != null)
@@ -45,6 +58,7 @@ public class MinestomBasicChunkLoader implements IChunkLoader {
         final byte[] bytes = storageLocation == null ? null : storageLocation.get(getChunkKey(chunkX, chunkZ));
 
         if (bytes == null) {
+            // Chunk is not saved in the storage location
             return false;
         } else {
             // Found, load from result bytes
