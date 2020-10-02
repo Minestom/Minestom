64ee739e1fa44bcc6029c87168ef6ed3c6310877
Articdive <13535885+Articdive@users.noreply.github.com>
2020-10-02 18:18:04 +0200

    Update Recipe code, add abstraction for different recipe packet types.

--- src/main/java/net/minestom/server/recipe/ShapelessRecipe.java
+++ src/main/java/net/minestom/server/recipe/ShapelessRecipe.java
@@ -2,34 +2,54 @@ package net.minestom.server.recipe;
 
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.network.packet.server.play.DeclareRecipesPacket;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public abstract class ShapelessRecipe extends Recipe {
+    private String group;
+    private final List<DeclareRecipesPacket.Ingredient> ingredients;
+    private ItemStack result;
 
-    private List<DeclareRecipesPacket.Ingredient> ingredients = new ArrayList<>();
-
-    private ItemStack result = ItemStack.getAirItem();
-
-    public ShapelessRecipe(String recipeId, String group) {
+    protected ShapelessRecipe(
+            @NotNull String recipeId,
+            @NotNull String group,
+            @Nullable List<DeclareRecipesPacket.Ingredient> ingredients,
+            @NotNull ItemStack result
+    ) {
         super(RecipeType.SHAPELESS, recipeId);
-        setGroup(group);
+        this.group = group;
+        this.ingredients = Objects.requireNonNullElseGet(ingredients, LinkedList::new);
+        this.result = result;
+    }
+
+    @NotNull
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(@NotNull String group) {
+        this.group = group;
     }
 
     public void addIngredient(DeclareRecipesPacket.Ingredient ingredient) {
         ingredients.add(ingredient);
     }
 
+    @NotNull
     public List<DeclareRecipesPacket.Ingredient> getIngredients() {
         return ingredients;
     }
 
+    @NotNull
     public ItemStack getResult() {
         return result;
     }
 
-    public void setResult(ItemStack result) {
+    public void setResult(@NotNull ItemStack result) {
         this.result = result;
     }
 }
--- src/main/java/net/minestom/server/recipe/SmeltingRecipe.java
+++ src/main/java/net/minestom/server/recipe/SmeltingRecipe.java
@@ -2,35 +2,53 @@ package net.minestom.server.recipe;
 
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.network.packet.server.play.DeclareRecipesPacket;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class SmeltingRecipe extends Recipe {
-
+    private String group;
     private DeclareRecipesPacket.Ingredient ingredient;
-
-    private ItemStack result = ItemStack.getAirItem();
-
+    private ItemStack result;
     private float experience;
-
     private int cookingTime;
 
-    public SmeltingRecipe(String recipeId, String group) {
+    protected SmeltingRecipe(
+            @NotNull String recipeId,
+            @NotNull String group,
+            @NotNull ItemStack result,
+            float experience,
+            int cookingTime
+    ) {
         super(RecipeType.SMELTING, recipeId);
-        setGroup(group);
+        this.group = group;
+        this.result = result;
+        this.experience = experience;
+        this.cookingTime = cookingTime;
     }
 
+    @NotNull
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(@NotNull String group) {
+        this.group = group;
+    }
+
+    @NotNull
     public DeclareRecipesPacket.Ingredient getIngredient() {
         return ingredient;
     }
 
-    public void setIngredient(DeclareRecipesPacket.Ingredient ingredient) {
+    public void setIngredient(@NotNull DeclareRecipesPacket.Ingredient ingredient) {
         this.ingredient = ingredient;
     }
 
+    @NotNull
     public ItemStack getResult() {
         return result;
     }
 
-    public void setResult(ItemStack result) {
+    public void setResult(@NotNull ItemStack result) {
         this.result = result;
     }
 
