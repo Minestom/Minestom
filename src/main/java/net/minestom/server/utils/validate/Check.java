6bb7186b3fe65215f76d46ac29413b7a69a4fa2b
Articdive <13535885+Articdive@users.noreply.github.com>
2020-10-24 10:45:27 +0200

    Cleanup and ensure extensions have valid names.

--- src/main/java/net/minestom/server/utils/validate/Check.java
+++ src/main/java/net/minestom/server/utils/validate/Check.java
@@ -1,22 +1,31 @@
 package net.minestom.server.utils.validate;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.Objects;
 
-public class Check {
+public final class Check {
+    private Check() {
 
-    public static void notNull(Object object, String reason) {
-        if (Objects.isNull(object))
+    }
+
+    public static void notNull(@Nullable Object object, @NotNull String reason) {
+        if (Objects.isNull(object)) {
             throw new NullPointerException(reason);
+        }
     }
 
-    public static void argCondition(boolean condition, String reason) {
-        if (condition)
+    public static void argCondition(boolean condition, @NotNull String reason) {
+        if (condition) {
             throw new IllegalArgumentException(reason);
+        }
     }
 
-    public static void stateCondition(boolean condition, String reason) {
-        if (condition)
+    public static void stateCondition(boolean condition, @NotNull String reason) {
+        if (condition) {
             throw new IllegalStateException(reason);
+        }
     }
 
 }
