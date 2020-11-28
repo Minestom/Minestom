a7b6a20cae9b7dc1651cac854b7a1c34d321f646
Felix Cravic <themode@outlook.fr>
2020-11-28 20:57:08 +0100

    Prevent enabling mojang auth multiple time + cleanup

--- src/main/java/net/minestom/server/extras/MojangAuth.java
+++ src/main/java/net/minestom/server/extras/MojangAuth.java
@@ -5,6 +5,7 @@ import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.extras.mojangAuth.MojangCrypt;
+import net.minestom.server.utils.validate.Check;
 import org.jetbrains.annotations.Nullable;
 
 import java.net.Proxy;
@@ -24,16 +25,15 @@ public final class MojangAuth {
      * Be aware that enabling a proxy will make Mojang authentication ignored.
      */
     public static void init() {
-        if (MinecraftServer.getNettyServer().getAddress() == null) {
-            enabled = true;
+        Check.stateCondition(enabled, "Mojang auth is already enabled!");
+        Check.stateCondition(MinecraftServer.isStarted(), "The server has already been started!");
 
-            // Generate necessary fields...
-            keyPair = MojangCrypt.generateKeyPair();
-            authService = new YggdrasilAuthenticationService(Proxy.NO_PROXY, "");
-            sessionService = authService.createMinecraftSessionService();
-        } else {
-            throw new IllegalStateException("The server has already been started");
-        }
+        enabled = true;
+
+        // Generate necessary fields...
+        keyPair = MojangCrypt.generateKeyPair();
+        authService = new YggdrasilAuthenticationService(Proxy.NO_PROXY, "");
+        sessionService = authService.createMinecraftSessionService();
     }
 
     public static boolean isEnabled() {
