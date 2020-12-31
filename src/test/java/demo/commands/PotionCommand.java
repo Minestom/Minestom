6a154eb15ada438ab4614c5e596b66f356b191be
ThatCreeper <creeper_host@yahoo.com>
2020-12-30 18:29:54 -0600

    Fix /potion demo command

--- src/test/java/demo/commands/PotionCommand.java
+++ src/test/java/demo/commands/PotionCommand.java
@@ -40,12 +40,10 @@ public class PotionCommand extends Command {
         final PotionEffect potion = args.getPotionEffect("potion");
         final int duration = args.getInteger("duration");
 
-        player.sendMessage(player.getActiveEffects().toString());
-
         player.addEffect(new Potion(
                 potion,
                 (byte) 0,
-                duration
+                duration * 20
         ));
     }
 
