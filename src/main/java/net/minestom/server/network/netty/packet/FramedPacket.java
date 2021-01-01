4d2dd7d25694d7720cc9385260ca41ed95f897d7
themode <themode@outlook.fr>
2021-01-01 23:40:22 +0100

    Fix javadoc build

--- src/main/java/net/minestom/server/network/netty/packet/FramedPacket.java
+++ src/main/java/net/minestom/server/network/netty/packet/FramedPacket.java
@@ -4,7 +4,7 @@ import io.netty.buffer.ByteBuf;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Represents a packet which is already framed. (packet id->payload) + optional compression
+ * Represents a packet which is already framed. (packet id+payload) + optional compression
  * Can be used if you want to send the exact same buffer to multiple clients without processing it more than once.
  */
 public class FramedPacket {
