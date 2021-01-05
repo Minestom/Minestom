852391f21062dff992f94f02f2a24b223a5e599b
themode <themode@outlook.fr>
2021-01-05 18:10:25 +0100

    Fix packet listeners not being called if there is no default listener

--- src/main/java/net/minestom/server/listener/manager/PacketListenerManager.java
+++ src/main/java/net/minestom/server/listener/manager/PacketListenerManager.java
@@ -71,8 +71,7 @@ public final class PacketListenerManager {
 
         // Listener can be null if none has been set before, call PacketConsumer anyway
         if (packetListenerConsumer == null) {
-            LOGGER.error("Packet " + clazz + " does not have any default listener! (The issue comes from Minestom)");
-            return;
+            LOGGER.warn("Packet " + clazz + " does not have any default listener! (The issue comes from Minestom)");
         }
 
         final PacketController packetController = new PacketController();
@@ -84,7 +83,9 @@ public final class PacketListenerManager {
             return;
 
         // Finally execute the listener
-        packetListenerConsumer.accept(packet, player);
+        if (packetListenerConsumer != null) {
+            packetListenerConsumer.accept(packet, player);
+        }
     }
 
     /**
