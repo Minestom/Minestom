99cf74276265e441910ea425fcb632e15d62b447
LeoDog896 <LeoDog896@hotmail.com>
2021-01-18 19:27:16 -0500

    *Unnoticed test fixed*

--- src/test/java/demo/commands/TeleportCommand.java
+++ src/test/java/demo/commands/TeleportCommand.java
@@ -6,26 +6,28 @@ import net.minestom.server.command.builder.Arguments;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.arguments.Argument;
 import net.minestom.server.command.builder.arguments.ArgumentType;
+import net.minestom.server.command.builder.arguments.ArgumentWord;
+import net.minestom.server.command.builder.arguments.relative.ArgumentRelativeVec3;
 import net.minestom.server.entity.Player;
 import net.minestom.server.utils.Position;
 import net.minestom.server.utils.location.RelativeVec;
 
 public class TeleportCommand extends Command {
 
+    private static final ArgumentRelativeVec3 posArg = ArgumentType.RelativeVec3("pos");
+    private static final ArgumentWord playerArg = ArgumentType.Word("player");
+
     public TeleportCommand() {
         super("tp");
 
         setDefaultExecutor((source, args) -> source.sendMessage("Usage: /tp x y z"));
-
-        Argument posArg = ArgumentType.RelativeVec3("pos");
-        Argument playerArg = ArgumentType.Word("player");
 
         addSyntax(this::onPlayerTeleport, playerArg);
         addSyntax(this::onPositionTeleport, posArg);
     }
 
     private void onPlayerTeleport(CommandSender sender, Arguments args) {
-        final String playerName = args.getWord("player");
+        final String playerName = args.get(playerArg);
         Player pl = MinecraftServer.getConnectionManager().getPlayer(playerName);
         if (pl != null && sender.isPlayer()) {
             Player player = (Player) sender;
@@ -37,7 +39,7 @@ public class TeleportCommand extends Command {
     private void onPositionTeleport(CommandSender sender, Arguments args) {
         final Player player = sender.asPlayer();
 
-        final RelativeVec relativeVec = args.getRelativeVector("pos");
+        final RelativeVec relativeVec = args.get(posArg);
         final Position position = relativeVec.fromRelativePosition(player).toPosition();
 
         player.teleport(position);
