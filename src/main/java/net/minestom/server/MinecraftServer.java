dd1b67e5dee5c444ea1c3a8767486ad4d14a1a8c
themode <themode@outlook.fr>
2021-02-01 18:22:36 +0100

    Enable netty error processing by default

--- src/main/java/net/minestom/server/MinecraftServer.java
+++ src/main/java/net/minestom/server/MinecraftServer.java
@@ -98,7 +98,7 @@ public final class MinecraftServer {
     private static PacketProcessor packetProcessor;
     private static NettyServer nettyServer;
     private static int nettyThreadCount = Runtime.getRuntime().availableProcessors();
-    private static boolean processNettyErrors = false;
+    private static boolean processNettyErrors = true;
 
     private static ExceptionManager exceptionManager;
 
