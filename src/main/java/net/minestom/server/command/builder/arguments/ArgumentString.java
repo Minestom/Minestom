367889a37417af7a759a7e413c108f016ff84615
LeoDog896 <LeoDog896@hotmail.com>
2021-02-12 15:27:18 -0500

    No magic strings in ArgumentString

--- src/main/java/net/minestom/server/command/builder/arguments/ArgumentString.java
+++ src/main/java/net/minestom/server/command/builder/arguments/ArgumentString.java
@@ -1,5 +1,6 @@
 package net.minestom.server.command.builder.arguments;
 
+import io.netty.util.internal.StringUtil;
 import net.minestom.server.command.builder.NodeMaker;
 import net.minestom.server.command.builder.exception.ArgumentSyntaxException;
 import net.minestom.server.network.packet.server.play.DeclareCommandsPacket;
@@ -12,6 +13,8 @@ import org.jetbrains.annotations.NotNull;
  * Example: "Hey I am a string"
  */
 public class ArgumentString extends Argument<String> {
+
+    public static final char BACKSLASH = '\\';
 
     public static final int QUOTE_ERROR = 1;
 
@@ -42,7 +45,7 @@ public class ArgumentString extends Argument<String> {
         // Check if value start and end with quote
         final char first = input.charAt(0);
         final char last = input.charAt(input.length() - 1);
-        final boolean quote = first == '\"' && last == '\"';
+        final boolean quote = first == StringUtil.DOUBLE_QUOTE && last == StringUtil.DOUBLE_QUOTE;
         if (!quote)
             throw new ArgumentSyntaxException("String argument needs to start and end with quotes", input, QUOTE_ERROR);
 
@@ -52,9 +55,9 @@ public class ArgumentString extends Argument<String> {
         // Verify backslashes
         for (int i = 1; i < input.length(); i++) {
             final char c = input.charAt(i);
-            if (c == '\"') {
+            if (c == StringUtil.DOUBLE_QUOTE) {
                 final char lastChar = input.charAt(i - 1);
-                if (lastChar != '\\') {
+                if (lastChar != BACKSLASH) {
                     throw new ArgumentSyntaxException("Non-escaped quote", input, QUOTE_ERROR);
                 }
             }
