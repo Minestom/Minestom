437afc28ffc781ec4e7179c6141f3a7858c330cb
themode <themode@outlook.fr>
2021-02-14 07:52:27 +0100

    Enable suggestion by default for ArgumentEntityType

--- src/main/java/net/minestom/server/command/builder/arguments/minecraft/registry/ArgumentEntityType.java
+++ src/main/java/net/minestom/server/command/builder/arguments/minecraft/registry/ArgumentEntityType.java
@@ -1,6 +1,7 @@
 package net.minestom.server.command.builder.arguments.minecraft.registry;
 
 import net.minestom.server.command.builder.NodeMaker;
+import net.minestom.server.command.builder.arguments.minecraft.SuggestionType;
 import net.minestom.server.entity.EntityType;
 import net.minestom.server.network.packet.server.play.DeclareCommandsPacket;
 import net.minestom.server.registry.Registries;
@@ -22,8 +23,9 @@ public class ArgumentEntityType extends ArgumentRegistry<EntityType> {
 
     @Override
     public void processNodes(@NotNull NodeMaker nodeMaker, boolean executable) {
-        DeclareCommandsPacket.Node argumentNode = simpleArgumentNode(this, executable, false, false);
+        DeclareCommandsPacket.Node argumentNode = simpleArgumentNode(this, executable, false, true);
         argumentNode.parser = "minecraft:entity_summon";
+        argumentNode.suggestionsType = SuggestionType.SUMMONABLE_ENTITIES.getIdentifier();
 
         nodeMaker.addNodes(new DeclareCommandsPacket.Node[]{argumentNode});
     }
