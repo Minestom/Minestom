e1e546d9fa4545bd10b131bfb952621e07c9f88d
Konstantin Shandurenko <iam@kostya.sexy>
2021-03-01 16:52:19 +0300

    EntityAIGroup typo fix

--- src/main/java/net/minestom/server/entity/ai/EntityAIGroup.java
+++ src/main/java/net/minestom/server/entity/ai/EntityAIGroup.java
@@ -86,6 +86,7 @@ public class EntityAIGroup {
                 currentGoalSelector = selector;
                 setCurrentGoalSelector(currentGoalSelector);
                 currentGoalSelector.start();
+                break;
             }
         }
 
