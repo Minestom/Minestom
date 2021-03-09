51facb9f14eac1a0cc56b47db8f4002d4eb5de3c
themode <themode@outlook.fr>
2021-03-09 17:21:43 +0100

    Prevent modified clients from messing with the chunk range

--- src/main/java/net/minestom/server/entity/Player.java
+++ src/main/java/net/minestom/server/entity/Player.java
@@ -2326,15 +2326,15 @@ public class Player extends LivingEntity implements CommandSender {
      * based on which one is the lowest
      */
     public int getChunkRange() {
-        final int serverRange = MinecraftServer.getChunkViewDistance();
         final int playerRange = getSettings().viewDistance;
-        if (playerRange == 0) {
+        if (playerRange < 1) {
             // Didn't receive settings packet yet (is the case on login)
             // In this case we send the smallest amount of chunks possible
             // Will be updated in PlayerSettings#refresh.
             // Non-compliant clients might also be stuck with this view
             return 1;
         } else {
+            final int serverRange = MinecraftServer.getChunkViewDistance();
             return Math.min(playerRange, serverRange);
         }
     }
