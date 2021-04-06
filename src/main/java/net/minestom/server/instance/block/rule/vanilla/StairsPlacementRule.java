c13f65d0868c1909acc747e5ca13bd3c33f8988b
Alex <72355308+alexwith@users.noreply.github.com>
2021-04-06 15:48:39 +0200

    (WIP) Redo StairsPlacementRule

--- src/main/java/net/minestom/server/instance/block/rule/vanilla/StairsPlacementRule.java
+++ src/main/java/net/minestom/server/instance/block/rule/vanilla/StairsPlacementRule.java
@@ -3,13 +3,15 @@ package net.minestom.server.instance.block.rule.vanilla;
 import net.minestom.server.entity.Player;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.instance.block.Block;
-import net.minestom.server.instance.block.BlockAlternative;
 import net.minestom.server.instance.block.BlockFace;
 import net.minestom.server.instance.block.rule.BlockPlacementRule;
 import net.minestom.server.utils.BlockPosition;
-import org.apache.commons.lang3.tuple.Pair;
+import net.minestom.server.utils.Direction;
+import net.minestom.server.utils.MathUtils;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class StairsPlacementRule extends BlockPlacementRule {
 
@@ -18,149 +20,90 @@ public class StairsPlacementRule extends BlockPlacementRule {
     }
 
     @Override
-    public short blockUpdate(@NotNull Instance instance, @NotNull BlockPosition blockPosition, short currentStateID) {
-        return currentStateID;
+    public short blockUpdate(@NotNull Instance instance, @NotNull BlockPosition blockPosition, short stateId) {
+        return stateId;
     }
 
     @Override
     public short blockPlace(@NotNull Instance instance, @NotNull Block block, @NotNull BlockFace blockFace, @NotNull BlockPosition blockPosition, @NotNull Player player) {
-        Facing facing = this.getFacing(player);
-        Shape shape = this.getShape(instance, blockPosition, facing);
-        BlockFace half = BlockFace.BOTTOM; // waiting for new block faces to be implemented
-        boolean waterlogged = false; // waiting for water to be implemented
+        Direction direction = this.getDirection(player);
+        BlockFace half = BlockFace.BOTTOM;
+        Shape shape = this.getShape(instance, blockPosition, direction);
+        boolean waterlogged = false;
         return block.withProperties(
-                "facing=" + facing.toString().toLowerCase(),
-                "half=" + half.toString().toLowerCase(),
-                "shape=" + shape.toString().toLowerCase(),
-                "waterlogged=" + waterlogged
+                "facing=".concat(direction.name().toLowerCase()),
+                "half=".concat(half.name().toLowerCase()),
+                "shape=".concat(shape.toString()),
+                "waterlogged=".concat(Boolean.toString(waterlogged))
         );
     }
 
-    private enum Shape {
-
-        STRAIGHT,
-        OUTER_LEFT,
-        OUTER_RIGHT,
-        INNER_LEFT,
-        INNER_RIGHT;
+    private Direction getDirection(Player player) {
+        float yaw = player.getPosition().getYaw();
+        return Direction.valueOf(MathUtils.getHorizontalDirection(yaw).toString());
     }
 
-    private enum Facing {
-
-        NORTH(
-                new BlockPosition(0, 0, 1),
-                new BlockPosition(0, 0, -1)
-        ),
-        EAST(
-                new BlockPosition(-1, 0, 0),
-                new BlockPosition(1, 0, 0)
-        ),
-        SOUTH(
-                new BlockPosition(0, 0, -1),
-                new BlockPosition(0, 0, 1)
-        ),
-        WEST(
-                new BlockPosition(1, 0, 0),
-                new BlockPosition(-1, 0, 0)
-        );
-
-        private final BlockPosition front;
-        private final BlockPosition back;
-
-        Facing(@NotNull BlockPosition front, @NotNull BlockPosition back) {
-            this.front = front;
-            this.back = back;
-        }
-
-        @NotNull
-        public Pair<@Nullable Shape, @Nullable Facing> getFront(@NotNull Instance instance, @NotNull BlockPosition blockPosition) {
-            return this.getProperties(instance, blockPosition.clone().add(this.front));
-        }
-
-        @NotNull
-        public Pair<@Nullable Shape, @Nullable Facing> getBack(@NotNull Instance instance, @NotNull BlockPosition blockPosition) {
-            return this.getProperties(instance, blockPosition.clone().add(this.back));
-        }
-
-        @NotNull
-        private Pair<@Nullable Shape, @Nullable Facing> getProperties(@NotNull Instance instance, @NotNull BlockPosition blockPosition) {
-            Block block = instance.getBlock(blockPosition);
-            if (block == null) {
-                return Pair.of(null, null);
-            }
-            short stateId = instance.getBlockStateId(blockPosition);
-            BlockAlternative alternative = block.getAlternative(stateId);
-            try {
-                Shape shape = Shape.valueOf(alternative.getProperty("shape").toUpperCase());
-                Facing facing = Facing.valueOf(alternative.getProperty("facing").toUpperCase());
-                return Pair.of(shape, facing);
-            } catch (Exception ex) {
-                return Pair.of(null, null);
-            }
-        }
-    }
-
-    @NotNull
-    private Shape getShape(@NotNull Instance instance, @NotNull BlockPosition blockPosition, @NotNull Facing facing) {
-        Pair<Shape, Facing> front = facing.getFront(instance, blockPosition);
-        Pair<Shape, Facing> back = facing.getBack(instance, blockPosition);
-        Shape shape = this.getShapeFromSide(front, facing, Shape.INNER_RIGHT, Shape.INNER_LEFT);
+    private Shape getShape(Instance instance, BlockPosition position, Direction direction) {
+        Shape shape = this.getOffsetShape(instance, direction, this.getOffset(position, direction), Shape.OUTER_RIGHT, Shape.OUTER_LEFT);
         if (shape == null) {
-            shape = this.getShapeFromSide(back, facing, Shape.OUTER_RIGHT, Shape.OUTER_LEFT);
+            shape = this.getOffsetShape(instance, direction, this.getOffset(position, direction.opposite()), Shape.INNER_RIGHT, Shape.INNER_LEFT);
         }
         return shape == null ? Shape.STRAIGHT : shape;
     }
 
-    @Nullable
-    private Shape getShapeFromSide(@NotNull Pair<Shape, Facing> side, @NotNull Facing facing, @NotNull Shape right, @NotNull Shape left) {
-        if (side.getLeft() == null) {
-            return null;
-        }
-        Facing sideFacing = side.getRight();
-        if (facing.equals(Facing.NORTH)) {
-            if (sideFacing.equals(Facing.EAST)) {
-                return right;
-            } else if (sideFacing.equals(Facing.WEST)) {
-                return left;
-            }
-        } else if (facing.equals(Facing.SOUTH)) {
-            if (sideFacing.equals(Facing.EAST)) {
-                return left;
-            } else if (sideFacing.equals(Facing.WEST)) {
-                return right;
-            }
-        } else if (facing.equals(Facing.EAST)) {
-            if (sideFacing.equals(Facing.SOUTH)) {
-                return right;
-            } else if (sideFacing.equals(Facing.NORTH)) {
-                return left;
-            }
-        } else if (facing.equals(Facing.WEST)) {
-            if (sideFacing.equals(Facing.SOUTH)) {
-                return left;
-            } else if (sideFacing.equals(Facing.NORTH)) {
+    private Shape getOffsetShape(Instance instance, Direction direction, BlockPosition position, Shape right, Shape left) {
+        Block block = instance.getBlock(position);
+        if (block.getName().toUpperCase().contains("STAIRS")) {
+            Direction blockDirection = Direction.valueOf(block.getAlternative(instance.getBlockStateId(position)).getProperty("facing").toUpperCase());
+            if (!this.areAxesSame(blockDirection, direction)) {
+                if (blockDirection.equals(this.rotate(direction))) {
+                    return left;
+                }
                 return right;
             }
         }
         return null;
     }
 
-    @NotNull
-    private Facing getFacing(@NotNull Player player) {
-        float degrees = (player.getPosition().getYaw() - 90) % 360;
-        if (degrees < 0) {
-            degrees += 360;
+    private BlockPosition getOffset(BlockPosition position, Direction direction) {
+        return position.clone().add(direction.normalX(), direction.normalY(), direction.normalZ());
+    }
+
+    private boolean areAxesSame(Direction direction1, Direction direction2) {
+        Predicate<Function<Direction, Integer>> compare = (axis) -> Math.abs(axis.apply(direction1)) == Math.abs(axis.apply(direction2));
+        return compare.test(Direction::normalX) && compare.test(Direction::normalY) && compare.test(Direction::normalZ);
+    }
+
+    private Direction rotate(Direction direction) {
+        switch (direction) {
+            case NORTH:
+                return Direction.WEST;
+            case SOUTH:
+                return Direction.EAST;
+            case WEST:
+                return Direction.SOUTH;
+            case EAST:
+                return Direction.NORTH;
+            default:
+                return null;
         }
-        if (0 <= degrees && degrees < 45) {
-            return Facing.WEST;
-        } else if (45 <= degrees && degrees < 135) {
-            return Facing.NORTH;
-        } else if (135 <= degrees && degrees < 225) {
-            return Facing.EAST;
-        } else if (225 <= degrees && degrees < 315) {
-            return Facing.SOUTH;
-        } else { // 315 <= degrees && degrees < 360
-            return Facing.WEST;
+    }
+
+    private enum Shape {
+        STRAIGHT("straight"),
+        INNER_LEFT("inner_left"),
+        INNER_RIGHT("inner_right"),
+        OUTER_LEFT("outer_left"),
+        OUTER_RIGHT("outer_right");
+
+        private final String name;
+
+        Shape(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            return this.name;
         }
     }
 }
