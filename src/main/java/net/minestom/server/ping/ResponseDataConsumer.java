232e4b8a2807e503c1bf0a43e879fd0f067f6eee
TheMode <themode@outlook.fr>
2021-04-15 08:10:32 +0200

    Fix build

--- src/main/java/net/minestom/server/ping/ResponseDataConsumer.java
+++ src/main/java/net/minestom/server/ping/ResponseDataConsumer.java
@@ -8,17 +8,17 @@ import net.minestom.server.network.player.PlayerConnection;
  * <p>Can be specified in {@link net.minestom.server.MinecraftServer#start(String, int,
  * ResponseDataConsumer)}.
  *
- *  @deprecated listen to the {@link net.minestom.server.event.server.StatusRequestEvent} instead
+ * @deprecated listen to the {@link net.minestom.server.event.server.ServerListPingEvent} instead
  */
 @FunctionalInterface
 @Deprecated
 public interface ResponseDataConsumer {
 
-  /**
-   * A method to fill the data of the response.
-   *
-   * @param playerConnection The player connection to which the response should be sent.
-   * @param responseData The data for the response.
-   */
-  void accept(PlayerConnection playerConnection, ResponseData responseData);
+    /**
+     * A method to fill the data of the response.
+     *
+     * @param playerConnection The player connection to which the response should be sent.
+     * @param responseData     The data for the response.
+     */
+    void accept(PlayerConnection playerConnection, ResponseData responseData);
 }
