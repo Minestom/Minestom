8f3ee93191e4eede49f581b1d44d9444b285d772
TheMode <themode@outlook.fr>
2021-04-16 21:51:33 +0200

    Use literal as the default argument when generating a syntax

--- src/test/java/demo/commands/TestCommand.java
+++ src/test/java/demo/commands/TestCommand.java
@@ -4,8 +4,7 @@ import net.kyori.adventure.text.Component;
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.CommandContext;
-
-import static net.minestom.server.command.builder.arguments.ArgumentType.ResourceLocation;
+import net.minestom.server.command.builder.arguments.ArgumentType;
 
 public class TestCommand extends Command {
 
@@ -13,9 +12,7 @@ public class TestCommand extends Command {
         super("testcmd");
         setDefaultExecutor(this::usage);
 
-        var test = ResourceLocation("msg");
-
-        addSyntax((sender, context) -> System.out.println("executed"),test);
+        addSyntax((sender, context) -> System.out.println("executed"), ArgumentType.generate("test get"));
     }
 
     private void usage(CommandSender sender, CommandContext context) {
