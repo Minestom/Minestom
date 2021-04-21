86faa435f782392e739bf909f8265411fce718e1
TheMode <themode@outlook.fr>
2021-04-21 18:33:51 +0200

    Added SingleThreadProvider

---
+++ src/main/java/net/minestom/server/thread/SingleThreadProvider.java
@@ -0,0 +1,19 @@
+package net.minestom.server.thread;
+
+import net.minestom.server.instance.Chunk;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Uses a single thread for all chunks.
+ */
+public class SingleThreadProvider extends ThreadProvider {
+
+    public SingleThreadProvider() {
+        super(1);
+    }
+
+    @Override
+    public long findThread(@NotNull Chunk chunk) {
+        return 0;
+    }
+}
