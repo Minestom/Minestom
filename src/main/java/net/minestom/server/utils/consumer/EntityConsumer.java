2713650b23baaebf8a9d50d22c6bbe5455813262
TheMode <themode@outlook.fr>
2021-04-25 01:46:27 +0200

    Remove unused consumers

--- src/main/java/net/minestom/server/utils/consumer/EntityConsumer.java
+++
@@ -1,9 +0,0 @@
-package net.minestom.server.utils.consumer;
-
-import net.minestom.server.entity.Entity;
-import org.jetbrains.annotations.NotNull;
-
-@FunctionalInterface
-public interface EntityConsumer {
-    void accept(@NotNull Entity entity);
-}
--- src/main/java/net/minestom/server/utils/consumer/PlayerConsumer.java
+++
@@ -1,8 +0,0 @@
-package net.minestom.server.utils.consumer;
-
-import net.minestom.server.entity.Player;
-import org.jetbrains.annotations.NotNull;
-
-public interface PlayerConsumer extends EntityConsumer {
-    void accept(@NotNull Player player);
-}
