9c99cb9b2ee0a90d8016d6b34c9fbfdf744037ce
TheMode <themode@outlook.fr>
2021-04-24 20:53:41 +0200

    Removed unused method

--- src/main/java/net/minestom/server/acquirable/AcquirableImpl.java
+++ src/main/java/net/minestom/server/acquirable/AcquirableImpl.java
@@ -1,7 +1,6 @@
 package net.minestom.server.acquirable;
 
 import net.minestom.server.entity.Entity;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.stream.Stream;
@@ -9,19 +8,6 @@ import java.util.stream.Stream;
 class AcquirableImpl<T> implements Acquirable<T> {
 
     protected static final ThreadLocal<Stream<Entity>> CURRENT_ENTITIES = ThreadLocal.withInitial(Stream::empty);
-
-    /**
-     * Changes the stream returned by {@link #currentEntities()}.
-     * <p>
-     * Mostly for internal use, external calls are unrecommended as they could lead
-     * to unexpected behavior.
-     *
-     * @param entities the new entity stream
-     */
-    @ApiStatus.Internal
-    static void refreshEntities(@NotNull Stream<@NotNull Entity> entities) {
-        AcquirableImpl.CURRENT_ENTITIES.set(entities);
-    }
 
     private final T value;
     private final Acquirable.Handler handler;
