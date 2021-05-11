622f0c5d5010d140a63abcf7865954f837064f20
Kieran Wallbanks <kieran.wallbanks@gmail.com>
2021-05-11 13:09:08 +0100

    Add shadowJar and application plugin to demo

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,5 +1,17 @@
 description = "An example Minestom implementation for testing and demo purposes"
 
+plugins {
+    // application - produce an executable application
+    application
+
+    // shadow - produce a jar with dependencies
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+application {
+    mainClass.set("net.minestom.demo.Main")
+}
+
 dependencies {
     implementation(projects.minestomCore)
     implementation(projects.minestomExtrasLwjgl)
