2910c25ba067be37fd8fe965a035227084174f93
TheMode <themode@outlook.fr>
2021-05-17 19:35:35 +0200

    Remove BlockStateImpl.java in favor of a single implementation

--- src/main/java/net/minestom/server/instance/block/incubator/BlockImpl.java
+++ src/main/java/net/minestom/server/instance/block/incubator/BlockImpl.java
@@ -3,20 +3,44 @@ package net.minestom.server.instance.block.incubator;
 import net.minestom.server.utils.NamespaceID;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 class BlockImpl implements BlockType {
 
-    private final NamespaceID namespaceID;
-    private final short id;
-    private final List<BlockProperty<?>> properties;
+    protected BlockImpl original = null;
 
-    protected BlockImpl(NamespaceID namespaceID, short id, BlockProperty<?>... properties) {
+    private final NamespaceID namespaceID;
+    private final short minStateId, stateId;
+    private final List<BlockProperty<Object>> properties;
+
+    private LinkedHashMap<BlockProperty<Object>, Object> propertiesMap;
+
+    private BlockImpl(NamespaceID namespaceID, short minStateId, short stateId, List<BlockProperty<Object>> properties) {
         this.namespaceID = namespaceID;
-        this.id = id;
-        this.properties = Arrays.asList(properties);
+        this.minStateId = minStateId;
+        this.stateId = stateId;
+        this.properties = properties;
+    }
+
+    protected static BlockImpl firstBlock(NamespaceID namespaceID, short id, List<BlockProperty<Object>> properties) {
+        var block = new BlockImpl(namespaceID, id, id, properties);
+        block.original = block;
+        return block;
+    }
+
+    protected static BlockImpl defaultBlock(NamespaceID namespaceID, short minStateId, short stateId,
+                                            List<BlockProperty<Object>> properties) {
+        var block = new BlockImpl(namespaceID, minStateId, stateId, properties);
+        block.original = block;
+        return block;
+    }
+
+    protected static BlockImpl defaultBlock(NamespaceID namespaceID, short minStateId,
+                                            List<BlockProperty<Object>> properties,
+                                            LinkedHashMap<BlockProperty<Object>, Object> propertiesMap) {
+        var block = new BlockImpl(namespaceID, minStateId, computeId(minStateId, propertiesMap), properties);
+        block.original = block;
+        return block;
     }
 
     @Override
@@ -31,15 +55,27 @@ class BlockImpl implements BlockType {
             return this;
         }
 
-        // Create property
-        LinkedHashMap<BlockProperty<?>, T> map = new LinkedHashMap<>();
-        properties.forEach(prop -> map.put(prop, prop.equals(property) ? value : null));
-        return new BlockStateImpl(this, map);
+        // Find properties map
+        LinkedHashMap<BlockProperty<Object>, Object> map;
+        if (propertiesMap == null) {
+            // Represents the first id, create a new map
+            map = new LinkedHashMap<>();
+            properties.forEach(prop -> map.put(prop, prop.equals(property) ? value : null));
+        } else {
+            // Change property
+            map = (LinkedHashMap<BlockProperty<Object>, Object>) propertiesMap.clone();
+            map.put((BlockProperty<Object>) property, value);
+        }
+
+        var block = new BlockImpl(namespaceID, minStateId, computeId(minStateId, map), properties);
+        block.original = original;
+        block.propertiesMap = map;
+        return block;
     }
 
     @Override
     public @NotNull BlockType getDefaultBlock() {
-        return this;
+        return original;
     }
 
     @Override
@@ -49,6 +85,31 @@ class BlockImpl implements BlockType {
 
     @Override
     public short getProtocolId() {
+        return stateId;
+    }
+
+    private static short computeId(short id, LinkedHashMap<BlockProperty<Object>, Object> properties) {
+        var reverse = reverse(properties);
+        int factor = 1;
+        for (var entry : reverse.entrySet()) {
+            var property = entry.getKey();
+            var value = entry.getValue();
+            var values = property.getPossibleValues();
+            if (value != null) {
+                id += values.indexOf(value) * factor;
+            }
+            factor *= values.size();
+        }
         return id;
+    }
+
+    private static <K, V> LinkedHashMap<K, V> reverse(LinkedHashMap<K, V> map) {
+        LinkedHashMap<K, V> reversedMap = new LinkedHashMap<>();
+        ListIterator<Map.Entry<K, V>> it = new ArrayList<>(map.entrySet()).listIterator(map.entrySet().size());
+        while (it.hasPrevious()) {
+            Map.Entry<K, V> el = it.previous();
+            reversedMap.put(el.getKey(), el.getValue());
+        }
+        return reversedMap;
     }
 }
---
+++ src/main/java/net/minestom/server/instance/block/incubator/Blocks.java
@@ -0,0 +1,11 @@
+package net.minestom.server.instance.block.incubator;
+
+import net.minestom.server.utils.NamespaceID;
+
+import java.util.Collections;
+
+public class Blocks {
+
+    public static final BlockType STONE = BlockImpl.firstBlock(NamespaceID.from("minecraft:stone"), (short) 1, Collections.emptyList());
+
+}
