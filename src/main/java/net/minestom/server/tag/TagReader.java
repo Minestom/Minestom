1ec631fa5b46c282e13f9c802cbaf4cca620306b
TheMode <themode@outlook.fr>
2021-05-17 14:05:03 +0200

    Create TagReader/Writer from compound

--- src/main/java/net/minestom/server/tag/TagReader.java
+++ src/main/java/net/minestom/server/tag/TagReader.java
@@ -2,9 +2,24 @@ package net.minestom.server.tag;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jglrxavpok.hephaistos.nbt.NBTCompound;
 
 public interface TagReader {
     <T> @Nullable T getTag(@NotNull Tag<T> tag);
 
     boolean hasTag(@NotNull Tag<?> tag);
+
+    static @NotNull TagReader fromCompound(@NotNull NBTCompound compound) {
+        return new TagReader() {
+            @Override
+            public <T> @Nullable T getTag(@NotNull Tag<T> tag) {
+                return tag.read(compound);
+            }
+
+            @Override
+            public boolean hasTag(@NotNull Tag<?> tag) {
+                return compound.containsKey(tag.getKey());
+            }
+        };
+    }
 }
--- src/main/java/net/minestom/server/tag/TagWriter.java
+++ src/main/java/net/minestom/server/tag/TagWriter.java
@@ -2,7 +2,17 @@ package net.minestom.server.tag;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jglrxavpok.hephaistos.nbt.NBTCompound;
 
 public interface TagWriter {
     <T> void setTag(@NotNull Tag<T> tag, @Nullable T value);
+
+    static @NotNull TagWriter fromCompound(@NotNull NBTCompound compound) {
+        return new TagWriter() {
+            @Override
+            public <T> void setTag(@NotNull Tag<T> tag, @Nullable T value) {
+                tag.write(compound, value);
+            }
+        };
+    }
 }
