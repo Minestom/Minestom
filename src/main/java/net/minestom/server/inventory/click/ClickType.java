cb4ce09d85f74698445a17053475604f87ea888a
Stef van Schie <stefvanschiedev@gmail.com>
2021-05-28 20:24:02 +0200

    Remove deprecated click types

--- src/main/java/net/minestom/server/inventory/click/ClickType.java
+++ src/main/java/net/minestom/server/inventory/click/ClickType.java
@@ -9,14 +9,8 @@ public enum ClickType {
     START_SHIFT_CLICK,
     SHIFT_CLICK,
 
-    @Deprecated
-    START_DRAGGING,
-
     START_LEFT_DRAGGING,
     START_RIGHT_DRAGGING,
-
-    @Deprecated
-    DRAGGING,
 
     LEFT_DRAGGING,
     RIGHT_DRAGGING,
