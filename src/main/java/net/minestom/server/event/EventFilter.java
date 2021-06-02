d58cd3b578bc48e3e811d23b6c2d2c7d660b74da
TheMode <themode@outlook.fr>
2021-06-02 22:47:45 +0200

    Add remaining filters

--- src/main/java/net/minestom/server/event/EventFilter.java
+++ src/main/java/net/minestom/server/event/EventFilter.java
@@ -3,8 +3,10 @@ package net.minestom.server.event;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.handler.EventHandler;
-import net.minestom.server.event.trait.EntityEvent;
-import net.minestom.server.event.trait.PlayerEvent;
+import net.minestom.server.event.trait.*;
+import net.minestom.server.instance.Instance;
+import net.minestom.server.inventory.Inventory;
+import net.minestom.server.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -15,6 +17,9 @@ public interface EventFilter<E extends Event, H> {
     EventFilter<Event, EventHandler> ALL = from(Event.class);
     EventFilter<EntityEvent, Entity> ENTITY = from(EntityEvent.class, Entity.class, EntityEvent::getEntity);
     EventFilter<PlayerEvent, Player> PLAYER = from(PlayerEvent.class, Player.class, PlayerEvent::getPlayer);
+    EventFilter<ItemEvent, ItemStack> ITEM = from(ItemEvent.class, ItemStack.class, ItemEvent::getItemStack);
+    EventFilter<InstanceEvent, Instance> INSTANCE = from(InstanceEvent.class, Instance.class, InstanceEvent::getInstance);
+    EventFilter<InventoryEvent, Inventory> INVENTORY = from(InventoryEvent.class, Inventory.class, InventoryEvent::getInventory);
 
     static <E extends Event, H> EventFilter<E, H> from(@NotNull Class<E> eventType,
                                                        @NotNull Class<H> handlerType,
