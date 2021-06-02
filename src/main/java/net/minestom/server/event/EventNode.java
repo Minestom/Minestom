77f74bb7dc3355d22be6bf0b6ecf3a9a6a975284
TheMode <themode@outlook.fr>
2021-06-02 08:17:21 +0200

    Introduce EventNode

---
+++ src/main/java/net/minestom/server/event/EventNode.java
@@ -0,0 +1,37 @@
+package net.minestom.server.event;
+
+import net.minestom.server.event.handler.EventHandler;
+import net.minestom.server.event.trait.EventTrait;
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class EventNode<T extends EventTrait> {
+
+    private EventNode() {
+    }
+
+    public static EventNode<?> create() {
+        return null;
+    }
+
+    public static <T extends EventTrait> EventNode<T> create(Class<T> eventType) {
+        return new EventNode<>();
+    }
+
+    public static <T extends EventTrait, E> EventNode<T> conditional(Class<T> eventType,
+                                                                     Class<E> handlerType,
+                                                                     Predicate<E> predicate) {
+        return new EventNode<>();
+    }
+
+    public static <T extends EventTrait> EventNode<T> unique(Class<T> eventType, EventHandler handler) {
+        return new EventNode<>();
+    }
+
+    public void addListener(EventListener<? extends T> listener) {
+    }
+
+    public <E extends Event> void addListener(Class<E> eventClass, Consumer<E> listener) {
+    }
+}
