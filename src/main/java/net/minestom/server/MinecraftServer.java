31d2f3488b2bec887cd56b27f12b2e519f3f9d3f
TheMode <themode@outlook.fr>
2021-06-05 00:46:58 +0200

    Update protocol version to `1.17-rc1`

--- src/main/java/net/minestom/server/MinecraftServer.java
+++ src/main/java/net/minestom/server/MinecraftServer.java
@@ -68,8 +68,8 @@ public final class MinecraftServer {
 
     public final static Logger LOGGER = LoggerFactory.getLogger(MinecraftServer.class);
 
-    public static final String VERSION_NAME = "1.17-pre2";
-    public static final int PROTOCOL_VERSION = 1073741854;
+    public static final String VERSION_NAME = "1.17-rc1";
+    public static final int PROTOCOL_VERSION = 1073741858;
 
     // Threads
     public static final String THREAD_NAME_BENCHMARK = "Ms-Benchmark";
