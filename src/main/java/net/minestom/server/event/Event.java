d800c94dcc3873f874977a708ebe2d443c07d2ac
TheMode <themode@outlook.fr>
2021-06-09 08:25:56 +0200

    Remove EventHandler mention in Event

--- src/main/java/net/minestom/server/event/Event.java
+++ src/main/java/net/minestom/server/event/Event.java
@@ -1,7 +1,7 @@
 package net.minestom.server.event;
 
 /**
- * Event which can be listened to by an {@link EventNode}.
+ * Event which can be listened to by an {@link EventNode} using {@link EventNode#addListener(EventListener)}.
  * <p>
  * Called using {@link EventDispatcher#call(Event)}.
  */
