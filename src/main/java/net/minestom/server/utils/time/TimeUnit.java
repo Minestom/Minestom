ba2dcaac43b8c344073e8bd4e91d92f328ad0326
Németh Noel <nemeth.noel@thekingsteam.tk>
2021-06-30 13:22:46 +0200

    Deprecate TimeUnit#TICK instead of removing

--- src/main/java/net/minestom/server/utils/time/TimeUnit.java
+++ src/main/java/net/minestom/server/utils/time/TimeUnit.java
@@ -11,6 +11,11 @@ public class TimeUnit {
     public static final TemporalUnit MILLISECOND = ChronoUnit.MILLIS;
     public static final TemporalUnit SERVER_TICK = Tick.SERVER_TICKS;
     public static final TemporalUnit CLIENT_TICK = Tick.CLIENT_TICKS;
+    /**
+     * @deprecated Please use either {@link #SERVER_TICK} or {@link #CLIENT_TICK}
+     */
+    @Deprecated(forRemoval = true)
+    public static final TemporalUnit TICK = CLIENT_TICK;
 
     private TimeUnit() {
     }
