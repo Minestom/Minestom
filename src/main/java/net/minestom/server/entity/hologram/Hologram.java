323fcbe114691e77e1b90ab1f879454880122641
BuildTools <james.jenour@protonmail.com>
2021-07-04 14:04:55 +0800

    Remove MarkerHologram class, add parameter to Hologram constructor

--- src/main/java/net/minestom/server/entity/hologram/Hologram.java
+++ src/main/java/net/minestom/server/entity/hologram/Hologram.java
@@ -20,8 +20,10 @@ import java.util.Set;
 public class Hologram implements Viewable {
 
     private static final float OFFSET_Y = -0.9875f;
+    private static final float MARKER_OFFSET_Y = -0.40625f;
 
     private final Entity entity;
+    private final float yOffset;
 
     private Position position;
     private Component text;
@@ -75,45 +77,43 @@ public class Hologram implements Viewable {
      * @param autoViewable  {@code true}if the hologram should be visible automatically, otherwise {@code false}.
      */
     public Hologram(Instance instance, Position spawnPosition, Component text, boolean autoViewable) {
+        this(instance, spawnPosition, text, autoViewable, false);
+    }
+
+    /**
+     * Constructs a new {@link Hologram} with the given parameters.
+     *
+     * @param instance      The instance where the hologram should be spawned.
+     * @param spawnPosition The spawn position of this hologram.
+     * @param text          The text of this hologram.
+     * @param autoViewable  {@code true}if the hologram should be visible automatically, otherwise {@code false}.
+     */
+    public Hologram(Instance instance, Position spawnPosition, Component text, boolean autoViewable, boolean marker) {
         this.entity = new Entity(EntityType.ARMOR_STAND);
 
         ArmorStandMeta armorStandMeta = (ArmorStandMeta) entity.getEntityMeta();
 
         armorStandMeta.setNotifyAboutChanges(false);
 
-        updateDefaultMeta(armorStandMeta);
-
-        armorStandMeta.setNotifyAboutChanges(true);
-
-        this.entity.setInstance(instance, spawnPosition.clone().add(0, getOffsetY(), 0));
-        this.entity.setAutoViewable(autoViewable);
-
-        this.position = spawnPosition;
-        setText(text);
-    }
-
-    /**
-     * Sets the default {@link ArmorStandMeta} flags for this Hologram,
-     * subclasses may override this method to modify the metadata.
-     *
-     * @param armorStandMeta the meta to update
-     */
-    protected void updateDefaultMeta(ArmorStandMeta armorStandMeta) {
-        armorStandMeta.setSmall(true);
+        if(marker) {
+            this.yOffset = MARKER_OFFSET_Y;
+            armorStandMeta.setMarker(true);
+        } else {
+            this.yOffset = OFFSET_Y;
+            armorStandMeta.setSmall(true);
+        }
         armorStandMeta.setHasNoGravity(true);
         armorStandMeta.setCustomName(Component.empty());
         armorStandMeta.setCustomNameVisible(true);
         armorStandMeta.setInvisible(true);
-    }
 
-    /**
-     * Vertical offset used to center the nametag,
-     * subclasses may override this method to modify the position
-     *
-     * @return the vertical offset used to center the nametag
-     */
-    protected float getOffsetY() {
-        return OFFSET_Y;
+        armorStandMeta.setNotifyAboutChanges(true);
+
+        this.entity.setInstance(instance, spawnPosition.clone().add(0, this.yOffset, 0));
+        this.entity.setAutoViewable(autoViewable);
+
+        this.position = spawnPosition;
+        setText(text);
     }
 
     /**
@@ -132,7 +132,7 @@ public class Hologram implements Viewable {
      */
     public void setPosition(Position position) {
         checkRemoved();
-        position.add(0, getOffsetY(), 0);
+        position.add(0, this.yOffset, 0);
         this.position = position;
         this.entity.teleport(position);
     }
