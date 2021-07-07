697a33bad3c1f8187e8bb729268bd3d78bc65556
Németh Noel <nemeth.noel@thekingsteam.tk>
2021-07-07 21:25:57 +0200

    Remove tests from PlayerInit.java

--- src/test/java/demo/PlayerInit.java
+++ src/test/java/demo/PlayerInit.java
@@ -3,10 +3,8 @@ package demo;
 import demo.generator.ChunkGeneratorDemo;
 import demo.generator.NoiseTestGenerator;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.event.ClickEvent;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.adventure.audience.Audiences;
-import net.minestom.server.command.builder.Command;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.GameMode;
 import net.minestom.server.entity.ItemEntity;
@@ -43,7 +41,6 @@ import net.minestom.server.world.DimensionType;
 
 import java.time.Duration;
 import java.util.Collection;
-import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
@@ -99,17 +96,6 @@ public class PlayerInit {
                 int x = Math.abs(ThreadLocalRandom.current().nextInt()) % 500 - 250;
                 int z = Math.abs(ThreadLocalRandom.current().nextInt()) % 500 - 250;
                 player.setRespawnPoint(new Position(0, 42f, 0));
-
-                // Command test
-                final String commandName = MinecraftServer.getCommandManager().register(Command.builder(((command, sender, context) -> {
-                   sender.asPlayer().getInventory().addItemStack(ItemStack.of(Material.DIAMOND));
-                })).maxUsage(5).whitelistedSenders(List.of(player)).build());
-
-                MinecraftServer.getCommandManager().register(Command.builder(((command, sender, context) -> {
-                    sender.sendMessage(command.getTimeUntilExpiration().toString());
-                })).ttl(Duration.ofSeconds(90)).hidden(false).build());
-
-                player.sendMessage(Component.text("Get 1 diamond (only usable 5 times)").clickEvent(ClickEvent.runCommand("/"+commandName)));
             })
             .addListener(PlayerSpawnEvent.class, event -> {
                 final Player player = event.getPlayer();
