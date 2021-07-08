478ed0a85db5f66f62111722fe246120a558b61f
Németh Noel <nemeth.noel@thekingsteam.tk>
2021-07-08 16:03:15 +0200

    condition test

--- src/test/java/demo/commands/TestCommand.java
+++ src/test/java/demo/commands/TestCommand.java
@@ -4,18 +4,26 @@ import net.kyori.adventure.text.Component;
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.CommandContext;
+import net.minestom.server.command.builder.arguments.ArgumentString;
+import net.minestom.server.command.builder.condition.conditions.MaxUsageCondition;
+import org.jetbrains.annotations.NotNull;
 
 public class TestCommand extends Command {
+    ArgumentString str = new ArgumentString("str");
 
     public TestCommand() {
         super("testcmd");
-        setDefaultExecutor(this::usage);
+        setDefaultExecutor(this::defaultExecutor);
 
-        addSyntax((sender, context) -> System.out.println("executed"), "test Get integer<number>");
+        addConditions(new MaxUsageCondition(5));
+        addConditionalSyntax(new MaxUsageCondition(2), this::syntaxExecutor, str);
     }
 
-    private void usage(CommandSender sender, CommandContext context) {
-        sender.sendMessage(Component.text("Incorrect usage"));
+    private void syntaxExecutor(@NotNull CommandSender commandSender, @NotNull CommandContext context) {
+        commandSender.sendMessage(context.get(str));
     }
 
+    private void defaultExecutor(CommandSender sender, CommandContext context) {
+        sender.sendMessage(Component.text("Can be used " + getCondition(MaxUsageCondition.class).getRemainingUsage() + "times"));
+    }
 }
