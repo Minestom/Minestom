5992d52429855a96db996adc155d42c57b1c5f24
Németh Noel <nemeth.noel@thekingsteam.tk>
2021-07-09 15:27:36 +0200

    Update /setblock command

--- src/test/java/demo/commands/SetBlockCommand.java
+++ src/test/java/demo/commands/SetBlockCommand.java
@@ -1,22 +1,23 @@
 package demo.commands;
 
 import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.arguments.minecraft.ArgumentBlockState;
+import net.minestom.server.command.builder.arguments.relative.ArgumentRelativeBlockPosition;
 import net.minestom.server.entity.Player;
-import net.minestom.server.instance.block.Block;
-import net.minestom.server.utils.location.RelativeVec;
 
 import static net.minestom.server.command.builder.arguments.ArgumentType.BlockState;
-import static net.minestom.server.command.builder.arguments.ArgumentType.RelativeVec3;
+import static net.minestom.server.command.builder.arguments.ArgumentType.RelativeBlockPosition;
 
 public class SetBlockCommand extends Command {
     public SetBlockCommand() {
         super("setblock");
 
+        final ArgumentRelativeBlockPosition position = RelativeBlockPosition("position");
+        final ArgumentBlockState block = BlockState("block");
+
         addSyntax((sender, context) -> {
-            RelativeVec relativeVec = context.get("position");
-            Block block = context.get("block");
             final Player player = sender.asPlayer();
-            player.getInstance().setBlock(relativeVec.from(player), block);
-        }, RelativeVec3("position"), BlockState("block"));
+            player.getInstance().setBlock(context.get(position).from(player), context.get(block));
+        }, position, block);
     }
 }
