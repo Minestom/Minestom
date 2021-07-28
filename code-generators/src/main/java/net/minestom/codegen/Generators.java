7df7d3ad74ccc069aae549e5b8346da2e8028d5c
TheMode <themode@outlook.fr>
2021-07-28 13:43:05 +0200

    Reuse code for the generator

--- code-generators/src/main/java/net/minestom/codegen/Generators.java
+++ code-generators/src/main/java/net/minestom/codegen/Generators.java
@@ -1,10 +1,6 @@
 package net.minestom.codegen;
 
-import net.minestom.codegen.blocks.BlockGenerator;
-import net.minestom.codegen.entity.EntityTypeGenerator;
 import net.minestom.codegen.fluid.FluidGenerator;
-import net.minestom.codegen.item.EnchantmentGenerator;
-import net.minestom.codegen.item.MaterialGenerator;
 import net.minestom.codegen.particle.ParticleGenerator;
 import net.minestom.codegen.potion.PotionEffectGenerator;
 import net.minestom.codegen.potion.PotionTypeGenerator;
@@ -14,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public class Generators {
@@ -26,16 +21,17 @@ public class Generators {
             return;
         }
         File outputFolder = new File(args[0]);
-        // Generate blocks
-        new BlockGenerator(resource("blocks.json"), outputFolder).generate();
+        var generator = new CodeGenerator(outputFolder);
+        generator.generate(resource("blocks.json"), "net.minestom.server.instance.block", "Block", "BlockLoader", "BlockConstants");
+        generator.generate(resource("items.json"), "net.minestom.server.item", "Material", "MaterialLoader", "MaterialConstants");
+        generator.generate(resource("entities.json"), "net.minestom.server.entity", "EntityType", "EntityTypeLoader", "EntityTypeConstants");
+        generator.generate(resource("enchantments.json"), "net.minestom.server.item", "Enchantment", "EnchantmentLoader", "EnchantmentConstants");
+
+        if (true)
+            return; // TODO complete
+
         // Generate fluids
         new FluidGenerator(resource("fluids.json"), outputFolder).generate();
-        // Generate entities
-        new EntityTypeGenerator(resource("entities.json"), outputFolder).generate();
-        // Generate items
-        new MaterialGenerator(resource("items.json"), outputFolder).generate();
-        // Generate enchantments
-        new EnchantmentGenerator(resource("enchantments.json"), outputFolder).generate();
         // TODO: Generate attributes
 //        new AttributeGenerator(
 //                new File(inputFolder, targetVersion + "_attributes.json"),
