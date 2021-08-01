9a74051cdb5929c90a0271eae1b9410ea30a3626
TheMode <themode@outlook.fr>
2021-08-01 14:00:57 +0200

    Use jdk method for array comparison

--- src/main/java/net/minestom/server/utils/ArrayUtils.java
+++ src/main/java/net/minestom/server/utils/ArrayUtils.java
@@ -4,7 +4,6 @@ import it.unimi.dsi.fastutil.ints.IntList;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
 import java.util.function.LongConsumer;
 
 @ApiStatus.Internal
@@ -51,27 +50,6 @@ public final class ArrayUtils {
         int[] array = new int[list.size()];
         list.getElements(0, array, 0, array.length);
         return array;
-    }
-
-    /**
-     * Gets if two arrays share the same start until {@code length}.
-     *
-     * @param first  the first array
-     * @param second the second array
-     * @param length the length to check (0-length)
-     * @param <T>    the type of the arrays
-     * @return true if both arrays share the same start
-     */
-    public static <T> boolean sameStart(@NotNull T[] first, @NotNull T[] second, int length) {
-        if (Math.min(first.length, second.length) < length) {
-            return false;
-        }
-        for (int i = 0; i < length; i++) {
-            if (!Objects.equals(first[i], second[i])) {
-                return false;
-            }
-        }
-        return true;
     }
 
     public static boolean empty(byte[] array) {
