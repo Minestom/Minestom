c94344a22e42070bc74e07c7ed3a936b2a3085dd
Németh Noel <nemeth.noel@thekingsteam.tk>
2021-08-10 22:18:11 +0200

    Reduce boilerplate, add missing toString method

--- src/main/java/net/minestom/server/command/builder/arguments/number/ArgumentDouble.java
+++ src/main/java/net/minestom/server/command/builder/arguments/number/ArgumentDouble.java
@@ -1,60 +1,11 @@
 package net.minestom.server.command.builder.arguments.number;
 
-import net.minestom.server.command.builder.NodeMaker;
-import net.minestom.server.command.builder.exception.ArgumentSyntaxException;
-import net.minestom.server.network.packet.server.play.DeclareCommandsPacket;
 import net.minestom.server.utils.binary.BinaryWriter;
-import org.jetbrains.annotations.NotNull;
 
 public class ArgumentDouble extends ArgumentNumber<Double> {
 
     public ArgumentDouble(String id) {
-        super(id);
-    }
-
-    @NotNull
-    @Override
-    public Double parse(@NotNull String input) throws ArgumentSyntaxException {
-        try {
-            final double value;
-            {
-                String parsed = parseValue(input);
-                int radix = getRadix(input);
-                if (radix != 10) {
-                    value = (double) Long.parseLong(parsed, radix);
-                } else {
-                    value = Double.parseDouble(parsed);
-                }
-            }
-
-            // Check range
-            if (hasMin && value < min) {
-                throw new ArgumentSyntaxException("Input is lower than the minimum required value", input, RANGE_ERROR);
-            }
-            if (hasMax && value > max) {
-                throw new ArgumentSyntaxException("Input is higher than the minimum required value", input, RANGE_ERROR);
-            }
-
-            return value;
-        } catch (NumberFormatException | NullPointerException e) {
-            throw new ArgumentSyntaxException("Input is not a number/long", input, NOT_NUMBER_ERROR);
-        }
-    }
-
-    @Override
-    public void processNodes(@NotNull NodeMaker nodeMaker, boolean executable) {
-        DeclareCommandsPacket.Node argumentNode = simpleArgumentNode(this, executable, false, false);
-
-        argumentNode.parser = "brigadier:double";
-        argumentNode.properties = BinaryWriter.makeArray(packetWriter -> {
-            packetWriter.writeByte(getNumberProperties());
-            if (this.hasMin())
-                packetWriter.writeDouble(this.getMin());
-            if (this.hasMax())
-                packetWriter.writeDouble(this.getMax());
-        });
-
-        nodeMaker.addNodes(new DeclareCommandsPacket.Node[]{argumentNode});
+        super(id, "brigadier:double", Double::parseDouble, ((s, radix) -> (double) Long.parseLong(s, radix)), BinaryWriter::writeDouble, Double::compare);
     }
 
     @Override
