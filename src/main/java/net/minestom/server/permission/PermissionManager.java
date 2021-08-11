6944c45a6a22df8cdfb16722d7b054e156c6938d
LeoDog896 <LeoDog896@hotmail.com>
2021-08-10 21:48:53 -0400

    Switch to using map internally, add unregisterPermission

--- src/main/java/net/minestom/server/permission/PermissionManager.java
+++ src/main/java/net/minestom/server/permission/PermissionManager.java
@@ -2,27 +2,25 @@ package net.minestom.server.permission;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class PermissionManager {
+    private final Map<String, PermissionDescriptor> permissionMap = new HashMap<>();
 
-    private final List<String> permissionStrings = new ArrayList<>();
-    private final List<PermissionDescriptor> permissionDescriptors = new ArrayList<>();
-
-    public void registerPermission(PermissionDescriptor permissionDescriptor) {
-        permissionStrings.add(permissionDescriptor.getName());
-        permissionDescriptors.add(permissionDescriptor);
+    public void registerPermission(@NotNull PermissionDescriptor permissionDescriptor) {
+        permissionMap.put(permissionDescriptor.getName(), permissionDescriptor);
     }
 
-    public @NotNull Collection<@NotNull String> getPermissionStrings() {
-        return Collections.unmodifiableCollection(permissionStrings);
+    public void unregisterPermission(@NotNull String permission) {
+        permissionMap.remove(permission);
+    }
+
+    public @NotNull Set<@NotNull String> getPermissionStrings() {
+        return Collections.unmodifiableSet(permissionMap.keySet());
     }
 
     public @NotNull Collection<@NotNull PermissionDescriptor> getPermissionDescriptors() {
-        return Collections.unmodifiableCollection(permissionDescriptors);
+        return Collections.unmodifiableCollection(permissionMap.values());
     }
 
 }
