e96334e3150d311b118343ed84ca4c3a46d202bc
TheMode <themode@outlook.fr>
2021-08-13 23:37:40 +0200

    Ignore creative inventory action when the item is already present.

--- src/main/java/net/minestom/server/listener/CreativeInventoryActionListener.java
+++ src/main/java/net/minestom/server/listener/CreativeInventoryActionListener.java
@@ -6,19 +6,25 @@ import net.minestom.server.item.ItemStack;
 import net.minestom.server.network.packet.client.play.ClientCreativeInventoryActionPacket;
 import net.minestom.server.utils.inventory.PlayerInventoryUtils;
 
+import java.util.Objects;
+
 public final class CreativeInventoryActionListener {
     public static void listener(ClientCreativeInventoryActionPacket packet, Player player) {
         if (!player.isCreative()) return;
         short slot = packet.slot;
         final ItemStack item = packet.item;
-        if (slot != -1) {
-            // Set item
-            slot = (short) PlayerInventoryUtils.convertPlayerInventorySlot(slot, PlayerInventoryUtils.OFFSET);
-            PlayerInventory inventory = player.getInventory();
-            inventory.setItemStack(slot, item);
-        } else {
+        if (slot == -1) {
             // Drop item
             player.dropItem(item);
+            return;
         }
+        // Set item
+        slot = (short) PlayerInventoryUtils.convertPlayerInventorySlot(slot, PlayerInventoryUtils.OFFSET);
+        PlayerInventory inventory = player.getInventory();
+        if (Objects.equals(inventory.getItemStack(slot), item)) {
+            // Item is already present, ignore
+            return;
+        }
+        inventory.setItemStack(slot, item);
     }
 }
