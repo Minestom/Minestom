9f34eaf761d0f0adcb66f4c1bf347aadf8b7e94b
Matt Worzala <mattheworzala@gmail.com>
2021-08-23 10:04:12 -0400

    remove code modification, bootstrap + MinestomRootClassLoader still required for now

--- src/test/java/improveextensions/missingmodifiers/MissingCodeModifiersExtension.java
+++ src/test/java/improveextensions/missingmodifiers/MissingCodeModifiersExtension.java
@@ -11,10 +11,10 @@ public class MissingCodeModifiersExtension extends Extension {
     public void initialize() {
         // force load of InstanceContainer class
         try {
-            Assertions.assertFalse(areCodeModifiersAllLoadedCorrectly(), "Mixin configuration could not be loaded and code modifiers are unavailable, the failure should be reported");
-            Assertions.assertTrue(getOrigin().hasFailedToLoadMixin(), "Mixin configuration does not exist and should not be loaded");
-            Assertions.assertEquals(1, getOrigin().getMissingCodeModifiers().size(), "Code modifier does not exist, it should be reported as missing");
-            Assertions.assertEquals("InvalidCodeModifierClass", getOrigin().getMissingCodeModifiers().get(0));
+//            Assertions.assertFalse(areCodeModifiersAllLoadedCorrectly(), "Mixin configuration could not be loaded and code modifiers are unavailable, the failure should be reported");
+//            Assertions.assertTrue(getOrigin().hasFailedToLoadMixin(), "Mixin configuration does not exist and should not be loaded");
+//            Assertions.assertEquals(1, getOrigin().getMissingCodeModifiers().size(), "Code modifier does not exist, it should be reported as missing");
+//            Assertions.assertEquals("InvalidCodeModifierClass", getOrigin().getMissingCodeModifiers().get(0));
             System.out.println("All tests passed.");
         } catch (AssertionFailedError e) {
             e.printStackTrace();
