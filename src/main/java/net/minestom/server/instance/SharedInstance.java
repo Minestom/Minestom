6ce456c14fcd19cdc7d42695e78dcfe17733f784
Lorenzo Lapucci <halexercraft@gmail.com>
2021-08-24 20:23:07 +0200

    Revert commit

--- src/main/java/net/minestom/server/instance/SharedInstance.java
+++ src/main/java/net/minestom/server/instance/SharedInstance.java
@@ -104,11 +104,6 @@ public class SharedInstance extends Instance {
         return instanceContainer.isInVoid(point);
     }
 
-    @Override
-    protected void setRegistered(boolean registered) {
-        instanceContainer.setRegistered(registered);
-    }
-
     /**
      * Gets the {@link InstanceContainer} from where this instance takes its chunks from.
      *
