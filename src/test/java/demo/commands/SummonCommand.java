811e3c542b80b23434f93d2426e30d94feb8f382
LeoDog896 <LeoDog896@hotmail.com>
2021-08-25 18:35:51 -0400

    Fix up tab completion for EntityType

--- src/test/java/demo/commands/SummonCommand.java
+++ src/test/java/demo/commands/SummonCommand.java
@@ -3,32 +3,39 @@ package demo.commands;
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.CommandContext;
+import net.minestom.server.command.builder.arguments.Argument;
 import net.minestom.server.command.builder.arguments.ArgumentEnum;
 import net.minestom.server.command.builder.arguments.ArgumentType;
 import net.minestom.server.command.builder.arguments.minecraft.registry.ArgumentEntityType;
 import net.minestom.server.command.builder.arguments.relative.ArgumentRelativeVec3;
 import net.minestom.server.command.builder.condition.Conditions;
+import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.EntityCreature;
 import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.LivingEntity;
+import net.minestom.server.utils.location.RelativeVec;
 import org.jetbrains.annotations.NotNull;
 
 public class SummonCommand extends Command {
 
     private final ArgumentEntityType entity;
-    private final ArgumentRelativeVec3 pos;
-    private final ArgumentEnum<EntityClass> entityClass;
+    private final Argument<RelativeVec> pos;
+    private final Argument<EntityClass> entityClass;
 
     public SummonCommand() {
         super("summon");
         setCondition(Conditions::playerOnly);
 
         entity = ArgumentType.EntityType("entity type");
-        pos = ArgumentType.RelativeVec3("pos");
-        entityClass = ArgumentType.Enum("class", EntityClass.class);
-        entityClass.setFormat(ArgumentEnum.Format.LOWER_CASED);
-        entityClass.setDefaultValue(EntityClass.CREATURE);
+        pos = ArgumentType.RelativeVec3("pos").setDefaultValue(() -> new RelativeVec(
+                new Vec(0, 0, 0),
+                RelativeVec.CoordinateType.RELATIVE,
+                true, true, true
+        ));
+        entityClass = ArgumentType.Enum("class", EntityClass.class)
+                .setFormat(ArgumentEnum.Format.LOWER_CASED)
+                .setDefaultValue(EntityClass.CREATURE);;
         addSyntax(this::execute, entity, pos, entityClass);
         setDefaultExecutor((sender, context) -> sender.sendMessage("Usage: /summon <type> <x> <y> <z> <class>"));
     }
