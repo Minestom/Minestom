38181a711dd103c6784e16601210289b13f2ebbc
TheMode <themode@outlook.fr>
2021-10-05 21:19:28 +0200

    Add assert
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/event/trait/EntityInstanceEvent.java
+++ src/main/java/net/minestom/server/event/trait/EntityInstanceEvent.java
@@ -7,11 +7,15 @@ import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents an {@link EntityEvent} which happen in {@link Entity#getInstance()}.
+ * <p>
+ * Be aware that the entity's instance must be non-null.
  */
 @ApiStatus.Internal
 public interface EntityInstanceEvent extends EntityEvent, InstanceEvent {
     @Override
     default @NotNull Instance getInstance() {
-        return getEntity().getInstance();
+        final Instance instance = getEntity().getInstance();
+        assert instance != null : "EntityInstanceEvent is only supported on events where the entity's instance is non-null!";
+        return instance;
     }
 }
