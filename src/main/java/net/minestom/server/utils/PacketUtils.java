0921a18e210fb7046bce990fda99afe8c94b696b
TheMode <themode@outlook.fr>
2021-10-13 06:32:08 +0200

    Cleanup
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/utils/PacketUtils.java
+++ src/main/java/net/minestom/server/utils/PacketUtils.java
@@ -120,7 +120,7 @@ public final class PacketUtils {
             for (Player player : players) {
                 if (!player.isOnline() || !playerValidator.isValid(player))
                     continue;
-                player.getPlayerConnection().sendPacket(framedPacket);
+                player.sendPacket(framedPacket);
             }
         } else {
             // Write the same packet for each individual players
