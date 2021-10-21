5897fcb6e3b03d8fc25da2424dfaff81dc2a458b
TheMode <themode@outlook.fr>
2021-10-21 21:08:51 +0200

    Update registry
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/entity/Player.java
+++ src/main/java/net/minestom/server/entity/Player.java
@@ -292,8 +292,7 @@ public class Player extends LivingEntity implements CommandSender, Localizable, 
         // Recipes end
 
         // Tags
-        // FIXME: 1.18
-        //this.playerConnection.sendPacket(TagsPacket.DEFAULT_TAGS);
+        this.playerConnection.sendPacket(TagsPacket.DEFAULT_TAGS);
 
         // Some client updates
         this.playerConnection.sendPacket(getPropertiesPacket()); // Send default properties
