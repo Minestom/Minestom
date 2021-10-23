0b1c04dc19e795af15f69f7403cff00fb2c905c7
TheMode <themode@outlook.fr>
2021-10-23 13:47:56 +0200

    Make new packets record (reading method still to be defined)
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/network/packet/server/play/ChunkDataPacket.java
+++ src/main/java/net/minestom/server/network/packet/server/play/ChunkDataPacket.java
@@ -4,31 +4,17 @@ import net.minestom.server.network.packet.server.ServerPacket;
 import net.minestom.server.network.packet.server.ServerPacketIdentifier;
 import net.minestom.server.network.packet.server.play.data.ChunkData;
 import net.minestom.server.network.packet.server.play.data.LightData;
-import net.minestom.server.utils.binary.BinaryReader;
 import net.minestom.server.utils.binary.BinaryWriter;
 import org.jetbrains.annotations.NotNull;
 
-public class ChunkDataPacket implements ServerPacket {
-    public int chunkX, chunkZ;
-    public ChunkData chunkData;
-    public LightData lightData;
-
-    public ChunkDataPacket() {
-    }
-
+public record ChunkDataPacket(int chunkX, int chunkZ, ChunkData chunkData, LightData lightData)
+        implements ServerPacket {
     @Override
     public void write(@NotNull BinaryWriter writer) {
         writer.writeInt(chunkX);
         writer.writeInt(chunkZ);
         this.chunkData.write(writer);
         this.lightData.write(writer);
-    }
-
-    @Override
-    public void read(@NotNull BinaryReader reader) {
-        this.chunkX = reader.readInt();
-        this.chunkZ = reader.readInt();
-        // TODO read
     }
 
     @Override
