674f2e8acd5e50fe30f50e9d063e850b28e3f8d7
TheMode <themode@outlook.fr>
2021-11-04 08:18:50 +0100

    Update to 21w44a
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/MinecraftServer.java
+++ src/main/java/net/minestom/server/MinecraftServer.java
@@ -54,8 +54,8 @@ public final class MinecraftServer {
 
     public final static Logger LOGGER = LoggerFactory.getLogger(MinecraftServer.class);
 
-    public static final String VERSION_NAME = "21w42a";
-    public static final int PROTOCOL_VERSION = 1073741870;
+    public static final String VERSION_NAME = "21w44a";
+    public static final int PROTOCOL_VERSION = 1073741872;
 
     // Threads
     public static final String THREAD_NAME_BENCHMARK = "Ms-Benchmark";
