7a4086c889e48ca3912d39345a1b9bbd84ccabe3
TheMode <themode@outlook.fr>
2021-11-05 06:26:41 +0100

    Reduce default socket send buffer size
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/network/socket/Server.java
+++ src/main/java/net/minestom/server/network/socket/Server.java
@@ -18,7 +18,7 @@ public final class Server {
     public static final int WORKER_COUNT = Integer.getInteger("minestom.workers",
             Math.max(1, Runtime.getRuntime().availableProcessors() / 2));
     public static final int MAX_PACKET_SIZE = 2_097_151; // 3 bytes var-int
-    public static final int SOCKET_SEND_BUFFER_SIZE = Integer.getInteger("minestom.send-buffer-size", MAX_PACKET_SIZE);
+    public static final int SOCKET_SEND_BUFFER_SIZE = Integer.getInteger("minestom.send-buffer-size", 262_143);
     public static final int SOCKET_RECEIVE_BUFFER_SIZE = Integer.getInteger("minestom.receive-buffer-size", 32_767);
 
     public static final boolean NO_DELAY = true;
