5b0c38049d6f525efbc44ab439bea18e6bb62cdb
Legitimoose <legitimoose@gmail.com>
2021-11-06 18:52:33 -0700

    make holograms area effect clouds instead of armor stands (2-3x higher  FPS when tested with 1000 of each)

--- src/main/java/net/minestom/server/entity/hologram/Hologram.java
+++ src/main/java/net/minestom/server/entity/hologram/Hologram.java
@@ -6,6 +6,7 @@ import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.Player;
+import net.minestom.server.entity.metadata.other.AreaEffectCloudMeta;
 import net.minestom.server.entity.metadata.other.ArmorStandMeta;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.utils.validate.Check;
@@ -14,15 +15,13 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Set;
 
 /**
- * Represents an invisible armor stand showing a {@link Component}.
+ * Represents an invisible area effect cloud showing a {@link Component}.
  */
 public class Hologram implements Viewable {
 
-    private static final float OFFSET_Y = -0.9875f;
-    private static final float MARKER_OFFSET_Y = -0.40625f;
+    private static final float OFFSET_Y = -0.5f;
 
     private final Entity entity;
-    private final float yOffset;
 
     private Pos position;
     private Component text;
@@ -61,27 +60,20 @@ public class Hologram implements Viewable {
      * @param autoViewable  {@code true}if the hologram should be visible automatically, otherwise {@code false}.
      */
     public Hologram(Instance instance, Pos spawnPosition, Component text, boolean autoViewable, boolean marker) {
-        this.entity = new Entity(EntityType.ARMOR_STAND);
+        this.entity = new Entity(EntityType.AREA_EFFECT_CLOUD);
 
-        ArmorStandMeta armorStandMeta = (ArmorStandMeta) entity.getEntityMeta();
+        AreaEffectCloudMeta areaEffectCloudMeta = (AreaEffectCloudMeta) entity.getEntityMeta();
 
-        armorStandMeta.setNotifyAboutChanges(false);
+        areaEffectCloudMeta.setNotifyAboutChanges(false);
 
-        if (marker) {
-            this.yOffset = MARKER_OFFSET_Y;
-            armorStandMeta.setMarker(true);
-        } else {
-            this.yOffset = OFFSET_Y;
-            armorStandMeta.setSmall(true);
-        }
-        armorStandMeta.setHasNoGravity(true);
-        armorStandMeta.setCustomName(Component.empty());
-        armorStandMeta.setCustomNameVisible(true);
-        armorStandMeta.setInvisible(true);
+        areaEffectCloudMeta.setHasNoGravity(true);
+        areaEffectCloudMeta.setCustomName(Component.empty());
+        areaEffectCloudMeta.setCustomNameVisible(true);
+        areaEffectCloudMeta.setRadius(0f);
 
-        armorStandMeta.setNotifyAboutChanges(true);
+        areaEffectCloudMeta.setNotifyAboutChanges(true);
 
-        this.entity.setInstance(instance, spawnPosition.add(0, this.yOffset, 0));
+        this.entity.setInstance(instance, spawnPosition.add(0, OFFSET_Y, 0));
         this.entity.setAutoViewable(autoViewable);
 
         this.position = spawnPosition;
@@ -104,7 +96,7 @@ public class Hologram implements Viewable {
      */
     public void setPosition(Pos position) {
         checkRemoved();
-        this.position = position.add(0, this.yOffset, 0);
+        this.position = position.add(0, OFFSET_Y, 0);
         this.entity.teleport(this.position);
     }
 
