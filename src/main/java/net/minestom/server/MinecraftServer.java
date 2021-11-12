d54e77b30f044b146a7aa069e2165b4fb4a9a558
TheMode <themode@outlook.fr>
2021-11-12 17:55:22 +0100

    Update to 1.18-pre1
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/MinecraftServer.java
+++ src/main/java/net/minestom/server/MinecraftServer.java
@@ -54,8 +54,8 @@ public final class MinecraftServer {
 
     public final static Logger LOGGER = LoggerFactory.getLogger(MinecraftServer.class);
 
-    public static final String VERSION_NAME = "21w44a";
-    public static final int PROTOCOL_VERSION = 1073741872;
+    public static final String VERSION_NAME = "1.18-pre1";
+    public static final int PROTOCOL_VERSION = 1073741873;
 
     // Threads
     public static final String THREAD_NAME_BENCHMARK = "Ms-Benchmark";
