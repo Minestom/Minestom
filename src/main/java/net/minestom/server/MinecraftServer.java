e8fdcc7568bc7efa629952563c7435eb390a8407
TheMode <themode@outlook.fr>
2021-11-29 19:24:34 +0100

    1.18-rc4
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/MinecraftServer.java
+++ src/main/java/net/minestom/server/MinecraftServer.java
@@ -54,8 +54,8 @@ public final class MinecraftServer {
 
     public final static Logger LOGGER = LoggerFactory.getLogger(MinecraftServer.class);
 
-    public static final String VERSION_NAME = "1.18-rc3";
-    public static final int PROTOCOL_VERSION = 1073741883;
+    public static final String VERSION_NAME = "1.18-rc4";
+    public static final int PROTOCOL_VERSION = 1073741884;
 
     // Threads
     public static final String THREAD_NAME_BENCHMARK = "Ms-Benchmark";
