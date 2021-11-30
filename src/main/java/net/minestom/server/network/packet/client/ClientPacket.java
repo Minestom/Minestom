c0315990624d20dd866418106051a91b3b9e1942
TheMode <themode@outlook.fr>
2021-11-30 19:15:16 +0100

    Packets are value based
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/network/packet/client/ClientPacket.java
+++ src/main/java/net/minestom/server/network/packet/client/ClientPacket.java
@@ -4,6 +4,8 @@ import net.minestom.server.utils.binary.Writeable;
 
 /**
  * Represents a packet received from a client.
+ * <p>
+ * Packets are value-based, and should therefore be reliant on identity.
  */
 public interface ClientPacket extends Writeable {
 }
