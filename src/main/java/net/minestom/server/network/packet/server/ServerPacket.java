767f3ce3f49ba660e0947a0643900a896c717d9f
TheMode <themode@outlook.fr>
2021-11-30 19:15:16 +0100

    Packets are value based
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/network/packet/server/ServerPacket.java
+++ src/main/java/net/minestom/server/network/packet/server/ServerPacket.java
@@ -5,6 +5,8 @@ import net.minestom.server.utils.binary.Writeable;
 
 /**
  * Represents a packet which can be sent to a player using {@link PlayerConnection#sendPacket(SendablePacket)}.
+ * <p>
+ * Packets are value-based, and should therefore be reliant on identity.
  */
 public non-sealed interface ServerPacket extends Writeable, SendablePacket {
 
