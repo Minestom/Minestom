47097e819f5bd7ae25ac1375da22eae38f888f58
TheMode <themode@outlook.fr>
2021-12-05 07:59:24 +0100

    Add tests for the scheduler api
    Signed-off-by: TheMode <themode@outlook.fr>

---
+++ src/test/java/timer/TestScheduler.java
@@ -0,0 +1,123 @@
+package timer;
+
+import net.minestom.server.timer.ExecutionType;
+import net.minestom.server.timer.Scheduler;
+import net.minestom.server.timer.TaskSchedule;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class TestScheduler {
+
+    @Test
+    public void tickTask() {
+        Scheduler scheduler = Scheduler.newScheduler();
+        AtomicBoolean result = new AtomicBoolean(false);
+        scheduler.buildTask(() -> result.set(true))
+                .delay(TaskSchedule.tick(1))
+                .schedule();
+        Assertions.assertFalse(result.get(), "Tick task should not be executed after scheduling");
+        scheduler.process();
+        Assertions.assertFalse(result.get(), "Tick task should not be executed after process");
+        scheduler.processTick();
+        Assertions.assertTrue(result.get(), "Tick task must be executed after tick process");
+    }
+
+    @Test
+    public void durationTask() throws InterruptedException {
+        Scheduler scheduler = Scheduler.newScheduler();
+        AtomicBoolean result = new AtomicBoolean(false);
+        scheduler.buildTask(() -> result.set(true))
+                .executionType(ExecutionType.SYNC)
+                .delay(TaskSchedule.seconds(1))
+                .schedule();
+        Thread.sleep(100);
+        scheduler.process();
+        Assertions.assertFalse(result.get(), "900ms remaining");
+        Thread.sleep(1200);
+        scheduler.process();
+        Assertions.assertTrue(result.get(), "Tick task must be executed after 1 second");
+    }
+
+    @Test
+    public void cancelTask() {
+        Scheduler scheduler = Scheduler.newScheduler();
+        AtomicBoolean result = new AtomicBoolean(false);
+        var task = scheduler.buildTask(() -> result.set(true))
+                .executionType(ExecutionType.SYNC)
+                .schedule();
+        Assertions.assertTrue(task.isAlive(), "Task should still be alive");
+        task.cancel();
+        Assertions.assertFalse(task.isAlive(), "Task should not be alive anymore");
+        scheduler.process();
+        Assertions.assertFalse(result.get(), "Task should be cancelled");
+    }
+
+    @Test
+    public void parkTask() {
+        Scheduler scheduler = Scheduler.newScheduler();
+        // Ignored parked task
+        scheduler.buildTask(() -> Assertions.fail("This parked task should never be executed"))
+                .executionType(ExecutionType.SYNC)
+                .delay(TaskSchedule.park())
+                .schedule();
+
+        // Unpark task
+        AtomicBoolean result = new AtomicBoolean(false);
+        var task = scheduler.buildTask(() -> result.set(true))
+                .executionType(ExecutionType.SYNC)
+                .delay(TaskSchedule.park())
+                .schedule();
+        task.unpark();
+        Assertions.assertTrue(result.get(), "Parked task should be executed");
+    }
+
+    @Test
+    public void futureTask() {
+        Scheduler scheduler = Scheduler.newScheduler();
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        AtomicBoolean result = new AtomicBoolean(false);
+        scheduler.buildTask(() -> result.set(true))
+                .executionType(ExecutionType.SYNC)
+                .delay(TaskSchedule.future(future))
+                .schedule();
+        Assertions.assertFalse(result.get(), "Future is not completed yet");
+
+        future.complete(null);
+        Assertions.assertTrue(result.get(), "Future should be completed");
+    }
+
+    @Test
+    public void futureTaskThread() {
+        Scheduler scheduler = Scheduler.newScheduler();
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        AtomicBoolean result = new AtomicBoolean(false);
+        scheduler.buildTask(() -> result.set(true))
+                .executionType(ExecutionType.SYNC)
+                .delay(TaskSchedule.future(future))
+                .schedule();
+        Assertions.assertFalse(result.get(), "Future is not completed yet");
+        future.complete(null);
+        Assertions.assertTrue(result.get(), "Future should be completed");
+    }
+
+    @Test
+    public void asyncTask() throws InterruptedException {
+        final Thread currentThread = Thread.currentThread();
+        Scheduler scheduler = Scheduler.newScheduler();
+        AtomicBoolean result = new AtomicBoolean(false);
+        scheduler.buildTask(() -> {
+                    Assertions.assertNotEquals(currentThread, Thread.currentThread(),
+                            "Task should be executed in a different thread");
+                    result.set(true);
+                })
+                .executionType(ExecutionType.ASYNC)
+                .schedule();
+        Assertions.assertFalse(result.get(), "Async task should only be executed after process()");
+        scheduler.process();
+        Thread.sleep(250);
+        Assertions.assertTrue(result.get(), "Async task didn't get executed");
+    }
+}
