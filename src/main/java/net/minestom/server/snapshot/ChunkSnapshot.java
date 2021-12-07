d916b9bb7a341e44003ff771574c6c267784db16
TheMode <themode@outlook.fr>
2021-12-07 01:45:06 +0100

    Add default impl for block/biome getters
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/snapshot/ChunkSnapshot.java
+++ src/main/java/net/minestom/server/snapshot/ChunkSnapshot.java
@@ -4,13 +4,30 @@ import net.minestom.server.instance.block.Block;
 import net.minestom.server.tag.TagReadable;
 import net.minestom.server.world.biomes.Biome;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.UnknownNullability;
 
 import java.util.List;
+import java.util.Objects;
 
 public interface ChunkSnapshot extends Snapshot, Block.Getter, Biome.Getter, TagReadable {
     int chunkX();
 
     int chunkZ();
+
+    @Override
+    default @UnknownNullability Block getBlock(int x, int y, int z, @NotNull Condition condition) {
+        SectionSnapshot section = section(y);
+        return Objects.requireNonNull(section).getBlock(x, y, z, condition);
+    }
+
+    @Override
+    default @NotNull Biome getBiome(int x, int y, int z) {
+        SectionSnapshot section = section(y);
+        return Objects.requireNonNull(section).getBiome(x, y, z);
+    }
+
+    @Nullable SectionSnapshot section(int y);
 
     @NotNull List<@NotNull SectionSnapshot> sections();
 
