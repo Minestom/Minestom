11c77f492f8bafe43721c17f032523a2f8872206
GoldenStack <goldenfire64yt@gmail.com>
2021-12-07 22:21:26 -0600

    Add cursed ArgumentComponent read method
    Signed-off-by: GoldenStack <goldenfire64yt@gmail.com>

--- src/main/java/net/minestom/server/command/builder/arguments/minecraft/ArgumentComponent.java
+++ src/main/java/net/minestom/server/command/builder/arguments/minecraft/ArgumentComponent.java
@@ -1,20 +1,53 @@
 package net.minestom.server.command.builder.arguments.minecraft;
 
 import com.google.gson.JsonParseException;
+import com.google.gson.stream.JsonReader;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import net.minestom.server.command.StringReader;
 import net.minestom.server.command.builder.NodeMaker;
 import net.minestom.server.command.builder.arguments.Argument;
 import net.minestom.server.command.builder.exception.ArgumentSyntaxException;
+import net.minestom.server.command.builder.exception.CommandException;
 import net.minestom.server.network.packet.server.play.DeclareCommandsPacket;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.lang.reflect.Field;
+
 public class ArgumentComponent extends Argument<Component> {
+
+    private static Field posField = null;
 
     public static final int INVALID_JSON_ERROR = 1;
 
     public ArgumentComponent(@NotNull String id) {
         super(id, true);
+    }
+
+    @Override
+    public @NotNull Component parse(@NotNull StringReader input) throws CommandException {
+
+        JsonReader reader = new JsonReader(new java.io.StringReader(input.unread()));
+
+        if (posField == null) {
+            try {
+                posField = JsonReader.class.getDeclaredField("pos");
+            } catch (NoSuchFieldException exception) {
+                throw CommandException.ARGUMENT_COMPONENT_INVALID.generateException(input, exception.getMessage());
+            }
+            posField.setAccessible(true);
+        }
+
+        var adapter = GsonComponentSerializer.gson().serializer().getAdapter(Component.class);
+
+        try {
+            Component read = adapter.read(reader);
+            input.position(input.position() + posField.getInt(reader));
+            return read;
+        } catch (IllegalAccessException | IOException | JsonParseException exception) {
+            throw CommandException.ARGUMENT_COMPONENT_INVALID.generateException(input, exception.getMessage());
+        }
     }
 
     @NotNull
