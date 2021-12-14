d46b7595ee57b1bab354383d6ddfda27b9dc2c92
mworzala <mattheworzala@gmail.com>
2021-12-13 22:24:39 -0500

    Optional & platform specific dependencies

--- src/main/java/net/minestom/server/extensions/descriptor/Dependency.java
+++ src/main/java/net/minestom/server/extensions/descriptor/Dependency.java
@@ -2,7 +2,7 @@ package net.minestom.server.extensions.descriptor;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import org.jetbrains.annotations.NotNull;
+import net.minestom.server.utils.PlatformUtil;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,6 +39,12 @@ public sealed interface Dependency permits ExtensionDependency, MavenDependency 
             if (object.has("optional")) {
                 optional = object.get("optional").getAsBoolean();
             }
+
+            // Platform validation
+            if (!isApplicableToCurrentPlatform(object)) {
+                LOGGER.debug("Dependency {} is not applicable to current platform", id);
+                return null;
+            }
         } else {
             LOGGER.error("Invalid dependency format: " + json);
             return null;
@@ -60,4 +66,33 @@ public sealed interface Dependency permits ExtensionDependency, MavenDependency 
     String id();
 
     boolean isOptional();
+
+    /**
+     * Checks if a dependency is applicable on the current platform. Currently this
+     * involves a check for the current OS and architecture.
+     *
+     * OS Values: macos, windows, linux
+     * Arch Values: x86, x86_64, arm64
+     *
+     * @param json The json to investigate
+     * @return True if the dependency is applicable, false otherwise.
+     */
+    private static boolean isApplicableToCurrentPlatform(JsonObject json) {
+        try {
+            if (json.has("os")) {
+                String os = json.get("os").getAsString();
+                if (!os.equals(PlatformUtil.OS)) return false;
+            }
+
+            if (json.has("arch")) {
+                String arch = json.get("arch").getAsString();
+                if (!arch.equals(PlatformUtil.ARCH)) return false;
+            }
+
+            return true;
+        } catch (Throwable exception) {
+            LOGGER.warn("Failed to parse validate platform spec: " + json, exception);
+            return false;
+        }
+    }
 }
--- src/test/java/net/minestom/server/extensions/descriptor/DependencyTest.java
+++ src/test/java/net/minestom/server/extensions/descriptor/DependencyTest.java
@@ -2,13 +2,18 @@ package net.minestom.server.extensions.descriptor;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import net.minestom.server.utils.PlatformUtil;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.params.provider.Arguments.of;
 
 public class DependencyTest {
@@ -36,6 +41,32 @@ public class DependencyTest {
         assertEquals(expectedOptional, dependency.isOptional());
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"LuckPerms", "LuckPerms:5.0.0"})
+    public void testIdParsing(String id) {
+        JsonElement json = JsonParser.parseString("\"" + id + "\"");
+        Dependency dependency = Dependency.fromJson(json);
+
+        assertNotNull(dependency);
+        assertEquals("luckperms", dependency.id());
+    }
+
+    @ParameterizedTest
+    @MethodSource("platformValidationProvider")
+    public void testPlatformValidation(String dependencyString, boolean expectedValid) {
+        assumeTrue(PlatformUtil.OS.equals("macos"));
+        assumeTrue(PlatformUtil.ARCH.equals("arm64"));
+
+        JsonElement json = JsonParser.parseString(dependencyString);
+        Dependency dependency = Dependency.fromJson(json);
+
+        if (expectedValid) {
+            assertNotNull(dependency);
+        } else {
+            assertNull(dependency);
+        }
+    }
+
     private static Stream<Arguments> dependencyTypesProvider() {
         return Stream.of(
                 of("\"LuckPerms\"", ExtensionDependency.class),
@@ -51,6 +82,56 @@ public class DependencyTest {
                 of("{\"id\"=\"LuckPerms\"}", false),
                 of("{\"id\"=\"LuckPerms\",\"optional\":false}", false),
                 of("{\"id\"=\"LuckPerms\",\"optional\":true}", true)
+        );
+    }
+
+    private static Stream<Arguments> platformValidationProvider() {
+        return Stream.of(
+                of("""
+                {
+                    "id": "org.lwjgl:lwjgl:3.2.2-natives-macos-arm64",
+                    "os": "macos",
+                    "arch": "arm64"
+                }
+                """, true),
+                of("""
+                {
+                    "id": "org.lwjgl:lwjgl:3.2.2-natives-macos-arm64",
+                    "os": "macos"
+                }
+                """, true),
+                of("""
+                {
+                    "id": "org.lwjgl:lwjgl:3.2.2-natives-macos-arm64",
+                    "arch": "arm64"
+                }
+                """, true),
+                of("""
+                {
+                    "id": "org.lwjgl:lwjgl:3.2.2-natives-macos-arm64",
+                    "os": "windows",
+                    "arch": "arm64"
+                }
+                """, false),
+                of("""
+                {
+                    "id": "org.lwjgl:lwjgl:3.2.2-natives-macos-arm64",
+                    "os": "macos",
+                    "arch": "x64"
+                }
+                """, false),
+                of("""
+                {
+                    "id": "org.lwjgl:lwjgl:3.2.2-natives-macos-arm64",
+                    "os": "windows"
+                }
+                """, false),
+                of("""
+                {
+                    "id": "org.lwjgl:lwjgl:3.2.2-natives-macos-arm64",
+                    "arch": "x64"
+                }
+                """, false)
         );
     }
 }
