71f05188cfb49dedb5b5daf33d19bc7bf84caf9f
TheMode <themode@outlook.fr>
2021-12-17 19:02:29 +0100

    Add SendablePacket#extractServerPacket
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/network/packet/server/SendablePacket.java
+++ src/main/java/net/minestom/server/network/packet/server/SendablePacket.java
@@ -2,6 +2,7 @@ package net.minestom.server.network.packet.server;
 
 import net.minestom.server.network.player.PlayerConnection;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents a packet that can be sent to a {@link PlayerConnection}.
@@ -9,4 +10,17 @@ import org.jetbrains.annotations.ApiStatus;
 @ApiStatus.Experimental
 public sealed interface SendablePacket
         permits ServerPacket, CachedPacket, FramedPacket {
+
+    @ApiStatus.Experimental
+    static @NotNull ServerPacket extractServerPacket(@NotNull SendablePacket packet) {
+        if (packet instanceof ServerPacket serverPacket) {
+            return serverPacket;
+        } else if (packet instanceof CachedPacket cachedPacket) {
+            return cachedPacket.packet();
+        } else if (packet instanceof FramedPacket framedPacket) {
+            return framedPacket.packet();
+        } else {
+            throw new RuntimeException("Unknown packet type: " + packet.getClass().getName());
+        }
+    }
 }
