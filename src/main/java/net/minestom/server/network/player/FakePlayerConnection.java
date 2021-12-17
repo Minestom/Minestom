63beadd223d5b8c29f900a05a418c0a01c3de800
TheMode <themode@outlook.fr>
2021-12-17 19:02:29 +0100

    Add SendablePacket#extractServerPacket
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/network/player/FakePlayerConnection.java
+++ src/main/java/net/minestom/server/network/player/FakePlayerConnection.java
@@ -4,8 +4,6 @@ import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Player;
 import net.minestom.server.entity.fakeplayer.FakePlayer;
 import net.minestom.server.entity.fakeplayer.FakePlayerController;
-import net.minestom.server.network.packet.server.CachedPacket;
-import net.minestom.server.network.packet.server.FramedPacket;
 import net.minestom.server.network.packet.server.SendablePacket;
 import net.minestom.server.network.packet.server.ServerPacket;
 import net.minestom.server.utils.validate.Check;
@@ -19,16 +17,9 @@ public class FakePlayerConnection extends PlayerConnection {
     @Override
     public void sendPacket(@NotNull SendablePacket packet) {
         FakePlayerController controller = getFakePlayer().getController();
-        if (packet instanceof ServerPacket serverPacket) {
-            if (!shouldSendPacket(serverPacket)) return;
-            controller.consumePacket(serverPacket);
-        } else if (packet instanceof FramedPacket framedPacket) {
-            controller.consumePacket(framedPacket.packet());
-        } else if (packet instanceof CachedPacket cachedPacket) {
-            controller.consumePacket(cachedPacket.packet());
-        } else {
-            throw new RuntimeException("Unknown packet type: " + packet.getClass().getName());
-        }
+        final ServerPacket serverPacket = SendablePacket.extractServerPacket(packet);
+        if (!shouldSendPacket(serverPacket)) return;
+        controller.consumePacket(serverPacket);
     }
 
     @NotNull
