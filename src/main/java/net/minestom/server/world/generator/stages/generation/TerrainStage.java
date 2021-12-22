825f76e209a2bb885d7eeedd45db51606b4d7c7f
Németh Noel <noel@noelnemeth.com>
2021-12-22 19:57:40 +0100

    Fix biome id

--- src/main/java/net/minestom/server/world/generator/stages/generation/TerrainStage.java
+++ src/main/java/net/minestom/server/world/generator/stages/generation/TerrainStage.java
@@ -8,7 +8,7 @@ import net.minestom.server.world.generator.GenerationContext;
 public class TerrainStage implements GenerationStage {
     @Override
     public void process(GenerationContext context, SectionBlockCache blockCache, Palette biomePalette, int sectionX, int sectionY, int sectionZ) {
-        if (sectionY < 3) {
+        if (sectionY < 2) {
             for (int x = 0; x < 16; x++) {
                 for (int y = 0; y < 16; y++) {
                     for (int z = 0; z < 16; z++) {
@@ -16,11 +16,19 @@ public class TerrainStage implements GenerationStage {
                     }
                 }
             }
+
+            if (sectionY == -4) {
+                for (int x = 0; x < 16; x++) {
+                    for (int z = 0; z < 16; z++) {
+                        blockCache.setBlock(x,0,z, Block.BEDROCK);
+                    }
+                }
+            }
         }
         for (int x = 0; x < 4; x++) {
             for (int y = 0; y < 4; y++) {
                 for (int z = 0; z < 4; z++) {
-                    biomePalette.set(x,y,z, 1);
+                    biomePalette.set(x,y,z, 0);
                 }
             }
         }
