7cf5bede0af80f320bab9e421c0cb4eb673961bf
themode <themode@outlook.fr>
2021-12-24 10:07:55 +0100

    Make ItemAttribute record

--- src/main/java/net/minestom/server/item/attribute/ItemAttribute.java
+++ src/main/java/net/minestom/server/item/attribute/ItemAttribute.java
@@ -2,48 +2,13 @@ package net.minestom.server.item.attribute;
 
 import net.minestom.server.attribute.Attribute;
 import net.minestom.server.attribute.AttributeOperation;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
-public class ItemAttribute {
-
-    private final UUID uuid;
-    private final String internalName;
-    private final Attribute attribute;
-    private final AttributeOperation operation;
-    private final double value;
-    private final AttributeSlot slot;
-
-    public ItemAttribute(UUID uuid, String internalName, Attribute attribute, AttributeOperation operation, double value, AttributeSlot slot) {
-        this.uuid = uuid;
-        this.internalName = internalName;
-        this.attribute = attribute;
-        this.operation = operation;
-        this.value = value;
-        this.slot = slot;
-    }
-
-    public UUID getUuid() {
-        return uuid;
-    }
-
-    public String getInternalName() {
-        return internalName;
-    }
-
-    public Attribute getAttribute() {
-        return attribute;
-    }
-
-    public AttributeOperation getOperation() {
-        return operation;
-    }
-
-    public double getValue() {
-        return value;
-    }
-
-    public AttributeSlot getSlot() {
-        return slot;
-    }
+public record ItemAttribute(@NotNull UUID uuid,
+                            @NotNull String name,
+                            @NotNull Attribute attribute,
+                            @NotNull AttributeOperation operation, double amount,
+                            @NotNull AttributeSlot slot) {
 }
