5dc2e522329f14d66c3c69ae2f14e9f6ddd0a5f3
Németh Noel <noel@noelnemeth.com>
2021-12-25 17:16:04 +0100

    Make the writer optional, rename method to better reflect meaning

--- src/main/java/net/minestom/server/world/generator/stages/pregeneration/StageData.java
+++ src/main/java/net/minestom/server/world/generator/stages/pregeneration/StageData.java
@@ -1,9 +1,21 @@
 package net.minestom.server.world.generator.stages.pregeneration;
 
+import net.minestom.server.utils.binary.BinaryWriter;
 import net.minestom.server.utils.binary.Writeable;
+import org.jetbrains.annotations.NotNull;
 
 public interface StageData extends Writeable {
-    boolean generated();
+    //TODO Default implementation returning false?
+    boolean supportsSaving();
+
+    @Override
+    default void write(@NotNull BinaryWriter writer) {
+        if (supportsSaving()) {
+            throw new IllegalStateException("Savable StageData doesn't implement the writer method!");
+        } else {
+            throw new IllegalStateException("Saving isn't supported for this data!");
+        }
+    }
 
     abstract class Instance implements StageData {}
     abstract class Chunk implements StageData {}
