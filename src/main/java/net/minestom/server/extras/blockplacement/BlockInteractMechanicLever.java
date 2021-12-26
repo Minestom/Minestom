7a16e06257bececebeb4848e2aa2913445c7ee40
Moulberry <james.jenour@protonmail.com>
2021-12-26 20:24:38 +0800

    Actually add interactions

--- src/main/java/net/minestom/server/extras/blockplacement/BlockInteractMechanicLever.java
+++ src/main/java/net/minestom/server/extras/blockplacement/BlockInteractMechanicLever.java
@@ -4,33 +4,19 @@ import net.minestom.server.coordinate.Point;
 import net.minestom.server.event.player.PlayerBlockInteractEvent;
 import net.minestom.server.instance.block.Block;
 
-class BlockInteractMechanicOpen {
+class BlockInteractMechanicLever {
 
     static void onInteract(Block block, PlayerBlockInteractEvent event) {
         if (event.getPlayer().isSneaking()) return;
 
         block = event.getBlock();
 
-        boolean open = "true".equals(block.getProperty("open"));
+        boolean powered = "true".equals(block.getProperty("powered"));
 
-        String value = open ? "false" : "true";
+        String value = powered ? "false" : "true";
 
-        event.setBlock(block.withProperty("open", value));
+        event.setBlock(block.withProperty("powered", value));
         event.setBlockingItemUse(true);
-
-        // Doors need to set the open property on both blocks
-        String half = block.getProperty("half");
-        if (half != null) {
-            int offset = 1;
-            if (half.equals("upper")) {
-                offset = -1;
-            }
-            Point pos = event.getBlockPosition().add(0, offset, 0);
-            Block offsetBlock = event.getInstance().getBlock(pos);
-            if (offsetBlock.compare(block)) {
-                event.getInstance().setBlock(pos, offsetBlock.withProperty("open", value));
-            }
-        }
     }
 
 }
--- src/main/java/net/minestom/server/extras/blockplacement/BlockInteractMechanicOpen.java
+++ src/main/java/net/minestom/server/extras/blockplacement/BlockInteractMechanicOpen.java
@@ -1,17 +1,36 @@
 package net.minestom.server.extras.blockplacement;
 
+import net.minestom.server.coordinate.Point;
 import net.minestom.server.event.player.PlayerBlockInteractEvent;
 import net.minestom.server.instance.block.Block;
 
 class BlockInteractMechanicOpen {
 
     static void onInteract(Block block, PlayerBlockInteractEvent event) {
+        if (event.getPlayer().isSneaking()) return;
+
         block = event.getBlock();
 
         boolean open = "true".equals(block.getProperty("open"));
 
-        event.setBlock(block.withProperty("open", open ? "false" : "true"));
+        String value = open ? "false" : "true";
+
+        event.setBlock(block.withProperty("open", value));
         event.setBlockingItemUse(true);
+
+        // Doors need to set the open property on both blocks
+        String half = block.getProperty("half");
+        if (half != null) {
+            int offset = 1;
+            if (half.equals("upper")) {
+                offset = -1;
+            }
+            Point pos = event.getBlockPosition().add(0, offset, 0);
+            Block offsetBlock = event.getInstance().getBlock(pos);
+            if (offsetBlock.compare(block)) {
+                event.getInstance().setBlock(pos, offsetBlock.withProperty("open", value));
+            }
+        }
     }
 
 }
