6831e9593bdabf4ba190b79d5104d3a241c33d6e
Németh Noel <noel@noelnemeth.com>
2021-12-26 22:34:31 +0100

    Add scaling methods

--- src/main/java/net/minestom/server/utils/noise/Noise2D.java
+++ src/main/java/net/minestom/server/utils/noise/Noise2D.java
@@ -3,4 +3,14 @@ package net.minestom.server.utils.noise;
 @FunctionalInterface
 public interface Noise2D {
     double getValue(double x, double y);
+
+    static Noise2D scale(Noise2D base, double factor) {
+        return ((x, y) -> base.getValue(x*factor, y*factor));
+    }
+    static Noise2D scaleX(Noise2D base, double factor) {
+        return ((x, y) -> base.getValue(x*factor, y));
+    }
+    static Noise2D scaleY(Noise2D base, double factor) {
+        return ((x, y) -> base.getValue(x, y*factor));
+    }
 }
