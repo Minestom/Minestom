b21c3b5aaa5f759d85d832914998f2ec7beb1914
Loïs Duplain <loisdupl42@gmail.com>
2021-12-29 19:40:59 +0100

    Fix and optimize getLookYaw and getLookPitch in PositionUtils.java

--- src/main/java/net/minestom/server/utils/position/PositionUtils.java
+++ src/main/java/net/minestom/server/utils/position/PositionUtils.java
@@ -1,11 +1,13 @@
 package net.minestom.server.utils.position;
 
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.utils.MathUtils;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 @ApiStatus.Internal
 public final class PositionUtils {
+
     public static Pos lookAlong(@NotNull Pos position, double dx, double dy, double dz) {
         final float yaw = getLookYaw(dx, dz);
         final float pitch = getLookPitch(dx, dy, dz);
@@ -13,11 +15,10 @@ public final class PositionUtils {
     }
 
     public static float getLookYaw(double dx, double dz) {
-        final double horizontalAngle = Math.atan2(dz, dx);
-        return (float) (horizontalAngle * (180.0 / Math.PI)) - 90;
+        return (float) (Math.atan2(dz, dx) * MathUtils.RADIANS_TO_DEGREES) - 90;
     }
 
     public static float getLookPitch(double dx, double dy, double dz) {
-        return (float) Math.atan2(dy, Math.max(Math.abs(dx), Math.abs(dz)));
+        return (float) (Math.atan2(dy, MathUtils.distance2(dx, dz)) * -MathUtils.RADIANS_TO_DEGREES);
     }
 }
