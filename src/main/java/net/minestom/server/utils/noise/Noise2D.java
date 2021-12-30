79f829bc9715a57b946687d43aabfbb630bba814
Németh Noel <noel@noelnemeth.com>
2021-12-30 14:46:36 +0100

    Formatting, mapper method

--- src/main/java/net/minestom/server/utils/noise/Noise2D.java
+++ src/main/java/net/minestom/server/utils/noise/Noise2D.java
@@ -7,13 +7,20 @@ public interface Noise2D {
     static Noise2D scale(Noise2D base, double factor) {
         return ((x, y) -> base.getValue(x*factor, y*factor));
     }
+
     static Noise2D scaleX(Noise2D base, double factor) {
         return ((x, y) -> base.getValue(x*factor, y));
     }
+
     static Noise2D scaleY(Noise2D base, double factor) {
         return ((x, y) -> base.getValue(x, y*factor));
     }
+
     static Noise2D combine(Noise2D noiseA, Noise2D noiseB) {
         return ((x, y) -> (noiseA.getValue(x,y)+ noiseB.getValue(x,y))/2);
+    }
+
+    static Noise2D map(Noise2D noise, double slope, double min) {
+        return ((x, y) -> slope * (noise.getValue(x, y) - min));
     }
 }
