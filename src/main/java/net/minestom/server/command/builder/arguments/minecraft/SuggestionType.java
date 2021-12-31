e770ca8caa1e47654e2384317bea900dcbf5e276
GoldenStack <goldenfire64yt@gmail.com>
2021-12-30 22:14:52 -0600

    Add SuggestionType.AVAILABLE_BIOMES and some documentation
    Signed-off-by: GoldenStack <goldenfire64yt@gmail.com>

--- src/main/java/net/minestom/server/command/builder/arguments/minecraft/SuggestionType.java
+++ src/main/java/net/minestom/server/command/builder/arguments/minecraft/SuggestionType.java
@@ -4,9 +4,29 @@ import org.jetbrains.annotations.NotNull;
 
 public enum SuggestionType {
 
+    /**
+     * Tells the client to ask the server for the tab completions for an argument
+     */
     ASK_SERVER("minecraft:ask_server"),
+
+    /**
+     * Tells the client to use the NamespaceIDs of registered recipes as the tab completions for an argument
+     */
     ALL_RECIPES("minecraft:all_recipes"),
+
+    /**
+     * Tells the client to use the names of available sounds as the tab completions for an argument
+     */
     AVAILABLE_SOUNDS("minecraft:available_sounds"),
+
+    /**
+     * Tells the client to use the NamespaceIDs of registered biomes for the tab completions for an argument
+     */
+    AVAILABLE_BIOMES("minecraft:available_biomes"),
+
+    /**
+     * Tells the client to use summonable entities as the tab completions for an argument
+     */
     SUMMONABLE_ENTITIES("minecraft:summonable_entities");
 
     private final String identifier;
