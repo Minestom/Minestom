4d97d3cba8149be5639634911260f2e882a5a264
themode <themode@outlook.fr>
2021-12-31 16:54:39 +0100

    Test concurrent ticks

--- src/test/java/thread/ThreadDispatcherTest.java
+++ src/test/java/thread/ThreadDispatcherTest.java
@@ -2,23 +2,27 @@ package thread;
 
 import net.minestom.server.Tickable;
 import net.minestom.server.thread.ThreadDispatcher;
+import net.minestom.server.thread.ThreadProvider;
+import net.minestom.server.thread.TickThread;
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ThreadDispatcherTest {
 
     @Test
     public void elementTick() {
-        record Partition() {
-        }
         final AtomicInteger counter = new AtomicInteger();
-        ThreadDispatcher<Partition> dispatcher = ThreadDispatcher.singleThread();
+        ThreadDispatcher<Object> dispatcher = ThreadDispatcher.singleThread();
         assertEquals(1, dispatcher.threads().size());
 
-        var partition = new Partition();
+        var partition = new Object();
         Tickable element = (time) -> counter.incrementAndGet();
         dispatcher.createPartition(partition);
         dispatcher.updateElement(element, partition);
@@ -38,7 +42,7 @@ public class ThreadDispatcherTest {
     }
 
     @Test
-    public void partitionTick(){
+    public void partitionTick() {
         final AtomicInteger counter1 = new AtomicInteger();
         final AtomicInteger counter2 = new AtomicInteger();
         ThreadDispatcher<Tickable> dispatcher = ThreadDispatcher.singleThread();
@@ -63,5 +67,29 @@ public class ThreadDispatcherTest {
         dispatcher.updateAndAwait(System.currentTimeMillis());
         assertEquals(2, counter1.get());
         assertEquals(2, counter2.get());
+    }
+
+    @Test
+    public void uniqueThread() {
+        // Ensure that partitions are properly dispatched across threads
+        ThreadDispatcher<Tickable> dispatcher = ThreadDispatcher.of(ThreadProvider.counter(), 10);
+        assertEquals(10, dispatcher.threads().size());
+
+        final AtomicInteger counter = new AtomicInteger();
+        Set<Thread> threads = new CopyOnWriteArraySet<>();
+        Set<Tickable> partitions = IntStream.range(0, 10)
+                .mapToObj(value -> (Tickable) (time) -> {
+                    final Thread thread = Thread.currentThread();
+                    assertInstanceOf(TickThread.class, thread);
+                    assertEquals(1, ((TickThread) thread).entries().size());
+                    assertTrue(threads.add(thread));
+                    counter.getAndIncrement();
+                })
+                .collect(Collectors.toUnmodifiableSet());
+
+        partitions.forEach(dispatcher::createPartition);
+        assertEquals(0, counter.get());
+        dispatcher.updateAndAwait(System.currentTimeMillis());
+        assertEquals(10, counter.get());
     }
 }
