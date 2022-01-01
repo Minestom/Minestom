501af960fc10c4d76c08e1452bf3c78bb0887345
Articdive <13535885+Articdive@users.noreply.github.com>
2022-01-01 18:27:52 +0100

    Build system update (#527)

---
+++ demo/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    application
+    id("minestom.common-conventions")
+    id("minestom.native-conventions")
+    id("com.github.johnrengelman.shadow") version("7.1.1")
+}
+
+application {
+    mainClass.set("net.minestom.demo.Main")
+    // This is included because Shadow is buggy. Wait for https://github.com/johnrengelman/shadow/issues/613 to befixed.
+    @Suppress("DEPRECATION")
+    mainClassName = "net.minestom.demo.Main"
+}
+
+dependencies {
+    implementation(rootProject)
+    implementation(libs.jNoise)
+}
