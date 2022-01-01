12cdb944ae852e13a3503187335a78923fa237a1
Németh Noel <noel@noelnemeth.com>
2022-01-01 16:45:27 +0100

    Remove unnecessary pool

--- src/main/java/net/minestom/server/instance/generator/Generator.java
+++ src/main/java/net/minestom/server/instance/generator/Generator.java
@@ -1,16 +1,11 @@
 package net.minestom.server.instance.generator;
 
-import net.minestom.server.MinecraftServer;
 import net.minestom.server.instance.Instance;
-import net.minestom.server.thread.MinestomThreadPool;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
 
 @FunctionalInterface
 public interface Generator {
-    ExecutorService WORLD_GEN_POOL = new MinestomThreadPool(MinecraftServer.THREAD_COUNT_WORLD_GEN, MinecraftServer.THREAD_NAME_WORLD_GEN);
-
     List<CompletableFuture<SectionResult>> generate(Instance instance, GenerationRequest request);
 }
