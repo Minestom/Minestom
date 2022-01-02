72a84f6dcf2b6fc698b8fca4336cfb360079efc3
Gatt <1311244+RealGatt@users.noreply.github.com>
2022-01-02 15:11:33 +1100

    Fix shaped recipes

--- src/main/java/net/minestom/server/network/packet/server/play/DeclareRecipesPacket.java
+++ src/main/java/net/minestom/server/network/packet/server/play/DeclareRecipesPacket.java
@@ -1,14 +1,21 @@
 package net.minestom.server.network.packet.server.play;
 
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.google.gson.GsonBuilder;
+
+import org.jetbrains.annotations.NotNull;
+
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.network.packet.server.ServerPacket;
 import net.minestom.server.network.packet.server.ServerPacketIdentifier;
+import net.minestom.server.network.packet.server.play.DeclareRecipesPacket.DeclaredRecipe;
 import net.minestom.server.utils.binary.BinaryReader;
 import net.minestom.server.utils.binary.BinaryWriter;
 import net.minestom.server.utils.binary.Writeable;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
 
 public record DeclareRecipesPacket(@NotNull List<DeclaredRecipe> recipes) implements ServerPacket {
     public DeclareRecipesPacket {
@@ -42,18 +49,18 @@ public record DeclareRecipesPacket(@NotNull List<DeclaredRecipe> recipes) implem
         return ServerPacketIdentifier.DECLARE_RECIPES;
     }
 
-    public sealed interface DeclaredRecipe extends Writeable
-            permits DeclaredShapelessCraftingRecipe, DeclaredShapedCraftingRecipe,
-            DeclaredSmeltingRecipe, DeclaredBlastingRecipe, DeclaredSmokingRecipe,
-            DeclaredCampfireCookingRecipe, DeclaredStonecutterRecipe, DeclaredSmithingRecipe {
-        @NotNull String type();
+    public sealed interface DeclaredRecipe extends
+            Writeable permits DeclaredShapelessCraftingRecipe,DeclaredShapedCraftingRecipe,DeclaredSmeltingRecipe,DeclaredBlastingRecipe,DeclaredSmokingRecipe,DeclaredCampfireCookingRecipe,DeclaredStonecutterRecipe,DeclaredSmithingRecipe {
+        @NotNull
+        String type();
 
-        @NotNull String recipeId();
+        @NotNull
+        String recipeId();
     }
 
     public record DeclaredShapelessCraftingRecipe(String recipeId, String group,
-                                                  List<Ingredient> ingredients,
-                                                  ItemStack result) implements DeclaredRecipe {
+            List<Ingredient> ingredients,
+            ItemStack result) implements DeclaredRecipe {
         private DeclaredShapelessCraftingRecipe(@NotNull BinaryReader reader) {
             this(reader.readSizedString(), reader.readSizedString(),
                     reader.readVarIntList(Ingredient::new), reader.readItemStack());
@@ -74,8 +81,8 @@ public record DeclareRecipesPacket(@NotNull List<DeclaredRecipe> recipes) implem
     }
 
     public record DeclaredShapedCraftingRecipe(@NotNull String recipeId, int width, int height,
-                                               @NotNull String group, @NotNull List<Ingredient> ingredients,
-                                               @NotNull ItemStack result) implements DeclaredRecipe {
+            @NotNull String group, @NotNull List<Ingredient> ingredients,
+            @NotNull ItemStack result) implements DeclaredRecipe {
         public DeclaredShapedCraftingRecipe {
             ingredients = List.copyOf(ingredients);
         }
@@ -92,7 +99,11 @@ public record DeclareRecipesPacket(@NotNull List<DeclaredRecipe> recipes) implem
             writer.writeVarInt(width);
             writer.writeVarInt(height);
             writer.writeSizedString(group);
-            writer.writeVarIntList(ingredients, BinaryWriter::write);
+
+            for (Ingredient ingredient : ingredients) {
+                ingredient.write(writer);
+            }
+
             writer.writeItemStack(result);
         }
 
@@ -103,8 +114,8 @@ public record DeclareRecipesPacket(@NotNull List<DeclaredRecipe> recipes) implem
     }
 
     public record DeclaredSmeltingRecipe(@NotNull String recipeId, @NotNull String group,
-                                         @NotNull Ingredient ingredient, @NotNull ItemStack result,
-                                         float experience, int cookingTime) implements DeclaredRecipe {
+            @NotNull Ingredient ingredient, @NotNull ItemStack result,
+            float experience, int cookingTime) implements DeclaredRecipe {
         public DeclaredSmeltingRecipe(BinaryReader reader) {
             this(reader.readSizedString(), reader.readSizedString(),
                     new Ingredient(reader), reader.readItemStack(),
@@ -128,8 +139,8 @@ public record DeclareRecipesPacket(@NotNull List<DeclaredRecipe> recipes) implem
     }
 
     public record DeclaredBlastingRecipe(@NotNull String recipeId, @NotNull String group,
-                                         @NotNull Ingredient ingredient, @NotNull ItemStack result,
-                                         float experience, int cookingTime) implements DeclaredRecipe {
+            @NotNull Ingredient ingredient, @NotNull ItemStack result,
+            float experience, int cookingTime) implements DeclaredRecipe {
         public DeclaredBlastingRecipe(BinaryReader reader) {
             this(reader.readSizedString(), reader.readSizedString(),
                     new Ingredient(reader), reader.readItemStack(),
@@ -153,8 +164,8 @@ public record DeclareRecipesPacket(@NotNull List<DeclaredRecipe> recipes) implem
     }
 
     public record DeclaredSmokingRecipe(@NotNull String recipeId, @NotNull String group,
-                                        @NotNull Ingredient ingredient, @NotNull ItemStack result,
-                                        float experience, int cookingTime) implements DeclaredRecipe {
+            @NotNull Ingredient ingredient, @NotNull ItemStack result,
+            float experience, int cookingTime) implements DeclaredRecipe {
         public DeclaredSmokingRecipe(BinaryReader reader) {
             this(reader.readSizedString(), reader.readSizedString(),
                     new Ingredient(reader), reader.readItemStack(),
@@ -178,8 +189,8 @@ public record DeclareRecipesPacket(@NotNull List<DeclaredRecipe> recipes) implem
     }
 
     public record DeclaredCampfireCookingRecipe(@NotNull String recipeId, @NotNull String group,
-                                                @NotNull Ingredient ingredient, @NotNull ItemStack result,
-                                                float experience, int cookingTime) implements DeclaredRecipe {
+            @NotNull Ingredient ingredient, @NotNull ItemStack result,
+            float experience, int cookingTime) implements DeclaredRecipe {
         public DeclaredCampfireCookingRecipe(BinaryReader reader) {
             this(reader.readSizedString(), reader.readSizedString(),
                     new Ingredient(reader), reader.readItemStack(),
@@ -203,7 +214,7 @@ public record DeclareRecipesPacket(@NotNull List<DeclaredRecipe> recipes) implem
     }
 
     public record DeclaredStonecutterRecipe(String recipeId, String group,
-                                            Ingredient ingredient, ItemStack result) implements DeclaredRecipe {
+            Ingredient ingredient, ItemStack result) implements DeclaredRecipe {
         public DeclaredStonecutterRecipe(@NotNull BinaryReader reader) {
             this(reader.readSizedString(), reader.readSizedString(),
                     new Ingredient(reader), reader.readItemStack());
@@ -224,7 +235,7 @@ public record DeclareRecipesPacket(@NotNull List<DeclaredRecipe> recipes) implem
     }
 
     public record DeclaredSmithingRecipe(String recipeId, Ingredient base, Ingredient addition,
-                                         ItemStack result) implements DeclaredRecipe {
+            ItemStack result) implements DeclaredRecipe {
         public DeclaredSmithingRecipe(@NotNull BinaryReader reader) {
             this(reader.readSizedString(), new Ingredient(reader), new Ingredient(reader), reader.readItemStack());
         }
