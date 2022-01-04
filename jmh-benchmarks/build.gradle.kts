dee18150def378d5b6d0630fa2e98574bccf2fcb
Articdive <13535885+Articdive@users.noreply.github.com>
2022-01-04 20:46:43 +0100

    Correctly implement JMH and fix singular Benchmarks not running.

--- jmh-benchmarks/build.gradle.kts
+++ jmh-benchmarks/build.gradle.kts
@@ -4,5 +4,7 @@ plugins {
 }
 
 dependencies {
-    implementation(rootProject)
+    jmhImplementation(rootProject)
+    jmh(libs.jmh.core)
+    jmhAnnotationProcessor(libs.jmh.annotationprocessor)
 }
