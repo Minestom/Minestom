9074e29b47378a49deb7fc78adcb5310e7de7994
foi705 <jenya.ushenin@gmail.com>
2022-01-07 14:55:14 +0300

    delete mention

--- src/main/java/net/minestom/server/event/permission/PermissionUnregisterEvent.java
+++ src/main/java/net/minestom/server/event/permission/PermissionUnregisterEvent.java
@@ -3,9 +3,6 @@ package net.minestom.server.event.permission;
 import net.minestom.server.event.trait.PermissionEvent;
 import net.minestom.server.permission.PermissionDescriptor;
 
-/**
- * @author Jenya705
- */
 public class PermissionUnregisterEvent implements PermissionEvent {
 
     private final PermissionDescriptor permission;
