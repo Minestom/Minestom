f0f96ad428f6b0d60f05584ff815ecff60229427
mworzala <mattheworzala@gmail.com>
2022-01-18 21:36:35 -0500

    reorder initialization in MinecraftServer

--- src/main/java/net/minestom/server/ServerProcessImpl.java
+++ src/main/java/net/minestom/server/ServerProcessImpl.java
@@ -79,6 +79,7 @@ final class ServerProcessImpl implements ServerProcess {
 
     public ServerProcessImpl() throws IOException {
         this.exception = new ExceptionManager();
+        this.eventHandler = new GlobalEventHandler();
         this.extension = new ExtensionManager(this);
         this.connection = new ConnectionManager();
         this.packetProcessor = new PacketProcessor();
@@ -88,7 +89,6 @@ final class ServerProcessImpl implements ServerProcess {
         this.command = new CommandManager();
         this.recipe = new RecipeManager();
         this.team = new TeamManager();
-        this.eventHandler = new GlobalEventHandler();
         this.scheduler = new SchedulerManager();
         this.benchmark = new BenchmarkManager();
         this.dimension = new DimensionTypeManager();
