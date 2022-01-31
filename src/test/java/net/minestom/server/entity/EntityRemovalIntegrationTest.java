6f099a403651ebb684f415bdd333a6eacc8e1ff8
TheMode <themode@outlook.fr>
2022-01-31 12:52:20 +0100

    Ensure that DestroyEntitiesPacket is received on entity removal
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/test/java/net/minestom/server/entity/EntityRemovalIntegrationTest.java
+++ src/test/java/net/minestom/server/entity/EntityRemovalIntegrationTest.java
@@ -3,18 +3,34 @@ package net.minestom.server.entity;
 import net.minestom.server.api.Env;
 import net.minestom.server.api.EnvTest;
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.network.packet.server.play.DestroyEntitiesPacket;
 import net.minestom.server.utils.time.TimeUnit;
 import org.junit.jupiter.api.Test;
 
 import java.time.temporal.TemporalUnit;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @EnvTest
-public class EntityScheduledRemovalIntegrationTest {
+public class EntityRemovalIntegrationTest {
 
     @Test
-    public void tickRemoval(Env env) throws InterruptedException {
+    public void destructionPacket(Env env) {
+        var instance = env.createFlatInstance();
+        var connection = env.createConnection();
+        connection.connect(instance, new Pos(0, 40, 0)).join();
+
+        var entity = new TestEntity(2, TimeUnit.SERVER_TICK);
+        entity.setInstance(instance, new Pos(0, 40, 0)).join();
+
+        var tracker = connection.trackIncoming(DestroyEntitiesPacket.class);
+        entity.remove();
+        tracker.assertSingle(packet -> assertEquals(List.of(entity.getEntityId()), packet.entityIds()));
+    }
+
+    @Test
+    public void tickTimedRemoval(Env env) throws InterruptedException {
         var instance = env.createFlatInstance();
         var entity = new TestEntity(2, TimeUnit.SERVER_TICK);
         entity.setInstance(instance, new Pos(0, 40, 0)).join();
