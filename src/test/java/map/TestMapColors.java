22128a21853052abb0ea9be17e9b11ab4ca8eb50
Luis <luisjk266@gmail.com>
2022-02-18 14:22:42 +0000

    Make argb values with no alpha return MapColors#NONE

---
+++ src/test/java/map/TestMapColors.java
@@ -0,0 +1,20 @@
+package map;
+
+import net.minestom.server.map.MapColors;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMapColors {
+
+    @Test
+    public void closestColor() {
+        assertEquals(MapColors.closestColor(0x539D87FF).getBaseColor(), MapColors.ICE);
+    }
+
+    @Test
+    public void closestColorNoAlpha() {
+        assertEquals(MapColors.closestColor(0xFFFFFF00).getBaseColor(), MapColors.NONE);
+    }
+
+}
