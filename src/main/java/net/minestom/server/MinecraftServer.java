d7ef1d5a0ead6dd4b66d35b0d93b2675acc7ca4a
themode <themode@outlook.fr>
2022-02-28 23:31:12 +0100

    1.18.2 protocol

--- src/main/java/net/minestom/server/MinecraftServer.java
+++ src/main/java/net/minestom/server/MinecraftServer.java
@@ -47,7 +47,7 @@ public final class MinecraftServer {
     public final static Logger LOGGER = LoggerFactory.getLogger(MinecraftServer.class);
 
     public static final String VERSION_NAME = "1.18.2";
-    public static final int PROTOCOL_VERSION = 1073741897;
+    public static final int PROTOCOL_VERSION = 758;
 
     // Threads
     public static final String THREAD_NAME_BENCHMARK = "Ms-Benchmark";
