1388d9d02455e7dd03fb85fdf9586e3c25ce961f
themode <themode@outlook.fr>
2022-03-03 07:41:32 +0100

    Ensure proper instance/chunk reference

--- src/test/java/net/minestom/server/snapshot/SnapshotIntegrationTest.java
+++ src/test/java/net/minestom/server/snapshot/SnapshotIntegrationTest.java
@@ -7,7 +7,8 @@ import net.minestom.server.entity.EntityType;
 import net.minestom.server.instance.block.Block;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @EnvTest
 public class SnapshotIntegrationTest {
@@ -29,7 +30,7 @@ public class SnapshotIntegrationTest {
 
         var inst = snapshot.instances().iterator().next();
 
-        assertNotNull(inst.server(), "Instance must have access to the server snapshot");
+        assertEquals(snapshot, inst.server(), "Instance must have access to the server snapshot");
 
         assertEquals(0, inst.time());
         assertEquals(0, inst.worldAge());
@@ -69,6 +70,8 @@ public class SnapshotIntegrationTest {
         assertEquals(ent.getEntityId(), entity.id());
         assertEquals(ent.getPosition(), entity.position());
         assertEquals(ent.getVelocity(), entity.velocity());
+        assertEquals(inst, entity.instance());
+        assertEquals(inst.chunkAt(entity.position()), entity.chunk());
         assertEquals(ent.getViewers().size(), entity.viewers().size());
         assertEquals(ent.getPassengers().size(), entity.passengers().size());
         assertNull(ent.getVehicle());
