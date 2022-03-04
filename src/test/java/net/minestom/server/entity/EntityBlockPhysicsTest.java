e47facd0f7796eebb580f93d505a43f0f58e58bf
iam4722202468 <aceparent@gmail.com>
2022-03-04 15:44:00 -0500

    Start writing tests

--- src/test/java/net/minestom/server/entity/EntityBlockPhysicsTest.java
+++ src/test/java/net/minestom/server/entity/EntityBlockPhysicsTest.java
@@ -1,17 +1,40 @@
 package net.minestom.server.entity;
 
+import net.minestom.server.api.Env;
+import net.minestom.server.api.EnvTest;
+import net.minestom.server.collision.CollisionUtils;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.coordinate.Vec;
+import net.minestom.server.instance.block.Block;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@EnvTest
 public class EntityBlockPhysicsTest {
 
     @Test
-    public void ensureRegistration() throws IllegalAccessException {
-        var fields = EntityTypes.class.getDeclaredFields();
-        for (var field : fields) {
-            EntityType entityType = (EntityType) field.get(this);
-            assertNotNull(EntityTypeImpl.ENTITY_META_SUPPLIER.get(entityType.name()), "Meta for " + entityType.name() + " is null");
-        }
+    public void entityPhysicsCheckCollision(Env env) {
+        var instance = env.createFlatInstance();
+        instance.setBlock(0, 42, 1, Block.STONE);
+        var entity = new Entity(EntityTypes.ZOMBIE);
+
+        entity.setInstance(instance, new Pos(0, 42, 0)).join();
+        assertEquals(instance, entity.getInstance());
+
+        CollisionUtils.PhysicsResult res = CollisionUtils.handlePhysics(entity, new Vec(0, 0, 10));
+        assertEquals(new Pos(0, 42, 0.7), res.newPosition());
+    }
+
+    @Test
+    public void entityPhysicsCheckNoCollision(Env env) {
+        var instance = env.createFlatInstance();
+        var entity = new Entity(EntityTypes.ZOMBIE);
+
+        entity.setInstance(instance, new Pos(0, 42, 0)).join();
+        assertEquals(instance, entity.getInstance());
+
+        CollisionUtils.PhysicsResult res = CollisionUtils.handlePhysics(entity, new Vec(0, 0, 10));
+        assertEquals(new Pos(0, 42, 10), res.newPosition());
     }
 }
