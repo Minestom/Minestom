c22c4e914a51378ef256e6ed4287255a9197f30f
TheMode <themode@outlook.fr>
2022-04-09 15:29:14 +0200

    Test read from compounds
    Signed-off-by: TheMode <themode@outlook.fr>

---
+++ src/test/java/net/minestom/server/tag/TagNbtTest.java
@@ -0,0 +1,41 @@
+package net.minestom.server.tag;
+
+import org.jglrxavpok.hephaistos.nbt.NBT;
+import org.jglrxavpok.hephaistos.nbt.NBTCompound;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Ensure that NBT tag can be read from other tags properly.
+ */
+public class TagNbtTest {
+
+    @Test
+    public void compoundRead() {
+        var handler = TagHandler.newHandler();
+        Tag<NBTCompound> nbtTag = Tag.NBT("path1");
+
+        var nbt = NBT.Compound(Map.of("key", NBT.Int(5)));
+        handler.setTag(nbtTag, nbt);
+        assertEquals(nbt, handler.getTag(nbtTag));
+
+        var path = Tag.Integer("key").path("path1");
+        assertEquals(5, handler.getTag(path));
+    }
+
+    @Test
+    public void doubleCompoundRead() {
+        var handler = TagHandler.newHandler();
+        Tag<NBTCompound> nbtTag = Tag.NBT("path1");
+
+        var nbt = NBT.Compound(Map.of("path2", NBT.Compound(Map.of("key", NBT.Int(5)))));
+        handler.setTag(nbtTag, nbt);
+        assertEquals(nbt, handler.getTag(nbtTag));
+
+        var path = Tag.Integer("key").path("path1", "path2");
+        assertEquals(5, handler.getTag(path));
+    }
+}
