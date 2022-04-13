ab76fc9a762218b3c6cd44f51abb86a0d6df76f8
TheMode <themode@outlook.fr>
2022-04-13 16:02:10 +0200

    Make ItemMetaView experimental
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/item/ItemMetaView.java
+++ src/main/java/net/minestom/server/item/ItemMetaView.java
@@ -2,9 +2,12 @@ package net.minestom.server.item;
 
 import net.minestom.server.tag.TagReadable;
 import net.minestom.server.tag.Taggable;
+import org.jetbrains.annotations.ApiStatus;
 
 @SuppressWarnings("ALL")
+@ApiStatus.Experimental
 public interface ItemMetaView<T extends ItemMetaView.Builder> extends TagReadable {
+    @ApiStatus.Experimental
     interface Builder extends Taggable {
     }
 }
