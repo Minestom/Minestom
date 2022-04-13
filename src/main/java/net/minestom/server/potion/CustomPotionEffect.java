eae67c06996941b0caa9d9dee9648d08f0634b66
TheMode <themode@outlook.fr>
2022-04-13 13:38:50 +0200

    Unnecessary change
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/potion/CustomPotionEffect.java
+++ src/main/java/net/minestom/server/potion/CustomPotionEffect.java
@@ -1,5 +1,8 @@
 package net.minestom.server.potion;
 
+/**
+ * Represents a custom effect in {@link net.minestom.server.item.metadata.PotionMeta}.
+ */
 public record CustomPotionEffect(byte id, byte amplifier, int duration,
                                  boolean isAmbient, boolean showParticles,
                                  boolean showIcon) {
