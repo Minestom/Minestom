625142b9948dce92920071eb23ae0b0159353822
CatDevz <codyquinn1122@gmail.com>
2022-05-01 03:30:46 -0500

    Improve tests

--- src/test/java/net/minestom/server/entity/player/PlayerBlockPlacementIntegrationTest.java
+++ src/test/java/net/minestom/server/entity/player/PlayerBlockPlacementIntegrationTest.java
@@ -11,22 +11,30 @@ import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 import net.minestom.server.network.packet.client.play.ClientPlayerBlockPlacementPacket;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @EnvTest
 public class PlayerBlockPlacementIntegrationTest {
 
-    @Test
-    public void placeBlockFromAdventureMode(Env env) {
+    @ParameterizedTest
+    @MethodSource("placeBlockFromAdventureModeParams")
+    public void placeBlockFromAdventureMode(Block baseBlock, Block canPlaceOn, Env env) {
         var instance = env.createFlatInstance();
         var connection = env.createConnection();
         var player = connection.connect(instance, new Pos(0, 42, 0)).join();
 
-        instance.setBlock(2, 41, 0, Block.ACACIA_STAIRS.withProperty("facing", "south"));
+        instance.setBlock(2, 41, 0, baseBlock);
 
         player.setGameMode(GameMode.ADVENTURE);
-        player.setItemInMainHand(ItemStack.builder(Material.WHITE_WOOL).meta(m -> m.canPlaceOn(Block.ACACIA_STAIRS)).build());
+        player.setItemInMainHand(ItemStack.builder(Material.WHITE_WOOL).meta(m -> m.canPlaceOn(canPlaceOn)).build());
 
         var packet = new ClientPlayerBlockPlacementPacket(
                 Player.Hand.MAIN, new Pos(2, 41, 0), BlockFace.WEST,
@@ -40,5 +48,13 @@ public class PlayerBlockPlacementIntegrationTest {
 
         assertEquals("minecraft:white_wool", placedBlock.name());
     }
+
+    private static Stream<Arguments> placeBlockFromAdventureModeParams() {
+        return Stream.of(
+                Arguments.of(Block.ACACIA_STAIRS.withProperty("facing", "south"), Block.ACACIA_STAIRS),
+                Arguments.of(Block.ACACIA_STAIRS, Block.ACACIA_STAIRS.withProperty("facing", "south")),
+                Arguments.of(Block.AMETHYST_BLOCK, Block.AMETHYST_BLOCK));
+    }
+
 
 }
