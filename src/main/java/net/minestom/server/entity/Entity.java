c4042900f7fb98ae599259567372862a8952f100
TogAr2 <59421074+TogAr2@users.noreply.github.com>
2022-05-03 21:13:36 +0200

    Set bounding box after setting meta

--- src/main/java/net/minestom/server/entity/Entity.java
+++ src/main/java/net/minestom/server/entity/Entity.java
@@ -188,9 +188,9 @@ public class Entity implements Viewable, Tickable, Schedulable, Snapshotable, Ev
         this.previousPosition = Pos.ZERO;
         this.lastSyncedPosition = Pos.ZERO;
 
-        setBoundingBox(entityType.registry().boundingBox());
-
         this.entityMeta = EntityTypeImpl.createMeta(entityType, this, this.metadata);
+
+        setBoundingBox(entityType.registry().boundingBox());
 
         Entity.ENTITY_BY_ID.put(id, this);
         Entity.ENTITY_BY_UUID.put(uuid, this);
