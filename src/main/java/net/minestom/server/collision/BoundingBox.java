6fda15db17511caf5723882ce54a8063da5452d3
TogAr2 <59421074+TogAr2@users.noreply.github.com>
2022-05-03 21:29:17 +0200

    Implement equals in BoundingBox

--- src/main/java/net/minestom/server/collision/BoundingBox.java
+++ src/main/java/net/minestom/server/collision/BoundingBox.java
@@ -153,4 +153,17 @@ public final class BoundingBox implements Shape {
     public double maxZ() {
         return relativeEnd().z();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BoundingBox that = (BoundingBox) o;
+
+        if (Double.compare(that.width, width) != 0) return false;
+        if (Double.compare(that.height, height) != 0) return false;
+        if (Double.compare(that.depth, depth) != 0) return false;
+        return offset.equals(that.offset);
+    }
 }
