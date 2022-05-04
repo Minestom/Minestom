fdd3e2c53c65593c446588d15ff2759a79a35d6b
TheMode <themode@outlook.fr>
2022-05-04 13:25:24 +0200

    Seal snapshot interfaces
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/ServerProcessImpl.java
+++ src/main/java/net/minestom/server/ServerProcessImpl.java
@@ -23,10 +23,7 @@ import net.minestom.server.network.PacketProcessor;
 import net.minestom.server.network.socket.Server;
 import net.minestom.server.recipe.RecipeManager;
 import net.minestom.server.scoreboard.TeamManager;
-import net.minestom.server.snapshot.EntitySnapshot;
-import net.minestom.server.snapshot.InstanceSnapshot;
-import net.minestom.server.snapshot.ServerSnapshot;
-import net.minestom.server.snapshot.SnapshotUpdater;
+import net.minestom.server.snapshot.*;
 import net.minestom.server.terminal.MinestomTerminal;
 import net.minestom.server.thread.Acquirable;
 import net.minestom.server.thread.ThreadDispatcher;
@@ -36,14 +33,12 @@ import net.minestom.server.utils.collection.MappedCollection;
 import net.minestom.server.world.DimensionTypeManager;
 import net.minestom.server.world.biomes.BiomeManager;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.UnknownNullability;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.SocketAddress;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
@@ -274,21 +269,7 @@ final class ServerProcessImpl implements ServerProcess {
                 entityRefs.put(entity.getEntityId(), updater.reference(entity));
             }
         }
-        return new SnapshotImpl(MappedCollection.plainReferences(instanceRefs), entityRefs);
-    }
-
-    record SnapshotImpl(Collection<InstanceSnapshot> instances,
-                        Int2ObjectOpenHashMap<AtomicReference<EntitySnapshot>> entityRefs) implements ServerSnapshot {
-        @Override
-        public @NotNull Collection<EntitySnapshot> entities() {
-            return MappedCollection.plainReferences(entityRefs.values());
-        }
-
-        @Override
-        public @UnknownNullability EntitySnapshot entity(int id) {
-            var ref = entityRefs.get(id);
-            return ref != null ? ref.getPlain() : null;
-        }
+        return new SnapshotImpl.Server(MappedCollection.plainReferences(instanceRefs), entityRefs);
     }
 
     private final class TickerImpl implements Ticker {
