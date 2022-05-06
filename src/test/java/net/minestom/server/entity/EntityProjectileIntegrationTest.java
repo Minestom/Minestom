2171af3f4f2a7896d999e3d16f91840420dbcf39
kiip1 <25848425+kiipy@users.noreply.github.com>
2022-05-06 20:51:50 +0200

    Update

--- src/test/java/net/minestom/server/entity/EntityProjectileIntegrationTest.java
+++ src/test/java/net/minestom/server/entity/EntityProjectileIntegrationTest.java
@@ -3,18 +3,12 @@ package net.minestom.server.entity;
 import net.minestom.server.api.Env;
 import net.minestom.server.api.EnvTest;
 import net.minestom.server.coordinate.Pos;
-import net.minestom.server.coordinate.Vec;
 import org.junit.jupiter.api.Test;
 
-import java.util.stream.IntStream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @EnvTest
 public class EntityProjectileIntegrationTest {
-    private static final double EPSILON = 1.0E-08;
-
     @Test
     public void gravityVelocity(Env env) {
         var instance = env.createFlatInstance();
@@ -27,13 +21,6 @@ public class EntityProjectileIntegrationTest {
         projectile.setInstance(instance, from).join();
         projectile.shoot(target, 1, 0);
 
-        // z velocity = dz / sqrt(dx^2 + dy^2 + dz^2) * 20 * power =
-        // 10 / sqrt(2.0000000298023224^2 + 10^2) * 20 * 1 = 10 / 10.198039033 * 20 * 1 = 19.6116135026.
-        assertEquals(19.6116135026, projectile.velocity.z(), EPSILON);
-
-        // The entity is currently falling (in the air), so it does have a velocity.
-        assertTrue(projectile.hasVelocity());
-
         var before = projectile.getPosition(); // at start
         var after = projectile.getPosition(); // now - 1 tick, closest to target
         var smallestDistance = 1e6;
@@ -43,21 +30,7 @@ public class EntityProjectileIntegrationTest {
             else break;
 
             after = projectile.getPosition();
-
-            var expected = projectile.velocity
-                    .div(20)
-                    .apply((x, y, z) -> new Vec(
-                            x * 0.98,
-                            (y - projectile.gravityAcceleration) * (1 - projectile.gravityDragPerTick),
-                            z * 0.98
-                    ))
-                    .mul(20);
-
             env.tick();
-
-            assertEquals(0, projectile.velocity.x());
-            assertEquals(expected.y(), projectile.velocity.y());
-            assertEquals(expected.z(), projectile.velocity.z());
         }
 
         // Ensure the position is correct.
@@ -81,44 +54,21 @@ public class EntityProjectileIntegrationTest {
         projectile.setInstance(instance, from).join();
         projectile.shoot(target, 1, 0);
 
-        // z velocity = dz / sqrt(dx^2 + dy^2 + dz^2) * 20 * power = 10 / 10 * 20 * 1 = 20.
-        assertEquals(20, projectile.velocity.z());
-
-        // The entity is currently falling (in the air), so it does have a velocity.
-        assertTrue(projectile.hasVelocity());
-
         var before = projectile.getPosition(); // at start
         var after = projectile.getPosition(); // now - 1 tick, closest to target
         var smallestDistance = 1e6;
-        var ticksPassed = 0;
         while (true) {
             final var distance = projectile.getPosition().distanceSquared(target);
             if (distance <= smallestDistance) smallestDistance = distance;
             else break;
 
             after = projectile.getPosition();
-
-            // Only drag for non-living entities should apply since there is no gravity.
-            // We do this by multiplying by 0.98, so 20 * 0.98^n.
-            assertEquals(0, projectile.velocity.x());
-            assertEquals(0, projectile.velocity.y());
-            assertEquals(20 * Math.pow(0.98, ticksPassed), projectile.velocity.z(), EPSILON);
-
             env.tick();
-            ticksPassed++;
         }
 
-        // Ensure the position is correct.
         // x and y don't change (no gravity) and z changes by Σz velocity.
         assertEquals(before.x(), after.x());
         assertEquals(before.y(), after.y());
-        assertEquals(before.z() + zAfterNoGravity(ticksPassed), after.z(), EPSILON);
         assertEquals(target.z(), after.z(), 0.05);
-    }
-
-    private static double zAfterNoGravity(int ticks) {
-        return IntStream.range(0, ticks - 1)
-                .mapToDouble(i -> Math.pow(0.98, i))
-                .sum();
     }
 }
