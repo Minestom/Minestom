327cd3e9daf28bd5b3d6250080e9c6664deb874d
kiip1 <25848425+kiipy@users.noreply.github.com>
2022-05-06 19:37:52 +0200

    Update tests

--- src/test/java/net/minestom/server/entity/EntityProjectileIntegrationTest.java
+++ src/test/java/net/minestom/server/entity/EntityProjectileIntegrationTest.java
@@ -3,9 +3,9 @@ package net.minestom.server.entity;
 import net.minestom.server.api.Env;
 import net.minestom.server.api.EnvTest;
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.coordinate.Vec;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,7 +14,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @EnvTest
 public class EntityProjectileIntegrationTest {
     private static final double EPSILON = 1.0E-08;
-    private static final double MOVEMENT_EPSILON = 1.0E-03;
 
     @Test
     public void gravityVelocity(Env env) {
@@ -24,8 +23,9 @@ public class EntityProjectileIntegrationTest {
         var projectile = new EntityProjectile(shooter, EntityType.ARROW);
         var from = new Pos(0, 42, 0).add(0,
                 shooter.getEyeHeight(), shooter.getPosition().direction().z());
+        var target = from.add(0, 0, 10);
         projectile.setInstance(instance, from).join();
-        projectile.shoot(from.add(0, 0, 10), 1, 0);
+        projectile.shoot(target, 1, 0);
 
         // z velocity = dz / sqrt(dx^2 + dy^2 + dz^2) * 20 * power =
         // 10 / sqrt(2.0000000298023224^2 + 10^2) * 20 * 1 = 10 / 10.198039033 * 20 * 1 = 19.6116135026.
@@ -34,15 +34,34 @@ public class EntityProjectileIntegrationTest {
         // The entity is currently falling (in the air), so it does have a velocity.
         assertTrue(projectile.hasVelocity());
 
-        // Check whether gravity gets applied correctly
-        testMovement(env, projectile, List.of(
-                new Pos(0, 43.6915, 1),
-                new Pos(0, 43.8876, 1.98058),
-                new Pos(0, 44.0322, 2.94154),
-                new Pos(0, 44.1259, 3.88329),
-                new Pos(0, 44.1692, 4.80621),
-                new Pos(0, 44.1625, 5.71067)
-        ));
+        var before = projectile.getPosition();
+
+        for (int i = 0; i < 11; i++) {
+            Vec expected = projectile.velocity
+                    .div(20)
+                    .apply((x, y, z) -> new Vec(
+                        x * 0.98,
+                        (y - 0.05) * (1 - 0.01),
+                        z * 0.98
+                    ))
+                    .mul(20);
+
+            // First tick has to be env tick, then entity ticks will work
+            if (i == 0) env.tick();
+            else projectile.tick(0);
+
+            assertEquals(0, projectile.velocity.x());
+            assertEquals(expected.y(), projectile.velocity.y());
+            assertEquals(expected.z(), projectile.velocity.z());
+        }
+
+        // Ensure the position is correct.
+        // x doesn't change
+        // Big delta because ticks aren't very accurate
+        var after = projectile.getPosition();
+        assertEquals(before.x(), after.x());
+        assertEquals(target.y(), after.y(), 0.6);
+        assertEquals(target.z(), after.z(), 0.6);
     }
 
     @Test
@@ -53,9 +72,10 @@ public class EntityProjectileIntegrationTest {
         var projectile = new EntityProjectile(shooter, EntityType.ARROW);
         var from = new Pos(0, 42, 0).add(0,
                 shooter.getEyeHeight(), shooter.getPosition().direction().z());
+        var target = from.add(0, 0, 10);
         projectile.setNoGravity(true);
         projectile.setInstance(instance, from).join();
-        projectile.shoot(from.add(0, 0, 10), 1, 0);
+        projectile.shoot(target, 1, 0);
 
         // z velocity = dz / sqrt(dx^2 + dy^2 + dz^2) * 20 * power = 10 / 10 * 20 * 1 = 20.
         assertEquals(20, projectile.velocity.z());
@@ -65,14 +85,16 @@ public class EntityProjectileIntegrationTest {
 
         var before = projectile.getPosition();
 
-        for (int i = 1; i <= 5; i++) {
-            env.tick();
-
-            // Only drag for non-living entities should apply since there is no gravity .
+        for (int i = 0; i < 11; i++) {
+            // Only drag for non-living entities should apply since there is no gravity.
             // We do this by multiplying by 0.98, so 20 * 0.98^n.
             assertEquals(0, projectile.velocity.x());
             assertEquals(0, projectile.velocity.y());
             assertEquals(20 * Math.pow(0.98, i), projectile.velocity.z(), EPSILON);
+
+            // First tick has to be env tick, then entity ticks will work
+            if (i == 0) env.tick();
+            else projectile.tick(0);
         }
 
         // Ensure the position is correct.
@@ -80,17 +102,8 @@ public class EntityProjectileIntegrationTest {
         var after = projectile.getPosition();
         assertEquals(before.x(), after.x());
         assertEquals(before.y(), after.y());
-        assertEquals(before.z() + zAfterNoGravity(5), after.z(), EPSILON);
-    }
-
-    private void testMovement(Env env, Entity entity, List<Pos> samples) {
-        for (Pos sample : samples) {
-            var position = entity.getPosition();
-            assertEquals(sample.x(), position.x(), MOVEMENT_EPSILON);
-            assertEquals(sample.y(), position.y(), MOVEMENT_EPSILON);
-            assertEquals(sample.z(), position.z(), MOVEMENT_EPSILON);
-            env.tick();
-        }
+        assertEquals(before.z() + zAfterNoGravity(11), after.z(), EPSILON);
+        assertEquals(target.z(), after.z(), 0.05);
     }
 
     private static double zAfterNoGravity(int ticks) {
