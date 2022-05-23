7859ddabf70428dd24eea2231b0854b423ccf281
DasLixou <einlixou@gmail.com>
2022-05-23 16:12:59 +0200

    Added Facing to BlockFace

--- src/main/java/net/minestom/server/instance/block/BlockFace.java
+++ src/main/java/net/minestom/server/instance/block/BlockFace.java
@@ -4,22 +4,28 @@ import net.minestom.server.utils.Direction;
 import org.jetbrains.annotations.NotNull;
 
 public enum BlockFace {
-    BOTTOM(Direction.DOWN),
-    TOP(Direction.UP),
-    NORTH(Direction.NORTH),
-    SOUTH(Direction.SOUTH),
-    WEST(Direction.WEST),
-    EAST(Direction.EAST);
+    BOTTOM(Direction.DOWN, "down"),
+    TOP(Direction.UP, "up"),
+    NORTH(Direction.NORTH, "north"),
+    SOUTH(Direction.SOUTH, "south"),
+    WEST(Direction.WEST, "west"),
+    EAST(Direction.EAST, "east");
 
     private final Direction direction;
+    private final String facing;
 
-    BlockFace(Direction direction) {
+    BlockFace(Direction direction, String facing) {
         this.direction = direction;
+        this.facing = facing;
     }
 
     public Direction toDirection() {
         return direction;
     }
+
+    public String toFacing() { return facing; }
+
+    public Block applyFacing(Block block) { return block.withProperty("facing", facing); }
 
     @NotNull
     public BlockFace getOppositeFace() {
