ab84bd63b331b65ce241df05b6b33509f1f20f4e
Noel Németh <noel@noelnemeth.com>
2022-06-10 21:34:34 +0200

    Implement Component signature verification

--- src/main/java/net/minestom/server/crypto/SignatureValidator.java
+++ src/main/java/net/minestom/server/crypto/SignatureValidator.java
@@ -1,13 +1,22 @@
 package net.minestom.server.crypto;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import net.minestom.server.entity.Player;
 import net.minestom.server.utils.crypto.KeyUtils;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
 import java.security.*;
+import java.util.UUID;
 
 @FunctionalInterface
 public interface SignatureValidator {
-    SignatureValidator NONE = (payload, signature) -> true;
+    SignatureValidator PASS = (payload, signature) -> true;
+    SignatureValidator FAIL = (payload, signature) -> false;
     SignatureValidator YGGDRASIL = createYggdrasilValidator();
 
     boolean validate(byte[] payload, byte[] signature);
@@ -24,6 +33,22 @@ public interface SignatureValidator {
                 return false;
             }
         });
+    }
+
+    static boolean validate(SignatureValidator validator, UUID signer, MessageSignature signature, Component component) {
+        final byte[] componentBytes = GsonComponentSerializer.gson().serialize(component).getBytes(StandardCharsets.UTF_8);
+        byte[] signerDetails = new byte[32+ componentBytes.length];
+        ByteBuffer bytebuffer = ByteBuffer.wrap(signerDetails).order(ByteOrder.BIG_ENDIAN);
+        bytebuffer.putLong(signature.salt());
+        bytebuffer.putLong(signer.getMostSignificantBits()).putLong(signer.getLeastSignificantBits());
+        bytebuffer.putLong(signature.timestamp().getEpochSecond());
+        bytebuffer.put(componentBytes);
+        return validator.validate(bytebuffer.array(), signature.signature());
+    }
+
+    static @Nullable SignatureValidator from(Player player) {
+        if (player.getPlayerConnection().getPlayerPublicKey() == null) return null;
+        return from(player.getPlayerConnection().getPlayerPublicKey().publicKey(), KeyUtils.SignatureAlgorithm.SHA256withRSA);
     }
 
     private static SignatureValidator createYggdrasilValidator() {
