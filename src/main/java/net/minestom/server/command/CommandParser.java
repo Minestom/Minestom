079439896fdd41f9e9468439ff17fd77e5f5a827
Noel Németh <noel@noelnemeth.com>
2022-07-07 18:33:13 +0200

    Move parser method

--- src/main/java/net/minestom/server/command/CommandParser.java
+++ src/main/java/net/minestom/server/command/CommandParser.java
@@ -28,7 +28,7 @@ public final class CommandParser {
         final CommandReader reader = new CommandReader(withoutPrefix);
         List<Map.Entry<Node, Object>> syntax = new ArrayList<>();
 
-        Map.Entry<Node, Object> result = graph.parseChild(graph.root(), reader);
+        Map.Entry<Node, Object> result = parseChild(graph, graph.root(), reader);
 
         while (result != null) {
             syntax.add(result);
@@ -37,7 +37,7 @@ public final class CommandParser {
                 return new SyntaxErrorResult(withoutPrefix.toString(), result.getKey().executionInfo().get().condition(),
                         result.getKey().arg().getCallback(), e, syntaxMapper(syntax));
             }
-            result = graph.parseChild(result.getKey(), reader);
+            result = parseChild(graph, result.getKey(), reader);
         }
 
         if (syntax.isEmpty()) {
@@ -54,6 +54,22 @@ public final class CommandParser {
                 .skip(1) // skip root
                 .map(x -> Map.entry(x.getKey().arg().getId(), x.getValue()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private static @Nullable Map.Entry<Node, Object> parseChild(NodeGraph graph, Node parent, CommandReader reader) {
+        for (Node child : graph.getChildren(parent)) {
+            final int remaining = reader.remaining();
+            try {
+                final Object parse = child.arg().parse(reader);
+                return Map.entry(child, parse);
+            } catch (ArgumentSyntaxException e) {
+                if (remaining != reader.remaining()) {
+                    // Node accepted the input, but it was malformed or otherwise failed validation
+                    return Map.entry(child, e);
+                }
+            }
+        }
+        return null;
     }
 
     public sealed interface Result {
