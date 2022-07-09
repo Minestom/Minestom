cb3e2eba502e0ec484a5dea40fc23e4ba5a1d488
Noel Németh <noel@noelnemeth.com>
2022-07-09 20:46:17 +0200

    Implement converter for new graph type

--- src/main/java/net/minestom/server/command/GraphConverter.java
+++ src/main/java/net/minestom/server/command/GraphConverter.java
@@ -5,6 +5,10 @@ import net.minestom.server.command.builder.arguments.ArgumentLiteral;
 import net.minestom.server.network.packet.server.play.DeclareCommandsPacket;
 import org.jetbrains.annotations.Contract;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 final class GraphConverter {
     private GraphConverter() {
         //no instance
@@ -37,5 +41,32 @@ final class GraphConverter {
     @Contract("_ -> new")
     public static DeclareCommandsPacket createPacket(NodeGraph graph) {
         return new DeclareCommandsPacket(graph.nodes().stream().map(GraphConverter::getPacketNode).toList(), graph.root().id());
+    }
+
+    @Contract("_ -> new")
+    public static DeclareCommandsPacket createPacket(Graph graph) {
+        List<DeclareCommandsPacket.Node> nodes = new ArrayList<>();
+        final AtomicInteger idSource = new AtomicInteger(0);
+        return new DeclareCommandsPacket(nodes, append(graph.root(), nodes, idSource));
+    }
+
+    private static int append(Graph.Node of, List<DeclareCommandsPacket.Node> to, AtomicInteger id) {
+        final DeclareCommandsPacket.Node node = new DeclareCommandsPacket.Node();
+        node.children = of.next().stream().mapToInt(x -> append(x, to, id)).toArray();
+        if (of.argument() instanceof ArgumentLiteral literal) {
+            if (literal.getId().isEmpty()) {
+                node.flags = DeclareCommandsPacket.getFlag(DeclareCommandsPacket.NodeType.ROOT, false, false, false);
+            } else {
+                node.flags = DeclareCommandsPacket.getFlag(DeclareCommandsPacket.NodeType.LITERAL, false, false, false);
+                node.name = of.argument().getId();
+            }
+        } else {
+            node.flags = DeclareCommandsPacket.getFlag(DeclareCommandsPacket.NodeType.ARGUMENT, false, false, false);
+            node.name = of.argument().getId();
+            node.parser = of.argument().parser();
+            node.properties = of.argument().nodeProperties();
+        }
+        to.add(node);
+        return id.getAndIncrement();
     }
 }
