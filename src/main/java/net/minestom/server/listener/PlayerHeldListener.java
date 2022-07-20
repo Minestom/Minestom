64a98e68bac9e862fe31f2189307e6e045f83910
GreatWyrm <alecmusante@gmail.com>
2022-07-20 16:23:52 -0700

    Properly rename variable

--- src/main/java/net/minestom/server/listener/PlayerHeldListener.java
+++ src/main/java/net/minestom/server/listener/PlayerHeldListener.java
@@ -27,7 +27,7 @@ public class PlayerHeldListener {
             final byte resultSlot = changeHeldSlotEvent.getNewSlot();
 
             // If the held slot has been changed by the event, send the change to the player
-            if (resultSlot != slot) {
+            if (resultSlot != oldSlot) {
                 player.setHeldItemSlot(resultSlot);
             } else {
                 // Otherwise, simply refresh the player field
