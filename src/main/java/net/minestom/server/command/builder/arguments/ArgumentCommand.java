f9ed02bd2b20822ca29fce188047c3b09b541086
Noel Németh <noel@noelnemeth.com>
2022-07-21 16:28:51 +0200

    Update arg command

--- src/main/java/net/minestom/server/command/builder/arguments/ArgumentCommand.java
+++ src/main/java/net/minestom/server/command/builder/arguments/ArgumentCommand.java
@@ -1,19 +1,15 @@
 package net.minestom.server.command.builder.arguments;
 
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.command.builder.CommandDispatcher;
-import net.minestom.server.command.builder.CommandResult;
+import net.minestom.server.command.ParseResult;
 import net.minestom.server.command.builder.exception.ArgumentSyntaxException;
-import net.minestom.server.utils.StringUtils;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-public class ArgumentCommand extends Argument<CommandResult> {
+public class ArgumentCommand extends Argument<ParseResult> {
 
     public static final int INVALID_COMMAND_ERROR = 1;
 
     private boolean onlyCorrect;
-    private String shortcut = "";
 
     public ArgumentCommand(@NotNull String id) {
         super(id, true, true);
@@ -21,17 +17,16 @@ public class ArgumentCommand extends Argument<CommandResult> {
 
     @NotNull
     @Override
-    public CommandResult parse(@NotNull String input) throws ArgumentSyntaxException {
-        final String commandString = !shortcut.isEmpty() ?
-                shortcut + StringUtils.SPACE + input
-                : input;
-        CommandDispatcher dispatcher = MinecraftServer.getCommandManager().getDispatcher();
-        CommandResult result = dispatcher.parse(commandString);
+    public ParseResult parse(@NotNull String input) throws ArgumentSyntaxException {
+        if (!input.startsWith(getId())) throw new ArgumentSyntaxException("Incompatible", input, -1);
 
-        if (onlyCorrect && result.getType() != CommandResult.Type.SUCCESS)
+        final String commandString = input.substring(getId().length());
+        final ParseResult parseResult = MinecraftServer.getCommandManager().parseCommand(commandString);
+
+        if (onlyCorrect && !(parseResult instanceof ParseResult.KnownCommand.Valid))
             throw new ArgumentSyntaxException("Invalid command", input, INVALID_COMMAND_ERROR);
 
-        return result;
+        return parseResult;
     }
 
     @Override
@@ -45,17 +40,6 @@ public class ArgumentCommand extends Argument<CommandResult> {
 
     public ArgumentCommand setOnlyCorrect(boolean onlyCorrect) {
         this.onlyCorrect = onlyCorrect;
-        return this;
-    }
-
-    @NotNull
-    public String getShortcut() {
-        return shortcut;
-    }
-
-    @ApiStatus.Experimental
-    public ArgumentCommand setShortcut(@NotNull String shortcut) {
-        this.shortcut = shortcut;
         return this;
     }
 
