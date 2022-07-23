470cdd6e34bae55fdd34a5bc1319a1ffd7bd4684
TheMode <themode@outlook.fr>
2022-07-23 10:59:53 +0200

    Small CommandParserImpl clarity improvement
    Signed-off-by: TheMode <themode@outlook.fr>

--- src/main/java/net/minestom/server/command/CommandParserImpl.java
+++ src/main/java/net/minestom/server/command/CommandParserImpl.java
@@ -30,20 +30,14 @@ final class CommandParserImpl implements CommandParser {
     private static final Logger LOGGER = LoggerFactory.getLogger(CommandParserImpl.class);
     static final CommandParserImpl PARSER = new CommandParserImpl();
 
-    @Override
-    public @NotNull CommandParser.Result parse(@NotNull Graph graph, @NotNull String input) {
-        // Create reader & parse
-        final CommandStringReader reader = new CommandStringReader(input);
+    static final class Chain {
+        CommandExecutor defaultExecutor = null;
         final List<NodeResult> syntax = new ArrayList<>();
         final List<CommandCondition> conditions = new ArrayList<>();
         final List<CommandExecutor> globalListeners = new ArrayList<>();
-        CommandExecutor defaultExecutor = null;
 
-        NodeResult result;
-        Node parent = graph.root();
-
-        while ((result = parseChild(parent, reader)) != null) {
-            syntax.add(result);
+        void append(NodeResult result) {
+            this.syntax.add(result);
             final Graph.Execution execution = result.node.execution();
             if (execution != null) {
                 // Create condition chain
@@ -56,58 +50,61 @@ final class CommandParserImpl implements CommandParser {
                 final CommandExecutor globalListener = execution.globalListener();
                 if (globalListener != null) globalListeners.add(globalListener);
             }
+        }
+    }
+
+    @Override
+    public @NotNull CommandParser.Result parse(@NotNull Graph graph, @NotNull String input) {
+        // Create reader & parse
+        final CommandStringReader reader = new CommandStringReader(input);
+        final Chain chain = new Chain();
+
+        NodeResult result;
+        Node parent = graph.root();
+        while ((result = parseChild(parent, reader)) != null) {
+            chain.append(result);
             // Check parse result
-            if (result.io.output instanceof SyntaxError e) {
+            if (result.io.output instanceof SyntaxError<?> e) {
                 // Syntax error stop at this arg
                 final ArgumentCallback argumentCallback = parent.argument().getCallback();
-                if (argumentCallback == null && defaultExecutor != null) {
-                    return new ValidCommand(input, chainConditions(conditions), defaultExecutor, syntaxMapper(syntax),
-                            mergeExecutors(globalListeners), extractSuggestionCallback(syntax));
+                if (argumentCallback == null && chain.defaultExecutor != null) {
+                    return ValidCommand.defaultExecutor(input, chain);
                 } else {
-                    return new InvalidCommand(input, chainConditions(conditions),
-                            argumentCallback, e, syntaxMapper(syntax), mergeExecutors(globalListeners),
-                            extractSuggestionCallback(syntax));
+                    return new InvalidCommand(input, chainConditions(chain.conditions),
+                            argumentCallback, e, syntaxMapper(chain.syntax), mergeExecutors(chain.globalListeners),
+                            extractSuggestionCallback(chain.syntax));
                 }
             }
             parent = result.node;
         }
-
-        if (syntax.size() < 1) {
+        // Check if any syntax has been found
+        if (chain.syntax.size() < 1) {
             return UnknownCommandResult.INSTANCE;
-        } else {
-            final Map<String, InputOutputPair<Object>> args = syntaxMapper(syntax);
-            final CommandExecutor executor = locateExecutor(syntax.get(syntax.size() - 1).node, args);
-            if (executor == null) {
-                // Syntax error
-                if (defaultExecutor != null) {
-                    return new ValidCommand(input, chainConditions(conditions), defaultExecutor, syntaxMapper(syntax),
-                            mergeExecutors(globalListeners), extractSuggestionCallback(syntax));
-                } else {
-                    return new InvalidCommand(input, chainConditions(conditions),
-                            null/*todo command syntax callback*/,
-                            ArgumentResult.syntaxError("INTERNAL ERROR: Couldn't locate executor.", null, -1),
-                            args, mergeExecutors(globalListeners), extractSuggestionCallback(syntax));
-                }
-            }
-            if (reader.hasRemaining()) {
-                // Command had trailing data
-                if (defaultExecutor != null) {
-                    return new ValidCommand(input, chainConditions(conditions), defaultExecutor,
-                            syntaxMapper(syntax), mergeExecutors(globalListeners), extractSuggestionCallback(syntax));
-                } else {
-                    return new InvalidCommand(input, chainConditions(conditions),
-                            null/*todo command syntax callback*/,
-                            ArgumentResult.syntaxError("Command has trailing data.", null, -1),
-                            args, mergeExecutors(globalListeners), extractSuggestionCallback(syntax));
-                }
-            }
-            return new ValidCommand(input, chainConditions(conditions), executor, args, mergeExecutors(globalListeners),
-                    extractSuggestionCallback(syntax));
         }
+        // Verify syntax(s)
+        final Map<String, InputOutputPair<Object>> args = syntaxMapper(chain.syntax);
+        final CommandExecutor executor = locateExecutor(chain.syntax.get(chain.syntax.size() - 1).node, args);
+        if (executor == null) {
+            // Syntax error
+            if (chain.defaultExecutor != null) {
+                return ValidCommand.defaultExecutor(input, chain);
+            } else {
+                return InvalidCommand.invalid(input, chain, args);
+            }
+        }
+        if (reader.hasRemaining()) {
+            // Command had trailing data
+            if (chain.defaultExecutor != null) {
+                return ValidCommand.defaultExecutor(input, chain);
+            } else {
+                return InvalidCommand.invalid(input, chain, args);
+            }
+        }
+        return ValidCommand.executor(input, chain, executor, args);
     }
 
     private static SuggestionCallback extractSuggestionCallback(List<NodeResult> syntax) {
-        return syntax.get(syntax.size()-1).callback;
+        return syntax.get(syntax.size() - 1).callback;
     }
 
     private static Map<String, InputOutputPair<Object>> syntaxMapper(List<NodeResult> syntax) {
@@ -223,7 +220,7 @@ final class CommandParserImpl implements CommandParser {
             final SuggestionCallback callback = suggestionCallback();
             if (callback == null) return null;
             final int lastSpace = input().lastIndexOf(" ");
-            final Suggestion suggestion = new Suggestion(input(), lastSpace+2, input().length()-lastSpace-1);
+            final Suggestion suggestion = new Suggestion(input(), lastSpace + 2, input().length() - lastSpace - 1);
             final CommandContext context = new CommandContext(input());
             for (var entry : arguments().entrySet()) {
                 final String identifier = entry.getKey();
@@ -239,6 +236,14 @@ final class CommandParserImpl implements CommandParser {
                           @NotNull Map<String, InputOutputPair<Object>> arguments, CommandExecutor globalListener,
                           @Nullable SuggestionCallback suggestionCallback)
             implements InternalKnownCommand, Result.KnownCommand.Invalid {
+
+        static InvalidCommand invalid(String input, Chain chain, Map<String, InputOutputPair<Object>> args) {
+            return new InvalidCommand(input, chainConditions(chain.conditions),
+                    null/*todo command syntax callback*/,
+                    ArgumentResult.syntaxError("Command has trailing data.", null, -1),
+                    args, mergeExecutors(chain.globalListeners), extractSuggestionCallback(chain.syntax));
+        }
+
         @Override
         public @NotNull ExecutableCommand executable() {
             return new InvalidExecutableCmd(condition, globalListener, callback, error, input, arguments);
@@ -249,6 +254,17 @@ final class CommandParserImpl implements CommandParser {
                         @NotNull Map<String, InputOutputPair<Object>> arguments,
                         CommandExecutor globalListener, @Nullable SuggestionCallback suggestionCallback)
             implements InternalKnownCommand, Result.KnownCommand.Valid {
+
+        static ValidCommand defaultExecutor(String input, Chain chain) {
+            return new ValidCommand(input, chainConditions(chain.conditions), chain.defaultExecutor, syntaxMapper(chain.syntax),
+                    mergeExecutors(chain.globalListeners), extractSuggestionCallback(chain.syntax));
+        }
+
+        static ValidCommand executor(String input, Chain chain, CommandExecutor executor, Map<String, InputOutputPair<Object>> args) {
+            return new ValidCommand(input, chainConditions(chain.conditions), executor, args, mergeExecutors(chain.globalListeners),
+                    extractSuggestionCallback(chain.syntax));
+        }
+
         @Override
         public @NotNull ExecutableCommand executable() {
             return new ValidExecutableCmd(condition, globalListener, executor, input, arguments);
@@ -265,7 +281,8 @@ final class CommandParserImpl implements CommandParser {
     }
 
     record ValidExecutableCmd(CommandCondition condition, CommandExecutor globalListener, CommandExecutor executor,
-                              String input, Map<String, InputOutputPair<Object>> arguments) implements ExecutableCommand {
+                              String input,
+                              Map<String, InputOutputPair<Object>> arguments) implements ExecutableCommand {
 
         @Override
         public @NotNull Result execute(@NotNull CommandSender sender) {
@@ -288,7 +305,8 @@ final class CommandParserImpl implements CommandParser {
     }
 
     record InvalidExecutableCmd(CommandCondition condition, CommandExecutor globalListener, ArgumentCallback callback,
-                                SyntaxError<?> error, String input, Map<String, InputOutputPair<Object>> arguments) implements ExecutableCommand {
+                                SyntaxError<?> error, String input,
+                                Map<String, InputOutputPair<Object>> arguments) implements ExecutableCommand {
 
         @Override
         public @NotNull Result execute(@NotNull CommandSender sender) {
