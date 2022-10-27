2a6223b1367251e7cc669440fa5a96326d45cd72
offbyone <offbyone@disroot.org>
2022-10-27 13:43:22 +0200

    Hopefully preventing inline by javac

--- src/main/java/net/minestom/server/Git.java
+++ src/main/java/net/minestom/server/Git.java
@@ -1,9 +1,17 @@
 package net.minestom.server;
 
 public class Git {
-    public static final String COMMIT = "&COMMIT";
-    public static final String BRANCH = "&BRANCH";
+    private static final String COMMIT = "&COMMIT";
+    private static final String BRANCH = "&BRANCH";
 
-    public static final String GROUP = "&GROUP";
-    public static final String ARTIFACT = "&ARTIFACT";
+    private static final String GROUP = "&GROUP";
+    private static final String ARTIFACT = "&ARTIFACT";
+
+
+
+    public String getCommit() { return COMMIT; }
+    public String getBranch() { return BRANCH; }
+
+    public String getGroup() { return GROUP; }
+    public String getArtifact() { return ARTIFACT; }
 }
