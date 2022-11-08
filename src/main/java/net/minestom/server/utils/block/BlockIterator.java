9ea81a6102592b9d35e08c768940457575cdd082
iam4722202468 <aceparent@gmail.com>
2022-11-07 23:16:41 -0500

    Rewrite BlockIterator

--- src/main/java/net/minestom/server/utils/block/BlockIterator.java
+++ src/main/java/net/minestom/server/utils/block/BlockIterator.java
@@ -4,38 +4,22 @@ import net.minestom.server.coordinate.Point;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.Entity;
-import net.minestom.server.instance.block.BlockFace;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 /**
  * This class performs ray tracing and iterates along blocks on a line
  */
 public class BlockIterator implements Iterator<Point> {
+    private final double maxDistance;
+    private final Vec direction;
+    private final Point start;
 
-    private final int maxDistance;
-
-    private static final int gridSize = 1 << 24;
-
-    private boolean end = false;
-
-    private Point[] blockQueue = new Point[3];
-    private int currentBlock = 0;
-    private int currentDistance = 0;
-    private int maxDistanceInt;
-
-    private int secondError;
-    private int thirdError;
-
-    private int secondStep;
-    private int thirdStep;
-
-    private BlockFace mainFace;
-    private BlockFace secondFace;
-    private BlockFace thirdFace;
+    Point[] points = new Point[3];
+    double[] distances = new double[3];
+    int[] signums = new int[3];
 
     /**
      * Constructs the BlockIterator.
@@ -50,201 +34,40 @@ public class BlockIterator implements Iterator<Point> {
      *                    trace. Setting this value above 140 may lead to problems with
      *                    unloaded chunks. A value of 0 indicates no limit
      */
-    public BlockIterator(@NotNull Vec start, @NotNull Vec direction, double yOffset, int maxDistance) {
+    public BlockIterator(@NotNull Vec start, @NotNull Vec direction, double yOffset, double maxDistance) {
         this.maxDistance = maxDistance;
+        this.direction = direction;
+        this.start = start.add(0, yOffset, 0);
 
-        if (direction.isZero()) {
-            currentBlock = -1;
-            return;
+        points[0] = this.start;
+        points[1] = this.start;
+        points[2] = this.start;
+
+        distances[0] = 0;
+        distances[1] = 0;
+        distances[2] = 0;
+
+        if (direction.x() == 0) {
+            points[0] = null;
+            distances[0] = Double.MAX_VALUE;
+        }
+        if (direction.y() == 0) {
+            points[1] = null;
+            distances[1] = Double.MAX_VALUE;
+        }
+        if (direction.z() == 0) {
+            points[2] = null;
+            distances[2] = Double.MAX_VALUE;
         }
 
-        Vec startClone = start.withY(y -> y+yOffset);
-
-        currentDistance = 0;
-
-        double mainDirection = 0;
-        double secondDirection = 0;
-        double thirdDirection = 0;
-
-        double mainPosition = 0;
-        double secondPosition = 0;
-        double thirdPosition = 0;
-
-        Vec startBlock = startClone.apply(Vec.Operator.FLOOR);
-
-        if (getXLength(direction) > mainDirection) {
-            mainFace = getXFace(direction);
-            mainDirection = getXLength(direction);
-            mainPosition = getXPosition(direction, startClone, startBlock);
-
-            secondFace = getYFace(direction);
-            secondDirection = getYLength(direction);
-            secondPosition = getYPosition(direction, startClone, startBlock);
-
-            thirdFace = getZFace(direction);
-            thirdDirection = getZLength(direction);
-            thirdPosition = getZPosition(direction, startClone, startBlock);
-        }
-        if (getYLength(direction) > mainDirection) {
-            mainFace = getYFace(direction);
-            mainDirection = getYLength(direction);
-            mainPosition = getYPosition(direction, startClone, startBlock);
-
-            secondFace = getZFace(direction);
-            secondDirection = getZLength(direction);
-            secondPosition = getZPosition(direction, startClone, startBlock);
-
-            thirdFace = getXFace(direction);
-            thirdDirection = getXLength(direction);
-            thirdPosition = getXPosition(direction, startClone, startBlock);
-        }
-        if (getZLength(direction) > mainDirection) {
-            mainFace = getZFace(direction);
-            mainDirection = getZLength(direction);
-            mainPosition = getZPosition(direction, startClone, startBlock);
-
-            secondFace = getXFace(direction);
-            secondDirection = getXLength(direction);
-            secondPosition = getXPosition(direction, startClone, startBlock);
-
-            thirdFace = getYFace(direction);
-            thirdDirection = getYLength(direction);
-            thirdPosition = getYPosition(direction, startClone, startBlock);
-        }
-
-        // trace line backwards to find intercept with plane perpendicular to the main axis
-
-        double d = mainPosition / mainDirection; // how far to hit face behind
-        double second = secondPosition - secondDirection * d;
-        double third = thirdPosition - thirdDirection * d;
-
-        // Guarantee that the ray will pass though the start block.
-        // It is possible that it would miss due to rounding
-        // This should only move the ray by 1 grid position
-        secondError = floor(second * gridSize);
-        secondStep = round(secondDirection / mainDirection * gridSize);
-        thirdError = floor(third * gridSize);
-        thirdStep = round(thirdDirection / mainDirection * gridSize);
-
-        if (secondError + secondStep <= 0) {
-            secondError = -secondStep + 1;
-        }
-
-        if (thirdError + thirdStep <= 0) {
-            thirdError = -thirdStep + 1;
-        }
-
-        Vec lastBlock;
-
-        lastBlock = startBlock.relative(mainFace.getOppositeFace());
-
-        if (secondError < 0) {
-            secondError += gridSize;
-            lastBlock = lastBlock.relative(secondFace.getOppositeFace());
-        }
-
-        if (thirdError < 0) {
-            thirdError += gridSize;
-            lastBlock = lastBlock.relative(thirdFace.getOppositeFace());
-        }
-
-        // This means that when the variables are positive, it means that the coord=1 boundary has been crossed
-        secondError -= gridSize;
-        thirdError -= gridSize;
-
-        blockQueue[0] = lastBlock;
-        currentBlock = -1;
-
-        scan();
-
-        boolean startBlockFound = false;
-
-        for (int cnt = currentBlock; cnt >= 0; cnt--) {
-            if (blockEquals(blockQueue[cnt], startBlock)) {
-                currentBlock = cnt;
-                startBlockFound = true;
-                break;
-            }
-        }
-
-        if (!startBlockFound) {
-            throw new IllegalStateException("Start block missed in BlockIterator");
-        }
-
-        // Calculate the number of planes passed to give max distance
-        maxDistanceInt = round(maxDistance / (Math.sqrt(mainDirection * mainDirection + secondDirection * secondDirection + thirdDirection * thirdDirection) / mainDirection));
-
+        signums[0] = (int) Math.signum(direction.x());
+        signums[1] = (int) Math.signum(direction.y());
+        signums[2] = (int) Math.signum(direction.z());
     }
 
-    private boolean blockEquals(@NotNull Point a, @NotNull Point b) {
-        return a.x() == b.x() && a.y() == b.y() && a.z() == b.z();
-    }
-
-    private BlockFace getXFace(@NotNull Point direction) {
-        return ((direction.x() > 0) ? BlockFace.EAST : BlockFace.WEST);
-    }
-
-    private BlockFace getYFace(@NotNull Point direction) {
-        return ((direction.y() > 0) ? BlockFace.TOP : BlockFace.BOTTOM);
-    }
-
-    private BlockFace getZFace(@NotNull Point direction) {
-        return ((direction.z() > 0) ? BlockFace.SOUTH : BlockFace.NORTH);
-    }
-
-    private double getXLength(@NotNull Point direction) {
-        return Math.abs(direction.x());
-    }
-
-    private double getYLength(@NotNull Point direction) {
-        return Math.abs(direction.y());
-    }
-
-    private double getZLength(@NotNull Point direction) {
-        return Math.abs(direction.z());
-    }
-
-    private double getPosition(double direction, double position, int blockPosition) {
-        return direction > 0 ? (position - blockPosition) : (blockPosition + 1 - position);
-    }
-
-    private double getXPosition(@NotNull Point direction, @NotNull Point position, @NotNull Point block) {
-        return getPosition(direction.x(), position.x(), block.blockX());
-    }
-
-    private double getYPosition(@NotNull Point direction, @NotNull Point position, @NotNull Point block) {
-        return getPosition(direction.y(), position.y(), block.blockY());
-    }
-
-    private double getZPosition(@NotNull Point direction, @NotNull Point position, @NotNull Point block) {
-        return getPosition(direction.z(), position.z(), block.blockZ());
-    }
-
-    /**
-     * Constructs the BlockIterator.
-     * <p>
-     * This considers all blocks as 1x1x1 in size.
-     *
-     * @param pos         The position for the start of the ray trace
-     * @param yOffset     The trace begins vertically offset from the start vector
-     *                    by this value
-     * @param maxDistance This is the maximum distance in blocks for the
-     *                    trace. Setting this value above 140 may lead to problems with
-     *                    unloaded chunks. A value of 0 indicates no limit
-     */
     public BlockIterator(@NotNull Pos pos, double yOffset, int maxDistance) {
         this(pos.asVec(), pos.direction(), yOffset, maxDistance);
     }
-
-    /**
-     * Constructs the BlockIterator.
-     * <p>
-     * This considers all blocks as 1x1x1 in size.
-     *
-     * @param pos     The position for the start of the ray trace
-     * @param yOffset The trace begins vertically offset from the start vector
-     *                by this value
-     */
 
     public BlockIterator(@NotNull Pos pos, double yOffset) {
         this(pos.asVec(), pos.direction(), yOffset, 0);
@@ -289,88 +112,68 @@ public class BlockIterator implements Iterator<Point> {
         this(entity, 0);
     }
 
-    /**
-     * Returns true if the iteration has more elements
-     */
+    private void calculateIntersectionX(Point start, Vec direction) {
+        double x = Math.floor(start.x()) + signums[0];
+        double y = start.y() + (x - start.x()) * direction.y() / direction.x();
+        double z = start.z() + (x - start.x()) * direction.z() / direction.x();
+        points[0] = new Vec(x, y, z);
+        distances[0] = this.start.distance(points[0]);
+    }
+
+    private void calculateIntersectionY(Point start, Vec direction) {
+        double y = Math.floor(start.y()) + signums[1];
+        double x = start.x() + (y - start.y()) * direction.x() / direction.y();
+        double z = start.z() + (y - start.y()) * direction.z() / direction.y();
+        points[1] = new Vec(x, y, z);
+        distances[1] = this.start.distance(points[1]);
+    }
+
+    private void calculateIntersectionZ(Point start, Vec direction) {
+        double z = Math.floor(start.z()) + signums[2];
+        double x = start.x() + (z - start.z()) * direction.x() / direction.z();
+        double y = start.y() + (z - start.z()) * direction.y() / direction.z();
+        points[2] = new Vec(x, y, z);
+        distances[2] = this.start.distance(points[2]);
+    }
+
+    private Point updateClosest() {
+        double minDistance = Double.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            if (distances[i] < minDistance) {
+                minDistance = distances[i];
+            }
+        }
+
+        boolean[] needsUpdate = new boolean[3];
+        needsUpdate[0] = distances[0] == minDistance && points[0] != null;
+        needsUpdate[1] = distances[1] == minDistance && points[1] != null;
+        needsUpdate[2] = distances[2] == minDistance && points[2] != null;
+
+        Point closest = null;
+        if(needsUpdate[0]) {
+            closest = points[0];
+            calculateIntersectionX(points[0], direction);
+        }
+        else if(needsUpdate[1]) {
+            closest = points[1];
+            calculateIntersectionY(points[1], direction);
+        }
+        else if(needsUpdate[2]) {
+            closest = points[2];
+            calculateIntersectionZ(points[2], direction);
+        }
+
+        return closest;
+    }
 
     @Override
     public boolean hasNext() {
-        scan();
-        return currentBlock != -1;
-    }
-
-    /**
-     * Returns the next BlockPosition in the trace
-     *
-     * @return the next BlockPosition in the trace
-     */
-    @Override
-    @NotNull
-    public Point next() throws NoSuchElementException {
-        scan();
-        if (currentBlock <= -1) {
-            throw new NoSuchElementException();
-        } else {
-            return blockQueue[currentBlock--];
-        }
+        return !(distances[0] > maxDistance && distances[1] > maxDistance && distances[2] > maxDistance);
     }
 
     @Override
-    public void remove() {
-        throw new UnsupportedOperationException("[BlockIterator] doesn't support block removal");
-    }
-
-    private void scan() {
-        if (currentBlock >= 0) {
-            return;
-        }
-        if (maxDistance != 0 && currentDistance > maxDistanceInt) {
-            end = true;
-            return;
-        }
-        if (end) {
-            return;
-        }
-
-        currentDistance++;
-
-        secondError += secondStep;
-        thirdError += thirdStep;
-
-        if (secondError > 0 && thirdError > 0) {
-            blockQueue[2] = blockQueue[0].relative(mainFace);
-            if (((long) secondStep) * ((long) thirdError) - ((long) thirdStep) * ((long) secondError) <= 5955451) {
-                blockQueue[1] = blockQueue[2].relative(secondFace);
-                blockQueue[0] = blockQueue[1].relative(thirdFace);
-            } else {
-                blockQueue[1] = blockQueue[2].relative(thirdFace);
-                blockQueue[0] = blockQueue[1].relative(secondFace);
-            }
-            thirdError -= gridSize;
-            secondError -= gridSize;
-            currentBlock = 2;
-        } else if (secondError > 0) {
-            blockQueue[1] = blockQueue[0].relative(mainFace);
-            blockQueue[0] = blockQueue[1].relative(secondFace);
-            secondError -= gridSize;
-            currentBlock = 1;
-        } else if (thirdError > 0) {
-            blockQueue[1] = blockQueue[0].relative(mainFace);
-            blockQueue[0] = blockQueue[1].relative(thirdFace);
-            thirdError -= gridSize;
-            currentBlock = 1;
-        } else {
-            blockQueue[0] = blockQueue[0].relative(mainFace);
-            currentBlock = 0;
-        }
-    }
-
-    public static int floor(double num) {
-        final int floor = (int) num;
-        return floor == num ? floor : floor - (int) (Double.doubleToRawLongBits(num) >>> 63);
-    }
-
-    public static int round(double num) {
-        return floor(num + 0.5d);
+    public Point next() {
+        var res = updateClosest();
+        return new Vec(Math.floor(res.x()), Math.floor(res.y()), Math.floor(res.z()));
     }
 }
--- src/test/java/net/minestom/server/utils/block/BlockIteratorTest.java
+++ src/test/java/net/minestom/server/utils/block/BlockIteratorTest.java
@@ -3,34 +3,51 @@ package net.minestom.server.utils.block;
 import net.minestom.server.coordinate.Vec;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
 public class BlockIteratorTest {
     @Test
-    public void moveExactYBlock() {
-        Vec v1 = new Vec(41.21612735985405, 21, -51.0);
-        Vec v2 = new Vec(0.7195944535812537, 0.5000002169949778, -0.6943945725414694);
-        BlockIterator iterator = new BlockIterator(v1, v2, 0, 2);
+    public void blockIteratorRandom() {
+        for (int i = 0; i < 1; ++i) {
+            double r1 = Math.random() * 1000 - 500;
+            double r2 = Math.random() * 1000 - 500;
+            double r3 = Math.random() * 1000 - 500;
 
-        assertEquals(new Vec(41, 21, -51), iterator.next());
-        assertEquals(new Vec(41, 21, -52), iterator.next());
-        assertEquals(new Vec(42, 21, -52), iterator.next());
-        assertEquals(new Vec(42, 21, -53), iterator.next());
-        assertEquals(new Vec(42, 22, -53), iterator.next());
-        assertFalse(iterator.hasNext());
+            if (Math.random() < 0.1) r1 = (int)r1;
+            if (Math.random() < 0.1) r2 = (int)r2;
+            if (Math.random() < 0.1) r3 = (int)r3;
+
+            Vec v1 = new Vec(r1, r2, r3);
+
+            double r4 = Math.random() * 1000 - 500;
+            double r5 = Math.random() * 1000 - 500;
+            double r6 = Math.random() * 1000 - 500;
+
+            if (Math.random() < 0.1) r4 = (int)r4;
+            if (Math.random() < 0.1) r5 = (int)r5;
+            if (Math.random() < 0.1) r6 = (int)r6;
+
+            Vec v2 = new Vec(r4, r5, r6);
+            BlockIterator iterator = new BlockIterator(v1, v2, 0, (int) (Math.random() * 100));
+        }
     }
 
     @Test
-    public void moveExactBlock() {
-        Vec v1 = new Vec(41, 21, -51.0);
-        Vec v2 = new Vec(1, 1, 1);
-        BlockIterator iterator = new BlockIterator(v1, v2, 0, 2);
+    public void testBroken() {
+        Vec s = new Vec(0, -0.6, 0);
+        Vec e = new Vec(-1, 1, 2);
+        BlockIterator iterator = new BlockIterator(s, e, 0, 40);
+    }
 
-        assertEquals(new Vec(41, 21, -51), iterator.next());
-        assertEquals(new Vec(42, 21, -51), iterator.next());
-        assertEquals(new Vec(42, 22, -51), iterator.next());
-        assertEquals(new Vec(42, 22, -50), iterator.next());
-        assertFalse(iterator.hasNext());
+    @Test
+    public void testSimple() {
+        Vec s = new Vec(0,  0, 0);
+        Vec e = new Vec(2, 1, 0);
+        BlockIterator iterator = new BlockIterator(s, e, 0, 4);
+    }
+
+    @Test
+    public void testComplex() {
+        Vec s = new Vec(0.3,  44, 0.3);
+        Vec e = new Vec(0, -10, 0);
+        BlockIterator iterator = new BlockIterator(s, e, 0, 10);
     }
 }
