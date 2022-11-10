db06c145c0586207c78dcc21cc9e202e8b9e7ac9
iam4722202468 <aceparent@gmail.com>
2022-11-09 23:55:39 -0500

    Fix

--- src/test/java/net/minestom/server/utils/block/BlockIteratorTest.java
+++ src/test/java/net/minestom/server/utils/block/BlockIteratorTest.java
@@ -83,15 +83,83 @@ public class BlockIteratorTest {
     }
 
     @Test
-    public void failing() {
+    public void testLongDistance() {
         Vec s = new Vec(42.5, 0, 51.5);
         Vec e = new Vec(-12, 0, -36);
         BlockIterator iterator = new BlockIterator(s, e, 0, 37);
 
+        List<Point> points = new ArrayList<>();
         while (iterator.hasNext()) {
-            System.out.println(iterator.next());
+            points.add(iterator.next());
         }
 
+        Point[] validPoints = new Point[] {
+            new Vec(42.0, 0.0, 51.0),
+            new Vec(42.0, 0.0, 50.0),
+            new Vec(41.0, 0.0, 50.0),
+            new Vec(42.0, 0.0, 49.0),
+            new Vec(41.0, 0.0, 49.0),
+            new Vec(41.0, 0.0, 48.0),
+            new Vec(41.0, 0.0, 47.0),
+            new Vec(40.0, 0.0, 47.0),
+            new Vec(41.0, 0.0, 46.0),
+            new Vec(40.0, 0.0, 46.0),
+            new Vec(40.0, 0.0, 45.0),
+            new Vec(40.0, 0.0, 44.0),
+            new Vec(39.0, 0.0, 44.0),
+            new Vec(40.0, 0.0, 43.0),
+            new Vec(39.0, 0.0, 43.0),
+            new Vec(39.0, 0.0, 42.0),
+            new Vec(39.0, 0.0, 41.0),
+            new Vec(38.0, 0.0, 41.0),
+            new Vec(39.0, 0.0, 40.0),
+            new Vec(38.0, 0.0, 40.0),
+            new Vec(38.0, 0.0, 39.0),
+            new Vec(38.0, 0.0, 38.0),
+            new Vec(37.0, 0.0, 38.0),
+            new Vec(38.0, 0.0, 37.0),
+            new Vec(37.0, 0.0, 37.0),
+            new Vec(37.0, 0.0, 36.0),
+            new Vec(37.0, 0.0, 35.0),
+            new Vec(36.0, 0.0, 35.0),
+            new Vec(37.0, 0.0, 34.0),
+            new Vec(36.0, 0.0, 34.0),
+            new Vec(36.0, 0.0, 33.0),
+            new Vec(36.0, 0.0, 32.0),
+            new Vec(35.0, 0.0, 32.0),
+            new Vec(36.0, 0.0, 31.0),
+            new Vec(35.0, 0.0, 31.0),
+            new Vec(35.0, 0.0, 30.0),
+            new Vec(35.0, 0.0, 29.0),
+            new Vec(34.0, 0.0, 29.0),
+            new Vec(35.0, 0.0, 28.0),
+            new Vec(34.0, 0.0, 28.0),
+            new Vec(34.0, 0.0, 27.0),
+            new Vec(34.0, 0.0, 26.0),
+            new Vec(33.0, 0.0, 26.0),
+            new Vec(34.0, 0.0, 25.0),
+            new Vec(33.0, 0.0, 25.0),
+            new Vec(33.0, 0.0, 24.0),
+            new Vec(33.0, 0.0, 23.0),
+            new Vec(32.0, 0.0, 23.0),
+            new Vec(33.0, 0.0, 22.0),
+            new Vec(32.0, 0.0, 22.0),
+            new Vec(32.0, 0.0, 21.0),
+            new Vec(32.0, 0.0, 20.0),
+            new Vec(31.0, 0.0, 20.0),
+            new Vec(32.0, 0.0, 19.0),
+            new Vec(31.0, 0.0, 19.0),
+            new Vec(31.0, 0.0, 18.0),
+            new Vec(31.0, 0.0, 17.0),
+            new Vec(30.0, 0.0, 17.0),
+            new Vec(31.0, 0.0, 16.0),
+            new Vec(30.0, 0.0, 16.0)
+        };
+
+        for (Point p : validPoints) {
+            assertContains(points, p);
+        }
+        assertEquals(validPoints.length, points.size());
     }
 
     @Test
