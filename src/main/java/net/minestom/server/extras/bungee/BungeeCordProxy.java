e5d0a43ba42c7023a93e8e33356909e614783ac7
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2022-11-28 00:13:09 -0500

    add BungeeGuard authentication (#1502)

--- src/main/java/net/minestom/server/extras/bungee/BungeeCordProxy.java
+++ src/main/java/net/minestom/server/extras/bungee/BungeeCordProxy.java
@@ -1,11 +1,21 @@
 package net.minestom.server.extras.bungee;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Set;
+
 /**
- * BungeeCord forwarding support. This does not count as a security feature, and you will still be required to manage your firewall.
+ * BungeeCord forwarding support. Enabling BungeeGuard support with {@link #setBungeeGuardTokens(Set)} helps to secure the server,
+ * but managing your firewall is still recommended.
  * <p>
  * Please consider using {@link net.minestom.server.extras.velocity.VelocityProxy} instead.
  */
 public final class BungeeCordProxy {
+
+    private static Set<String> bungeeGuardTokens = null;
 
     private static volatile boolean enabled;
 
@@ -24,4 +34,35 @@ public final class BungeeCordProxy {
     public static boolean isEnabled() {
         return enabled;
     }
+
+    /**
+     * Sets the tokens used by BungeeGuard authentication.
+     * Setting the tokens to a not-null value enables BungeeGuard authentication,
+     * and setting it to a null value disables BungeeGuard authentication.
+     *
+     * @param tokens The new BungeeGuard authentication tokens
+     */
+    public static void setBungeeGuardTokens(@Nullable Set<String> tokens) {
+        bungeeGuardTokens = tokens;
+    }
+
+    /**
+     * Checks whether BungeeGuard authentication is enabled.
+     *
+     * @return Whether BungeeGuard authentication is enabled
+     */
+    public static boolean isBungeeGuardEnabled() {
+        return bungeeGuardTokens != null;
+    }
+
+    /**
+     * Checks whether a token is one of the valid BungeeGuard tokens
+     *
+     * @param token The token to test
+     * @return Whether the token is a valid BungeeGuard token
+     */
+    public static boolean isValidBungeeGuardToken(@NotNull String token) {
+        return isBungeeGuardEnabled() && bungeeGuardTokens.contains(token);
+    }
+
 }
