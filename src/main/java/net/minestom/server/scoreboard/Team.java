e902f40feaf8510099a61bc93d77facd7f1adac3
iam4722202468 <aceparent@gmail.com>
2022-12-07 01:34:37 -0500

    Improve consistency of team packets

--- src/main/java/net/minestom/server/scoreboard/Team.java
+++ src/main/java/net/minestom/server/scoreboard/Team.java
@@ -103,13 +103,26 @@ public class Team implements PacketGroupingAudience {
     /**
      * Adds a member to the {@link Team}.
      * <br>
-     * This member can be a {@link Player} or an {@link LivingEntity}.
+     * This member collection can contain {@link Player} or {@link LivingEntity}.
+     * For players use their username, for entities use their UUID
      *
      * @param member The member to be added
      */
     public void addMember(@NotNull String member) {
+        addMembers(List.of(member));
+    }
+
+    /**
+     * Adds a members to the {@link Team}.
+     * <br>
+     * This member collection can contain {@link Player} or {@link LivingEntity}.
+     * For players use their username, for entities use their UUID
+     *
+     * @param members The members to be added
+     */
+    public void addMembers(@NotNull Collection<@NotNull String> members) {
         // Adds a new member to the team
-        this.members.add(member);
+        this.members.addAll(members);
 
         // Initializes add player packet
         final TeamsPacket addPlayerPacket = new TeamsPacket(teamName,
@@ -123,18 +136,33 @@ public class Team implements PacketGroupingAudience {
 
     /**
      * Removes a member from the {@link Team}.
+     * <br>
+     * This member collection can contain {@link Player} or {@link LivingEntity}.
+     * For players use their username, for entities use their UUID
      *
      * @param member The member to be removed
      */
     public void removeMember(@NotNull String member) {
+        removeMembers(List.of(member));
+    }
+
+    /**
+     * Removes members from the {@link Team}.
+     * <br>
+     * This member collection can contain {@link Player} or {@link LivingEntity}.
+     * For players use their username, for entities use their UUID
+     *
+     * @param members The members to be removed
+     */
+    public void removeMembers(@NotNull Collection<@NotNull String> members) {
         // Initializes remove player packet
         final TeamsPacket removePlayerPacket = new TeamsPacket(teamName,
-                new TeamsPacket.RemoveEntitiesToTeamAction(List.of(member)));
-        // Sends to all online player teh remove player packet
+                new TeamsPacket.RemoveEntitiesToTeamAction(members));
+        // Sends to all online player the remove player packet
         PacketUtils.broadcastPacket(removePlayerPacket);
 
         // Removes the member from the team
-        this.members.remove(member);
+        this.members.removeAll(members);
 
         // invalidate player members
         this.isPlayerMembersUpToDate = false;
