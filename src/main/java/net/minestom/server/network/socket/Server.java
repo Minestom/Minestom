048593c894a36817d79eef365350eee5441c3a61
NitrinCloud <admin@nitrin.net>
2023-01-06 05:35:24 +0100

    Support dynamic selection of a port using '0'

--- src/main/java/net/minestom/server/network/socket/Server.java
+++ src/main/java/net/minestom/server/network/socket/Server.java
@@ -63,6 +63,10 @@ public final class Server {
         server.register(selector, SelectionKey.OP_ACCEPT);
         this.serverSocket = server;
         this.socketAddress = address;
+
+        if (address instanceof InetSocketAddress && port == 0) {
+            port = server.socket().getLocalPort();
+        }
     }
 
     @ApiStatus.Internal
