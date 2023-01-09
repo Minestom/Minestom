75ebd72d921b306fefdbac9f8c066a12e24fe007
emortaldev <me@emortal.dev>
2023-01-09 22:07:28 +0000

    Update FollowTargetGoal.java

--- src/main/java/net/minestom/server/entity/ai/goal/FollowTargetGoal.java
+++ src/main/java/net/minestom/server/entity/ai/goal/FollowTargetGoal.java
@@ -34,7 +34,7 @@ public class FollowTargetGoal extends GoalSelector {
         Entity target = entityCreature.getTarget();
         if (target == null) target = findTarget();
         if (target == null) return false;
-        final boolean result = target.getPosition().distance(entityCreature.getPosition()) >= 2;
+        final boolean result = target.getPosition().distanceSquared(entityCreature.getPosition()) >= 2 * 2;
         if (result) {
             this.target = target;
         }
