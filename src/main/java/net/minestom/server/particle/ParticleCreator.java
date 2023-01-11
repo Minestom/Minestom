869e01cfff47f8f72ff393ca60f02482b0f36494
_Pesto <pestonotpasta@gmail.com>
2023-01-11 16:04:01 +0300

    Improve createParticlePacket methods

--- src/main/java/net/minestom/server/particle/ParticleCreator.java
+++ src/main/java/net/minestom/server/particle/ParticleCreator.java
@@ -2,6 +2,7 @@ package net.minestom.server.particle;
 
 import net.minestom.server.network.packet.server.play.ParticlePacket;
 import net.minestom.server.utils.binary.BinaryWriter;
+import net.minestom.server.utils.validate.Check;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
@@ -11,10 +12,16 @@ import java.util.function.Consumer;
  */
 public class ParticleCreator {
 
+    /**
+     * @deprecated Use {@link #createParticlePacket(Particle, boolean, double, double, double, float, float, float, float, int, ParticleOptions)} instead
+     */
+
+    @Deprecated(forRemoval = true)
     public static ParticlePacket createParticlePacket(Particle particleType, boolean distance,
                                                       double x, double y, double z,
                                                       float offsetX, float offsetY, float offsetZ,
                                                       float particleData, int count, @Nullable Consumer<BinaryWriter> dataWriter) {
+
         byte[] data;
         if (dataWriter != null) {
             BinaryWriter writer = new BinaryWriter();
@@ -23,14 +30,27 @@ public class ParticleCreator {
         } else {
             data = new byte[0];
         }
+
         return new ParticlePacket(particleType.id(), distance, x, y, z, offsetX, offsetY, offsetZ, particleData, count, data);
     }
 
-    public static ParticlePacket createParticlePacket(Particle particleType,
+
+    public static ParticlePacket createParticlePacket(Particle particleType, boolean distance,
+                                                      double x, double y, double z,
+                                                      float offsetX, float offsetY, float offsetZ,
+                                                      float particleData, int count, ParticleOptions options) {
+
+
+        Check.argCondition(particleType == options.type(), "ParticleOptions is not applicable to Particle{0}", particleType.name());
+        return new ParticlePacket(particleType.id(), distance, x, y, z, offsetX, offsetY, offsetZ, particleData, count, options.getData());
+    }
+
+
+    public static ParticlePacket createParticlePacket(Particle particleType, boolean distance,
                                                       double x, double y, double z,
                                                       float offsetX, float offsetY, float offsetZ,
                                                       int count) {
-        return createParticlePacket(particleType, true, x, y, z,
-                offsetX, offsetY, offsetZ, 0, count, null);
+        return new ParticlePacket(particleType.id(), distance, x, y, z,
+                offsetX, offsetY, offsetZ, 0, count, new byte[0]);
     }
 }
