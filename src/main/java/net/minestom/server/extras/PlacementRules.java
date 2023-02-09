e54c6787c96d48d8415c721cd9a1454127bc3c9a
NxDs <7994264+NxDs@users.noreply.github.com>
2023-02-10 00:36:08 +0100

    Improve PlacementRules

--- src/main/java/net/minestom/server/extras/PlacementRules.java
+++ src/main/java/net/minestom/server/extras/PlacementRules.java
@@ -5,50 +5,48 @@ import net.minestom.server.instance.block.Block;
 import net.minestom.server.instance.block.BlockManager;
 import net.minestom.server.instance.block.rule.vanilla.AxisPlacementRule;
 import net.minestom.server.instance.block.rule.vanilla.RedstonePlacementRule;
+import net.minestom.server.instance.block.rule.vanilla.StairsPlacementRule;
 import net.minestom.server.instance.block.rule.vanilla.WallPlacementRule;
 
 public final class PlacementRules {
 
-	public static void init() {
-		BlockManager blockManager = MinecraftServer.getBlockManager();
-		blockManager.registerBlockPlacementRule(new RedstonePlacementRule());
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.BONE_BLOCK));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.HAY_BLOCK));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.OAK_LOG));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.SPRUCE_LOG));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.BIRCH_LOG));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.JUNGLE_LOG));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.ACACIA_LOG));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.DARK_OAK_LOG));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.CRIMSON_STEM));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.WARPED_STEM));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_OAK_LOG));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_SPRUCE_LOG));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_BIRCH_LOG));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_JUNGLE_LOG));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_ACACIA_LOG));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_DARK_OAK_LOG));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_CRIMSON_STEM));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_WARPED_STEM));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.PURPUR_PILLAR));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.QUARTZ_PILLAR));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.OAK_WOOD));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.SPRUCE_WOOD));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.BIRCH_WOOD));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.JUNGLE_WOOD));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.ACACIA_WOOD));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.DARK_OAK_WOOD));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.CRIMSON_STEM));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.WARPED_STEM));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_OAK_WOOD));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_SPRUCE_WOOD));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_BIRCH_WOOD));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_JUNGLE_WOOD));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_ACACIA_WOOD));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_DARK_OAK_WOOD));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_CRIMSON_STEM));
-		blockManager.registerBlockPlacementRule(new AxisPlacementRule(Block.STRIPPED_WARPED_STEM));
-		blockManager.registerBlockPlacementRule(new WallPlacementRule(Block.COBBLESTONE_WALL));
-		blockManager.registerBlockPlacementRule(new WallPlacementRule(Block.MOSSY_COBBLESTONE_WALL));
-	}
+    public static void init() {
+        initAxisBlocks();
+        initStairs();
+        initWalls();
+        initRedstone();
+    }
+
+    public static void initAxisBlocks() {
+        BlockManager blockManager = MinecraftServer.getBlockManager();
+        for (Block block : Block.values()) {
+            if (block.getProperty("axis") != null) {
+                blockManager.registerBlockPlacementRule(new AxisPlacementRule(block));
+            }
+        }
+    }
+
+    public static void initStairs() {
+        BlockManager blockManager = MinecraftServer.getBlockManager();
+        for (Block block : Block.values()) {
+            if (block.namespace().value().endsWith("stairs")) {
+                blockManager.registerBlockPlacementRule(new StairsPlacementRule(block));
+            }
+        }
+    }
+
+    public static void initWalls() {
+        BlockManager blockManager = MinecraftServer.getBlockManager();
+        for (Block block : Block.values()) {
+            String namespace = block.namespace().value();
+            if (namespace.endsWith("wall") || namespace.endsWith("fence")) {
+                blockManager.registerBlockPlacementRule(new WallPlacementRule(block));
+            }
+        }
+    }
+
+    public static void initRedstone() {
+        BlockManager blockManager = MinecraftServer.getBlockManager();
+        blockManager.registerBlockPlacementRule(new RedstonePlacementRule());
+    }
 }
