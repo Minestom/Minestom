2aa0c8c470cd3137248d7ea64231ccdef014f5aa
NxDs <7994264+NxDs@users.noreply.github.com>
2023-02-23 08:35:12 +0100

    Add wall placement support

--- src/main/java/net/minestom/server/instance/block/rule/vanilla/BannerPlacementRule.java
+++ src/main/java/net/minestom/server/instance/block/rule/vanilla/BannerPlacementRule.java
@@ -6,6 +6,7 @@ import net.minestom.server.instance.Instance;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.instance.block.BlockFace;
 import net.minestom.server.instance.block.rule.BlockPlacementRule;
+import net.minestom.server.utils.NamespaceID;
 import org.jetbrains.annotations.NotNull;
 
 public class BannerPlacementRule extends BlockPlacementRule {
@@ -23,12 +24,36 @@ public class BannerPlacementRule extends BlockPlacementRule {
     public Block blockPlace(@NotNull Instance instance,
                             @NotNull Block block, @NotNull BlockFace blockFace, @NotNull Point blockPosition,
                             @NotNull Player pl) {
-        float yaw = pl.getPosition().yaw() + 180;
-        int rotation = (int) (Math.round(yaw / 22.5d) % 16);
+        // Can't place at the bottom of a block
+        if (blockFace == BlockFace.BOTTOM) {
+            return null;
+        }
 
+        if (blockFace == BlockFace.TOP) {
+            float yaw = pl.getPosition().yaw() + 180;
+            int rotation = (int) (Math.round(yaw / 22.5d) % 16);
+
+            return withBannerData(block).withProperty("rotation", String.valueOf(rotation));
+        }
+
+        return withBannerData(toWallBlock(block))
+                .withProperty("facing", blockFace.name().toLowerCase());
+    }
+
+    private Block toWallBlock(Block block) {
+        // Same as skulls, maybe there's a better way
+        String name = block.namespace().value();
+
+        // white_banner -> white
+        String rawName = name.substring(0, name.lastIndexOf("_"));
+
+        return Block.fromNamespaceId(NamespaceID.from(block.namespace().domain(), rawName + "_wall_banner"));
+    }
+
+    private Block withBannerData(Block block/*, BannerMeta meta*/) {
         // TODO missing banner meta, waiting for https://github.com/Minestom/Minestom/pull/1274
-
-        return block.withProperty("rotation", String.valueOf(rotation));
+        //  Also missing ItemMeta from placeBlock() in https://github.com/Minestom/Minestom/pull/1758
+        return block;
     }
 
 }
