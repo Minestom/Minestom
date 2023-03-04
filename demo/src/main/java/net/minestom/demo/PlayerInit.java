07cbfe22b99a135f54c36e7bec193a9632ecb90c
Freddi <mail@FrederikHeinrich.de>
2023-03-04 16:20:44 +0100

    Update PlayerInit.java

--- demo/src/main/java/net/minestom/demo/PlayerInit.java
+++ demo/src/main/java/net/minestom/demo/PlayerInit.java
@@ -15,6 +15,7 @@ import net.minestom.server.event.Event;
 import net.minestom.server.event.EventNode;
 import net.minestom.server.event.entity.EntityAttackEvent;
 import net.minestom.server.event.entity.EntityVelocityEvent;
+import net.minestom.server.event.instance.AddEntityToInstanceEvent;
 import net.minestom.server.event.item.ItemDropEvent;
 import net.minestom.server.event.item.PickupItemEvent;
 import net.minestom.server.event.player.*;
@@ -88,6 +89,7 @@ public class PlayerInit {
                 itemEntity.setVelocity(velocity);
             })
             .addListener(PlayerDisconnectEvent.class, event -> System.out.println("DISCONNECTION " + event.getPlayer().getUsername()))
+
             .addListener(PlayerLoginEvent.class, event -> {
                 final Player player = event.getPlayer();
 
@@ -160,55 +162,55 @@ public class PlayerInit {
 
             });
 
-static {
-        InstanceManager instanceManager=MinecraftServer.getInstanceManager();
+    static {
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
 
-        InstanceContainer instanceContainer=instanceManager.createInstanceContainer(DimensionType.OVERWORLD);
-        instanceContainer.setGenerator(unit->unit.modifier().fillHeight(0,40,Block.STONE));
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer(DimensionType.OVERWORLD);
+        instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.STONE));
 
-        if(false){
-        System.out.println("start");
-        ChunkUtils.forChunksInRange(0,0,10,(x,z)->instanceContainer.loadChunk(x,z).join());
-        System.out.println("load end");
+        if (false) {
+            System.out.println("start");
+            ChunkUtils.forChunksInRange(0, 0, 10, (x, z) -> instanceContainer.loadChunk(x, z).join());
+            System.out.println("load end");
         }
 
-        inventory=new Inventory(InventoryType.CHEST_1_ROW,Component.text("Test inventory"));
-        inventory.setItemStack(3,ItemStack.of(Material.DIAMOND,34));
-        }
+        inventory = new Inventory(InventoryType.CHEST_1_ROW, Component.text("Test inventory"));
+        inventory.setItemStack(3, ItemStack.of(Material.DIAMOND, 34));
+    }
 
-private static final AtomicReference<TickMonitor> LAST_TICK=new AtomicReference<>();
+    private static final AtomicReference<TickMonitor> LAST_TICK = new AtomicReference<>();
 
-public static void init(){
-        var eventHandler=MinecraftServer.getGlobalEventHandler();
+    public static void init() {
+        var eventHandler = MinecraftServer.getGlobalEventHandler();
         eventHandler.addChild(DEMO_NODE);
 
-        eventHandler.addListener(ServerTickMonitorEvent.class,event->LAST_TICK.set(event.getTickMonitor()));
+        eventHandler.addListener(ServerTickMonitorEvent.class, event -> LAST_TICK.set(event.getTickMonitor()));
 
-        BenchmarkManager benchmarkManager=MinecraftServer.getBenchmarkManager();
-        MinecraftServer.getSchedulerManager().buildTask(()->{
-        Collection<Player> players=MinecraftServer.getConnectionManager().getOnlinePlayers();
-        if(players.isEmpty())
-        return;
+        BenchmarkManager benchmarkManager = MinecraftServer.getBenchmarkManager();
+        MinecraftServer.getSchedulerManager().buildTask(() -> {
+            Collection<Player> players = MinecraftServer.getConnectionManager().getOnlinePlayers();
+            if (players.isEmpty())
+                return;
 
-        long ramUsage=benchmarkManager.getUsedMemory();
-        ramUsage/=1e6; // bytes to MB
+            long ramUsage = benchmarkManager.getUsedMemory();
+            ramUsage /= 1e6; // bytes to MB
 
-        TickMonitor tickMonitor=LAST_TICK.get();
-final Component header=Component.text("RAM USAGE: "+ramUsage+" MB")
-        .append(Component.newline())
-        .append(Component.text("TICK TIME: "+MathUtils.round(tickMonitor.getTickTime(),2)+"ms"))
-        .append(Component.newline())
-        .append(Component.text("ACQ TIME: "+MathUtils.round(tickMonitor.getAcquisitionTime(),2)+"ms"));
-final Component footer=benchmarkManager.getCpuMonitoringMessage();
-        Audiences.players().sendPlayerListHeaderAndFooter(header,footer);
-        }).repeat(10,TimeUnit.SERVER_TICK).schedule();
-        MinecraftServer.getSchedulerManager().buildTask(()->{
-        Collection<Player> players=MinecraftServer.getConnectionManager().getOnlinePlayers();
-        if(players.isEmpty())
-        return;
-        players.forEach(player->{
-        player.sendMessage("shouldUnmount: "+player.getVehicleInformation().shouldUnmount());
-        });
-        }).repeat(20,TimeUnit.SERVER_TICK).schedule();
-        }
-        }
+            TickMonitor tickMonitor = LAST_TICK.get();
+            final Component header = Component.text("RAM USAGE: " + ramUsage + " MB")
+                    .append(Component.newline())
+                    .append(Component.text("TICK TIME: " + MathUtils.round(tickMonitor.getTickTime(), 2) + "ms"))
+                    .append(Component.newline())
+                    .append(Component.text("ACQ TIME: " + MathUtils.round(tickMonitor.getAcquisitionTime(), 2) + "ms"));
+            final Component footer = benchmarkManager.getCpuMonitoringMessage();
+            Audiences.players().sendPlayerListHeaderAndFooter(header, footer);
+        }).repeat(10, TimeUnit.SERVER_TICK).schedule();
+        MinecraftServer.getSchedulerManager().buildTask(() -> {
+            Collection<Player> players = MinecraftServer.getConnectionManager().getOnlinePlayers();
+            if (players.isEmpty())
+                return;
+            players.forEach(player -> {
+                player.sendMessage("shouldUnmount: " + player.getVehicleInformation().shouldUnmount());
+            });
+        }).repeat(20, TimeUnit.SERVER_TICK).schedule();
+    }
+}
