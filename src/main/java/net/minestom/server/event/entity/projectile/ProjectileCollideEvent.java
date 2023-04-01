2e66d76592e9bed9b8d0dedd2915cb586e2dd593
TogAr2 <59421074+TogAr2@users.noreply.github.com>
2023-04-02 01:10:36 +0200

    Make projectile events cancellable again

--- src/main/java/net/minestom/server/event/entity/projectile/ProjectileCollideEvent.java
+++ src/main/java/net/minestom/server/event/entity/projectile/ProjectileCollideEvent.java
@@ -1,16 +1,17 @@
 package net.minestom.server.event.entity.projectile;
 
 import net.minestom.server.coordinate.Point;
-import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Entity;
+import net.minestom.server.event.trait.CancellableEvent;
 import net.minestom.server.event.trait.EntityInstanceEvent;
 import net.minestom.server.event.trait.RecursiveEvent;
 import org.jetbrains.annotations.NotNull;
 
-class ProjectileCollideEvent implements EntityInstanceEvent, RecursiveEvent {
+class ProjectileCollideEvent implements EntityInstanceEvent, RecursiveEvent, CancellableEvent {
 
     private final @NotNull Entity projectile;
     private final @NotNull Point position;
+    private boolean cancelled;
 
     protected ProjectileCollideEvent(@NotNull Entity projectile, @NotNull Point position) {
         this.projectile = projectile;
@@ -24,5 +25,15 @@ class ProjectileCollideEvent implements EntityInstanceEvent, RecursiveEvent {
 
     public @NotNull Point getCollisionPosition() {
         return position;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        cancelled = cancel;
     }
 }
--- src/main/java/net/minestom/server/event/entity/projectile/ProjectileCollideWithBlockEvent.java
+++ src/main/java/net/minestom/server/event/entity/projectile/ProjectileCollideWithBlockEvent.java
@@ -7,11 +7,10 @@ import net.minestom.server.event.trait.CancellableEvent;
 import net.minestom.server.instance.block.Block;
 import org.jetbrains.annotations.NotNull;
 
-public final class ProjectileCollideWithBlockEvent extends ProjectileCollideEvent implements CancellableEvent {
+public final class ProjectileCollideWithBlockEvent extends ProjectileCollideEvent {
 
     private final @NotNull Point collidedPosition;
     private final @NotNull Block block;
-    private boolean cancelled;
 
     public ProjectileCollideWithBlockEvent(@NotNull Entity projectile, @NotNull Pos collisionPosition,
                                            @NotNull Point collidedPosition, @NotNull Block block) {
@@ -26,15 +25,5 @@ public final class ProjectileCollideWithBlockEvent extends ProjectileCollideEven
 
     public @NotNull Block getBlock() {
         return block;
-    }
-
-    @Override
-    public boolean isCancelled() {
-        return cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean cancel) {
-        cancelled = cancel;
     }
 }
