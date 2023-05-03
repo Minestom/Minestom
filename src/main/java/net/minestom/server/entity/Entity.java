bc601b0bf54b88e7fe87a0db3118691d1f7f3520
iam4722202468 <aceparent@gmail.com>
2023-05-03 14:34:05 -0400

    Remove println

--- src/main/java/net/minestom/server/entity/Entity.java
+++ src/main/java/net/minestom/server/entity/Entity.java
@@ -645,7 +645,6 @@ public class Entity implements Viewable, Tickable, Schedulable, Snapshotable, Ev
             final Chunk chunk = ChunkUtils.retrieve(instance, currentChunk, position);
             synchronized (chunk) {
                 drag = chunk.getBlock(positionBeforeMove.sub(0, 0.5000001, 0)).registry().friction() * airDrag;
-                System.out.println(drag);
             }
         } else drag = airDrag;
 
