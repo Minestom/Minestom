4a3e113526f37a8180d4ce0724462371891c7a3c
mworzala <mattheworzala@gmail.com>
2023-05-05 22:43:12 +0300

    in order to make an omelet, you have to break a few eggs. aka absolutely decimate the gradle project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,7 @@
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-enableFeaturePreview("VERSION_CATALOGS")
-
-dependencyResolutionManagement {
-    repositories {
-        maven("https://jitpack.io")
-        mavenCentral()
-    }
-}
-
-pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    includeBuild("build-logic")
-}
-
-rootProject.name = "Minestom"
-include("code-generators")
-include("jmh-benchmarks")
-include("jcstress-tests")
-include("demo")
-include("testing")
+rootProject.name = "minestom-ce"
+//include("code-generators")
+//include("jmh-benchmarks")
+//include("jcstress-tests")
+//include("demo")
+//include("testing")
+//
