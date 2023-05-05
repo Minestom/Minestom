7018ad70342d5a99f805b1a1b56df41111a36e12
mworzala <mattheworzala@gmail.com>
2023-05-05 23:39:55 +0300

    far more sane gradle, no publishing yet tho

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,8 @@
 rootProject.name = "minestom-ce"
-//include("code-generators")
+
+include("testing")
+include("code-generators")
 //include("jmh-benchmarks")
 //include("jcstress-tests")
-//include("demo")
-//include("testing")
-//
+
+include("demo")
