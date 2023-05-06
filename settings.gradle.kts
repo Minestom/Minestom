381d89497a8c63dc3c4d89b60a6bd98947df1477
Matt Worzala <35708499+mworzala@users.noreply.github.com>
2023-05-06 23:18:22 +0300

    hollow-cube/gradle-rework (#8)
    * in order to make an omelet, you have to break a few eggs. aka absolutely decimate the gradle project
    
    * far more sane gradle, no publishing yet tho
    
    * attempt maven deploy
    
    * fix publish url and use snapshot repo
    
    * fix secret names
    
    * release, and properly set version
    
    * attempt to set group correctly
    
    * more gradle fixups

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,8 @@
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-enableFeaturePreview("VERSION_CATALOGS")
+rootProject.name = "minestom-ce"
 
-dependencyResolutionManagement {
-    repositories {
-        maven("https://jitpack.io")
-        mavenCentral()
-    }
-}
-
-pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    includeBuild("build-logic")
-}
-
-rootProject.name = "Minestom"
-include("code-generators")
-include("jmh-benchmarks")
-include("jcstress-tests")
-include("demo")
 include("testing")
+include("code-generators")
+//include("jmh-benchmarks")
+//include("jcstress-tests")
+
+include("demo")
