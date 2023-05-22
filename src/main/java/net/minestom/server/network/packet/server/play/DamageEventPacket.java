12e55375c038309e1b0f8f7038cfa232647f1ad1
MelonHell <MelonHell@ya.ru>
2023-05-22 22:23:37 +0300

    small changes to DamageEventPacket

--- src/main/java/net/minestom/server/network/packet/server/play/DamageEventPacket.java
+++ src/main/java/net/minestom/server/network/packet/server/play/DamageEventPacket.java
@@ -1,5 +1,6 @@
 package net.minestom.server.network.packet.server.play;
 
+import net.minestom.server.coordinate.Point;
 import net.minestom.server.coordinate.Vec;
 import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.packet.server.ServerPacket;
@@ -12,7 +13,7 @@ import static net.minestom.server.network.NetworkBuffer.*;
 // Notes
 // sourceEntityId - 0 indicates no source entity, otherwise it is entityId + 1
 // sourceDirectId - 0 indicates no direct source. Direct attacks (e.g. melee) will have this number me the same as sourceEntityId, indirect attacks (e.g. projectiles) will have this be be the projectile entity id + 1
-public record DamageEventPacket(int targetEntityId, int damageTypeId, int sourceEntityId, int sourceDirectId, @Nullable Vec sourcePos) implements ServerPacket {
+public record DamageEventPacket(int targetEntityId, int damageTypeId, int sourceEntityId, int sourceDirectId, @Nullable Point sourcePos) implements ServerPacket {
 
     public DamageEventPacket(@NotNull NetworkBuffer reader) {
         this(reader.read(VAR_INT), reader.read(VAR_INT), reader.read(VAR_INT), reader.read(VAR_INT),
@@ -30,13 +31,12 @@ public record DamageEventPacket(int targetEntityId, int damageTypeId, int source
         writer.write(VAR_INT, damageTypeId);
         writer.write(VAR_INT, sourceEntityId);
         writer.write(VAR_INT, sourceDirectId);
-        if (sourcePos != null) {
-            writer.write(BOOLEAN, true);
+        boolean hasSourcePos = sourcePos != null;
+        writer.write(BOOLEAN, hasSourcePos);
+        if (hasSourcePos) {
             writer.write(DOUBLE, sourcePos.x());
             writer.write(DOUBLE, sourcePos.y());
             writer.write(DOUBLE, sourcePos.z());
-        } else {
-            writer.write(BOOLEAN, false);
         }
     }
 }
