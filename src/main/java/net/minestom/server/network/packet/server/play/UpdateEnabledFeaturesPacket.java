7eb92388b5103668306059d39e6a7ab0a2fb2302
MelonHell <MelonHell@ya.ru>
2023-05-22 17:22:00 +0300

    change UpdateEnabledFeaturesPacket constructor

--- src/main/java/net/minestom/server/network/packet/server/play/UpdateEnabledFeaturesPacket.java
+++ src/main/java/net/minestom/server/network/packet/server/play/UpdateEnabledFeaturesPacket.java
@@ -6,14 +6,14 @@ import net.minestom.server.network.packet.server.ServerPacketIdentifier;
 import net.minestom.server.utils.NamespaceID;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Set;
+import java.util.Collection;
 
 import static net.minestom.server.network.NetworkBuffer.*;
 
-public record UpdateEnabledFeaturesPacket(int totalFeatures, Set<NamespaceID> featureFlags) implements ServerPacket {
+public record UpdateEnabledFeaturesPacket(Collection<NamespaceID> featureFlags) implements ServerPacket {
     @Override
     public void write(@NotNull NetworkBuffer writer) {
-        writer.write(VAR_INT, totalFeatures);
+        writer.write(VAR_INT, featureFlags.size());
         for (var featureFlag : featureFlags) {
             writer.write(STRING, featureFlag.asString());
         }
