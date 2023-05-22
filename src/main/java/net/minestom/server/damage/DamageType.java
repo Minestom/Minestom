afa205aa3f65f78fb169f6058c3ef9c12b214d64
MelonHell <MelonHell@ya.ru>
2023-05-22 23:16:33 +0300

    attempt to implement DamageType Registry

---
+++ src/main/java/net/minestom/server/damage/DamageType.java
@@ -0,0 +1,53 @@
+package net.minestom.server.damage;
+
+import net.minestom.server.registry.ProtocolObject;
+import net.minestom.server.registry.Registry;
+import net.minestom.server.utils.NamespaceID;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+
+public sealed interface DamageType extends ProtocolObject, DamageTypes permits DamageTypeImpl {
+    /**
+     * Returns the entity registry.
+     *
+     * @return the entity registry
+     */
+    @Contract(pure = true)
+    Registry.DamageTypeEntry registry();
+
+    @Override
+    default @NotNull NamespaceID namespace() {
+        return registry().namespace();
+    }
+
+    default double exhaustion() {
+        return registry().exhaustion();
+    }
+
+    default String message_id() {
+        return registry().message_id();
+    }
+
+    default String scaling() {
+        return registry().scaling();
+    }
+
+    static @NotNull Collection<@NotNull DamageType> values() {
+        return DamageTypeImpl.values();
+    }
+
+    static DamageType fromNamespaceId(@NotNull String namespaceID) {
+        return DamageTypeImpl.getSafe(namespaceID);
+    }
+
+    static DamageType fromNamespaceId(@NotNull NamespaceID namespaceID) {
+        return fromNamespaceId(namespaceID.asString());
+    }
+
+    static @Nullable DamageType fromId(int id) {
+        return DamageTypeImpl.getId(id);
+    }
+}
---
+++ src/main/java/net/minestom/server/damage/DamageTypeImpl.java
@@ -0,0 +1,37 @@
+package net.minestom.server.damage;
+
+import net.minestom.server.registry.Registry;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+
+public record DamageTypeImpl(Registry.DamageTypeEntry registry) implements DamageType {
+    private static final Registry.Container<DamageType> CONTAINER = Registry.createContainer(Registry.Resource.DAMAGE_TYPES,
+            (namespace, properties) -> new DamageTypeImpl(Registry.damageType(namespace, properties)));
+
+    static DamageType get(@NotNull String namespace) {
+        return CONTAINER.get(namespace);
+    }
+
+    static DamageType getSafe(@NotNull String namespace) {
+        return CONTAINER.getSafe(namespace);
+    }
+
+    static DamageType getId(int id) {
+        return CONTAINER.getId(id);
+    }
+
+    static Collection<DamageType> values() {
+        return CONTAINER.values();
+    }
+
+    @Override
+    public String toString() {
+        return name();
+    }
+
+    @Override
+    public int id() {
+        return CONTAINER.toId(name());
+    }
+}
