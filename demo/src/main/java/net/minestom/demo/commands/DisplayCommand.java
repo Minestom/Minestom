7dbacbbcca8482a24a85e2069fa211eee7fcb9bb
Freddi <mail@FrederikHeinrich.de>
2023-06-03 06:51:08 +0200

    Update DisplayCommand.java

--- demo/src/main/java/net/minestom/demo/commands/DisplayCommand.java
+++ demo/src/main/java/net/minestom/demo/commands/DisplayCommand.java
@@ -25,6 +25,7 @@ public class DisplayCommand extends Command {
         addSyntax(this::spawnItem, ArgumentType.Literal("item"));
         addSyntax(this::spawnBlock, ArgumentType.Literal("block"));
         addSyntax(this::spawnText, ArgumentType.Literal("text"));
+        addSyntax(this::spawnTextWithContext, ArgumentType.Literal("text"), ArgumentType.Component("component"));
     }
 
     public void spawnItem(@NotNull CommandSender sender, @NotNull CommandContext context) {
@@ -33,8 +34,9 @@ public class DisplayCommand extends Command {
 
         var entity = new Entity(EntityType.ITEM_DISPLAY);
         var meta = (ItemDisplayMeta) entity.getEntityMeta();
+        meta.setHasNoGravity(true);
         meta.setItemStack(ItemStack.of(Material.STICK));
-        entity.setInstance(player.getInstance(), player.getPosition());
+        entity.setInstance(player.getInstance(), player.getPosition().add(0,1.8,0));
     }
 
     public void spawnBlock(@NotNull CommandSender sender, @NotNull CommandContext context) {
@@ -43,8 +45,9 @@ public class DisplayCommand extends Command {
 
         var entity = new Entity(EntityType.BLOCK_DISPLAY);
         var meta = (BlockDisplayMeta) entity.getEntityMeta();
+        meta.setHasNoGravity(true);
         meta.setBlockState(Block.STONE_STAIRS.stateId());
-        entity.setInstance(player.getInstance(), player.getPosition());
+        entity.setInstance(player.getInstance(), player.getPosition().add(0,1.8,0));
     }
 
     public void spawnText(@NotNull CommandSender sender, @NotNull CommandContext context) {
@@ -54,7 +57,20 @@ public class DisplayCommand extends Command {
         var entity = new Entity(EntityType.TEXT_DISPLAY);
         var meta = (TextDisplayMeta) entity.getEntityMeta();
         meta.setBillboardRenderConstraints(AbstractDisplayMeta.BillboardConstraints.CENTER);
+        meta.setHasNoGravity(true);
         meta.setText(Component.text("Hello, world!"));
-        entity.setInstance(player.getInstance(), player.getPosition());
+        entity.setInstance(player.getInstance(), player.getPosition().add(0,1.8,0));
+    }public void spawnTextWithContext(@NotNull CommandSender sender, @NotNull CommandContext context) {
+        if (!(sender instanceof Player player))
+            return;
+
+        var entity = new Entity(EntityType.TEXT_DISPLAY);
+        var meta = (TextDisplayMeta) entity.getEntityMeta();
+        meta.setBillboardRenderConstraints(AbstractDisplayMeta.BillboardConstraints.CENTER);
+        Component component = context.get("component");
+        meta.setHasNoGravity(true);
+        meta.setText(component);
+        meta.setUseDefaultBackground(false);
+        entity.setInstance(player.getInstance(), player.getPosition().add(0,1.8,0));
     }
 }
