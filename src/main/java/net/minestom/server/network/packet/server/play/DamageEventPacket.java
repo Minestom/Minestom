d0711aed0f0a23b2b8d6935ef3e77099f161bb5e
MelonHell <MelonHell@ya.ru>
2023-07-04 11:36:58 +0300

    no +1 in DamageEventPacket

--- src/main/java/net/minestom/server/network/packet/server/play/DamageEventPacket.java
+++ src/main/java/net/minestom/server/network/packet/server/play/DamageEventPacket.java
@@ -14,7 +14,7 @@ import static net.minestom.server.network.NetworkBuffer.*;
  * See <a href="https://wiki.vg/Protocol#Damage_Event">https://wiki.vg/Protocol#Damage_Event</a> for more info.
  *
  * @param targetEntityId ID of the entity being damaged
- * @param damageTypeId   0 if there is no damage type, otherwise the damage type ID + 1
+ * @param damageTypeId   ID of damage type
  * @param sourceEntityId 0 if there is no source entity, otherwise it is entityId + 1
  * @param sourceDirectId 0 if there is no direct source. For direct attacks (e.g. melee), this is the same as sourceEntityId. For indirect attacks (e.g. projectiles), this is the projectile entity id + 1
  * @param sourcePos      null if there is no source position, otherwise the position of the source
