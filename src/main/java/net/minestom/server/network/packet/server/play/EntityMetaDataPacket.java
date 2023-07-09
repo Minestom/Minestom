d47db7242156097d538d510b71de56fb08ccc630
mworzala <mattheworzala@gmail.com>
2023-07-09 19:14:40 -0400

    hollow-cube/fix-entity-metadata-translation

--- src/main/java/net/minestom/server/network/packet/server/play/EntityMetaDataPacket.java
+++ src/main/java/net/minestom/server/network/packet/server/play/EntityMetaDataPacket.java
@@ -13,8 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.UnaryOperator;
 
-import static net.minestom.server.network.NetworkBuffer.BYTE;
-import static net.minestom.server.network.NetworkBuffer.VAR_INT;
+import static net.minestom.server.network.NetworkBuffer.*;
 
 public record EntityMetaDataPacket(int entityId,
                                    @NotNull Map<Integer, Metadata.Entry<?>> entries) implements ComponentHoldingServerPacket {
@@ -69,9 +68,17 @@ public record EntityMetaDataPacket(int entityId,
         final var entries = new HashMap<Integer, Metadata.Entry<?>>();
 
         this.entries.forEach((key, value) -> {
+            final var t = value.type();
             final var v = value.value();
 
-            entries.put(key, v instanceof Component c ? Metadata.OptChat(operator.apply(c)) : value);
+            if (v instanceof Component c) {
+                var translated = operator.apply(c);
+                entries.put(key, t == Metadata.TYPE_OPTCHAT ? Metadata.OptChat(translated) : Metadata.Chat(translated));
+            } else {
+                entries.put(key, value);
+            }
+
+            entries.put(key, v instanceof Component c ? Metadata.Chat(operator.apply(c)) : value);
         });
 
         return new EntityMetaDataPacket(this.entityId, entries);
