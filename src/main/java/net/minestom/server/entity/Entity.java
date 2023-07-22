4da527c25af7a3b2412754548a3809348ebae752
mworzala <mattheworzala@gmail.com>
2023-07-22 15:10:56 -0400

    hollow-cube/entity-teleport-head-fix

--- src/main/java/net/minestom/server/entity/Entity.java
+++ src/main/java/net/minestom/server/entity/Entity.java
@@ -333,8 +333,7 @@ public class Entity implements Viewable, Tickable, Schedulable, Snapshotable, Ev
         final Pos currentPosition = this.position;
         if (currentPosition.sameView(yaw, pitch)) return;
         this.position = currentPosition.withView(yaw, pitch);
-        sendPacketToViewersAndSelf(new EntityHeadLookPacket(getEntityId(), yaw));
-        sendPacketToViewersAndSelf(new EntityRotationPacket(getEntityId(), yaw, pitch, onGround));
+        synchronizeView();
     }
 
     /**
@@ -1568,6 +1567,11 @@ public class Entity implements Viewable, Tickable, Schedulable, Snapshotable, Ev
         PacketUtils.prepareViewablePacket(currentChunk, packet, this);
         this.lastAbsoluteSynchronizationTime = System.currentTimeMillis();
         this.lastSyncedPosition = posCache;
+    }
+
+    private void synchronizeView() {
+        sendPacketToViewersAndSelf(new EntityHeadLookPacket(getEntityId(), position.yaw()));
+        sendPacketToViewersAndSelf(new EntityRotationPacket(getEntityId(), position.yaw(), position.pitch(), onGround));
     }
 
     /**
