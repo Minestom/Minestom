619d23a67d80f1904e83ea04d2f2450d2c3321db
GoldenStack <goldenfire64yt@gmail.com>
2023-08-15 19:16:00 -0500

    Test click preprocessor

---
+++ src/test/java/net/minestom/server/inventory/click/ClickPreprocessorTest.java
@@ -0,0 +1,94 @@
+package net.minestom.server.inventory.click;
+
+import it.unimi.dsi.fastutil.ints.IntList;
+import net.minestom.server.entity.GameMode;
+import org.junit.jupiter.api.Test;
+
+import static net.minestom.server.inventory.click.ClickUtils.*;
+import static net.minestom.server.network.packet.client.play.ClientClickWindowPacket.ClickType.*;
+
+public class ClickPreprocessorTest {
+
+    @Test
+    public void testPickupType() {
+        assertProcessed(new ClickInfo.DropCursor(true), clickPacket(PICKUP, 1, 0, -999));
+        assertProcessed(new ClickInfo.LeftClick(0), clickPacket(PICKUP, 1, 0, 0));
+        assertProcessed(new ClickInfo.LeftClick(ClickPreprocessor.PLAYER_INVENTORY_OFFSET + 9), clickPacket(PICKUP, 1, 0, 5));
+        assertProcessed(null, clickPacket(PICKUP, 1, 0, 99));
+
+        assertProcessed(new ClickInfo.DropCursor(false), clickPacket(PICKUP, 1, 1, -999));
+        assertProcessed(new ClickInfo.RightClick(0), clickPacket(PICKUP, 1, 1, 0));
+        assertProcessed(new ClickInfo.RightClick(ClickPreprocessor.PLAYER_INVENTORY_OFFSET + 9), clickPacket(PICKUP, 1, 1, 5));
+        assertProcessed(null, clickPacket(PICKUP, 1, 1, 99));
+
+        assertProcessed(null, clickPacket(PICKUP, 1, -1, 0));
+        assertProcessed(null, clickPacket(PICKUP, 1, 2, 0));
+    }
+
+    @Test
+    public void testQuickMoveType() {
+        assertProcessed(new ClickInfo.ShiftClick(0), clickPacket(QUICK_MOVE, 1, 0, 0));
+        assertProcessed(new ClickInfo.ShiftClick(ClickPreprocessor.PLAYER_INVENTORY_OFFSET + 9), clickPacket(QUICK_MOVE, 1, 0, 5));
+        assertProcessed(null, clickPacket(QUICK_MOVE, 1, 0, -1));
+    }
+
+    @Test
+    public void testSwapType() {
+        assertProcessed(null, clickPacket(SWAP, 1, 0, -1));
+        assertProcessed(new ClickInfo.HotbarSwap(0, 2), clickPacket(SWAP, 1, 0, 2));
+        assertProcessed(new ClickInfo.HotbarSwap(8, 2), clickPacket(SWAP, 1, 8, 2));
+        assertProcessed(new ClickInfo.OffhandSwap(2), clickPacket(SWAP, 1, 40, 2));
+
+        assertProcessed(null, clickPacket(SWAP, 1, 9, 2));
+        assertProcessed(null, clickPacket(SWAP, 1, 39, 2));
+    }
+
+    @Test
+    public void testCloneType() {
+        var player = createPlayer();
+        player.setGameMode(GameMode.CREATIVE);
+
+        assertProcessed(null, clickPacket(CLONE, 1, 0, 0));
+        assertProcessed(player, new ClickInfo.CopyItem(0), clickPacket(CLONE, 1, 0, 0));
+        assertProcessed(player, null, clickPacket(CLONE, 1, 0, -1));
+    }
+
+    @Test
+    public void testThrowType() {
+        assertProcessed(new ClickInfo.DropSlot(0, true), clickPacket(THROW, 1, 1, 0));
+
+        assertProcessed(new ClickInfo.DropSlot(0, false), clickPacket(THROW, 1, 0, 0));
+        assertProcessed(new ClickInfo.DropSlot(0, true), clickPacket(THROW, 1, 1, 0));
+
+        assertProcessed(new ClickInfo.DropSlot(1, false), clickPacket(THROW, 1, 0, 1));
+        assertProcessed(new ClickInfo.DropSlot(1, true), clickPacket(THROW, 1, 1, 1));
+    }
+
+    @Test
+    public void testQuickCraft() {
+        var processor = createPreprocessor();
+        var player = createPlayer();
+
+        assertProcessed(player, null, clickPacket(QUICK_CRAFT, 1, 8, 0));
+        assertProcessed(player, null, clickPacket(QUICK_CRAFT, 1, 9, 0));
+        assertProcessed(player, null, clickPacket(QUICK_CRAFT, 1, 10, 0));
+
+        player.setGameMode(GameMode.CREATIVE);
+
+        assertProcessed(processor, player, null, clickPacket(QUICK_CRAFT, 1, 0, 0));
+        assertProcessed(processor, player, null, clickPacket(QUICK_CRAFT, 1, 1, 0));
+        assertProcessed(processor, player, null, clickPacket(QUICK_CRAFT, 1, 1, 1));
+        assertProcessed(processor, player, new ClickInfo.DistributeCursor(IntList.of(0, 1), true), clickPacket(QUICK_CRAFT, 1, 2, 0));
+
+        assertProcessed(processor, player, null, clickPacket(QUICK_CRAFT, 1, 4, 0));
+        assertProcessed(processor, player, null, clickPacket(QUICK_CRAFT, 1, 5, 0));
+        assertProcessed(processor, player, null, clickPacket(QUICK_CRAFT, 1, 5, 1));
+        assertProcessed(processor, player, new ClickInfo.DistributeCursor(IntList.of(0, 1), false), clickPacket(QUICK_CRAFT, 1, 6, 0));
+
+        assertProcessed(processor, player, null, clickPacket(QUICK_CRAFT, 1, 8, 0));
+        assertProcessed(processor, player, null, clickPacket(QUICK_CRAFT, 1, 9, 0));
+        assertProcessed(processor, player, null, clickPacket(QUICK_CRAFT, 1, 9, 1));
+        assertProcessed(processor, player, new ClickInfo.CopyCursor(IntList.of(0, 1)), clickPacket(QUICK_CRAFT, 1, 10, 0));
+    }
+
+}
--- src/test/java/net/minestom/server/inventory/click/ClickUtils.java
+++ src/test/java/net/minestom/server/inventory/click/ClickUtils.java
@@ -4,12 +4,15 @@ import net.minestom.server.entity.Player;
 import net.minestom.server.inventory.AbstractInventory;
 import net.minestom.server.inventory.Inventory;
 import net.minestom.server.inventory.InventoryType;
+import net.minestom.server.item.ItemStack;
+import net.minestom.server.network.packet.client.play.ClientClickWindowPacket;
 import net.minestom.server.network.packet.server.SendablePacket;
 import net.minestom.server.network.player.PlayerConnection;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.net.SocketAddress;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -34,11 +37,31 @@ public class ClickUtils {
         assertEquals(expectedChanges, changes);
     }
 
-    private static @NotNull AbstractInventory createInventory() {
+    public static void assertProcessed(@NotNull ClickPreprocessor preprocessor, @NotNull Player player, @Nullable ClickInfo info, @NotNull ClientClickWindowPacket packet) {
+        assertEquals(info, preprocessor.process(player, packet));
+    }
+
+    public static void assertProcessed(@NotNull Player player, @Nullable ClickInfo info, @NotNull ClientClickWindowPacket packet) {
+        assertProcessed(new ClickPreprocessor(createInventory()), player, info, packet);
+    }
+
+    public static void assertProcessed(@Nullable ClickInfo info, @NotNull ClientClickWindowPacket packet) {
+        assertProcessed(createPlayer(), info, packet);
+    }
+
+    public static @NotNull ClientClickWindowPacket clickPacket(@NotNull ClientClickWindowPacket.ClickType type, int windowId, int button, int slot) {
+        return new ClientClickWindowPacket((byte) windowId, 0, (short) slot, (byte) button, type, List.of(), ItemStack.AIR);
+    }
+
+    public static @NotNull ClickPreprocessor createPreprocessor() {
+        return new ClickPreprocessor(createInventory());
+    }
+
+    public static @NotNull AbstractInventory createInventory() {
         return new Inventory(InventoryType.HOPPER, "TestInventory");
     }
 
-    private static @NotNull Player createPlayer() {
+    public static @NotNull Player createPlayer() {
         return new Player(UUID.randomUUID(), "TestPlayer", new PlayerConnection() {
             @Override
             public void sendPacket(@NotNull SendablePacket packet) {}
