7174ce2b21d67fb7eeb974a69efc2fb781fc7eba
Samuel <pifnpafnpouf@gmail.com>
2023-09-26 09:40:52 -0400

    hollow-cube/editentitymeta_function (#62)
    * Add editEntityMeta
    
    * Log entityType instead
    
    * finally
    
    (cherry picked from commit 30699ec3fd33520bb7e97d0e97c964fa56818f23)

--- src/main/java/net/minestom/server/entity/Entity.java
+++ src/main/java/net/minestom/server/entity/Entity.java
@@ -292,6 +292,21 @@ public class Entity implements Viewable, Tickable, Schedulable, Snapshotable, Ev
     }
 
     /**
+     * Do a batch edit of this entity's metadata.
+     */
+    public <TMeta extends EntityMeta> void editEntityMeta(Class<TMeta> metaClass, Consumer<TMeta> editor) {
+        entityMeta.setNotifyAboutChanges(false);
+        try {
+            TMeta casted = metaClass.cast(entityMeta);
+            editor.accept(casted);
+        } catch (Throwable t) {
+            throw new RuntimeException("Error editing entity " + id + " " + entityType.name() + " meta", t);
+        } finally {
+            entityMeta.setNotifyAboutChanges(true);
+        }
+    }
+
+    /**
      * Teleports the entity only if the chunk at {@code position} is loaded or if
      * {@link Instance#hasEnabledAutoChunkLoad()} returns true.
      *
