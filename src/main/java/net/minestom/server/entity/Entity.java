517b195b5e2b1d8e08740208dacff34063126e59
GoldenStack <goldenfire64yt@gmail.com>
2023-10-07 13:49:16 -0500

    hollow-cube/remove-default-damage-behaviour (#64)
    * Remove automatic void damage
    
    * Remove automatic fire damage

--- src/main/java/net/minestom/server/entity/Entity.java
+++ src/main/java/net/minestom/server/entity/Entity.java
@@ -558,8 +558,6 @@ public class Entity implements Viewable, Tickable, Schedulable, Snapshotable, Ev
             // handle block contacts
             touchTick();
 
-            handleVoid();
-
             // Call the abstract update method
             update(time);
 
@@ -745,16 +743,6 @@ public class Entity implements Viewable, Tickable, Schedulable, Snapshotable, Ev
      */
     public long getAliveTicks() {
         return ticks;
-    }
-
-    /**
-     * How does this entity handle being in the void?
-     */
-    protected void handleVoid() {
-        // Kill if in void
-        if (getInstance().isInVoid(this.position)) {
-            remove();
-        }
     }
 
     /**
