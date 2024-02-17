0fcfa10cbce940f4f5bafb146e7ef093c61816c9
GoldenStack <goldenfire64yt@gmail.com>
2024-02-17 14:26:48 -0600

    Remap creative actions

--- src/main/java/net/minestom/server/listener/CreativeInventoryActionListener.java
+++ src/main/java/net/minestom/server/listener/CreativeInventoryActionListener.java
@@ -3,11 +3,12 @@ package net.minestom.server.listener;
 import net.minestom.server.entity.Player;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.network.packet.client.play.ClientCreativeInventoryActionPacket;
+import net.minestom.server.utils.inventory.PlayerInventoryUtils;
 
 public final class CreativeInventoryActionListener {
     public static void listener(ClientCreativeInventoryActionPacket packet, Player player) {
         if (!player.isCreative()) return;
-        short slot = packet.slot();
+        int slot = PlayerInventoryUtils.protocolToMinestom(packet.slot());
         final ItemStack item = packet.item();
         if (slot == -1) {
             player.dropItem(item);
