0b66dd61db74d6b48eff4084c46e6af204e4ba91
cosrnic <75631523+cosrnic@users.noreply.github.com>
2024-02-24 00:45:54 +0000

    Change offset to withOffset

--- src/main/java/net/minestom/server/collision/BoundingBox.java
+++ src/main/java/net/minestom/server/collision/BoundingBox.java
@@ -131,7 +131,7 @@ public final class BoundingBox implements Shape {
      * @param offset the offset
      * @return a new bounding box with an offset.
      */
-    public @NotNull BoundingBox offset(Point offset) {
+    public @NotNull BoundingBox wtihOffset(Point offset) {
         return new BoundingBox(this.width, this.height, this.depth, offset);
     }
 
