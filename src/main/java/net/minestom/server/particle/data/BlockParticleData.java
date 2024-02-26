e63a91cca4c475fdf1f8bc99a695b4234bb0d5f9
iam4722202468 <aceparent@gmail.com>
2024-02-26 01:28:50 -0500

    RGBLike

--- src/main/java/net/minestom/server/particle/data/BlockParticleData.java
+++ src/main/java/net/minestom/server/particle/data/BlockParticleData.java
@@ -2,11 +2,25 @@ package net.minestom.server.particle.data;
 
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.network.NetworkBuffer;
+import net.minestom.server.particle.Particle;
 import org.jetbrains.annotations.NotNull;
 
-public record BlockParticleData (Block block) implements ParticleData {
+public record BlockParticleData(Block block) implements ParticleData {
+    BlockParticleData(NetworkBuffer reader) {
+        this(Block.fromStateId(reader.read(NetworkBuffer.VAR_INT).shortValue()));
+    }
+
+    BlockParticleData() {
+        this(Block.STONE);
+    }
+
     @Override
     public void write(@NotNull NetworkBuffer writer) {
         writer.write(NetworkBuffer.VAR_INT, (int) block.stateId());
+    }
+
+    @Override
+    public boolean validate(int particleId) {
+        return particleId == Particle.BLOCK.id();
     }
 }
