009ba773edeb672c1eee1ba06f30dcbb51bf1f3a
GoldenStack <goldenfire64yt@gmail.com>
2024-02-27 07:35:30 -0600

    Fix incorrect slot updating (#2007)
    * Fix slot sending
    
    * Test fixed slot sending
    
    * Also send offhand slots

--- src/main/java/net/minestom/server/inventory/PlayerInventory.java
+++ src/main/java/net/minestom/server/inventory/PlayerInventory.java
@@ -166,7 +166,12 @@ public non-sealed class PlayerInventory extends AbstractInventory implements Equ
      * @param itemStack the item stack in the slot
      */
     protected void sendSlotRefresh(short slot, ItemStack itemStack) {
-        this.player.sendPacket(new SetSlotPacket((byte) 0, 0, slot, itemStack));
+        var openInventory = player.getOpenInventory();
+        if (openInventory != null && slot >= OFFSET && slot < OFFSET + INNER_INVENTORY_SIZE) {
+            this.player.sendPacket(new SetSlotPacket(openInventory.getWindowId(), 0, (short) (slot + openInventory.getSize() - OFFSET), itemStack));
+        } else if (openInventory == null || slot == OFFHAND_SLOT) {
+            this.player.sendPacket(new SetSlotPacket((byte) 0, 0, slot, itemStack));
+        }
     }
 
     /**
