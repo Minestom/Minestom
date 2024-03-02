cd13705747f9d33ac458a1da2ba76b2890ee2142
iam4722202468 <aceparent@gmail.com>
2024-03-02 16:06:16 -0500

    Add intersect entity to shape

--- src/main/java/net/minestom/server/collision/Shape.java
+++ src/main/java/net/minestom/server/collision/Shape.java
@@ -1,6 +1,7 @@
 package net.minestom.server.collision;
 
 import net.minestom.server.coordinate.Point;
+import net.minestom.server.entity.Entity;
 import net.minestom.server.instance.block.BlockFace;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
@@ -38,6 +39,18 @@ public interface Shape {
      */
     boolean intersectBoxSwept(@NotNull Point rayStart, @NotNull Point rayDirection,
                               @NotNull Point shapePos, @NotNull BoundingBox moving, @NotNull SweepResult finalResult);
+
+
+    /**
+     * Used to know if this {@link BoundingBox} intersects with the bounding box of an entity.
+     *
+     * @param entity the entity to check the bounding box
+     * @return true if this bounding box intersects with the entity, false otherwise
+     */
+    @ApiStatus.Experimental
+    default boolean intersectEntity(@NotNull Point src, @NotNull Entity entity) {
+        return intersectBox(src.sub(entity.getPosition()), entity.getBoundingBox());
+    }
 
     /**
      * Relative Start
