40ebd2b67f6ffab997ce4c1b615b2e3fccf7d00d
Zax71 <67716263+zax71@users.noreply.github.com>
2024-03-18 03:25:35 +0000

    Add `getUUID()` and `getUsername()` to MojangUtils (#2024)
    * Add MojangUtils#getUUID() and MojangUtils#getUsername()
    
    * Remove invalid UUID test as Java's UUID will not allow an invalid UUID
    
    * Add `@Blocking` and some JavaDoc comments
    
    * Override `MojangUtils#fromUuid(String)` with `MojangUtils#fromUuid(UUID)` and add a test
    
    * Switch to IOException over custom ones

--- src/test/java/net/minestom/server/utils/TestMojangUtils.java
+++ src/test/java/net/minestom/server/utils/TestMojangUtils.java
@@ -3,9 +3,13 @@ package net.minestom.server.utils;
 import net.minestom.server.utils.mojang.MojangUtils;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestMojangUtils {
+    private final UUID JEB_UUID = UUID.fromString("853c80ef-3c37-49fd-aa49-938b674adae6");
     @Test
     public void testValidNameWorks() {
         var result = MojangUtils.fromUsername("jeb_");
@@ -21,7 +25,7 @@ public class TestMojangUtils {
 
     @Test
     public void testValidUuidWorks() {
-        var result = MojangUtils.fromUuid("853c80ef3c3749fdaa49938b674adae6");
+        var result = MojangUtils.fromUuid(JEB_UUID.toString());
         assertNotNull(result);
         assertEquals("jeb_", result.get("name").getAsString());
         assertEquals("853c80ef3c3749fdaa49938b674adae6", result.get("id").getAsString());
@@ -37,5 +41,28 @@ public class TestMojangUtils {
     public void testNonExistentUuidReturnsNull() {
         var result = MojangUtils.fromUuid("00000000-0000-0000-0000-000000000000");
         assertNull(result);
+    }
+
+    @Test
+    public void testValidUUIDWorks() {
+        var result = MojangUtils.fromUuid(JEB_UUID);
+        assertNotNull(result);
+        assertEquals("jeb_", result.get("name").getAsString());
+        assertEquals("853c80ef3c3749fdaa49938b674adae6", result.get("id").getAsString());
+    }
+
+    @Test
+    public void testGetValidNameWorks() throws IOException {
+        assertEquals(JEB_UUID, MojangUtils.getUUID("jeb_"));
+    }
+
+    @Test
+    public void testGetValidUUIDWorks() throws IOException {
+        assertEquals("jeb_", MojangUtils.getUsername(JEB_UUID));
+    }
+
+    @Test
+    public void testGetInvalidNameThrows() {
+        assertThrows(IOException.class, () -> MojangUtils.getUUID("a")); // Too short
     }
 }
