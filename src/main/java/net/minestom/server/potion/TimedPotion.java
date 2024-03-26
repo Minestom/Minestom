1f9a84c946569748b31365a6916694b3e2806b1a
DeidaraMC <DeidaraJuice@gmail.com>
2024-03-26 00:14:51 -0400

    chore: remove redudant getter

--- src/main/java/net/minestom/server/potion/TimedPotion.java
+++ src/main/java/net/minestom/server/potion/TimedPotion.java
@@ -1,10 +1,3 @@
 package net.minestom.server.potion;
 
-import org.jetbrains.annotations.NotNull;
-
-public record TimedPotion(Potion potion, long startingTicks) {
-    @Override
-    public @NotNull Potion potion() {
-        return potion;
-    }
-}
+public record TimedPotion(Potion potion, long startingTicks) {}
