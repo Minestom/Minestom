fb60c6e355a639822aaece5e514969030a51afc6
DeidaraMC <DeidaraJuice@gmail.com>
2024-03-25 23:44:24 -0400

    feat: use ticks for potion duration instead of system time

--- src/main/java/net/minestom/server/potion/TimedPotion.java
+++ src/main/java/net/minestom/server/potion/TimedPotion.java
@@ -2,22 +2,9 @@ package net.minestom.server.potion;
 
 import org.jetbrains.annotations.NotNull;
 
-public class TimedPotion {
-
-    private final Potion potion;
-    private final long startingTime;
-
-    public TimedPotion(@NotNull Potion potion, long startingTime) {
-        this.potion = potion;
-        this.startingTime = startingTime;
-    }
-
-    @NotNull
-    public Potion getPotion() {
+public record TimedPotion(Potion potion, long startingTicks) {
+    @Override
+    public @NotNull Potion potion() {
         return potion;
-    }
-
-    public long getStartingTime() {
-        return startingTime;
     }
 }
