877e2bb4f230c1eb8e13bc5f9c38aa20397bc980
GreatWyrm <alecmusante@gmail.com>
2024-04-09 14:14:29 -0700

    chore: Cleanup some deprecations in the project (#2084)

--- src/main/java/net/minestom/server/listener/manager/PacketPlayListenerConsumer.java
+++ src/main/java/net/minestom/server/listener/manager/PacketPlayListenerConsumer.java
@@ -4,7 +4,7 @@ import net.minestom.server.entity.Player;
 import net.minestom.server.network.packet.client.ClientPacket;
 
 /**
- * Small convenient interface to use method references with {@link PacketListenerManager#setListener(Class, PacketPlayListenerConsumer)}.
+ * Small convenient interface to use method references with {@link PacketListenerManager#setPlayListener(Class, PacketPlayListenerConsumer)}.
  *
  * @param <T> the packet type
  */
