79ed2b5274c8ed860510bd80aa0a8cf4f07c4bf2
GoldenStack <goldenfire64yt@gmail.com>
2024-04-28 12:41:56 -0500

    Clean up InventoryCloseEvent and make it cancellable

--- src/main/java/net/minestom/server/event/inventory/InventoryCloseEvent.java
+++ src/main/java/net/minestom/server/event/inventory/InventoryCloseEvent.java
@@ -1,20 +1,20 @@
 package net.minestom.server.event.inventory;
 
 import net.minestom.server.entity.Player;
+import net.minestom.server.event.trait.CancellableEvent;
 import net.minestom.server.event.trait.InventoryEvent;
 import net.minestom.server.event.trait.PlayerInstanceEvent;
 import net.minestom.server.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * Called when an {@link Inventory} is closed by a player.
  */
-public class InventoryCloseEvent implements InventoryEvent, PlayerInstanceEvent {
+public class InventoryCloseEvent implements InventoryEvent, PlayerInstanceEvent, CancellableEvent {
 
     private final Inventory inventory;
     private final Player player;
-    private @Nullable Inventory newInventory;
+    private boolean cancelled;
 
     public InventoryCloseEvent(@NotNull Inventory inventory, @NotNull Player player) {
         this.inventory = inventory;
@@ -31,26 +31,18 @@ public class InventoryCloseEvent implements InventoryEvent, PlayerInstanceEvent 
         return player;
     }
 
-    /**
-     * Gets the new inventory to open.
-     *
-     * @return the new inventory to open, null if there isn't any
-     */
-    public @Nullable Inventory getNewInventory() {
-        return newInventory;
-    }
-
-    /**
-     * Can be used to open a new inventory after closing the previous one.
-     *
-     * @param newInventory the inventory to open, null to do not open any
-     */
-    public void setNewInventory(@Nullable Inventory newInventory) {
-        this.newInventory = newInventory;
-    }
-
     @Override
     public @NotNull Inventory getInventory() {
         return inventory;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
     }
 }
