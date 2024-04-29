d0c7d7350e863b873858b0f07cd99d7136e1a4b5
DeidaraMC <117625071+DeidaraMC@users.noreply.github.com>
2024-04-29 14:20:23 -0400

    fix: don't simulate movement if the chunk isn't loaded (#2112)
    * fix: don't simulate movement if the chunk isn't loaded
    
    * chore: changed wrong thing

--- src/main/java/net/minestom/server/entity/Entity.java
+++ src/main/java/net/minestom/server/entity/Entity.java
@@ -592,8 +592,9 @@ public class Entity implements Viewable, Tickable, Schedulable, Snapshotable, Ev
 
         boolean entityIsPlayer = this instanceof Player;
         boolean entityFlying = entityIsPlayer && ((Player) this).isFlying();
+        final Block.Getter chunkCache = new ChunkCache(instance, currentChunk, Block.STONE);
         PhysicsResult physicsResult = PhysicsUtils.simulateMovement(position, velocity.div(ServerFlag.SERVER_TICKS_PER_SECOND), boundingBox,
-                instance.getWorldBorder(), instance, aerodynamics, hasNoGravity(), hasPhysics, onGround, entityFlying, previousPhysicsResult);
+                instance.getWorldBorder(), chunkCache, aerodynamics, hasNoGravity(), hasPhysics, onGround, entityFlying, previousPhysicsResult);
         this.previousPhysicsResult = physicsResult;
 
         Chunk finalChunk = ChunkUtils.retrieve(instance, currentChunk, physicsResult.newPosition());
