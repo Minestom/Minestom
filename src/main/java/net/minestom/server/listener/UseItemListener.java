1d3feeebd8ee966908765968beaefc80a4b757f8
TogAr2 <59421074+togar2@users.noreply.github.com>
2024-05-04 07:24:42 -0400

    Item usage duration (#2128)
    Fix build error
    
    Move item usage completed to a different event
    
    Add a way to check if the item usage has completed, or is aborted by the player
    
    Only process animation when the item use time is not instant
    
    Update javadoc
    
    Re-add Player#isEating
    
    Remove deprecated Player#callItemUpdateStateEvent
    
    Allow specifying item usage duration inside PlayerUseItemEvent

--- src/main/java/net/minestom/server/listener/UseItemListener.java
+++ src/main/java/net/minestom/server/listener/UseItemListener.java
@@ -23,6 +23,10 @@ public class UseItemListener {
         ItemStack itemStack = hand == Player.Hand.MAIN ? inventory.getItemInMainHand() : inventory.getItemInOffHand();
         //itemStack.onRightClick(player, hand);
         PlayerUseItemEvent useItemEvent = new PlayerUseItemEvent(player, hand, itemStack);
+        ItemStack itemStack = player.getItemInHand(hand);
+        final Material material = itemStack.material();
+
+        PlayerUseItemEvent useItemEvent = new PlayerUseItemEvent(player, hand, itemStack, material.isFood() ? player.getDefaultEatingTime() : 0);
         EventDispatcher.call(useItemEvent);
 
         player.sendPacket(new AcknowledgeBlockChangePacket(packet.sequence()));
@@ -45,9 +49,8 @@ public class UseItemListener {
             }
         }
 
-        PlayerItemAnimationEvent.ItemAnimationType itemAnimationType = null;
-
-        boolean cancelAnimation = false;
+        long itemUseTime = useItemEvent.getItemUseTime();
+        PlayerItemAnimationEvent.ItemAnimationType itemAnimationType;
 
         if (material == Material.BOW) {
             itemAnimationType = PlayerItemAnimationEvent.ItemAnimationType.BOW;
@@ -57,21 +60,29 @@ public class UseItemListener {
             itemAnimationType = PlayerItemAnimationEvent.ItemAnimationType.SHIELD;
         } else if (material == Material.TRIDENT) {
             itemAnimationType = PlayerItemAnimationEvent.ItemAnimationType.TRIDENT;
+        } else if (material == Material.SPYGLASS) {
+            itemAnimationType = PlayerItemAnimationEvent.ItemAnimationType.SPYGLASS;
+        } else if (material == Material.GOAT_HORN) {
+            itemAnimationType = PlayerItemAnimationEvent.ItemAnimationType.HORN;
+        } else if (material == Material.BRUSH) {
+            itemAnimationType = PlayerItemAnimationEvent.ItemAnimationType.BRUSH;
         } else if (itemStack.has(ItemComponent.FOOD) || itemStack.material() == Material.POTION) {
             itemAnimationType = PlayerItemAnimationEvent.ItemAnimationType.EAT;
 
             // Eating code, contains the eating time customisation
             final Food food = itemStack.get(ItemComponent.FOOD);
             int defaultEatingTime = food != null ? food.eatDurationTicks() : PotionContents.POTION_DRINK_TIME;
-            PlayerPreEatEvent playerPreEatEvent = new PlayerPreEatEvent(player, itemStack, hand, defaultEatingTime);
-            EventDispatcher.callCancellable(playerPreEatEvent, () -> player.refreshEating(hand, playerPreEatEvent.getEatingTime()));
-
-            if (playerPreEatEvent.isCancelled()) {
-                cancelAnimation = true;
-            }
+            PlayerPreEatEvent playerPreEatEvent = new PlayerPreEatEvent(player, itemStack, hand, itemUseTime);
+            EventDispatcher.call(playerPreEatEvent);
+            if (playerPreEatEvent.isCancelled()) return;
+            itemUseTime = playerPreEatEvent.getEatingTime();
+        } else {
+            itemAnimationType = PlayerItemAnimationEvent.ItemAnimationType.OTHER;
         }
 
-        if (!cancelAnimation && itemAnimationType != null) {
+        if (itemUseTime > 0) {
+            player.refreshItemUse(hand, itemUseTime);
+
             PlayerItemAnimationEvent playerItemAnimationEvent = new PlayerItemAnimationEvent(player, itemAnimationType, hand);
             EventDispatcher.callCancellable(playerItemAnimationEvent, () -> {
                 player.refreshActiveHand(true, hand == Player.Hand.OFF, false);
@@ -79,5 +90,4 @@ public class UseItemListener {
             });
         }
     }
-
 }
