b86db28637f634b680220d87737417364cfa11f1
mworzala <mattheworzala@gmail.com>
2024-05-23 18:40:53 -0400

    fix: correct handling of known packs exchange

--- src/main/java/net/minestom/server/event/entity/EntityDamageEvent.java
+++ src/main/java/net/minestom/server/event/entity/EntityDamageEvent.java
@@ -3,7 +3,6 @@ package net.minestom.server.event.entity;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.LivingEntity;
 import net.minestom.server.entity.damage.Damage;
-import net.minestom.server.entity.damage.DamageType;
 import net.minestom.server.event.trait.CancellableEvent;
 import net.minestom.server.event.trait.EntityInstanceEvent;
 import net.minestom.server.sound.SoundEvent;
@@ -11,7 +10,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Called with {@link LivingEntity#damage(DamageType, float)}.
+ * Called with {@link LivingEntity#damage(net.minestom.server.registry.DynamicRegistry.Key, float)}.
  */
 public class EntityDamageEvent implements EntityInstanceEvent, CancellableEvent {
 
