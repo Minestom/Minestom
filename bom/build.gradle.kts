4ea40c03dd09ff559b47cfad0cf4545e9d1302f8
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-05-28 15:44:01 +0200

    Change bom deployment
    (cherry picked from commit f41860257d043b992ec1849b3ad6d5eec8b206ad)

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("io.github.gradlebom.generator-plugin") version "1.0.0.Final"
-    id("net.kyori.indra")
-    id("net.kyori.indra.publishing")
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     signing
 }
 
@@ -19,26 +18,53 @@ bomGenerator {
     includeDependency("net.onelitefeather.microtus.testing", "testing", version)
 }
 
-indra {
-    github("OneLiteFeatherNET", "Microtus") {
-        ci(true)
-        publishing(false)
+signing {
+    if (!project.hasProperty("skip.signing") && !version.toString().endsWith("-SNAPSHOT")) {
+        val signingKey: String? by project
+        val signingPassword: String? by project
+        useInMemoryPgpKeys(signingKey, signingPassword)
+        signing.isRequired
+        sign(publishing.publications)
     }
-    mitLicense()
-    signWithKeyFromPrefixedProperties("onelitefeather")
-    description = "Bill of materials for Microtus projects."
-    configurePublications {
-        pom {
-            developers {
-                developer {
-                    id.set("themeinerlp")
-                    name.set("Phillipp Glanz")
-                    email.set("p.glanz@madfix.me")
+}
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+
+            pom {
+                name.set(project.name)
+                description.set("Bill of materials for Microtus projects.")
+                url.set("https://github.com/OneLiteFeatherNET/microtus")
+
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                        distribution.set("repo")
+                    }
                 }
-                developer {
-                    id.set("theEvilReaper")
-                    name.set("Steffen Wonning")
-                    email.set("steffenwx@gmail.com")
+                developers {
+                    developer {
+                        id.set("themeinerlp")
+                        name.set("Phillipp Glanz")
+                        email.set("p.glanz@madfix.me")
+                    }
+                    developer {
+                        id.set("theEvilReaper")
+                        name.set("Steffen Wonning")
+                        email.set("steffenwx@gmail.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/OneLiteFeatherNET/microtus")
+                    connection.set("scm:git:https://github.com/OneLiteFeatherNET/microtus.git")
+                    developerConnection.set("scm:git:git@github.com:OneLiteFeatherNET/microtus.git")
+                    tag.set("${project.version}")
+                }
+
+                issueManagement{
+                    system.set("GitHub")
+                    url.set("https://github.com/OneLiteFeatherNET/microtus/issues")
                 }
             }
         }
