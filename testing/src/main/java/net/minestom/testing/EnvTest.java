40cb007cab9a1d76236eace868a3e0e5a9820735
TheMeinerLP <p.glanz@madfix.me>
2024-06-16 16:45:51 +0200

    Deprecated old testing and introduce into new method

--- testing/src/main/java/net/minestom/testing/EnvTest.java
+++ testing/src/main/java/net/minestom/testing/EnvTest.java
@@ -1,6 +1,7 @@
 package net.minestom.testing;
 
 import net.minestom.server.MinecraftServer;
+import org.jetbrains.annotations.ApiStatus;
 import org.junit.jupiter.api.extension.*;
 import org.junit.jupiter.api.extension.support.TypeBasedParameterResolver;
 
@@ -11,14 +12,17 @@ import java.lang.annotation.Target;
 import java.lang.reflect.Method;
 
 /**
- * @deprecated As of Microtus 1.4.2, because this version don't support Non Env tests and env tests at the same time use {@link net.minestom.testing.annotations.EnvironmentTest} instead.
+ * @deprecated As of Microtus 1.4.2, because this version doesn't support Non Env tests and env tests at the same time.
+ * Use {@link net.minestom.testing.extension.MicrotusExtension MicrotusExtension} instead of the {@code @EnvTest} annotation:
+ * {@code @ExtendWith(MicrotusExtension.class)}
  */
 @ExtendWith(EnvTest.EnvParameterResolver.class)
 @ExtendWith(EnvTest.EnvBefore.class)
 @ExtendWith(EnvTest.EnvCleaner.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Deprecated(since = "1.4.2", forRemoval = true)
+@Deprecated(forRemoval = true, since = "1.4.2")
+@ApiStatus.ScheduledForRemoval(inVersion = "1.6.0")
 public @interface EnvTest {
 
     final class EnvBefore implements BeforeEachCallback {
--- testing/src/main/java/net/minestom/testing/environment/TestEnvironmentParameterResolver.java
+++ testing/src/main/java/net/minestom/testing/environment/TestEnvironmentParameterResolver.java
@@ -2,6 +2,7 @@ package net.minestom.testing.environment;
 
 import net.minestom.server.MinecraftServer;
 import net.minestom.testing.Env;
+import org.jetbrains.annotations.ApiStatus;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
@@ -11,6 +12,8 @@ import org.junit.jupiter.api.extension.support.TypeBasedParameterResolver;
  * Handles {@link Env} parameter for JUnit Tests to inject the TestEnvironment
  * @since 1.4.1
  */
+@Deprecated(forRemoval = true, since = "1.5.0")
+@ApiStatus.ScheduledForRemoval(inVersion = "1.6.0")
 public final class TestEnvironmentParameterResolver extends TypeBasedParameterResolver<Env>  {
     @Override
     public Env resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
