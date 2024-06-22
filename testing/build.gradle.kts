fd340c7f873b47b1b74794064887416d61e37c7d
HttpMarco <mirco.lindenau@gmx.de>
2024-06-22 10:01:46 +0200

    Remove dead code

--- testing/build.gradle.kts
+++ testing/build.gradle.kts
@@ -1,10 +1,8 @@
 plugins {
     `java-library`
-//    `maven-publish`
 }
 
 group = "net.minestom.testing"
-// version declared by root project
 
 dependencies {
     api(rootProject)
@@ -15,15 +13,3 @@ dependencies {
     runtimeOnly(libs.junit.engine)
     runtimeOnly(libs.junit.suite.engine)
 }
-
-//publishing {
-//    publications {
-//        create<MavenPublication>("maven") {
-//            groupId = "net.minestom.testing"
-//            artifactId = "testing"
-//            version = "1.0"
-//
-//            from(components["java"])
-//        }
-//    }
-//}
