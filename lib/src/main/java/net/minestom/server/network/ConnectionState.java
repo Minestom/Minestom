f28cb9104f66b76e9bd949ded484d9c1e8f0e27f
themode <themode@outlook.fr>
2024-06-24 17:28:45 +0200

    Move ConnectionState/Unit

---
+++ lib/src/main/java/net/minestom/server/network/ConnectionState.java
@@ -0,0 +1,27 @@
+package net.minestom.server.network;
+
+/**
+ * Represents the connection state of a client.
+ */
+public enum ConnectionState {
+    /**
+     * Default state before any packet is received.
+     */
+    HANDSHAKE,
+    /**
+     * Client declares `Status` intent during handshake.
+     */
+    STATUS,
+    /**
+     * Client declares `Login` intent during handshake.
+     */
+    LOGIN,
+    /**
+     * Client acknowledged login and is now configuring the game.
+     */
+    CONFIGURATION,
+    /**
+     * Client finished configuration.
+     */
+    PLAY
+}
