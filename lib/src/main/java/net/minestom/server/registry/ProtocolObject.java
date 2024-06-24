471eee7c1c3dd70345b09b6fb349642c527c5c40
themode <themode@outlook.fr>
2024-06-24 15:58:49 +0200

    Move StaticProtocolObject

--- lib/src/main/java/net/minestom/server/registry/ProtocolObject.java
+++ lib/src/main/java/net/minestom/server/registry/ProtocolObject.java
@@ -3,7 +3,6 @@ package net.minestom.server.registry;
 import org.jetbrains.annotations.Nullable;
 
 public interface ProtocolObject {
-
     default @Nullable Object registry() {
         return null;
     }
