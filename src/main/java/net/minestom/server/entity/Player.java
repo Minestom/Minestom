0129aedae3e1b8c079734f06700515fb811fc09a
Steank <dnd.initiative.tracker@gmail.com>
2024-07-04 13:11:30 -0700

    feat: Acquirable, ThreadDispatcher usage improvements (#2237)
    * feat: fewer casts needed when working with Acquirable, ThreadDispatcher now usable with non-Entity tickables, AcquirableSource for synchronizing items added to dispatcher
    
    * chore: rename AcquirableSource#getAcquirable -> AcquirableSource#acquirable, re-add getAcquirable to Entity with old signature, deprecate it

--- src/main/java/net/minestom/server/entity/Player.java
+++ src/main/java/net/minestom/server/entity/Player.java
@@ -82,6 +82,7 @@ import net.minestom.server.snapshot.PlayerSnapshot;
 import net.minestom.server.snapshot.SnapshotImpl;
 import net.minestom.server.snapshot.SnapshotUpdater;
 import net.minestom.server.statistic.PlayerStatistic;
+import net.minestom.server.thread.Acquirable;
 import net.minestom.server.timer.Scheduler;
 import net.minestom.server.utils.MathUtils;
 import net.minestom.server.utils.PacketUtils;
@@ -2566,4 +2567,10 @@ public class Player extends LivingEntity implements CommandSender, Localizable, 
         return Integer.compare(chunkDistanceA, chunkDistanceB);
     }
 
+    @SuppressWarnings("unchecked")
+    @ApiStatus.Experimental
+    @Override
+    public @NotNull Acquirable<? extends Player> acquirable() {
+        return (Acquirable<? extends Player>) super.acquirable();
+    }
 }
