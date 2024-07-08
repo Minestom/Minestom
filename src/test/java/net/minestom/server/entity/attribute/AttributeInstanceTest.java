ec5909662f1f7502779b536ef78a147f90cb3b8d
mworzala <mattheworzala@gmail.com>
2024-07-08 11:44:26 -0400

    fix: use equals check for modifier difference when adding

---
+++ src/test/java/net/minestom/server/entity/attribute/AttributeInstanceTest.java
@@ -0,0 +1,36 @@
+package net.minestom.server.entity.attribute;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AttributeInstanceTest {
+
+    @Test
+    void testReplaceAttributeSameValue() {
+        var attribute = new AttributeInstance(Attribute.GENERIC_SAFE_FALL_DISTANCE, null);
+        var modifier = new AttributeModifier("test", 1.0, AttributeOperation.ADD_VALUE);
+
+        attribute.addModifier(modifier);
+        assertEquals(4, attribute.getValue());
+
+        attribute.addModifier(modifier);
+        assertEquals(4, attribute.getValue());
+    }
+
+    @Test
+    void testReplaceAttributeNewValue() {
+        var attribute = new AttributeInstance(Attribute.GENERIC_SAFE_FALL_DISTANCE, null);
+
+        attribute.addModifier(new AttributeModifier("test", 1.0, AttributeOperation.ADD_VALUE));
+        assertEquals(4, attribute.getValue());
+
+        attribute.addModifier(new AttributeModifier("test", 2.0, AttributeOperation.ADD_VALUE));
+        assertEquals(5, attribute.getValue()); // New value
+
+        attribute.addModifier(new AttributeModifier("test", 2.0, AttributeOperation.MULTIPLY_BASE));
+        assertEquals(9, attribute.getValue()); // New operation
+
+    }
+
+}
