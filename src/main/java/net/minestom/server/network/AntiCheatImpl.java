c0ac84b7262a5dcc32645aba858e553b53c8b29f
themode <themode@outlook.fr>
2024-07-12 01:35:45 +0200

    add player/change types

--- src/main/java/net/minestom/server/network/AntiCheatImpl.java
+++ src/main/java/net/minestom/server/network/AntiCheatImpl.java
@@ -10,33 +10,36 @@ import net.minestom.server.network.packet.server.play.ChangeGameStatePacket;
 import net.minestom.server.network.packet.server.play.JoinGamePacket;
 import net.minestom.server.network.packet.server.play.RespawnPacket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 final class AntiCheatImpl implements AntiCheat {
     private static final Action VALID = new Action.Valid();
     private boolean joined = false;
 
-    // TODO: should use a circular buffer to keep previous game modes to account for latency.
-    private GameMode gameMode;
-
-    private int estimedLatency = 300;
+    private int estimatedLatency = 300;
     private long lastKeepAlive = -1;
+
+    private Player player = new Player(null);
+    private final List<Change> changes = new ArrayList<>();
 
     @Override
     public void consume(ServerPacket.Play serverPacket) {
         switch (serverPacket) {
             case JoinGamePacket joinGamePacket -> {
                 this.joined = true;
-                this.gameMode = joinGamePacket.gameMode();
+                addChange(new Change.SetGameMode(joinGamePacket.gameMode()));
             }
             case KeepAlivePacket keepAlivePacket -> {
                 this.lastKeepAlive = System.currentTimeMillis();
             }
             case RespawnPacket respawnPacket -> {
-                this.gameMode = respawnPacket.gameMode();
+                addChange(new Change.SetGameMode(respawnPacket.gameMode()));
             }
             case ChangeGameStatePacket changeGameStatePacket -> {
                 switch (changeGameStatePacket.reason()) {
                     case CHANGE_GAMEMODE -> {
-                        this.gameMode = GameMode.fromId((int) changeGameStatePacket.value());
+                        addChange(new Change.SetGameMode(GameMode.fromId((int) changeGameStatePacket.value())));
                     }
                     default -> throw new IllegalStateException("Unexpected value: " + changeGameStatePacket);
                 }
@@ -53,18 +56,47 @@ final class AntiCheatImpl implements AntiCheat {
 
         return switch (clientPacket) {
             case ClientCreativeInventoryActionPacket packet -> {
-                if (gameMode != GameMode.CREATIVE)
+                if (gameMode() != GameMode.CREATIVE)
                     yield new Action.InvalidCritical("Creative inventory action packet received in non-creative game mode.");
                 yield VALID;
             }
             case ClientKeepAlivePacket packet -> {
                 final long lastKeepAlive = this.lastKeepAlive;
                 if (lastKeepAlive == -1) yield new Action.InvalidIgnore("Stray keep alive packet received.");
-                this.estimedLatency = (int) (System.currentTimeMillis() - lastKeepAlive);
+                this.estimatedLatency = (int) (System.currentTimeMillis() - lastKeepAlive);
                 this.lastKeepAlive = -1;
                 yield VALID;
             }
             default -> VALID;
         };
+    }
+
+    private void addChange(Change change) {
+        this.changes.add(change);
+        // TODO: delay
+        this.player = this.player.merge(this.changes);
+        this.changes.clear();
+    }
+
+    GameMode gameMode() {
+        // TODO: account for latency, loop over changes if not empty
+        return player.gameMode;
+    }
+
+    record Player(GameMode gameMode) {
+        Player merge(List<Change> changes) {
+            Player result = this;
+            for (Change change : changes) {
+                switch (change) {
+                    case Change.SetGameMode setGameMode -> result = new Player(setGameMode.gameMode());
+                }
+            }
+            return result;
+        }
+    }
+
+    sealed interface Change {
+        record SetGameMode(GameMode gameMode) implements Change {
+        }
     }
 }
