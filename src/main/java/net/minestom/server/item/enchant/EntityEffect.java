ae95715e57f23d50b74efb1985450949f29b3e6d
TogAr2 <59421074+TogAr2@users.noreply.github.com>
2024-07-17 03:25:15 +0200

    Add EntityEffect.Explode including serialization

--- src/main/java/net/minestom/server/item/enchant/EntityEffect.java
+++ src/main/java/net/minestom/server/item/enchant/EntityEffect.java
@@ -5,14 +5,20 @@ import net.minestom.server.codec.StructCodec;
 import net.minestom.server.entity.damage.DamageType;
 import net.minestom.server.gamedata.DataPack;
 import net.minestom.server.gamedata.tags.Tag;
+import net.minestom.server.instance.block.Block;
+import net.minestom.server.particle.Particle;
 import net.minestom.server.potion.PotionEffect;
 import net.minestom.server.registry.DynamicRegistry;
 import net.minestom.server.registry.ObjectSet;
 import net.minestom.server.registry.Registries;
+import net.minestom.server.sound.SoundEvent;
+import net.minestom.server.utils.validate.Check;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Objects;
 
 
 public non-sealed interface EntityEffect extends Enchantment.Effect {
@@ -107,12 +113,104 @@ public non-sealed interface EntityEffect extends Enchantment.Effect {
         }
     }
 
-    record Explode(/* todo */) implements EntityEffect, LocationEffect {
-        public static final StructCodec<Explode> CODEC = StructCodec.struct(Explode::new);
+    record Explode(
+            boolean attributeToUser,
+            @Nullable DynamicRegistry.Key<DamageType> damageType,
+            @Nullable ObjectSet<Block> immuneBlocks,
+            @Nullable LevelBasedValue knockbackMultiplier,
+            @Nullable Point offset,
+            @NotNull LevelBasedValue radius,
+            boolean createFire,
+            @NotNull BlockInteraction blockInteraction,
+            @NotNull Particle smallParticle,
+            @NotNull Particle largeParticle,
+            @NotNull SoundEvent sound
+    ) implements EntityEffect, LocationEffect {
+        private static final BinaryTagSerializer<ObjectSet<Block>> IMMUNE_BLOCKS_NBT_TYPE = ObjectSet.nbtType(Tag.BasicType.BLOCKS);
+        public static final BinaryTagSerializer<Explode> NBT_TYPE = new BinaryTagSerializer<>() {
+            @Override
+            public @NotNull BinaryTag write(@NotNull Context context, @NotNull Explode value) {
+                CompoundBinaryTag.Builder builder = CompoundBinaryTag.builder();
+
+                builder.putBoolean("attribute_to_user", value.attributeToUser);
+                if (value.damageType != null)
+                    builder.put("damage_type", DamageType.NBT_TYPE.write(context, value.damageType));
+                if (value.immuneBlocks != null)
+                    builder.put("immune_blocks", IMMUNE_BLOCKS_NBT_TYPE.write(context, value.immuneBlocks));
+                if (value.knockbackMultiplier != null)
+                    builder.put("knockback_multiplier", LevelBasedValue.NBT_TYPE.write(context, value.knockbackMultiplier));
+                if (value.offset != null)
+                    builder.put("offset", BinaryTagSerializer.BLOCK_POSITION.write(context, value.offset));
+                builder.put("radius", LevelBasedValue.NBT_TYPE.write(context, value.radius));
+                builder.putBoolean("create_fire", value.createFire);
+                builder.putString("block_interaction", value.blockInteraction.id());
+                builder.put("small_particle", Particle.NBT_TYPE.write(context, value.smallParticle));
+                builder.put("large_particle", Particle.NBT_TYPE.write(context, value.largeParticle));
+                builder.putString("sound", value.sound.namespace().asString());
+
+                return builder.build();
+            }
+
+            @Override
+            public @NotNull Explode read(@NotNull Context context, @NotNull BinaryTag tag) {
+                final CompoundBinaryTag compound = (CompoundBinaryTag) tag;
+
+                boolean attributeToUser = compound.getBoolean("attribute_to_user");
+                BinaryTag damageTypeTag = compound.get("damage_type");
+                DynamicRegistry.Key<DamageType> damageType = damageTypeTag == null ? null : DamageType.NBT_TYPE.read(damageTypeTag);
+                BinaryTag immuneBlocksTag = compound.get("immune_blocks");
+                ObjectSet<Block> immuneBlocks = immuneBlocksTag == null ? null : IMMUNE_BLOCKS_NBT_TYPE.read(immuneBlocksTag);
+                BinaryTag knockbackMultiplierTag = compound.get("knockback_multiplier");
+                LevelBasedValue knockbackMultiplier = knockbackMultiplierTag == null ? null : LevelBasedValue.NBT_TYPE.read(knockbackMultiplierTag);
+                BinaryTag offsetTag = compound.get("offset");
+                Point offset = offsetTag == null ? null : BinaryTagSerializer.BLOCK_POSITION.read(offsetTag);
+                LevelBasedValue radius = LevelBasedValue.NBT_TYPE.read(Objects.requireNonNull(compound.get("radius")));
+                boolean createFire = compound.getBoolean("create_fire");
+                BlockInteraction blockInteraction = BlockInteraction.fromId(compound.getString("block_interaction"));
+                Particle smallParticle = Particle.NBT_TYPE.read(Objects.requireNonNull(compound.get("small_particle")));
+                Particle largeParticle = Particle.NBT_TYPE.read(Objects.requireNonNull(compound.get("large_particle")));
+                SoundEvent sound = SoundEvent.fromNamespaceId(Objects.requireNonNull(compound.getString("sound")));
+                Check.notNull(sound, "Cannot find sound event");
+
+                return new Explode(
+                        attributeToUser, damageType, immuneBlocks,
+                        knockbackMultiplier, offset, radius, createFire,
+                        blockInteraction, smallParticle, largeParticle, sound
+                );
+            }
+        };
 
         @Override
         public @NotNull StructCodec<Explode> codec() {
             return CODEC;
+        }
+
+        public enum BlockInteraction {
+            NONE("none"),
+            BLOCK("block"),
+            MOB("mob"),
+            TNT("tnt"),
+            TRIGGER("trigger");
+
+            private final String id;
+
+            BlockInteraction(String id) {
+                this.id = id;
+            }
+
+            public String id() {
+                return id;
+            }
+
+            public static @NotNull BlockInteraction fromId(String id) {
+                for (BlockInteraction blockInteraction : values()) {
+                    if (blockInteraction.id.equals(id)) {
+                        return blockInteraction;
+                    }
+                }
+
+                return NONE;
+            }
         }
     }
 
