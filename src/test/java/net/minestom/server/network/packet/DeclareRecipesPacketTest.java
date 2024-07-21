a7a79e331272d5361b31d0b744946bd387e2a8a2
TheMode <themode@outlook.fr>
2024-07-21 18:49:19 +0200

    Recipe rework (#2285)
    * Recipe rework
    
    * Remove unnecessary lists
    
    * Add remaining recipes
    
    * Fix `dataSerializer`
    
    * Fix DecoratedPot naming

--- src/test/java/net/minestom/server/network/packet/DeclareRecipesPacketTest.java
+++ src/test/java/net/minestom/server/network/packet/DeclareRecipesPacketTest.java
@@ -4,6 +4,7 @@ import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.packet.server.play.DeclareRecipesPacket;
+import net.minestom.server.recipe.Recipe;
 import net.minestom.server.recipe.RecipeCategory;
 import org.junit.jupiter.api.Test;
 
@@ -16,10 +17,11 @@ public class DeclareRecipesPacketTest {
     @Test
     public void cannotWriteAirIngredient() {
         var packet = new DeclareRecipesPacket(List.of(
-                new DeclareRecipesPacket.DeclaredShapelessCraftingRecipe(
-                        "recipe1", "group1", RecipeCategory.Crafting.BUILDING,
-                        List.of(new DeclareRecipesPacket.Ingredient(List.of(ItemStack.AIR))),
-                        ItemStack.of(Material.DIAMOND)
+                new Recipe(
+                        "recipe1",
+                        new Recipe.Shapeless("group1", RecipeCategory.Crafting.BUILDING,
+                                List.of(new Recipe.Ingredient(List.of(ItemStack.AIR))),
+                                ItemStack.of(Material.DIAMOND))
                 )
         ));
 
