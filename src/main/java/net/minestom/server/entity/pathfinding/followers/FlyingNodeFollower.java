9c8593d79cea0db342a1f474e526295cf1be0561
AmGone_ <hygon806@gmail.com>
2024-07-24 00:51:18 +0200

    fix: node followers using the speed attribute base value instead of value + modifiers

--- src/main/java/net/minestom/server/entity/pathfinding/followers/FlyingNodeFollower.java
+++ src/main/java/net/minestom/server/entity/pathfinding/followers/FlyingNodeFollower.java
@@ -70,7 +70,7 @@ public class FlyingNodeFollower implements NodeFollower {
     @Override
     public double movementSpeed() {
         if (entity instanceof LivingEntity living) {
-            return living.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getBaseValue();
+            return living.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getValue();
         }
 
         return 0.1f;
