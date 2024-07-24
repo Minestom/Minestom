5eac2cb28d8e02c7998880e50740fff0d09af7ad
TheMode <themode@outlook.fr>
2024-07-24 21:49:52 +0200

    Remove BinaryReader/Writer (#2291)
    * Remove BinaryReader/Writer
    
    * Fix UpdateEnabledFeaturesPacket

--- src/main/java/net/minestom/server/command/builder/arguments/number/ArgumentDouble.java
+++ src/main/java/net/minestom/server/command/builder/arguments/number/ArgumentDouble.java
@@ -1,11 +1,12 @@
 package net.minestom.server.command.builder.arguments.number;
 
-import net.minestom.server.utils.binary.BinaryWriter;
+import net.minestom.server.network.NetworkBuffer;
 
 public class ArgumentDouble extends ArgumentNumber<Double> {
 
     public ArgumentDouble(String id) {
-        super(id, "brigadier:double", Double::parseDouble, ((s, radix) -> (double) Long.parseLong(s, radix)), BinaryWriter::writeDouble, Double::compare);
+        super(id, "brigadier:double", Double::parseDouble, ((s, radix) -> (double) Long.parseLong(s, radix)),
+                NetworkBuffer.DOUBLE, Double::compare);
     }
 
     @Override
