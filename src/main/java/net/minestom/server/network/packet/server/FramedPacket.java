d82a539fe71e9d2a02f40dde3f4a6fe2cfa749bc
themode <themode@outlook.fr>
2024-08-17 23:51:59 +0200

    Fix doc

--- src/main/java/net/minestom/server/network/packet/server/FramedPacket.java
+++ src/main/java/net/minestom/server/network/packet/server/FramedPacket.java
@@ -4,13 +4,9 @@ import net.minestom.server.network.NetworkBuffer;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-import java.nio.ByteBuffer;
-
 /**
  * Represents a packet which is already framed. (packet id+payload) + optional compression
  * Can be used if you want to send the exact same buffer to multiple clients without processing it more than once.
- * <p>
- * The {@link ByteBuffer} will ultimately become a MemorySegment once out of incubation.
  */
 @ApiStatus.Internal
 public record FramedPacket(@NotNull ServerPacket packet,
