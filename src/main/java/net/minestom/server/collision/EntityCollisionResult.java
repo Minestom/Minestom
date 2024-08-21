6183453663b664022350106cc4059561fb8d48c5
iam4722202468 <aceparent@gmail.com>
2024-08-21 19:47:09 -0400

    Return directional information from entity collision

--- src/main/java/net/minestom/server/collision/EntityCollisionResult.java
+++ src/main/java/net/minestom/server/collision/EntityCollisionResult.java
@@ -1,6 +1,7 @@
 package net.minestom.server.collision;
 
 import net.minestom.server.coordinate.Point;
+import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.instance.block.BlockFace;
 import org.jetbrains.annotations.NotNull;
@@ -10,12 +11,12 @@ import org.jetbrains.annotations.Nullable;
  * Represents the result of a collision with an entity
  * @param collisionPoint
  * @param entity
- * @param face null if the collision is not with a face
+ * @param direction the direction of the collision. ex. Vec(-1, 0, 0) means the entity collided with the west face of the entity
  */
 public record EntityCollisionResult(
         @NotNull Point collisionPoint,
         @NotNull Entity entity,
-        @Nullable BlockFace face,
+        @NotNull Vec direction,
         double percentage
 ) implements Comparable<EntityCollisionResult> {
     @Override
