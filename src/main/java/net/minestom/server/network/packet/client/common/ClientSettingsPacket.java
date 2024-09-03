19851de63e9ce5b3f1e365f0ed591890cd0f6b45
TheMode <themode@outlook.fr>
2024-09-04 01:39:40 +0200

    PlayerSettings record (#2365)

--- src/main/java/net/minestom/server/network/packet/client/common/ClientSettingsPacket.java
+++ src/main/java/net/minestom/server/network/packet/client/common/ClientSettingsPacket.java
@@ -1,31 +1,13 @@
 package net.minestom.server.network.packet.client.common;
 
-import net.minestom.server.entity.Player;
-import net.minestom.server.message.ChatMessageType;
 import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.NetworkBufferTemplate;
 import net.minestom.server.network.packet.client.ClientPacket;
+import net.minestom.server.network.player.ClientSettings;
 import org.jetbrains.annotations.NotNull;
 
-import static net.minestom.server.network.NetworkBuffer.*;
-
-public record ClientSettingsPacket(@NotNull String locale, byte viewDistance,
-                                   @NotNull ChatMessageType chatMessageType, boolean chatColors,
-                                   byte displayedSkinParts, @NotNull Player.MainHand mainHand,
-                                   boolean enableTextFiltering, boolean allowsListing) implements ClientPacket {
+public record ClientSettingsPacket(@NotNull ClientSettings settings) implements ClientPacket {
     public static final NetworkBuffer.Type<ClientSettingsPacket> SERIALIZER = NetworkBufferTemplate.template(
-            STRING, ClientSettingsPacket::locale,
-            BYTE, ClientSettingsPacket::viewDistance,
-            Enum(ChatMessageType.class), ClientSettingsPacket::chatMessageType,
-            BOOLEAN, ClientSettingsPacket::chatColors,
-            BYTE, ClientSettingsPacket::displayedSkinParts,
-            Enum(Player.MainHand.class), ClientSettingsPacket::mainHand,
-            BOOLEAN, ClientSettingsPacket::enableTextFiltering,
-            BOOLEAN, ClientSettingsPacket::allowsListing,
+            ClientSettings.NETWORK_TYPE, ClientSettingsPacket::settings,
             ClientSettingsPacket::new);
-
-    public ClientSettingsPacket {
-        if (locale.length() > 128)
-            throw new IllegalArgumentException("Locale cannot be longer than 128 characters.");
-    }
 }
