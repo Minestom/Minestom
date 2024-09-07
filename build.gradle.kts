13cc65798faa67ae3697cf9dae0f627614db8b03
themode <themode@outlook.fr>
2024-09-07 09:18:14 +0200

    Remove Caffeine lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,6 @@ dependencies {
     implementation(libs.minestomData)
 
     // Performance/data structures
-    implementation(libs.caffeine)
     api(libs.fastutil)
     implementation(libs.bundles.flare)
     api(libs.gson)
@@ -116,7 +115,7 @@ tasks {
         replaceToken("\"&ARTIFACT\"", if (artifact == null) "null" else "\"${artifact}\"", gitFile)
     }
 
-    nexusPublishing{
+    nexusPublishing {
         useStaging.set(true)
         this.packageGroup.set("net.minestom")
 
