0e756c3d3025a40eb90fd690fe7cfbda5beeb66d
themode <themode@outlook.fr>
2024-09-08 13:31:25 +0200

    basics

--- src/main/java/net/minestom/server/instance/painter/Painter.java
+++ src/main/java/net/minestom/server/instance/painter/Painter.java
@@ -1,13 +1,16 @@
 package net.minestom.server.instance.painter;
 
 import net.minestom.server.instance.block.Block;
+import net.minestom.server.instance.palette.Palette;
 
 import java.util.function.Consumer;
 
 public interface Painter {
     static Painter paint(Consumer<World> consumer) {
-        return new PainterImpl(consumer);
+        return PainterImpl.paint(consumer);
     }
+
+    Palette sectionAt(int x, int y, int z);
 
     interface World extends Block.Getter, Block.Setter {
     }
--- src/main/java/net/minestom/server/instance/painter/PainterImpl.java
+++ src/main/java/net/minestom/server/instance/painter/PainterImpl.java
@@ -1,6 +1,54 @@
 package net.minestom.server.instance.painter;
 
+import net.minestom.server.instance.block.Block;
+import net.minestom.server.instance.palette.Palette;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnknownNullability;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
-record PainterImpl(Consumer<Painter.World> consumer) implements Painter {
+record PainterImpl(List<Instruction> instructions) implements Painter {
+    public PainterImpl {
+        instructions = List.copyOf(instructions);
+    }
+
+    static PainterImpl paint(Consumer<World> consumer) {
+        WorldImpl world = new WorldImpl();
+        consumer.accept(world);
+        return new PainterImpl(world.instructions);
+    }
+
+    @Override
+    public Palette sectionAt(int x, int y, int z) {
+        Palette palette = Palette.blocks();
+        for (Instruction instruction : instructions) {
+            // TODO
+        }
+        return palette;
+    }
+
+    sealed interface Instruction {
+        record SetBlock(int x, int y, int z, int id) implements Instruction {
+        }
+    }
+
+    static final class WorldImpl implements World {
+        private final List<Instruction> instructions = new ArrayList<>();
+
+        @Override
+        public @UnknownNullability Block getBlock(int x, int y, int z, @NotNull Condition condition) {
+            return Block.AIR;
+        }
+
+        @Override
+        public void setBlock(int x, int y, int z, @NotNull Block block) {
+            append(new Instruction.SetBlock(x, y, z, block.id()));
+        }
+
+        void append(Instruction instruction) {
+            instructions.add(instruction);
+        }
+    }
 }
