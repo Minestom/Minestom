52fc77cdf781f0d8b9e5f69667909851ee6f56c1
themode <themode@outlook.fr>
2024-09-16 23:19:32 +0200

    Remove write condition

--- src/main/java/net/minestom/server/network/socket/Server.java
+++ src/main/java/net/minestom/server/network/socket/Server.java
@@ -103,8 +103,6 @@ public final class Server {
                 break;
             }
         }
-        // Ensure the connection is not stuck waiting for packets to write
-        connection.signalWrite();
     }
 
     private void playerWriteLoop(PlayerSocketConnection connection) {
