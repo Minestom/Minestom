fb952c4672fdf1362ca557a4593ba6d13824f70c
themode <themode@outlook.fr>
2024-09-16 04:44:50 +0200

    More events

--- src/main/java/net/minestom/server/monitoring/EventsJFR.java
+++ src/main/java/net/minestom/server/monitoring/EventsJFR.java
@@ -5,8 +5,14 @@ import jdk.jfr.*;
 @SuppressWarnings("ALL")
 public final class EventsJFR {
     public static final String SERVER_TICK = "minestom.ServerTickTime";
+
     public static final String CHUNK_GENERATION = "minestom.ChunkGeneration";
     public static final String CHUNK_LOADING = "minestom.ChunkLoading";
+
+    public static final String EVENT_CALL = "minestom.EventCall";
+    public static final String EVENT_HANDLE_INVALIDATE = "minestom.EventHandleInvalidate";
+
+    public static final String PACKET_READ = "minestom.PacketRead";
 
     @Name(SERVER_TICK)
     @Label("Server Tick")
@@ -55,6 +61,45 @@ public final class EventsJFR {
             this.loader = loader;
             this.chunkX = chunkX;
             this.chunkZ = chunkZ;
+        }
+    }
+
+    @Name(EVENT_CALL)
+    @Label("Event Call")
+    @Category({"Minestom", "Event"})
+    @Description("Event called")
+    public static final class EventCall extends Event {
+        @Label("Event Class")
+        Class event;
+
+        public EventCall(Class event) {
+            this.event = event;
+        }
+    }
+
+    @Name(EVENT_HANDLE_INVALIDATE)
+    @Label("Event Handle Invalidate")
+    @Category({"Minestom", "Event"})
+    @Description("Event handle has been invalidated due to a new listener. This cause the executor to be rebuild.")
+    public static final class EventHandleInvalidate extends Event {
+        @Label("Event Class")
+        Class event;
+
+        public EventHandleInvalidate(Class event) {
+            this.event = event;
+        }
+    }
+
+    @Name(PACKET_READ)
+    @Label("Packet Read")
+    @Category({"Minestom", "Network"})
+    @Description("Packet has been read from socket")
+    public static final class PacketRead extends Event {
+        @Label("Packet Class")
+        Class packet;
+
+        public PacketRead(Class packet) {
+            this.packet = packet;
         }
     }
 }
