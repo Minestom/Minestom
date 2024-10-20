ee0d0cbe2dd9574f1566800c953eea77d03704de
mworzala <mattheworzala@gmail.com>
2024-10-19 23:02:24 -0400

    feat: lots of minor fixes to item components

--- src/main/java/net/minestom/server/entity/EquipmentSlotGroup.java
+++ src/main/java/net/minestom/server/entity/EquipmentSlotGroup.java
@@ -4,27 +4,38 @@ import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.utils.nbt.BinaryTagSerializer;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-public enum EquipmentSlotGroup {
-    ANY(EquipmentSlot.values()),
-    MAIN_HAND(EquipmentSlot.MAIN_HAND),
-    OFF_HAND(EquipmentSlot.OFF_HAND),
-    HAND(EquipmentSlot.MAIN_HAND, EquipmentSlot.OFF_HAND),
-    FEET(EquipmentSlot.BOOTS),
-    LEGS(EquipmentSlot.LEGGINGS),
-    CHEST(EquipmentSlot.CHESTPLATE),
-    HEAD(EquipmentSlot.HELMET),
-    ARMOR(EquipmentSlot.CHESTPLATE, EquipmentSlot.LEGGINGS, EquipmentSlot.BOOTS, EquipmentSlot.HELMET),
-    BODY(EquipmentSlot.BODY);
+public enum EquipmentSlotGroup implements Predicate<EquipmentSlot> {
+    ANY("any", EquipmentSlot.values()),
+    MAIN_HAND("mainhand", EquipmentSlot.MAIN_HAND),
+    OFF_HAND("offhand", EquipmentSlot.OFF_HAND),
+    HAND("hand", EquipmentSlot.MAIN_HAND, EquipmentSlot.OFF_HAND),
+    FEET("feet", EquipmentSlot.BOOTS),
+    LEGS("legs", EquipmentSlot.LEGGINGS),
+    CHEST("chest", EquipmentSlot.CHESTPLATE),
+    HEAD("head", EquipmentSlot.HELMET),
+    ARMOR("armor", EquipmentSlot.CHESTPLATE, EquipmentSlot.LEGGINGS, EquipmentSlot.BOOTS, EquipmentSlot.HELMET),
+    BODY("body", EquipmentSlot.BODY);
+
+    private static final Map<String, EquipmentSlotGroup> BY_NBT_NAME = Arrays.stream(values())
+            .collect(Collectors.toMap(EquipmentSlotGroup::nbtName, Function.identity()));
 
     public static final NetworkBuffer.Type<EquipmentSlotGroup> NETWORK_TYPE = NetworkBuffer.Enum(EquipmentSlotGroup.class);
-    public static final BinaryTagSerializer<EquipmentSlotGroup> NBT_TYPE = BinaryTagSerializer.fromEnumStringable(EquipmentSlotGroup.class);
+    public static final BinaryTagSerializer<EquipmentSlotGroup> NBT_TYPE = BinaryTagSerializer.STRING
+            .map(BY_NBT_NAME::get, EquipmentSlotGroup::nbtName);
 
+    private final String nbtName;
     private final List<EquipmentSlot> equipmentSlots;
 
-    EquipmentSlotGroup(@NotNull EquipmentSlot... equipmentSlots) {
+    EquipmentSlotGroup(@NotNull String nbtName, @NotNull EquipmentSlot... equipmentSlots) {
         this.equipmentSlots = List.of(equipmentSlots);
+        this.nbtName = nbtName;
     }
 
     /**
@@ -34,10 +45,19 @@ public enum EquipmentSlotGroup {
         return this.equipmentSlots;
     }
 
+    public @NotNull String nbtName() {
+        return this.nbtName;
+    }
+
     /**
      * Returns true if this attribute slot has an effect on the given {@link EquipmentSlot}, false otherwise.
      */
     public boolean contains(@NotNull EquipmentSlot equipmentSlot) {
         return this.equipmentSlots.contains(equipmentSlot);
+    }
+
+    @Override
+    public boolean test(EquipmentSlot equipmentSlot) {
+        return this.contains(equipmentSlot);
     }
 }
--- src/main/java/net/minestom/server/item/component/FireworkList.java
+++ src/main/java/net/minestom/server/item/component/FireworkList.java
@@ -1,15 +1,11 @@
 package net.minestom.server.item.component;
 
-import net.kyori.adventure.nbt.BinaryTag;
-import net.kyori.adventure.nbt.BinaryTagTypes;
-import net.kyori.adventure.nbt.CompoundBinaryTag;
-import net.kyori.adventure.nbt.ListBinaryTag;
 import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.NetworkBufferTemplate;
 import net.minestom.server.utils.nbt.BinaryTagSerializer;
+import net.minestom.server.utils.nbt.BinaryTagTemplate;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public record FireworkList(byte flightDuration, @NotNull List<FireworkExplosion> explosions) {
@@ -18,28 +14,11 @@ public record FireworkList(byte flightDuration, @NotNull List<FireworkExplosion>
     public static final NetworkBuffer.Type<FireworkList> NETWORK_TYPE = NetworkBufferTemplate.template(
             NetworkBuffer.BYTE, FireworkList::flightDuration,
             FireworkExplosion.NETWORK_TYPE.list(256), FireworkList::explosions,
-            FireworkList::new
-    );
-
-    public static final BinaryTagSerializer<FireworkList> NBT_TYPE = BinaryTagSerializer.COMPOUND.map(
-            tag -> {
-                byte flightDuration = tag.getByte("flight_duration");
-                ListBinaryTag explosionsTag = tag.getList("explosions", BinaryTagTypes.COMPOUND);
-                List<FireworkExplosion> explosions = new ArrayList<>(explosionsTag.size());
-                for (BinaryTag explosionTag : explosionsTag)
-                    explosions.add(FireworkExplosion.NBT_TYPE.read(explosionTag));
-                return new FireworkList(flightDuration, explosions);
-            },
-            value -> {
-                ListBinaryTag.Builder<BinaryTag> explosionsTag = ListBinaryTag.builder();
-                for (FireworkExplosion explosion : value.explosions)
-                    explosionsTag.add(FireworkExplosion.NBT_TYPE.write(explosion));
-                return CompoundBinaryTag.builder()
-                        .putByte("flight_duration", value.flightDuration)
-                        .put("explosions", explosionsTag.build())
-                        .build();
-            }
-    );
+            FireworkList::new);
+    public static final BinaryTagSerializer<FireworkList> NBT_TYPE = BinaryTagTemplate.object(
+            "flight_duration", BinaryTagSerializer.BYTE, FireworkList::flightDuration,
+            "explosions", FireworkExplosion.NBT_TYPE.list().optional(List.of()), FireworkList::explosions,
+            FireworkList::new);
 
     public FireworkList {
         explosions = List.copyOf(explosions);
