f33be6009eeb827f3866c2df89e9f961719de9f1
mworzala <mattheworzala@gmail.com>
2024-10-19 23:02:24 -0400

    feat: lots of minor fixes to item components

--- src/main/java/net/minestom/server/item/component/Equippable.java
+++ src/main/java/net/minestom/server/item/component/Equippable.java
@@ -1,10 +1,14 @@
 package net.minestom.server.item.component;
 
+import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.EquipmentSlot;
+import net.minestom.server.gamedata.tags.Tag;
 import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.NetworkBufferTemplate;
+import net.minestom.server.registry.ObjectSet;
 import net.minestom.server.sound.SoundEvent;
 import net.minestom.server.utils.nbt.BinaryTagSerializer;
+import net.minestom.server.utils.nbt.BinaryTagTemplate;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -13,30 +17,29 @@ public record Equippable(
         @NotNull SoundEvent equipSound,
         @Nullable String model,
         @Nullable String cameraOverlay,
-//        @Nullable List<EntityType> allowedEntities, // TODO(1.21.2): this is an ObjectSet
+        @Nullable ObjectSet<EntityType> allowedEntities,
         boolean dispensable,
         boolean swappable,
         boolean damageOnHurt
-        ) {
-
+) {
     public static final NetworkBuffer.Type<Equippable> NETWORK_TYPE = NetworkBufferTemplate.template(
             EquipmentSlot.NETWORK_TYPE, Equippable::slot,
             SoundEvent.NETWORK_TYPE, Equippable::equipSound,
             NetworkBuffer.STRING.optional(), Equippable::model,
             NetworkBuffer.STRING.optional(), Equippable::cameraOverlay,
-//            STRING.list().optional(), Equippable::allowedEntities,
+            ObjectSet.<EntityType>networkType(Tag.BasicType.ENTITY_TYPES).optional(), Equippable::allowedEntities,
             NetworkBuffer.BOOLEAN, Equippable::dispensable,
             NetworkBuffer.BOOLEAN, Equippable::swappable,
             NetworkBuffer.BOOLEAN, Equippable::damageOnHurt,
             Equippable::new);
-    public static final BinaryTagSerializer<Equippable> NBT_TYPE = BinaryTagSerializer.object(
+    public static final BinaryTagSerializer<Equippable> NBT_TYPE = BinaryTagTemplate.object(
             "slot", EquipmentSlot.NBT_TYPE, Equippable::slot,
-            "equip_sound", SoundEvent.NBT_TYPE, Equippable::equipSound,
+            "equip_sound", SoundEvent.NBT_TYPE.optional(SoundEvent.ITEM_ARMOR_EQUIP_GENERIC), Equippable::equipSound,
             "model", BinaryTagSerializer.STRING.optional(), Equippable::model,
             "camera_overlay", BinaryTagSerializer.STRING.optional(), Equippable::cameraOverlay,
-//            "allowed_entities", STRING.list().optional(), Equippable::allowedEntities,
-            "dispensable", BinaryTagSerializer.BOOLEAN, Equippable::dispensable,
-            "swappable", BinaryTagSerializer.BOOLEAN, Equippable::swappable,
-            "damage_on_hurt", BinaryTagSerializer.BOOLEAN, Equippable::damageOnHurt,
+            "allowed_entities", ObjectSet.<EntityType>nbtType(Tag.BasicType.ENTITY_TYPES).optional(), Equippable::allowedEntities,
+            "dispensable", BinaryTagSerializer.BOOLEAN.optional(true), Equippable::dispensable,
+            "swappable", BinaryTagSerializer.BOOLEAN.optional(true), Equippable::swappable,
+            "damage_on_hurt", BinaryTagSerializer.BOOLEAN.optional(true), Equippable::damageOnHurt,
             Equippable::new);
 }
--- src/main/java/net/minestom/server/item/component/WritableBookContent.java
+++ src/main/java/net/minestom/server/item/component/WritableBookContent.java
@@ -1,15 +1,12 @@
 package net.minestom.server.item.component;
 
-import net.kyori.adventure.nbt.BinaryTag;
-import net.kyori.adventure.nbt.CompoundBinaryTag;
-import net.kyori.adventure.nbt.ListBinaryTag;
 import net.minestom.server.item.book.FilteredText;
 import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.NetworkBufferTemplate;
 import net.minestom.server.utils.nbt.BinaryTagSerializer;
+import net.minestom.server.utils.nbt.BinaryTagTemplate;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public record WritableBookContent(@NotNull List<FilteredText<String>> pages) {
@@ -17,32 +14,10 @@ public record WritableBookContent(@NotNull List<FilteredText<String>> pages) {
 
     public static final NetworkBuffer.Type<WritableBookContent> NETWORK_TYPE = NetworkBufferTemplate.template(
             FilteredText.STRING_NETWORK_TYPE.list(100), WritableBookContent::pages,
-            WritableBookContent::new
-    );
-
-    public static final BinaryTagSerializer<WritableBookContent> NBT_TYPE = new BinaryTagSerializer<>() {
-        @Override
-        public @NotNull BinaryTag write(@NotNull WritableBookContent value) {
-            ListBinaryTag.Builder<BinaryTag> pages = ListBinaryTag.builder();
-            for (FilteredText<String> page : value.pages) {
-                pages.add(FilteredText.STRING_NBT_TYPE.write(page));
-            }
-            return CompoundBinaryTag.builder().put("pages", pages.build()).build();
-        }
-
-        @Override
-        public @NotNull WritableBookContent read(@NotNull BinaryTag tag) {
-            if (!(tag instanceof CompoundBinaryTag compound)) return EMPTY;
-            ListBinaryTag pagesTag = compound.getList("pages");
-            if (pagesTag.size() == 0) return EMPTY;
-
-            List<FilteredText<String>> pages = new ArrayList<>(pagesTag.size());
-            for (BinaryTag pageTag : pagesTag) {
-                pages.add(FilteredText.STRING_NBT_TYPE.read(pageTag));
-            }
-            return new WritableBookContent(pages);
-        }
-    };
+            WritableBookContent::new);
+    public static final BinaryTagSerializer<WritableBookContent> NBT_TYPE = BinaryTagTemplate.object(
+            "pages", FilteredText.STRING_NBT_TYPE.list().optional(List.of()), WritableBookContent::pages,
+            WritableBookContent::new);
 
     public WritableBookContent {
         pages = List.copyOf(pages);
