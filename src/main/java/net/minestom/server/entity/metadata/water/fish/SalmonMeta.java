12586323ec3fb25fcfe0aab84a40affd8aa9fd3b
mworzala <mattheworzala@gmail.com>
2024-10-22 20:46:12 -0400

    feat: entity meta changes

--- src/main/java/net/minestom/server/entity/metadata/water/fish/SalmonMeta.java
+++ src/main/java/net/minestom/server/entity/metadata/water/fish/SalmonMeta.java
@@ -4,12 +4,38 @@ import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.MetadataHolder;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class SalmonMeta extends AbstractFishMeta {
     public static final byte OFFSET = AbstractFishMeta.MAX_OFFSET;
     public static final byte MAX_OFFSET = OFFSET + 0;
 
     public SalmonMeta(@NotNull Entity entity, @NotNull MetadataHolder metadata) {
         super(entity, metadata);
+    }
+
+    public @NotNull Variant getVariant() {
+        return Variant.BY_ID.getOrDefault(super.metadata.getIndex(OFFSET, ""), Variant.MEDIUM);
+    }
+
+    public enum Variant {
+        SMALL("small"),
+        MEDIUM("medium"),
+        LARGE("large");
+
+        private static final Map<String, Variant> BY_ID = Arrays.stream(values())
+                .collect(Collectors.toMap(Variant::id, (variant) -> variant));
+
+        private final String id;
+        Variant(@NotNull String id) {
+            this.id = id;
+        }
+
+        public @NotNull String id() {
+            return id;
+        }
     }
 
 }
