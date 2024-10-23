8b3f9c86655cd71bda45c1d7f4b097be230a37ff
mworzala <mattheworzala@gmail.com>
2024-10-22 20:46:12 -0400

    feat: entity meta changes

--- src/main/java/net/minestom/server/entity/metadata/water/AgeableWaterAnimalMeta.java
+++ src/main/java/net/minestom/server/entity/metadata/water/AgeableWaterAnimalMeta.java
@@ -2,14 +2,14 @@ package net.minestom.server.entity.metadata.water;
 
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.MetadataHolder;
+import net.minestom.server.entity.metadata.AgeableMobMeta;
 import org.jetbrains.annotations.NotNull;
 
-public class SquidMeta extends AgeableWaterAnimalMeta {
-    public static final byte OFFSET = WaterAnimalMeta.MAX_OFFSET;
+public class AgeableWaterAnimalMeta extends AgeableMobMeta {
+    public static final byte OFFSET = AgeableMobMeta.MAX_OFFSET;
     public static final byte MAX_OFFSET = OFFSET + 0;
 
-    public SquidMeta(@NotNull Entity entity, @NotNull MetadataHolder metadata) {
+    public AgeableWaterAnimalMeta(@NotNull Entity entity, @NotNull MetadataHolder metadata) {
         super(entity, metadata);
     }
-
 }
