ea562c5e0b20c8f1572f96cf17f20d5035f833ae
mworzala <mattheworzala@gmail.com>
2024-10-22 20:46:12 -0400

    feat: entity meta changes

--- src/main/java/net/minestom/server/entity/metadata/monster/CreakingMeta.java
+++ src/main/java/net/minestom/server/entity/metadata/monster/CreakingMeta.java
@@ -1,15 +1,31 @@
 package net.minestom.server.entity.metadata.monster;
 
 import net.minestom.server.entity.Entity;
+import net.minestom.server.entity.Metadata;
 import net.minestom.server.entity.MetadataHolder;
 import org.jetbrains.annotations.NotNull;
 
-// TODO(1.21.2) - Find out what the metadata fields of a creaking are
 public class CreakingMeta extends MonsterMeta {
     public static final byte OFFSET = MonsterMeta.MAX_OFFSET;
-    public static final byte MAX_OFFSET = OFFSET + 0;
+    public static final byte MAX_OFFSET = OFFSET + 2;
 
     public CreakingMeta(@NotNull Entity entity, @NotNull MetadataHolder metadata) {
         super(entity, metadata);
+    }
+
+    public boolean canMove() {
+        return super.metadata.getIndex(OFFSET, true);
+    }
+
+    public void setCanMove(boolean value) {
+        super.metadata.setIndex(OFFSET, Metadata.Boolean(value));
+    }
+
+    public boolean isActive() {
+        return super.metadata.getIndex(OFFSET + 1, false);
+    }
+
+    public void setActive(boolean value) {
+        super.metadata.setIndex(OFFSET + 1, Metadata.Boolean(value));
     }
 }
