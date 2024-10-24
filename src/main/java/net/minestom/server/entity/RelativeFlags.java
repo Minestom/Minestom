23f69ca94c7d73594aec3e324c2f9c2a33eeb1fc
MelonHell <53826469+MelonHell@users.noreply.github.com>
2024-10-24 04:53:00 +0300

    Update RelativeFlags (#2447)

--- src/main/java/net/minestom/server/entity/RelativeFlags.java
+++ src/main/java/net/minestom/server/entity/RelativeFlags.java
@@ -2,12 +2,21 @@ package net.minestom.server.entity;
 
 public class RelativeFlags {
     public static final int NONE = 0x00;
+
     public static final int X = 0x01;
     public static final int Y = 0x02;
     public static final int Z = 0x04;
+
     public static final int YAW = 0x08;
     public static final int PITCH = 0x10;
+
+    public static final int DELTA_X = 0x20;
+    public static final int DELTA_Y = 0x40;
+    public static final int DELTA_Z = 0x80;
+    public static final int ROTATE_DELTA = 0x100;
+
     public static final int COORD = X | Y | Z;
     public static final int VIEW = YAW | PITCH;
-    public static final int ALL = COORD | VIEW;
+    public static final int DELTA = DELTA_X | DELTA_Y | DELTA_Z | ROTATE_DELTA;
+    public static final int ALL = COORD | VIEW | DELTA;
 }
