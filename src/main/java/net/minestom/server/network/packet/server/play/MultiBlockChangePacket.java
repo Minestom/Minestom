59a97ca99eeb0806603613cb4fd960d8369d924c
mworzala <mattheworzala@gmail.com>
2024-10-24 21:29:06 -0400

    fix: update multi block change packet format

--- src/main/java/net/minestom/server/network/packet/server/play/MultiBlockChangePacket.java
+++ src/main/java/net/minestom/server/network/packet/server/play/MultiBlockChangePacket.java
@@ -10,11 +10,10 @@ import static net.minestom.server.network.NetworkBuffer.VAR_LONG_ARRAY;
 public record MultiBlockChangePacket(long chunkSectionPosition, long[] blocks) implements ServerPacket.Play {
     public static final NetworkBuffer.Type<MultiBlockChangePacket> SERIALIZER = NetworkBufferTemplate.template(
             LONG, MultiBlockChangePacket::chunkSectionPosition,
-            VAR_LONG_ARRAY.optional(), MultiBlockChangePacket::blocks,
+            VAR_LONG_ARRAY, MultiBlockChangePacket::blocks,
             MultiBlockChangePacket::new);
 
-    public MultiBlockChangePacket(int chunkX, int section, int chunkZ,
-                                  long[] blocks) {
+    public MultiBlockChangePacket(int chunkX, int section, int chunkZ, long[] blocks) {
         this(((long) (chunkX & 0x3FFFFF) << 42) | (section & 0xFFFFF) | ((long) (chunkZ & 0x3FFFFF) << 20), blocks);
     }
 }
