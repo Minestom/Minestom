c148954a47bdc591638ef3d186706ce6c8dfa3e1
GreatWyrm <alecmusante@gmail.com>
2024-10-31 12:24:53 -0700

    Change Map Reduction flag to be an int property to avoid number parsing (#2460)
    * Change Map Reduction flag to be an int property to avoid number parsing

--- src/main/java/net/minestom/server/map/MapColors.java
+++ src/main/java/net/minestom/server/map/MapColors.java
@@ -1,6 +1,5 @@
 package net.minestom.server.map;
 
-import net.minestom.server.MinecraftServer;
 import net.minestom.server.ServerFlag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -92,15 +91,10 @@ public enum MapColors {
         mappingStrategy = strategy;
 
         int reduction = 10;
-        if (ServerFlag.MAP_RGB_REDUCTION != null) {
-            try {
-                reduction = Integer.parseInt(ServerFlag.MAP_RGB_REDUCTION);
-            } catch (NumberFormatException e) {
-                logger.error("Invalid integer in reduction argument: {}", ServerFlag.MAP_RGB_REDUCTION);
-                MinecraftServer.getExceptionManager().handleException(e);
-            }
+        if (ServerFlag.MAP_RGB_REDUCTION != -1) {
+            reduction = ServerFlag.MAP_RGB_REDUCTION;
 
-            if (reduction < 0 || reduction >= 255) {
+            if (reduction < 0 || reduction > 255) {
                 logger.warn("Reduction was found to be invalid: {}. Must in 0-255, defaulting to 10.", reduction);
                 reduction = 10;
             }
