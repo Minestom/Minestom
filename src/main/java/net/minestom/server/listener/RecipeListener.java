ed7b0fc7d69a1a4538f714cd10c35e0092c703ae
mworzala <mattheworzala@gmail.com>
2024-11-02 19:20:12 -0400

    fix: make new packets ComponentHolding

--- src/main/java/net/minestom/server/listener/RecipeListener.java
+++ src/main/java/net/minestom/server/listener/RecipeListener.java
@@ -2,11 +2,11 @@ package net.minestom.server.listener;
 
 import net.minestom.server.entity.Player;
 import net.minestom.server.network.packet.client.play.ClientPlaceRecipePacket;
-import net.minestom.server.network.packet.server.play.PlaceGhostRecipePacket;
 
 public class RecipeListener {
 
     public static void listener(ClientPlaceRecipePacket packet, Player player) {
-        player.sendPacket(new PlaceGhostRecipePacket(packet.windowId(), packet.recipe()));
+        // TODO(1.21.2)
+//        player.sendPacket(new PlaceGhostRecipePacket(packet.windowId(), packet.recipe()));
     }
 }
