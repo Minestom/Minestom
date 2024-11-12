b5dc59d1850fb8efb2ca0fd909571a6c21c990ba
MelonHell <MelonHell@ya.ru>
2024-11-12 15:44:20 +0300

    oops

--- src/main/java/net/minestom/server/entity/metadata/item/EyeOfEnderMeta.java
+++ src/main/java/net/minestom/server/entity/metadata/item/EyeOfEnderMeta.java
@@ -1,12 +1,24 @@
 package net.minestom.server.entity.metadata.item;
 
 import net.minestom.server.entity.Entity;
+import net.minestom.server.entity.MetadataDef;
 import net.minestom.server.entity.MetadataHolder;
+import net.minestom.server.entity.metadata.EntityMeta;
+import net.minestom.server.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-public class EyeOfEnderMeta extends ThrownItemProjectileMeta {
+public class EyeOfEnderMeta extends EntityMeta {
     public EyeOfEnderMeta(@NotNull Entity entity, @NotNull MetadataHolder metadata) {
         super(entity, metadata);
+    }
+
+    @NotNull
+    public ItemStack getItem() {
+        return metadata.get(MetadataDef.EyeOfEnder.ITEM);
+    }
+
+    public void setItem(@NotNull ItemStack value) {
+        metadata.set(MetadataDef.EyeOfEnder.ITEM, value);
     }
 
 }
