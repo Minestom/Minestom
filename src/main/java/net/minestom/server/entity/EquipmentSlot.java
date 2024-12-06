b09b1414815fcec3a2b089f92ef34683266e789e
mworzala <mattheworzala@gmail.com>
2024-12-06 18:44:57 -0500

    chore: rename and explanation changes

--- src/main/java/net/minestom/server/entity/EquipmentSlot.java
+++ src/main/java/net/minestom/server/entity/EquipmentSlot.java
@@ -2,45 +2,82 @@ package net.minestom.server.entity;
 
 import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.utils.nbt.BinaryTagSerializer;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import net.minestom.server.item.component.Equippable;
-import net.minestom.server.network.packet.server.play.EntityEquipmentPacket;
 
 import static net.minestom.server.utils.inventory.PlayerInventoryUtils.*;
 
 public enum EquipmentSlot {
-    MAIN_HAND(false, -1, "mainhand", 0),
-    BOOTS(true, BOOTS_SLOT, "feet", 2),
-    LEGGINGS(true, LEGGINGS_SLOT, "legs", 3),
-    CHESTPLATE(true, CHESTPLATE_SLOT, "chest", 4),
-    HELMET(true, HELMET_SLOT, "head", 5),
-    OFF_HAND(false, -1, "offhand", 1),
-    BODY(false, -1, "body", 6);
+    MAIN_HAND(0, 0, "mainhand", false, -1),
+    BOOTS(2, 1, "feet", true, BOOTS_SLOT),
+    LEGGINGS(3, 2, "legs", true, LEGGINGS_SLOT),
+    CHESTPLATE(4, 3, "chest", true, CHESTPLATE_SLOT),
+    HELMET(5, 4, "head", true, HELMET_SLOT),
+    OFF_HAND(1, 5, "offhand", false, -1),
+    BODY(6, 6, "body", false, -1);
 
     private static final List<EquipmentSlot> ARMORS = List.of(BOOTS, LEGGINGS, CHESTPLATE, HELMET);
     private static final Map<String, EquipmentSlot> BY_NBT_NAME = Arrays.stream(values())
             .collect(Collectors.toMap(EquipmentSlot::nbtName, slot -> slot));
+    private static final Map<Integer, EquipmentSlot> BY_PROTOCOL_ID = Arrays.stream(values())
+            .collect(Collectors.toMap(EquipmentSlot::protocolId, slot -> slot));
+    private static final Map<Integer, EquipmentSlot> BY_LEGACY_PROTOCOL_ID = Arrays.stream(values())
+            .collect(Collectors.toMap(EquipmentSlot::protocolId, slot -> slot));
 
-    public static final NetworkBuffer.Type<EquipmentSlot> NETWORK_TYPE = NetworkBuffer.Enum(EquipmentSlot.class);
+    public static final NetworkBuffer.Type<EquipmentSlot> NETWORK_TYPE = NetworkBuffer.VAR_INT.transform(
+            BY_PROTOCOL_ID::get, EquipmentSlot::protocolId);
     public static final BinaryTagSerializer<EquipmentSlot> NBT_TYPE = BinaryTagSerializer.STRING.map(
             BY_NBT_NAME::get, EquipmentSlot::nbtName);
 
+    public static @NotNull List<@NotNull EquipmentSlot> armors() {
+        return ARMORS;
+    }
+
+    @Deprecated
+    public static @NotNull EquipmentSlot fromLegacyProtocolId(int legacyProtocolId) {
+        final EquipmentSlot slot = BY_LEGACY_PROTOCOL_ID.get(legacyProtocolId);
+        if (slot != null) return slot;
+
+        throw new IllegalStateException("Unexpected value: " + legacyProtocolId);
+    }
+
+    private final int protocolId;
+    private final int legacyProtocolId;
+    private final String nbtName;
     private final boolean armor;
     private final int armorSlot;
-    private final String nbtName;
-    private final  int equipmentSlot;
 
-    EquipmentSlot(boolean armor, int armorSlot, String nbtName, int equipmentSlot) {
+    EquipmentSlot(int protocolId, int legacyProtocolId, @NotNull String nbtName, boolean armor, int armorSlot) {
+        this.protocolId = protocolId;
+        this.legacyProtocolId = legacyProtocolId;
+        this.nbtName = nbtName;
         this.armor = armor;
         this.armorSlot = armorSlot;
-        this.nbtName = nbtName;
-        this.equipmentSlot = equipmentSlot;
+    }
+
+    public int protocolId() {
+        return protocolId;
+    }
+
+    /**
+     * Legacy protocol ID exists because that format is used in EntityEquipmentPacket
+     * It is being referred to as the legacy ID here because newer components are using
+     * the equipment slot stream codec (the more modern mechanism for network serialization)
+     * The legacy ID is expected to be removed eventually.
+     *
+     * @return the equipment slot
+     */
+    @Deprecated
+    public int legacyProtocolId() {
+        return legacyProtocolId;
+    }
+
+    public @NotNull String nbtName() {
+        return nbtName;
     }
 
     public boolean isHand() {
@@ -51,40 +88,7 @@ public enum EquipmentSlot {
         return armor;
     }
 
-    /**
-     * Differs from the ordinal only slightly - the ordinal is used for sending components such as {@link Equippable},
-     * but this value needs to be used instead for {@link EntityEquipmentPacket}.
-     *
-     * @return the equipment slot
-     */
-    @ApiStatus.Internal
-    public int equipmentSlot() {
-        return equipmentSlot;
-    }
-
     public int armorSlot() {
         return armorSlot;
-    }
-
-    public @NotNull String nbtName() {
-        return nbtName;
-    }
-
-    public static @NotNull List<@NotNull EquipmentSlot> armors() {
-        return ARMORS;
-    }
-
-    @ApiStatus.Internal
-    public static @NotNull EquipmentSlot fromEquipmentSlot(int equipmentSlot) {
-        return switch (equipmentSlot) {
-            case 0 -> EquipmentSlot.MAIN_HAND;
-            case 1 -> EquipmentSlot.OFF_HAND;
-            case 2 -> EquipmentSlot.BOOTS;
-            case 3 -> EquipmentSlot.LEGGINGS;
-            case 4 -> EquipmentSlot.CHESTPLATE;
-            case 5 -> EquipmentSlot.HELMET;
-            case 6 -> EquipmentSlot.BODY;
-            default -> throw new IllegalStateException("Unexpected value: " + equipmentSlot);
-        };
     }
 }
