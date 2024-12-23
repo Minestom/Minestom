e4abf9552a9ab3ae98648a600276438add4cd23d
CodeDoctor <codedoctor@linwood.dev>
2024-12-23 13:48:27 +0100

    Fix recipe book settings filter (#2532)

--- src/main/java/net/minestom/server/network/packet/server/play/RecipeBookSettingsPacket.java
+++ src/main/java/net/minestom/server/network/packet/server/play/RecipeBookSettingsPacket.java
@@ -4,8 +4,18 @@ import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.NetworkBufferTemplate;
 import net.minestom.server.network.packet.server.ServerPacket;
 
-public record RecipeBookSettingsPacket() implements ServerPacket.Play {
+import static net.minestom.server.network.NetworkBuffer.BOOLEAN;
+
+public record RecipeBookSettingsPacket(boolean craftingRecipeBookOpen,boolean craftingRecipeBookFilterActive,boolean smeltingRecipeBookOpen,boolean smeltingRecipeBookFilterActive,boolean blastFurnaceRecipeBookOpen,boolean blastFurnaceRecipeBookFilterActive,boolean smokerRecipeBookOpen,boolean smokerRecipeBookFilterActive) implements ServerPacket.Play {
     public static final NetworkBuffer.Type<RecipeBookSettingsPacket> SERIALIZER = NetworkBufferTemplate.template(
+            BOOLEAN, RecipeBookSettingsPacket::craftingRecipeBookOpen,
+            BOOLEAN, RecipeBookSettingsPacket::craftingRecipeBookFilterActive,
+            BOOLEAN, RecipeBookSettingsPacket::smeltingRecipeBookOpen,
+            BOOLEAN, RecipeBookSettingsPacket::smeltingRecipeBookFilterActive,
+            BOOLEAN, RecipeBookSettingsPacket::blastFurnaceRecipeBookOpen,
+            BOOLEAN, RecipeBookSettingsPacket::blastFurnaceRecipeBookFilterActive,
+            BOOLEAN, RecipeBookSettingsPacket::smokerRecipeBookOpen,
+            BOOLEAN, RecipeBookSettingsPacket::smokerRecipeBookFilterActive,
             RecipeBookSettingsPacket::new);
 
 }
