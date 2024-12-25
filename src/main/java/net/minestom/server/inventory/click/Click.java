ec7bf7308f5129ee5f04960617107d3ae7481f29
GoldenStack <git@goldenstack.net>
2024-12-24 18:54:10 -0600

    Remove usage of Java 22+ features

--- src/main/java/net/minestom/server/inventory/click/Click.java
+++ src/main/java/net/minestom/server/inventory/click/Click.java
@@ -105,17 +105,19 @@ public sealed interface Click {
             case LeftShift(int slot) -> toWindowSingle(slot, containerSize, LeftShift::new);
             case RightShift(int slot) -> toWindowSingle(slot, containerSize, RightShift::new);
             case Double(int slot) -> toWindowSingle(slot, containerSize, Double::new);
-            case Click.OffhandSwap(int slot) -> toWindowSingle(slot, containerSize, OffhandSwap::new);
-            case Click.DropSlot(int slot, boolean all) -> toWindowSingle(slot, containerSize, s -> new DropSlot(s, all));
-            case Click.HotbarSwap(int hotbarSlot, int slot) -> toWindowSingle(slot, containerSize, s -> new HotbarSwap(hotbarSlot, s));
+            case OffhandSwap(int slot) -> toWindowSingle(slot, containerSize, OffhandSwap::new);
+            case DropSlot(int slot, boolean all) -> toWindowSingle(slot, containerSize, s -> new DropSlot(s, all));
+            case HotbarSwap(int hotbarSlot, int slot) -> toWindowSingle(slot, containerSize, s -> new HotbarSwap(hotbarSlot, s));
 
             // Everything with zero slots
-            case Click.LeftDropCursor(), RightDropCursor(), MiddleDropCursor() -> new Window(false, click);
+            case LeftDropCursor() -> new Window(false, click);
+            case MiddleDropCursor() -> new Window(false, click);
+            case RightDropCursor() -> new Window(false, click);
 
             // Everything with multiple slots
-            case Click.LeftDrag(List<Integer> slots) -> toWindowMultiple(slots, containerSize, LeftDrag::new);
-            case Click.RightDrag(List<Integer> slots) -> toWindowMultiple(slots, containerSize, RightDrag::new);
-            case Click.MiddleDrag(List<Integer> slots) -> toWindowMultiple(slots, containerSize, MiddleDrag::new);
+            case LeftDrag(List<Integer> slots) -> toWindowMultiple(slots, containerSize, LeftDrag::new);
+            case RightDrag(List<Integer> slots) -> toWindowMultiple(slots, containerSize, RightDrag::new);
+            case MiddleDrag(List<Integer> slots) -> toWindowMultiple(slots, containerSize, MiddleDrag::new);
         };
     }
 
@@ -146,23 +148,25 @@ public sealed interface Click {
     static @NotNull Click fromWindow(@NotNull Click.Window window, @Nullable Integer containerSize) {
         return switch (window.click()) {
             // Everything with one dynamic slot
-            case Left(_) -> fromWindowSingle(window, containerSize, Left::new);
-            case Right(_) -> fromWindowSingle(window, containerSize, Right::new);
-            case Middle(_) -> fromWindowSingle(window, containerSize, Middle::new);
-            case LeftShift(_) -> fromWindowSingle(window, containerSize, LeftShift::new);
-            case RightShift(_) -> fromWindowSingle(window, containerSize, RightShift::new);
-            case Double(_) -> fromWindowSingle(window, containerSize, Double::new);
-            case Click.OffhandSwap(_) -> fromWindowSingle(window, containerSize, OffhandSwap::new);
-            case Click.DropSlot(_, boolean all) -> fromWindowSingle(window, containerSize, s -> new DropSlot(s, all));
-            case Click.HotbarSwap(int hotbarSlot, _) -> fromWindowSingle(window, containerSize, s -> new HotbarSwap(hotbarSlot, s));
+            case Left(int slot) -> fromWindowSingle(window, containerSize, Left::new);
+            case Right(int slot) -> fromWindowSingle(window, containerSize, Right::new);
+            case Middle(int slot) -> fromWindowSingle(window, containerSize, Middle::new);
+            case LeftShift(int slot) -> fromWindowSingle(window, containerSize, LeftShift::new);
+            case RightShift(int slot) -> fromWindowSingle(window, containerSize, RightShift::new);
+            case Double(int slot) -> fromWindowSingle(window, containerSize, Double::new);
+            case OffhandSwap(int slot) -> fromWindowSingle(window, containerSize, OffhandSwap::new);
+            case DropSlot(int slot, boolean all) -> fromWindowSingle(window, containerSize, s -> new DropSlot(s, all));
+            case HotbarSwap(int hotbarSlot, int slot) -> fromWindowSingle(window, containerSize, s -> new HotbarSwap(hotbarSlot, s));
 
             // Everything with zero slots
-            case Click.LeftDropCursor(), RightDropCursor(), MiddleDropCursor() -> window.click();
+            case LeftDropCursor() -> window.click();
+            case RightDropCursor() -> window.click();
+            case MiddleDropCursor() -> window.click();
 
             // Everything with multiple slots
-            case Click.LeftDrag(List<Integer> slots) -> fromWindowMultiple(window, slots, containerSize, LeftDrag::new);
-            case Click.RightDrag(List<Integer> slots) -> fromWindowMultiple(window, slots, containerSize, RightDrag::new);
-            case Click.MiddleDrag(List<Integer> slots) -> fromWindowMultiple(window, slots, containerSize, MiddleDrag::new);
+            case LeftDrag(List<Integer> slots) -> fromWindowMultiple(window, slots, containerSize, LeftDrag::new);
+            case RightDrag(List<Integer> slots) -> fromWindowMultiple(window, slots, containerSize, RightDrag::new);
+            case MiddleDrag(List<Integer> slots) -> fromWindowMultiple(window, slots, containerSize, MiddleDrag::new);
         };
     }
 
