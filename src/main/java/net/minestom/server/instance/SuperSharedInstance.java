3bcdb5d1497f028f089f6c0bd48c8c008df52abb
hapily04 <hapily04@gmail.com>
2025-01-09 18:08:00 -0800

    fix: getPlayers() returning all player entities including instanceContainer, causing players to be sent packets not from their instance

--- src/main/java/net/minestom/server/instance/SuperSharedInstance.java
+++ src/main/java/net/minestom/server/instance/SuperSharedInstance.java
@@ -1,8 +1,12 @@
 package net.minestom.server.instance;
 
 import net.minestom.server.entity.Entity;
+import net.minestom.server.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -17,5 +21,15 @@ public class SuperSharedInstance extends SharedInstance {
         for (Entity e : instanceContainer.getEntities()) {
             entityTracker.register(e, e.getPosition(), e.getTrackingTarget(), e.getTrackingUpdate());
         }
+    }
+
+    @Override
+    public @NotNull Set<@NotNull Player> getPlayers() {
+        Set<Player> allPlayers = super.getPlayers();
+        Set<Player> playersHere = new HashSet<>();
+        for (Player p : allPlayers) {
+            if (p.getInstance().equals(this)) playersHere.add(p);
+        }
+        return Collections.unmodifiableSet(playersHere);
     }
 }
