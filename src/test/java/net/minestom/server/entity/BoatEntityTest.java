92055fa157ac367e7db90d6421632750011b428f
Phillipp Glanz <p.glanz@madfix.me>
2025-01-20 07:48:09 +0100

    test: Ref issue for new test

--- src/test/java/net/minestom/server/entity/BoatEntityTest.java
+++ src/test/java/net/minestom/server/entity/BoatEntityTest.java
@@ -4,13 +4,17 @@ import net.minestom.server.coordinate.Pos;
 import net.minestom.server.instance.Instance;
 import net.minestom.testing.Env;
 import net.minestom.testing.EnvTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.Issue;
 import org.mockito.Mockito;
 
 @EnvTest
 class BoatEntityTest {
 
     @Test
+    @DisplayName("Test if velocity packet is not sent after 20 ticks if the entity a boat")
+    @Issue("1880")
     void testIfVelocityPacketIsNotSent(Env env) {
         final Entity entity = Mockito.spy(new Entity(EntityTypes.ACACIA_BOAT));
         Instance flatInstance = env.createFlatInstance();
@@ -22,6 +26,8 @@ class BoatEntityTest {
     }
 
     @Test
+    @DisplayName("Test if velocity packet is sent after 20 ticks if the entity is not a boat")
+    @Issue("1880")
     void testIfVelocityPacketGetSent(Env env) {
         Instance flatInstance = env.createFlatInstance();
         final Entity entity = Mockito.spy(new Entity(EntityTypes.ZOMBIE));
