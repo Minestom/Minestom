a60f5afca0c71093b92c27bbf7374daa673f66c2
GreatWyrm <alecmusante@gmail.com>
2025-01-19 17:20:17 -0800

    Removed unused import

--- code-generators/src/main/java/net/minestom/codegen/ConstantsGenerator.java
+++ code-generators/src/main/java/net/minestom/codegen/ConstantsGenerator.java
@@ -12,7 +12,6 @@ import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
-import java.util.Map;
 
 public class ConstantsGenerator extends MinestomCodeGenerator {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConstantsGenerator.class);
