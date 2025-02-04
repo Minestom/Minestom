83a7cdef309dc02deddfc502398d94b96bdcd763
Koboo <admin@koboo.eu>
2025-02-04 17:09:38 +0100

    feat: Update InventoryOpenStateTest

--- src/test/java/net/minestom/server/inventory/InventoryOpenStateTest.java
+++ src/test/java/net/minestom/server/inventory/InventoryOpenStateTest.java
@@ -4,11 +4,8 @@ import net.kyori.adventure.text.Component;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.event.EventNode;
-import net.minestom.server.event.entity.projectile.ProjectileCollideWithEntityEvent;
 import net.minestom.server.event.inventory.InventoryCloseEvent;
-import net.minestom.server.event.inventory.InventorySwapEvent;
-import net.minestom.server.network.packet.client.play.ClientCloseWindowPacket;
-import net.minestom.server.network.packet.server.play.CloseWindowPacket;
+import net.minestom.server.event.inventory.InventoryOpenEvent;
 import net.minestom.testing.Env;
 import net.minestom.testing.EnvTest;
 import org.junit.jupiter.api.Test;
@@ -18,7 +15,7 @@ import java.util.concurrent.atomic.AtomicReference;
 import static org.junit.jupiter.api.Assertions.*;
 
 @EnvTest
-public class InventorySwapStateTest {
+public class InventoryOpenStateTest {
 
     @Test
     public void doNotMismatchOldAndNewInventoryOnSwappingInventories(Env env) {
@@ -27,31 +24,36 @@ public class InventorySwapStateTest {
         var player = connection.connect(instance, new Pos(0, 42, 0));
         assertEquals(instance, player.getInstance());
 
-        final var eventRef = new AtomicReference<InventorySwapEvent>();
+        final var eventRef = new AtomicReference<InventoryOpenEvent>();
         final var eventNode = EventNode.all("inventory-swap-test");
-        eventNode.addListener(InventorySwapEvent.class, event -> {
-            eventRef.set(event);
-            MinecraftServer.getGlobalEventHandler().removeChild(eventNode);
-        });
+        eventNode.addListener(InventoryOpenEvent.class, eventRef::set);
         MinecraftServer.getGlobalEventHandler().addChild(eventNode);
+
+        // We are asserting, after the processing of the InventoryOpenEvent,
+        // so the event.getInventory() and event.getOldInventory() should always
+        // be the same as the inventory passen on player.openInventory(Inventory inventory)
 
         var firstInventory = new Inventory(InventoryType.CHEST_2_ROW, Component.text("First inventory"));
         player.openInventory(firstInventory);
+
+        final var firstEvent = eventRef.get();
+        assertNotNull(firstEvent, "Event wasn't called");
+        assertNotNull(firstEvent.getInventory(), "Opened inventory was null.");
+        assertNotNull(firstEvent.getOldInventory(), "Old inventory wasn't null.");
+        assertEquals(firstInventory, firstEvent.getInventory(), "Opened inventory wasn't the first inventory");
+        assertEquals(firstInventory, firstEvent.getOldInventory(), "Old inventory wasn't the second inventory");
+
         var secondInventory = new Inventory(InventoryType.CHEST_2_ROW, Component.text("Second inventory"));
         player.openInventory(secondInventory);
 
-        final var event = eventRef.get();
-        assertNotNull(event, "Event wasn't called");
-        assertNotNull(event.getOldInventory(), "Could not get old inventory.");
-        assertNotNull(event.getNewInventory(), "Could not get new inventory.");
-        assertNotNull(event.getPlayer(), "Could not get player.");
-        assertEquals(player, event.getPlayer(), "Wrong player in event.");
-        assertNotNull(event.getPlayer().getOpenInventory(), "Could not get players open inventory.");
-        assertNotEquals(firstInventory, event.getPlayer().getOpenInventory(), "First inventory should not be the open inventory.");
-        assertEquals(secondInventory, event.getPlayer().getOpenInventory(), "Second inventory should be the open inventory.");
-        assertEquals(firstInventory, event.getOldInventory(), "First opened inventory should be the old inventory.");
-        assertEquals(secondInventory, event.getNewInventory(), "Second opened inventory should be the new inventory.");
-        assertNotEquals(event.getOldInventory(), event.getNewInventory(), "Old inventory should not be the new inventory.");
+        final var secondEvent = eventRef.get();
+        assertNotNull(secondEvent, "Event wasn't called");
+        assertNotNull(secondEvent.getInventory(), "Opened inventory was null.");
+        assertNotNull(secondEvent.getOldInventory(), "Old inventory was null.");
+        assertEquals(secondInventory, secondEvent.getInventory(), "Opened inventory wasn't the second inventory");
+        assertEquals(secondInventory, secondEvent.getOldInventory(), "Old inventory wasn't the second inventory");
+
+        MinecraftServer.getGlobalEventHandler().removeChild(eventNode);
     }
 
     @Test
