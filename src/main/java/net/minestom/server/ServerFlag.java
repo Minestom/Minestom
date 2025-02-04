c06118b288e7605b4449780469fa59d0a41a0fed
Matt Worzala <35708499+mworzala@users.noreply.github.com>
2025-02-04 11:15:31 -0500

    chore: extend known packs timeout & make it configurable (#2650)

--- src/main/java/net/minestom/server/ServerFlag.java
+++ src/main/java/net/minestom/server/ServerFlag.java
@@ -20,7 +20,8 @@ public final class ServerFlag {
     public static final int ENTITY_SYNCHRONIZATION_TICKS = intProperty("minestom.entity-synchronization-ticks", 20);
     public static final int DISPATCHER_THREADS = intProperty("minestom.dispatcher-threads", 1);
     public static final int SEND_LIGHT_AFTER_BLOCK_PLACEMENT_DELAY = intProperty("minestom.send-light-after-block-placement-delay", 100);
-    public static final long LOGIN_PLUGIN_MESSAGE_TIMEOUT = longProperty("minestom.login-plugin-message-timeout", 5_000);
+    public static final long LOGIN_PLUGIN_MESSAGE_TIMEOUT = longProperty("minestom.login-plugin-message-timeout", 5_000); // 5s
+    public static final long KNOWN_PACKS_RESPONSE_TIMEOUT = longProperty("minestom.known-packs-response-timeout", 5 * 60_000); // 5m
 
     // Network rate limiting
     public static final int PLAYER_PACKET_PER_TICK = intProperty("minestom.packet-per-tick", 50);
