3f20a5be2b823e872d0e7a558728a85a2a0574d8
Ynverxe <nahubar65@gmail.com>
2025-02-16 22:12:02 -0300

    test: add test to verify argument callback execution and fix compilation errors caused by the past commit

--- src/test/java/net/minestom/server/command/ArgumentTest.java
+++ src/test/java/net/minestom/server/command/ArgumentTest.java
@@ -1,12 +1,21 @@
 package net.minestom.server.command;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.command.builder.ArgumentCallback;
+import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.CommandContext;
 import net.minestom.server.command.builder.arguments.Argument;
 import net.minestom.server.command.builder.arguments.ArgumentType;
 import net.minestom.server.command.builder.suggestion.Suggestion;
 import net.minestom.server.command.builder.suggestion.SuggestionEntry;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.utils.location.RelativeVec;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -52,5 +61,53 @@ public class ArgumentTest {
         arg.getSuggestionCallback().apply(new ServerSender(), new CommandContext("input"), suggestion);
 
         assertEquals(suggestion.getEntries(), List.of(new SuggestionEntry("entry")));
+    }
+
+    @Test
+    public void testArgumentCallbackExecution() {
+        MinecraftServer.init();
+
+        CommandManager commandManager = MinecraftServer.getCommandManager();
+        List<String> executedCallbacks = new ArrayList<>();
+
+        Command test = new Command("test");
+
+        ArgumentCallback defaultNumberCallback = (sender, exception) -> {
+            executedCallbacks.add("defaultNumberCallback");
+        };
+        Argument<Integer> integer = ArgumentType.Integer("number");
+        integer.setCallback(defaultNumberCallback);
+
+        // /test <integer>
+        test.addSyntax((sender, context) -> {}, integer);
+
+        Argument<RelativeVec> vec = ArgumentType.RelativeVec3("vec");
+        // /test <vec>
+        test.addSyntax((sender, context) -> {}, vec);
+
+        Argument<Integer> firstInt = ArgumentType.Integer("firstInt");
+        firstInt.setCallback(defaultNumberCallback);
+        Argument<Integer> secondInt = ArgumentType.Integer("secondInt");
+        secondInt.setCallback(defaultNumberCallback);
+
+        // /test <integer> <integer>
+        test.addSyntax((sender, context) -> {}, firstInt, secondInt);
+
+        test.setFallbackArgumentCallback((sender, exception) -> {
+            executedCallbacks.add("fallbackArgumentCallback");
+        });
+
+        // /test <integer> -> fallback argument callback should be executed if the 1st argument cannot be parsed
+        //       <vec> -> same as <integer>
+        //       <firstInt> <secondInt> -> <firstInt> has the same behaviour of <integer>, but if the second argument cannot be parsed,
+        //                                 its argument callback will be used
+        //                                 because there's no other parser that attempts to parse the second argument input
+
+        commandManager.register(test);
+
+        commandManager.executeServerCommand("test text");
+        assertTrue(executedCallbacks.contains("fallbackArgumentCallback"));
+        commandManager.executeServerCommand("test 1 text");
+        assertTrue(executedCallbacks.contains("defaultNumberCallback"));
     }
 }
