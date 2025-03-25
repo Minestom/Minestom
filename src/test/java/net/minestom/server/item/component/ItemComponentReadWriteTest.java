4ac3c24f01d57f1bdec6ebe532c3154cb19800fc
mworzala <mattheworzala@gmail.com>
2025-03-25 12:54:14 -0400

    fix: item component related test fixes

--- src/test/java/net/minestom/server/item/component/ItemComponentReadWriteTest.java
+++ src/test/java/net/minestom/server/item/component/ItemComponentReadWriteTest.java
@@ -1,9 +1,8 @@
 package net.minestom.server.item.component;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import net.kyori.adventure.nbt.BinaryTag;
-import net.kyori.adventure.nbt.TagStringIOExt;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.codec.Transcoder;
 import net.minestom.server.component.DataComponent;
@@ -26,11 +25,11 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ItemComponentReadWriteTest {
     private static final Gson GSON = new Gson();
 
-    private static final Transcoder<BinaryTag> CODER;
+    private static final Transcoder<JsonElement> CODER;
 
     static {
         MinecraftServer.init();
-        CODER = new RegistryTranscoder<>(Transcoder.NBT, MinecraftServer.process());
+        CODER = new RegistryTranscoder<>(Transcoder.JSON, MinecraftServer.process());
     }
 
     // This test will go through all of the default components present on vanilla items and make sure that we are
@@ -39,8 +38,8 @@ public class ItemComponentReadWriteTest {
     // Notably this does not test every component because they are not all used in vanilla, let alone on default items.
     //
     // Additional entries can be added by appending them to the following list:
-    private static final Map<String, String> EXTRA_CASES = Map.ofEntries(
-            entry("minecraft:glider", "{}")
+    private static final Map<String, JsonElement> EXTRA_CASES = Map.ofEntries(
+            entry("minecraft:glider", new JsonObject())
     );
 
     @Test
@@ -52,7 +51,7 @@ public class ItemComponentReadWriteTest {
             var object = GSON.fromJson(new InputStreamReader(is), JsonObject.class);
             for (var itemEntry : object.entrySet()) {
                 for (var componentEntry : itemEntry.getValue().getAsJsonObject().getAsJsonObject("components").entrySet()) {
-                    componentEntries.add(entry(componentEntry.getKey(), componentEntry.getValue().getAsString()));
+                    componentEntries.add(entry(componentEntry.getKey(), componentEntry.getValue()));
                 }
             }
         }
@@ -65,16 +64,18 @@ public class ItemComponentReadWriteTest {
         }));
     }
 
-    private static void readWriteTestImpl(@NotNull DataComponent<Object> component, @NotNull String input) {
+    private static void readWriteTestImpl(@NotNull DataComponent<Object> component, @NotNull JsonElement input) {
         try {
-            var nbt = TagStringIOExt.readTag(input);
-            var value = assertOk(component.decode(CODER, nbt));
+            var value = assertOk(component.decode(CODER, input));
             var actual = assertOk(component.encode(CODER, value));
+            // This is pretty cursed but we need to serialize and reparse because the JsonPrimitive number implementation changes
+            // When reading from a string it has LazilyParsedNumber which is NOT equal to `new JsonPrimitive(1)` for example.
+            var actualParsed = GSON.fromJson(actual.toString(), JsonElement.class);
 
             // Need to rewrite because adventure formats slightly different from vanilla.
-            assertEquals(nbt, actual, () -> "\n--- " + component.name() + " (NBT) ---\n" +
-                    "EXP: " + TagStringIOExt.writeTag(nbt) + "\n" +
-                    "ACT: " + TagStringIOExt.writeTag(actual));
+            assertEquals(input, actualParsed, () -> "\n--- " + component.name() + " (NBT) ---\n" +
+                    "EXP: " + input + "\n" +
+                    "ACT: " + actualParsed.toString());
 
             if (component.isSynced()) {
                 try {
@@ -83,8 +84,8 @@ public class ItemComponentReadWriteTest {
                     var comp2 = component.read(buffer);
                     var expected2 = assertOk(component.encode(CODER, comp2));
                     assertEquals(expected2, actual, () -> "\n--- " + component.name() + " (NETWORK) ---\n" +
-                            "EXP: " + TagStringIOExt.writeTag(expected2) + "\n" +
-                            "ACT: " + TagStringIOExt.writeTag(actual));
+                            "EXP: " + expected2 + "\n" +
+                            "ACT: " + actual);
                 } catch (UnsupportedOperationException ignored) {
                     // TODO(1.21.5) implement ObjectSet network type writer
                 }
