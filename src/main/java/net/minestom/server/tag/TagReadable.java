51f76e70241dc066c9702ffb3fc6d07cc351def0
unjoinable <135444615+unjoinable@users.noreply.github.com>
2025-04-24 23:52:05 +0530

    Improved docs in TagReadable
    Edited the docs to tell that if a Tag has default value then TagReadable#hasTag will always return true even if the tag is not present on the item

--- src/main/java/net/minestom/server/tag/TagReadable.java
+++ src/main/java/net/minestom/server/tag/TagReadable.java
@@ -18,10 +18,10 @@ public interface TagReadable {
     <T> @UnknownNullability T getTag(@NotNull Tag<T> tag);
 
     /**
-     * Returns if a tag is present.
+     * Returns if a tag is present or has a default value, returning true in both cases.
      *
      * @param tag the tag to check
-     * @return true if the tag is present, false otherwise
+     * @return true if the tag is explicitly present or has a default value, false only if the tag is not present and has no default value
      */
     default boolean hasTag(@NotNull Tag<?> tag) {
         return getTag(tag) != null;
