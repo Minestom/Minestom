6ef7e5c57a3dcfe5657a45698099773f76c57b79
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2025-05-06 12:15:17 -0400

    Uncomment and fix failing tests

--- src/main/java/net/minestom/server/instance/block/predicate/BlockPredicate.java
+++ src/main/java/net/minestom/server/instance/block/predicate/BlockPredicate.java
@@ -68,7 +68,7 @@ public record BlockPredicate(
             "blocks", BlockTypeFilter.CODEC.optional(), BlockPredicate::blocks,
             "state", PropertiesPredicate.CODEC.optional(), BlockPredicate::state,
             "nbt", Codec.NBT_COMPOUND.optional(), BlockPredicate::nbt,
-            StructCodec.INLINE, DataComponentPredicates.CODEC, BlockPredicate::componentPredicates,
+            StructCodec.INLINE, DataComponentPredicates.CODEC.optional(), BlockPredicate::componentPredicates,
             BlockPredicate::new
     );
 
