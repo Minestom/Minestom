a3ba617f4fb90cc646c355386c4911d9317223ce
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2025-05-05 21:33:04 -0400

    Remove unused network type

--- src/main/java/net/minestom/server/instance/block/predicate/Bounds.java
+++ src/main/java/net/minestom/server/instance/block/predicate/Bounds.java
@@ -2,11 +2,7 @@ package net.minestom.server.instance.block.predicate;
 
 import net.minestom.server.codec.Codec;
 import net.minestom.server.codec.StructCodec;
-import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.NetworkBufferTemplate;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.function.BiFunction;
 
 public interface Bounds<N extends Number> {
 
@@ -26,35 +22,8 @@ public interface Bounds<N extends Number> {
         return oneSided.orElse(twoSided);
     }
 
-    static <N extends Number, R extends Bounds<N>> NetworkBuffer.Type<R> createNetworkType(NetworkBuffer.Type<N> type, BiFunction<N, N, R> ctor) {
-        return new NetworkBuffer.Type<>() {
-            private static final byte MIN_FLAG = 1;
-            private static final byte MAX_FLAG = 2;
-
-            @Override
-            public void write(@NotNull NetworkBuffer buffer, R value) {
-                NetworkBuffer.BYTE.write(buffer, (byte) ((value.min() != null ? MIN_FLAG : 0) | (value.max() != null ? MAX_FLAG : 0)));
-                if (value.min() != null) {
-                    type.write(buffer, value.min());
-                }
-                if (value.max() != null) {
-                    type.write(buffer, value.max());
-                }
-            }
-
-            @Override
-            public R read(@NotNull NetworkBuffer buffer) {
-                byte flags = NetworkBuffer.BYTE.read(buffer);
-                N min = (flags & MIN_FLAG) != 0 ? type.read(buffer) : null;
-                N max = (flags & MAX_FLAG) != 0 ? type.read(buffer) : null;
-                return ctor.apply(min, max);
-            }
-        };
-    }
-
     record IntBounds(Integer min, Integer max) implements Bounds<Integer> {
         public static final Codec<IntBounds> CODEC = Bounds.createCodec(Codec.INT, IntBounds::new);
-        public static final NetworkBuffer.Type<IntBounds> NETWORK_TYPE = Bounds.createNetworkType(NetworkBuffer.VAR_INT, IntBounds::new);
         public static final IntBounds ANY = new IntBounds(null, null);
 
         public boolean matches(int i) {
@@ -64,7 +33,6 @@ public interface Bounds<N extends Number> {
 
     record DoubleBounds(Double min, Double max) implements Bounds<Double> {
         public static Codec<DoubleBounds> CODEC = Bounds.createCodec(Codec.DOUBLE, DoubleBounds::new);
-        public static NetworkBuffer.Type<DoubleBounds> NETWORK_TYPE = Bounds.createNetworkType(NetworkBuffer.DOUBLE, DoubleBounds::new);
         public static final DoubleBounds ANY = new DoubleBounds(null, null);
 
         public boolean matches(double d) {
