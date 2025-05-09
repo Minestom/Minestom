85471cab823cbef27f1aef7b76c943eed68a24cd
kermandev <46640204+kermandev@users.noreply.github.com>
2025-05-09 15:16:45 -0500

    Remove extra import

--- src/main/java/net/minestom/server/codec/ComponentCodecs.java
+++ src/main/java/net/minestom/server/codec/ComponentCodecs.java
@@ -11,7 +11,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.Map;
 
 @ApiStatus.Internal
 public final class ComponentCodecs {
