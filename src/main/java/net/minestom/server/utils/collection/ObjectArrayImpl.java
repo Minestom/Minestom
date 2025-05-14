08f4bb32a2c3c5f5a1823ec913ab5be47b385215
kermandev <46640204+kermandev@users.noreply.github.com>
2025-05-14 13:38:23 -0500

    Non-breaking: Add ObjectArray#toList
    List#of requires all elements to not be null;
    
    Implements a remove method,
    Also improves its trim by calculation by starting from -1 to get 0 size arrays, Also allows walking back the array to cut all trailing nulls as the behavior of #get just returns null.
    Mark the max as volatile as it's used in arrayCopy and toList.

--- src/main/java/net/minestom/server/utils/collection/ObjectArrayImpl.java
+++ src/main/java/net/minestom/server/utils/collection/ObjectArrayImpl.java
@@ -5,11 +5,12 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.UnknownNullability;
 
 import java.util.Arrays;
+import java.util.List;
 
 final class ObjectArrayImpl {
     static final class SingleThread<T> implements ObjectArray<T> {
         private T[] array;
-        private int max;
+        private int max = -1;
 
         SingleThread(int size) {
             //noinspection unchecked
@@ -24,6 +25,10 @@ final class ObjectArrayImpl {
 
         @Override
         public void set(int index, @Nullable T object) {
+            if (object == null) {
+                remove(index);
+                return;
+            }
             T[] array = this.array;
             if (index >= array.length) {
                 final int newLength = index * 2 + 1;
@@ -31,6 +36,23 @@ final class ObjectArrayImpl {
             }
             array[index] = object;
             this.max = Math.max(max, index);
+        }
+
+        @Override
+        public void remove(int index) {
+            final T[] array = this.array;
+            if (index >= array.length) return; // Will be null anyway
+            array[index] = null;
+            // Now we need to backtrack the max index,
+            // For example [0, 1, 2, null, 4] removing 4 requires us to backtrack past the null
+            final int max = this.max;
+            if (max == index) {
+                int lastNotNull = max - 1;
+                while (lastNotNull >= 0 && array[lastNotNull] == null) {
+                    lastNotNull--;
+                }
+                this.max = lastNotNull;
+            }
         }
 
         @Override
@@ -43,11 +65,18 @@ final class ObjectArrayImpl {
             //noinspection unchecked,rawtypes
             return (T[]) Arrays.<T, T>copyOf(array, max + 1, (Class) type.arrayType());
         }
+
+        @Override
+        public @NotNull List<@NotNull T> toList() {
+            // Trim the array to the maximum size, it internally will be copied regardless.
+            final T[] array = Arrays.copyOf(this.array, max + 1);
+            return List.of(array);
+        }
     }
 
     static final class Concurrent<T> implements ObjectArray<T> {
         private volatile T[] array;
-        private int max;
+        private volatile int max = -1;
 
         Concurrent(int size) {
             //noinspection unchecked
@@ -62,6 +91,10 @@ final class ObjectArrayImpl {
 
         @Override
         public synchronized void set(int index, @Nullable T object) {
+            if (object == null) {
+                remove(index);
+                return;
+            }
             T[] array = this.array;
             if (index >= array.length) {
                 final int newLength = index * 2 + 1;
@@ -69,6 +102,23 @@ final class ObjectArrayImpl {
             }
             array[index] = object;
             this.max = Math.max(max, index);
+        }
+
+        @Override
+        public synchronized void remove(int index) {
+            final T[] array = this.array;
+            if (index >= array.length) return; // Will be null anyway
+            array[index] = null;
+            // Now we need to backtrack the max index,
+            // For example [0, 1, 2, null, 4] removing 4 requires us to backtrack past the null
+            final int max = this.max;
+            if (max == index) {
+                int lastNotNull = max - 1;
+                while (lastNotNull >= 0 && array[lastNotNull] == null) {
+                    lastNotNull--;
+                }
+                this.max = lastNotNull;
+            }
         }
 
         @Override
@@ -80,6 +130,13 @@ final class ObjectArrayImpl {
         public @UnknownNullability T @NotNull [] arrayCopy(@NotNull Class<T> type) {
             //noinspection unchecked,rawtypes
             return (T[]) Arrays.<T, T>copyOf(array, max + 1, (Class) type.arrayType());
+        }
+
+        @Override
+        public @NotNull List<@NotNull T> toList() {
+            // Trim the array to the maximum size, it internally will be copied regardless.
+            final T[] array = Arrays.copyOf(this.array, this.max);
+            return List.of(array);
         }
     }
 }
