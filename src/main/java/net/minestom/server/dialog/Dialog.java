095d4f81a016e25571bc170da373d4fa026960a9
mworzala <mattheworzala@gmail.com>
2025-05-30 09:20:33 -0400

    chore: 1.21.6-pre1, sketching dialog types

--- src/main/java/net/minestom/server/dialog/Dialog.java
+++ src/main/java/net/minestom/server/dialog/Dialog.java
@@ -1,7 +1,24 @@
 package net.minestom.server.dialog;
 
+import org.jetbrains.annotations.NotNull;
+
 public sealed interface Dialog {
 
-    record Notice() implements Dialog {
+    record Notice(@NotNull DialogMetadata metadata) implements Dialog {
     }
+
+    record ServerLinks(@NotNull DialogMetadata metadata) implements Dialog {
+    }
+
+    record DialogList(@NotNull DialogMetadata metadata) implements Dialog {
+    }
+
+    record MultiAction(@NotNull DialogMetadata metadata) implements Dialog {
+    }
+
+    record Confirmation(@NotNull DialogMetadata metadata) implements Dialog {
+    }
+
+    @NotNull DialogMetadata metadata();
+    
 }
