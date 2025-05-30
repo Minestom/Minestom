90c97e5dbffa8a27f5f1249da04f4415f9035fa0
mworzala <mattheworzala@gmail.com>
2025-05-30 09:20:33 -0400

    chore: 1.21.6-pre1, sketching dialog types

--- src/main/java/net/minestom/server/network/packet/client/common/ClientCustomClickActionPacket.java
+++ src/main/java/net/minestom/server/network/packet/client/common/ClientCustomClickActionPacket.java
@@ -1,15 +1,16 @@
-package net.minestom.server.network.packet.client.play;
+package net.minestom.server.network.packet.client.common;
 
 import net.kyori.adventure.key.Key;
+import net.kyori.adventure.nbt.BinaryTag;
 import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.NetworkBufferTemplate;
 import net.minestom.server.network.packet.client.ClientPacket;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public record ClientCustomClickActionPacket(@NotNull Key key, @Nullable String payload) implements ClientPacket {
+public record ClientCustomClickActionPacket(@NotNull Key key, @Nullable BinaryTag payload) implements ClientPacket {
     public static final NetworkBuffer.Type<ClientCustomClickActionPacket> SERIALIZER = NetworkBufferTemplate.template(
             NetworkBuffer.KEY, ClientCustomClickActionPacket::key,
-            NetworkBuffer.STRING.optional(), ClientCustomClickActionPacket::payload,
+            NetworkBuffer.NBT.optional(), ClientCustomClickActionPacket::payload,
             ClientCustomClickActionPacket::new);
 }
