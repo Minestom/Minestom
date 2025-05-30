43da0555437ce69149ab7f0cbc6318c42b5caf78
mworzala <mattheworzala@gmail.com>
2025-05-30 10:34:34 -0400

    feat: wip initial dialog type definitions

--- src/main/java/net/minestom/server/dialog/DialogInput.java
+++ src/main/java/net/minestom/server/dialog/DialogInput.java
@@ -1,17 +1,135 @@
 package net.minestom.server.dialog;
 
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.Component;
+import net.minestom.server.codec.Codec;
+import net.minestom.server.codec.StructCodec;
+import net.minestom.server.registry.DynamicRegistry;
+import net.minestom.server.registry.Registry;
+import net.minestom.server.utils.validate.Check;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Map;
+
 public sealed interface DialogInput {
+    @NotNull Registry<StructCodec<? extends DialogInput>> REGISTRY = DynamicRegistry.fromMap("input_control_type",
+            Map.entry(Key.key("boolean"), Boolean.CODEC),
+            Map.entry(Key.key("number_range"), NumberRange.CODEC),
+            Map.entry(Key.key("single_option"), SingleOption.CODEC),
+            Map.entry(Key.key("text"), Text.CODEC));
+    @NotNull StructCodec<DialogInput> CODEC = Codec.RegistryTaggedUnion(REGISTRY, DialogInput::codec, "type");
 
-    record Boolean() implements DialogInput {
+    record Boolean(
+            @NotNull Component label,
+            boolean initial,
+            @NotNull String onTrue,
+            @NotNull String onFalse
+    ) implements DialogInput {
+        public static final StructCodec<Boolean> CODEC = StructCodec.struct(
+                "label", Codec.COMPONENT, Boolean::label,
+                "initial", StructCodec.BOOLEAN.optional(false), Boolean::initial,
+                "on_true", StructCodec.STRING.optional("true"), Boolean::onTrue,
+                "on_false", StructCodec.STRING.optional("false"), Boolean::onFalse,
+                Boolean::new);
+
+        @Override
+        public @NotNull StructCodec<? extends DialogInput> codec() {
+            return CODEC;
+        }
     }
 
-    record NumberRange() implements DialogInput {
+    record NumberRange(
+            int width,
+            @NotNull Component label,
+            @NotNull String labelFormat,
+            float start, float end,
+            @Nullable Float initial,
+            @Nullable Float step
+    ) implements DialogInput {
+        public static final StructCodec<NumberRange> CODEC = StructCodec.struct(
+                "width", Codec.INT.optional(200), NumberRange::width,
+                "label", Codec.COMPONENT, NumberRange::label,
+                "label_format", Codec.STRING.optional("options.generic_value"), NumberRange::labelFormat,
+                "start", Codec.FLOAT, NumberRange::start,
+                "end", Codec.FLOAT, NumberRange::end,
+                "initial", Codec.FLOAT.optional(), NumberRange::initial,
+                "step", Codec.FLOAT.optional(), NumberRange::step,
+                NumberRange::new);
+
+        @Override
+        public @NotNull StructCodec<? extends DialogInput> codec() {
+            return CODEC;
+        }
     }
 
-    record SingleOption() implements DialogInput {
+    record SingleOption(
+            int width,
+            @NotNull List<Option> options,
+            @NotNull Component label,
+            boolean labelVisible
+    ) implements DialogInput {
+        public static final StructCodec<SingleOption> CODEC = StructCodec.struct(
+                "width", Codec.INT.optional(200), SingleOption::width,
+                "options", Option.CODEC.list(), SingleOption::options,
+                "label", Codec.COMPONENT, SingleOption::label,
+                "label_visible", Codec.BOOLEAN.optional(true), SingleOption::labelVisible,
+                SingleOption::new);
+
+        public SingleOption {
+            boolean found = false;
+            for (var option : options) {
+                if (!option.initial) continue;
+                Check.argCondition(found, "Multiple initial options found in SingleOption input");
+                found = true;
+            }
+        }
+
+        @Override
+        public @NotNull StructCodec<? extends DialogInput> codec() {
+            return CODEC;
+        }
+
+        public record Option(@NotNull String id, @Nullable Component display, boolean initial) {
+            public static final StructCodec<Option> CODEC = StructCodec.struct(
+                    "id", Codec.STRING, Option::id,
+                    "display", Codec.COMPONENT.optional(), Option::display,
+                    "initial", Codec.BOOLEAN.optional(false), Option::initial,
+                    Option::new);
+        }
     }
 
-    record Text() implements DialogInput {
+    record Text(
+            int width,
+            @NotNull Component label,
+            boolean labelVisible,
+            @NotNull String initial,
+            int maxLength,
+            @Nullable Multiline multiline
+    ) implements DialogInput {
+        public static final StructCodec<Text> CODEC = StructCodec.struct(
+                "width", Codec.INT.optional(200), Text::width,
+                "label", Codec.COMPONENT, Text::label,
+                "label_visible", Codec.BOOLEAN.optional(true), Text::labelVisible,
+                "initial", Codec.STRING.optional(""), Text::initial,
+                "max_length", Codec.INT.optional(32), Text::maxLength,
+                "multiline", Multiline.CODEC.optional(), Text::multiline,
+                Text::new);
+
+        @Override
+        public @NotNull StructCodec<? extends DialogInput> codec() {
+            return CODEC;
+        }
+
+        public record Multiline(@Nullable Integer maxLines, @Nullable Integer height) {
+            public static final StructCodec<Multiline> CODEC = StructCodec.struct(
+                    "max_lines", Codec.INT.optional(), Multiline::maxLines,
+                    "height", Codec.INT.optional(), Multiline::height,
+                    Multiline::new);
+        }
     }
-    
+
+    @NotNull StructCodec<? extends DialogInput> codec();
+
 }
