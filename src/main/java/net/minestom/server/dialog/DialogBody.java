fa6d9e7135b2d7bb025019098514d2963ca01306
mworzala <mattheworzala@gmail.com>
2025-06-01 14:11:18 -0400

    feat: dialog types, registry rework (needs extraction to 1.21.5)

--- src/main/java/net/minestom/server/dialog/DialogBody.java
+++ src/main/java/net/minestom/server/dialog/DialogBody.java
@@ -13,7 +13,8 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Map;
 
 public sealed interface DialogBody {
-    @NotNull Registry<StructCodec<? extends DialogBody>> REGISTRY = DynamicRegistry.fromMap("dialog_body_type",
+    @NotNull Registry<StructCodec<? extends DialogBody>> REGISTRY = DynamicRegistry.fromMap(
+            Key.key("minecraft:dialog_body_type"),
             Map.entry(Key.key("item"), Item.CODEC),
             Map.entry(Key.key("plain_message"), PlainMessage.CODEC));
     @NotNull StructCodec<DialogBody> CODEC = Codec.RegistryTaggedUnion(REGISTRY, DialogBody::codec, "type");
@@ -41,7 +42,7 @@ public sealed interface DialogBody {
     }
 
     record PlainMessage(@NotNull Component contents, int width) implements DialogBody {
-        private static final int DEFAULT_WIDTH = 200;
+        public static final int DEFAULT_WIDTH = 200;
 
         private static final StructCodec<PlainMessage> COMPONENT_CODEC = StructCodec.struct(
                 StructCodec.INLINE, Codec.COMPONENT, PlainMessage::contents,
@@ -49,7 +50,7 @@ public sealed interface DialogBody {
         public static final StructCodec<PlainMessage> CODEC = StructCodec.struct(
                 "contents", Codec.COMPONENT, PlainMessage::contents,
                 "width", Codec.INT.optional(200), PlainMessage::width,
-                PlainMessage::new).orElse(COMPONENT_CODEC);
+                PlainMessage::new).orElseStruct(COMPONENT_CODEC);
 
         @Override
         public @NotNull StructCodec<? extends DialogBody> codec() {
