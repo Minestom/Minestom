d883eb9bd780a6c5f3c8b59c693cb9df998e3b29
themode <themode@Outlook.fr>
2025-06-17 17:41:20 +0200

    duplicate code with ArgumentBlockState

--- src/main/java/net/minestom/server/command/builder/arguments/minecraft/ArgumentBlockState.java
+++ src/main/java/net/minestom/server/command/builder/arguments/minecraft/ArgumentBlockState.java
@@ -1,10 +1,12 @@
 package net.minestom.server.command.builder.arguments.minecraft;
 
+import net.kyori.adventure.key.InvalidKeyException;
 import net.minestom.server.command.ArgumentParserType;
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.arguments.Argument;
 import net.minestom.server.command.builder.exception.ArgumentSyntaxException;
 import net.minestom.server.instance.block.Block;
+import net.minestom.server.utils.block.BlockUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class ArgumentBlockState extends Argument<Block> {
@@ -33,7 +35,39 @@ public class ArgumentBlockState extends Argument<Block> {
      */
     @Deprecated
     public static Block staticParse(@NotNull String input) throws ArgumentSyntaxException {
-        return Block.fromState(input);
+        final int nbtIndex = input.indexOf("[");
+        if (nbtIndex == 0)
+            throw new ArgumentSyntaxException("No block type", input, NO_BLOCK);
+
+        if (nbtIndex == -1) {
+            // Only block name
+            Block block;
+            try {
+                block = Block.fromKey(input);
+            } catch (InvalidKeyException ignored) {
+                block = null;
+            }
+            if (block == null)
+                throw new ArgumentSyntaxException("Invalid block type", input, INVALID_BLOCK);
+            return block;
+        } else {
+            if (!input.endsWith("]"))
+                throw new ArgumentSyntaxException("Property list need to end with ]", input, INVALID_PROPERTY);
+            // Block state
+            final String blockName = input.substring(0, nbtIndex);
+            Block block = Block.fromKey(blockName);
+            if (block == null)
+                throw new ArgumentSyntaxException("Invalid block type", input, INVALID_BLOCK);
+
+            // Compute properties
+            final String query = input.substring(nbtIndex);
+            final var propertyMap = BlockUtils.parseProperties(query);
+            try {
+                return block.withProperties(propertyMap);
+            } catch (IllegalArgumentException e) {
+                throw new ArgumentSyntaxException("Invalid property values", input, INVALID_PROPERTY_VALUE);
+            }
+        }
     }
 
     @Override
--- src/main/java/net/minestom/server/instance/block/BlockImpl.java
+++ src/main/java/net/minestom/server/instance/block/BlockImpl.java
@@ -3,10 +3,8 @@ package net.minestom.server.instance.block;
 import it.unimi.dsi.fastutil.longs.Long2ObjectArrayMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectMaps;
-import net.kyori.adventure.key.InvalidKeyException;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.nbt.CompoundBinaryTag;
-import net.minestom.server.command.builder.exception.ArgumentSyntaxException;
 import net.minestom.server.registry.Registry;
 import net.minestom.server.registry.RegistryData;
 import net.minestom.server.tag.Tag;
@@ -22,8 +20,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-
-import static net.minestom.server.command.builder.arguments.minecraft.ArgumentBlockState.*;
 
 record BlockImpl(@NotNull RegistryData.BlockEntry registry,
                  long propertiesArray,
@@ -121,38 +117,22 @@ record BlockImpl(@NotNull RegistryData.BlockEntry registry,
     }
 
     static @Nullable Block parseState(String input) {
+        if (input == null || input.isEmpty()) return null;
         final int nbtIndex = input.indexOf("[");
-        if (nbtIndex == 0)
-            throw new ArgumentSyntaxException("No block type", input, NO_BLOCK);
-
-        if (nbtIndex == -1) {
-            // Only block name
-            Block block;
-            try {
-                block = Block.fromKey(input);
-            } catch (InvalidKeyException ignored) {
-                block = null;
-            }
-            if (block == null)
-                throw new ArgumentSyntaxException("Invalid block type", input, INVALID_BLOCK);
-            return block;
-        } else {
-            if (!input.endsWith("]"))
-                throw new ArgumentSyntaxException("Property list need to end with ]", input, INVALID_PROPERTY);
-            // Block state
-            final String blockName = input.substring(0, nbtIndex);
-            Block block = Block.fromKey(blockName);
-            if (block == null)
-                throw new ArgumentSyntaxException("Invalid block type", input, INVALID_BLOCK);
-
-            // Compute properties
-            final String query = input.substring(nbtIndex);
-            final var propertyMap = BlockUtils.parseProperties(query);
-            try {
-                return block.withProperties(propertyMap);
-            } catch (IllegalArgumentException e) {
-                throw new ArgumentSyntaxException("Invalid property values", input, INVALID_PROPERTY_VALUE);
-            }
+        if (nbtIndex == 0) return null;
+        if (nbtIndex == -1) return Block.fromKey(input);
+        if (!input.endsWith("]")) return null;
+        // Block state
+        final String blockName = input.substring(0, nbtIndex);
+        Block block = Block.fromKey(blockName);
+        if (block == null) return null;
+        // Compute properties
+        final String query = input.substring(nbtIndex);
+        final Map<String, String> propertyMap = BlockUtils.parseProperties(query);
+        try {
+            return block.withProperties(propertyMap);
+        } catch (IllegalArgumentException e) {
+            return null;
         }
     }
 
