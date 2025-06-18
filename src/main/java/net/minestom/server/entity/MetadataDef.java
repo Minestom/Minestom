e770bb548a2419830adb0b793dc3148201e718d4
mworzala <mattheworzala@gmail.com>
2025-06-17 21:20:44 -0400

    fix: painting and item frame meta changes

--- src/main/java/net/minestom/server/entity/MetadataDef.java
+++ src/main/java/net/minestom/server/entity/MetadataDef.java
@@ -191,12 +191,16 @@ public sealed class MetadataDef {
         public static final Entry<Boolean> IS_SHOT_AT_ANGLE = index(2, Metadata::Boolean, false);
     }
 
-    public static final class ItemFrame extends MetadataDef {
+    public static sealed class Hanging extends MetadataDef {
+        public static final Entry<Direction> DIRECTION = index(0, Metadata::Direction, Direction.SOUTH);
+    }
+
+    public static final class ItemFrame extends Hanging {
         public static final Entry<ItemStack> ITEM = index(0, Metadata::ItemStack, ItemStack.AIR);
         public static final Entry<Integer> ROTATION = index(1, Metadata::VarInt, 0);
     }
 
-    public static final class Painting extends MetadataDef {
+    public static final class Painting extends Hanging {
         public static final Entry<Holder<PaintingVariant>> VARIANT = index(0, Metadata::PaintingVariant, PaintingVariant.KEBAB);
     }
 
