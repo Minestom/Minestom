fb3119f7ed4c2f47ffabcb8a6cd5ea34ca298915
kermandev <46640204+kermandev@users.noreply.github.com>
2025-06-18 11:47:20 -0500

    Revert workaround.

--- src/test/java/net/minestom/server/codec/ComponentCodecsTest.java
+++ src/test/java/net/minestom/server/codec/ComponentCodecsTest.java
@@ -13,7 +13,7 @@ class ComponentCodecsTest {
 
     @Test
     void readExpandFromStringInList() throws Exception {
-        var input = MinestomAdventure.tagStringIO().asCompound("{extra:[{color:\"red\",text:\"Hello\"},\" World\"],text:\"\"}");
+        var input = MinestomAdventure.tagStringIO().asTag("{extra:[{color:\"red\",text:\"Hello\"},\" World\"],text:\"\"}");
         var actual = ComponentCodecs.COMPONENT.decode(Transcoder.NBT, input).orElseThrow();
         var expected = Component.text()
                 .append(Component.text("Hello", NamedTextColor.RED))
