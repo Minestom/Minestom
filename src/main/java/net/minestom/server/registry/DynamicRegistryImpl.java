7eafa88391edbfe52b2531347a867eaa43106521
kermandev <46640204+kermandev@users.noreply.github.com>
2025-06-25 11:32:41 -0500

    fix(registry): Allow nullable pack in DynamicRegistry with unsafeOps

--- src/main/java/net/minestom/server/registry/DynamicRegistryImpl.java
+++ src/main/java/net/minestom/server/registry/DynamicRegistryImpl.java
@@ -135,12 +135,12 @@ final class DynamicRegistryImpl<T> implements DynamicRegistry<T> {
                 idToValue.add(object);
                 idToKey.add(registryKey);
                 keyToId.put(registryKey, idToValue.size() - 1);
-                packById.add(pack);
+                if (pack != null) packById.add(pack); // Dont add null packs, handled by getPack
             } else {
                 idToValue.set(id, object);
                 idToKey.set(id, registryKey);
                 keyToId.put(registryKey, id);
-                packById.set(id, pack);
+                packById.set(id, pack); //TODO determine way to handle this better if null.
             }
 
             vanillaRegistryDataPacket.invalidate();
@@ -268,6 +268,7 @@ final class DynamicRegistryImpl<T> implements DynamicRegistry<T> {
             packById = this.packById;
         }
         List<RegistryDataPacket.Entry> entries = new ArrayList<>(idToValue.size());
+        final int packByIdSize = packById.size();
         for (int i = 0; i < idToValue.size(); i++) {
             CompoundBinaryTag data = null;
             // sorta todo, sorta just a note:
@@ -279,7 +280,7 @@ final class DynamicRegistryImpl<T> implements DynamicRegistry<T> {
             // like material, block, etc generate entries which are behind feature flags, whereas the ones which inspect
             // static assets (the traditionally dynamic registries), do not generate those assets.
             T entry = idToValue.get(i);
-            DataPack pack = packById.get(i);
+            DataPack pack = i < packByIdSize ? packById.get(i) : null;
             if (!excludeVanilla || pack != DataPack.MINECRAFT_CORE) {
                 final Result<BinaryTag> entryResult = codec.encode(transcoder, entry);
                 if (entryResult instanceof Result.Ok(BinaryTag tag)) {
