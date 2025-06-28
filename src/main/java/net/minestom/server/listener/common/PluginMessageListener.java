ade5099e40a9ec1d799afb4ff019ff421af19561
mudkipdev <mudkip@mudkip.dev>
2025-06-28 19:47:14 +0200

    use listener handle

--- src/main/java/net/minestom/server/listener/common/PluginMessageListener.java
+++ src/main/java/net/minestom/server/listener/common/PluginMessageListener.java
@@ -2,6 +2,7 @@ package net.minestom.server.listener.common;
 
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.EventDispatcher;
+import net.minestom.server.event.ListenerHandle;
 import net.minestom.server.event.player.PlayerBrandEvent;
 import net.minestom.server.event.player.PlayerPluginMessageEvent;
 import net.minestom.server.network.NetworkBuffer;
@@ -9,12 +10,13 @@ import net.minestom.server.network.packet.client.common.ClientPluginMessagePacke
 
 public class PluginMessageListener {
     private static final String BRAND_CHANNEL = "minecraft:brand";
+    private static final ListenerHandle<PlayerBrandEvent> brandHandle = EventDispatcher.getHandle(PlayerBrandEvent.class);
 
     public static void listener(ClientPluginMessagePacket packet, Player player) {
         PlayerPluginMessageEvent pluginMessageEvent = new PlayerPluginMessageEvent(player, packet.channel(), packet.data());
         EventDispatcher.call(pluginMessageEvent);
 
-        if (BRAND_CHANNEL.equals(pluginMessageEvent.getIdentifier())) {
+        if (brandHandle.hasListener() && BRAND_CHANNEL.equals(pluginMessageEvent.getIdentifier())) {
             NetworkBuffer buffer = NetworkBuffer.wrap(packet.data(), 0, packet.data().length);
             EventDispatcher.call(new PlayerBrandEvent(player, buffer.read(NetworkBuffer.STRING)));
         }
