320b57e1515bcf53cff7ce79a1872d27d3b6cad4
kermandev <46640204+kermandev@users.noreply.github.com>
2025-07-04 17:04:40 -0500

    Fix PaintingVariant ordering

--- src/main/java/net/minestom/server/entity/metadata/other/PaintingVariant.java
+++ src/main/java/net/minestom/server/entity/metadata/other/PaintingVariant.java
@@ -14,19 +14,19 @@ import org.jetbrains.annotations.Nullable;
 
 public sealed interface PaintingVariant extends Holder.Direct<PaintingVariant>, PaintingVariants permits PaintingVariantImpl {
     @NotNull NetworkBuffer.Type<PaintingVariant> REGISTRY_NETWORK_TYPE = NetworkBufferTemplate.template(
-            NetworkBuffer.KEY, PaintingVariant::assetId,
             NetworkBuffer.INT, PaintingVariant::width,
             NetworkBuffer.INT, PaintingVariant::height,
+            NetworkBuffer.KEY, PaintingVariant::assetId,
             NetworkBuffer.COMPONENT.optional(), PaintingVariant::title,
             NetworkBuffer.COMPONENT.optional(), PaintingVariant::author,
-            PaintingVariant::create);
+            PaintingVariantImpl::new);
     @NotNull Codec<PaintingVariant> REGISTRY_CODEC = StructCodec.struct(
-            "asset_id", Codec.KEY, PaintingVariant::assetId,
             "width", Codec.INT, PaintingVariant::width,
             "height", Codec.INT, PaintingVariant::height,
+            "asset_id", Codec.KEY, PaintingVariant::assetId,
             "title", Codec.COMPONENT.optional(), PaintingVariant::title,
             "author", Codec.COMPONENT.optional(), PaintingVariant::author,
-            PaintingVariant::create);
+            PaintingVariantImpl::new);
 
     // For some unknown reason, the network type still uses a holder even though the codec does not.
     // This appears to be a mistake since stopping inline values was explicitly mentioned as a change in snapshot notes.
@@ -42,7 +42,7 @@ public sealed interface PaintingVariant extends Holder.Direct<PaintingVariant>, 
             @Nullable Component title,
             @Nullable Component author
     ) {
-        return new PaintingVariantImpl(assetId, width, height, title, author);
+        return new PaintingVariantImpl(width, height, assetId, title, author);
     }
 
     static @NotNull Builder builder() {
@@ -110,7 +110,7 @@ public sealed interface PaintingVariant extends Holder.Direct<PaintingVariant>, 
         }
 
         public @NotNull PaintingVariant build() {
-            return new PaintingVariantImpl(assetId, width, height, title, author);
+            return new PaintingVariantImpl(width, height, assetId, title, author);
         }
     }
 }
