d5ad4e76c3e94704df0b0f618094e33ad8d82e17
TheMode <themode@outlook.fr>
2025-07-11 18:06:13 +0200

    Slight event cleanup (#2821)

--- src/test/java/net/minestom/server/event/EventNodeTest.java
+++ src/test/java/net/minestom/server/event/EventNodeTest.java
@@ -151,24 +151,23 @@ public class EventNodeTest {
         assertTrue(called2.get());
     }
 
-    // FIXME: nodes are currently unable to retrieve sub handles
-    //@Test
-    //public void recursiveSuper() {
-    //    var node = EventNode.all("main");
-    //    AtomicBoolean result2 = new AtomicBoolean(false);
-    //    var listener2 = EventListener.of(Recursive2.class, event -> result2.set(true));
-    //    node.addListener(listener2);
-    //    node.call(new Recursive2());
-    //    assertTrue(result2.get(), "The event should be called after the call");
-    //
-    //    AtomicBoolean result1 = new AtomicBoolean(false);
-    //    var listener1 = EventListener.of(Recursive1.class, event -> result1.set(true));
-    //    node.addListener(listener1);
-    //    result2.set(false);
-    //    node.call(new Recursive2());
-    //    assertTrue(result2.get(), "Recursive2 should have been called directly");
-    //    assertTrue(result1.get(), "Recursive1 should be called due to the RecursiveEvent interface");
-    //}
+    @Test
+    public void recursiveSuper() {
+        var node = EventNode.all("main");
+        AtomicBoolean result2 = new AtomicBoolean(false);
+        var listener2 = EventListener.of(Recursive2.class, event -> result2.set(true));
+        node.addListener(listener2);
+        node.call(new Recursive2());
+        assertTrue(result2.get(), "The event should be called after the call");
+
+        AtomicBoolean result1 = new AtomicBoolean(false);
+        var listener1 = EventListener.of(Recursive1.class, event -> result1.set(true));
+        node.addListener(listener1);
+        result2.set(false);
+        node.call(new Recursive2());
+        assertTrue(result2.get(), "Recursive2 should have been called directly");
+        assertTrue(result1.get(), "Recursive1 should be called due to the RecursiveEvent interface");
+    }
 
     @Test
     public void testChildren() {
