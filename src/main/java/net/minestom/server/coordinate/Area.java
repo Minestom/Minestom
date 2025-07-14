24e7ac4903e8ccf2ec6f7dfb6ef1555b2adcd073
TheMode <themode@outlook.fr>
2025-07-14 20:46:02 +0200

    area offset

--- src/main/java/net/minestom/server/coordinate/Area.java
+++ src/main/java/net/minestom/server/coordinate/Area.java
@@ -1,7 +1,7 @@
 package net.minestom.server.coordinate;
 
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.NotNullByDefault;
 
 /**
  * Represents a collection of aligned block coordinates in a 3D space.
@@ -9,55 +9,74 @@ import org.jetbrains.annotations.NotNull;
  * If switched over, consider a fallback to the iterator as more implementations may be added in the future.
  */
 @ApiStatus.Experimental
+@NotNullByDefault
 public sealed interface Area extends Iterable<BlockVec> {
-    static @NotNull Area.Single single(@NotNull Point point) {
+
+    default Area offset(int x, int y, int z) {
+        return switch (this) {
+            case Single single -> single(single.point().add(x, y, z));
+            case Line line -> line(line.start().add(x, y, z), line.end().add(x, y, z));
+            case Cuboid cuboid -> cuboid(cuboid.min().add(x, y, z), cuboid.max().add(x, y, z));
+            case Sphere sphere -> sphere(sphere.center().add(x, y, z), sphere.radius());
+        };
+    }
+
+    default Area offset(Point offset) {
+        return offset(offset.blockX(), offset.blockY(), offset.blockZ());
+    }
+
+    static Area.Single single(Point point) {
         return new AreaImpl.Single(point.asBlockVec());
     }
 
-    static @NotNull Area.Line line(@NotNull Point start, @NotNull Point end) {
+    static Area.Single single(int x, int y, int z) {
+        return single(new BlockVec(x, y, z));
+    }
+
+    static Area.Line line(Point start, Point end) {
         return new AreaImpl.Line(start.asBlockVec(), end.asBlockVec());
     }
 
-    static @NotNull Area.Cuboid cuboid(@NotNull Point min, @NotNull Point max) {
+    static Area.Cuboid cuboid(Point min, Point max) {
         return new AreaImpl.Cuboid(min.asBlockVec(), max.asBlockVec());
     }
 
-    static Area.Cuboid cube(@NotNull Point center, int size) {
+    static Area.Cuboid cube(Point center, int size) {
         return cuboid(center.sub((double) size / 2), center.add((double) size / 2));
     }
 
-    static Area.Cuboid box(@NotNull Point center, @NotNull Point size) {
+    static Area.Cuboid box(Point center, Point size) {
         final Point half = size.div(2);
         return cuboid(center.sub(half), center.add(half));
     }
 
-    static @NotNull Area.Cuboid section(int sectionX, int sectionY, int sectionZ) {
+    static Area.Cuboid section(int sectionX, int sectionY, int sectionZ) {
         final Vec section = Vec.SECTION.mul(sectionX, sectionY, sectionZ);
         return cuboid(section, Vec.SECTION.add(section).sub(1));
     }
 
-    static @NotNull Area.Sphere sphere(@NotNull Point center, int radius) {
+    static Area.Sphere sphere(Point center, int radius) {
         return new AreaImpl.Sphere(center.asBlockVec(), radius);
     }
 
     sealed interface Single extends Area permits AreaImpl.Single {
-        @NotNull BlockVec point();
+        BlockVec point();
     }
 
     sealed interface Line extends Area permits AreaImpl.Line {
-        @NotNull BlockVec start();
+        BlockVec start();
 
-        @NotNull BlockVec end();
+        BlockVec end();
     }
 
     sealed interface Cuboid extends Area permits AreaImpl.Cuboid {
-        @NotNull BlockVec min();
+        BlockVec min();
 
-        @NotNull BlockVec max();
+        BlockVec max();
     }
 
     sealed interface Sphere extends Area permits AreaImpl.Sphere {
-        @NotNull BlockVec center();
+        BlockVec center();
 
         int radius();
     }
