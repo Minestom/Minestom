38990ca8a4ea42f830250a084b74c4c2a45a0c52
Ders <168863093+Garrett-EMS@users.noreply.github.com>
2025-07-27 14:44:49 -0600

    fix javadocs
    Signed-off-by: Ders <168863093+Garrett-EMS@users.noreply.github.com>

--- src/main/java/net/minestom/server/instance/InstanceContainer.java
+++ src/main/java/net/minestom/server/instance/InstanceContainer.java
@@ -18,7 +18,6 @@ import net.minestom.server.event.instance.InstanceChunkUnloadEvent;
 import net.minestom.server.event.player.PlayerBlockBreakEvent;
 import net.minestom.server.instance.anvil.AnvilLoader;
 import net.minestom.server.instance.block.Block;
-import net.minestom.server.instance.block.BlockFace;
 import net.minestom.server.instance.block.BlockChange;
 import net.minestom.server.instance.block.BlockHandler;
 import net.minestom.server.instance.block.rule.BlockPlacementRule;
@@ -137,10 +136,8 @@ public class InstanceContainer extends Instance {
      * Unsafe because the method is not synchronized and it does not verify if the chunk is loaded or not.
      *
      * @param chunk the {@link Chunk} which should be loaded
-     * @param x     the block X
-     * @param y     the block Y
-     * @param z     the block Z
-     * @param block the block to place
+     * @param mutation the {@link BlockChange} being applied to the block
+     * @param doBlockUpdates whether this should update blocks around it
      */
     private synchronized void UNSAFE_setBlock(@NotNull Chunk chunk, @NotNull BlockChange mutation, boolean doBlockUpdates) {
         if (chunk.isReadOnly()) return;
