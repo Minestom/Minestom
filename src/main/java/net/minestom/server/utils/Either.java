47ac6034a6987619de5d3f700f0f375034ef0a19
kermandev <46640204+kermandev@users.noreply.github.com>
2025-08-20 11:06:47 -0500

    Enforce Either

--- src/main/java/net/minestom/server/utils/Either.java
+++ src/main/java/net/minestom/server/utils/Either.java
@@ -1,5 +1,7 @@
 package net.minestom.server.utils;
 
+import java.util.Objects;
+
 public sealed interface Either<L, R> {
 
     static <L, R> Either<L, R> left(L value) {
@@ -11,9 +13,15 @@ public sealed interface Either<L, R> {
     }
 
     record Left<L, R>(L value) implements Either<L, R> {
+        public Left {
+            Objects.requireNonNull(value, "Left value must not be null");
+        }
     }
 
     record Right<L, R>(R value) implements Either<L, R> {
+        public Right {
+            Objects.requireNonNull(value, "Right value must not be null");
+        }
     }
 
 }
