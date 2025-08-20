eca275f7ba8ee432fe08c1ee36debf0f6b0a2b63
kermandev <46640204+kermandev@users.noreply.github.com>
2025-08-20 11:06:41 -0500

    Codec docs

--- src/main/java/net/minestom/server/codec/Codec.java
+++ src/main/java/net/minestom/server/codec/Codec.java
@@ -25,13 +25,19 @@ import java.util.function.Supplier;
 /**
  * <p>
  *     A {@link Codec} represents a combined {@link Encoder} and {@link Decoder} for a value.
- *     Enabling easy encoding and decoding of values to and from a between formats, making serialization simple, reuseable and type safe.
+ *     Enabling easy encoding and decoding of values to and from a between formats, making serialization simple, reusable and type safe.
  * </p>
  * <p>
  *     Most of the primitive or commonly used codecs are provided as static fields in this interface.
  *     For example, {@link Codec#INT} is a codec for integers, and {@link Codec#STRING} is a codec for strings.
  *     You can even use {@link Codec#Enum(Class)} for enums, which will convert the enum to a string
  *     representation and back.
+ * </p>
+ * <p>
+ *     Codecs are immutable, you must chain methods to create a codec that you want. For example
+ *     <pre>{@code
+ *         Codec<@Nullable String> optionalStringCodec = Codec.STRING.optional()
+ *     }</pre>
  * </p>
  * <p>
  *     Heavily inspired by <a href="https://github.com/Mojang/DataFixerUpper">Mojang/DataFixerUpper</a>,
@@ -43,11 +49,33 @@ import java.util.function.Supplier;
 @ApiStatus.Experimental
 public interface Codec<T extends @UnknownNullability Object> extends Encoder<T>, Decoder<T> {
 
+    /**
+     * A raw value wrapper for entry is an object combined with its current decoder.
+     * Allows converting of an intermediary object of a transcoder into the requested transcoder.
+     * <br>
+     * Useful when dealing with objects that have the same type required as their transcoder
+     * for example NBT and JSON.
+     */
     sealed interface RawValue permits CodecImpl.RawValueImpl {
+        /**
+         * Creates a RawValue instance
+         * @param coder the transcoder
+         * @param value the value
+         * @return the new raw value instance
+         * @param <D> The Object type
+         */
+        @Contract(pure = true)
         static <D> RawValue of(Transcoder<D> coder, D value) {
             return new CodecImpl.RawValueImpl<>(coder, value);
         }
 
+        /**
+         * Converts the current value into another transcoder
+         * @param coder the transcoder to convert the object into
+         * @return the {@link Result} of converting to {@code coder}.
+         * @param <D> the resultant type; transcoder type.
+         */
+        @Contract(pure = true)
         <D> Result<D> convertTo(Transcoder<D> coder);
     }
 
@@ -103,6 +131,15 @@ public interface Codec<T extends @UnknownNullability Object> extends Encoder<T>,
         return compound;
     }, compound -> compound);
 
+    /**
+     * Creates an enum codec from a given class
+     * <br>
+     * Converts the {@link Enum#name()} into lowercase when encoding
+     * and uppercase into decoding then passing it to {@link Enum#valueOf(Class, String)}
+     * @param enumClass the enum class
+     * @return the codec enum
+     * @param <E> Enum type, E must be an enum
+     */
     @Contract(pure = true)
     static <E extends Enum<E>> Codec<E> Enum(Class<E> enumClass) {
         return STRING.transform(
@@ -110,16 +147,41 @@ public interface Codec<T extends @UnknownNullability Object> extends Encoder<T>,
                 value -> value.name().toLowerCase(Locale.ROOT));
     }
 
+    /**
+     * Create a recursive codec from the parent codec
+     * <br>
+     * Useful when you want to keep encoding/decoding until there is nothing left.
+     * @param func the function to get the codec from.
+     * @return the recursive codec
+     * @param <T> The codec Type
+     */
     @Contract(pure = true)
     static <T> Codec<T> Recursive(Function<Codec<T>, Codec<T>> func) {
         return new CodecImpl.RecursiveImpl<>(func).delegate;
     }
 
+    /**
+     * Lazily gets the reference of a codec; considered immutably lazy.
+     * <br>
+     * Useful for breaking possible cyclic loading of recursive codecs.
+     * This may become a stable value in the future; don't rely on supplier getting called multiple times.
+     * @param supplier the supplier to load the codec from.
+     * @return the supplier
+     * @param <T> the codec type
+     */
     @Contract(pure = true)
-    static <T> Codec<T> ForwardRef(Supplier<Codec<T>> func) {
-        return new CodecImpl.ForwardRefImpl<>(func);
+    static <T> Codec<T> ForwardRef(Supplier<Codec<T>> supplier) {
+        return new CodecImpl.ForwardRefImpl<>(supplier);
     }
 
+    /**
+     * Shortcut for {@link Codec#RegistryTaggedUnion(Registries.Selector, Function, String)}
+     * @param registry the codec registry
+     * @param serializerGetter the codec getter
+     * @param key the map key
+     * @return a {@link StructCodec}
+     * @param <T> the struct codec type.
+     */
     @Contract(pure = true)
     static <T> StructCodec<T> RegistryTaggedUnion(
             Registry<StructCodec<? extends T>> registry,
@@ -129,6 +191,17 @@ public interface Codec<T extends @UnknownNullability Object> extends Encoder<T>,
         return Codec.RegistryTaggedUnion((ignored) -> registry, serializerGetter, key);
     }
 
+    /**
+     * Creates a {@link StructCodec} to bidirectionally map values of {@link T} to their encoded values
+     * <br>
+     * Registry selectors will be used to lookup values of codecs of {@link T}.
+     * Then will be used to map to object {@link T} from {@code key}
+     * @param registrySelector the registery selector used during lookup.
+     * @param serializerGetter the serializer for each value of {@link T}
+     * @param key the map key for {@link T}
+     * @return a {@link StructCodec} bidirectionally mapping values of {@link T}
+     * @param <T> the codec type
+     */
     @Contract(pure = true)
     static <T> StructCodec<T> RegistryTaggedUnion(
             Registries.Selector<StructCodec<? extends T>> registrySelector,
@@ -138,73 +211,161 @@ public interface Codec<T extends @UnknownNullability Object> extends Encoder<T>,
         return new CodecImpl.RegistryTaggedUnionImpl<>(registrySelector, serializerGetter, key);
     }
 
+    /**
+     * Creates an Either Codec, depending on the value of Either decides which codec to use.
+     * @param leftCodec the left codec
+     * @param rightCodec the right codec
+     * @return a {@link Codec} with {@link Either} of {@link L} and {@link R}
+     * @param <L> the left type
+     * @param <R> the right type
+     */
     @Contract(pure = true)
-    static <L, R> Codec<@UnknownNullability Either<L, R>> Either(Codec<L> leftCodec, Codec<R> rightCodec) {
+    static <L, R> Codec<Either<L, R>> Either(Codec<L> leftCodec, Codec<R> rightCodec) {
         return new CodecImpl.EitherImpl<>(leftCodec, rightCodec);
     }
 
+    /**
+     * Creates an optional codec, where null is encodable into {@link Transcoder#createNull()}.
+     * @return the optional codec of type {@link T}
+     */
     @Contract(pure = true)
     default Codec<@Nullable T> optional() {
         return new CodecImpl.OptionalImpl<>(this, null);
     }
 
+    /**
+     * Creates an optional codec, where null is encodable
+     * and is encoded when value equals {@code defaultValue} or null through {@link Transcoder#createNull()}.
+     * <br>
+     * The default value will be used if the decoding is null or fails to decode.
+     * @param defaultValue the default value
+     * @throws NullPointerException if defaultValue is null, use {@link #optional()} instead.
+     * @return the optional codec of type {@link T}
+     */
     @Contract(pure = true)
     default Codec<@UnknownNullability T> optional(T defaultValue) {
-        // We really have no idea what nullability this will have as optional still accepts null values, but we may never return null
+        // We really have no idea what nullability this will have as optional still accepts null values, but the default value could never be null
         return new CodecImpl.OptionalImpl<>(this, Objects.requireNonNull(defaultValue, "Default value cannot be null"));
     }
 
+    /**
+     * Transforms an object from {@link S} to {@link T} or from {@link T} to {@link S}
+     * @param to the function to {@link S} from {@link T}
+     * @param from the function from {@link S} to {@link T}
+     * @return the transforming codec of {@link S}
+     * @param <S> the type
+     */
     @Contract(pure = true)
     default <S extends @UnknownNullability Object> Codec<S> transform(ThrowingFunction<T, S> to, ThrowingFunction<S, T> from) {
         return new CodecImpl.TransformImpl<>(this, to, from);
     }
 
+    /**
+     * Creates a list codec of {@link T} where its size is no larger than {@code maxSize}.
+     * @param maxSize the max size of the list before returning an error result.
+     * @return the list codec of type {@link T}
+     */
     @Contract(pure = true)
     default Codec<List<T>> list(int maxSize) {
         return new CodecImpl.ListImpl<>(this, maxSize);
     }
 
+    /**
+     * Creates an unbounded list codec. See {@link #list(int)}
+     * @return the unbounded list codec of type {@link T}
+     */
     @Contract(pure = true)
     default Codec<List<T>> list() {
         return list(Integer.MAX_VALUE);
     }
 
+    /**
+     * Returns a list or the first element or null if no such element exists.
+     * @param maxSize the max size of the list before returning an error result
+     * @return the list codec of type {@link T}
+     */
     @Contract(pure = true)
-    default Codec<List<T>> listOrSingle(int maxSize) {
+    default Codec<List<@Nullable T>> listOrSingle(int maxSize) {
         return Codec.this.list(maxSize).orElse(Codec.this.transform(
                 List::of, list -> list.isEmpty() ? null : list.getFirst()));
     }
 
+    /**
+     * Returns an unbounded list or the first element or null if no such element exists.
+     * See {@link #listOrSingle(int)}
+     * @return the list codec of type {@link T}
+     */
     @Contract(pure = true)
-    default Codec<List<T>> listOrSingle() {
+    default Codec<List<@Nullable T>> listOrSingle() {
         return this.listOrSingle(Integer.MAX_VALUE);
     }
 
+    /**
+     * Creates a set where its max is no larger than {@code maxSize}
+     * @param maxSize the max size before returning an error result
+     * @return the set codec of type {@link T}
+     */
     @Contract(pure = true)
     default Codec<Set<T>> set(int maxSize) {
         return new CodecImpl.SetImpl<>(Codec.this, maxSize);
     }
 
+    /**
+     * Creates an unbounded set. See {@link #set(int)}
+     * @return the set codec of type {@link T}
+     */
     @Contract(pure = true)
     default Codec<Set<T>> set() {
         return set(Integer.MAX_VALUE);
     }
 
+    /**
+     * Creates a map of key {@link T} and value of {@link V}
+     * @param valueCodec the codec to use for {@link V}
+     * @param maxSize the max size before returning an error result.
+     * @return the map codec of type {@link T} and {@link V}
+     * @param <V> the value type
+     */
     @Contract(pure = true)
     default <V> Codec<Map<T, V>> mapValue(Codec<V> valueCodec, int maxSize) {
         return new CodecImpl.MapImpl<>(Codec.this, valueCodec, maxSize);
     }
 
+    /**
+     * Creates a map of key {@link T} and value of {@link V}. See {@link #mapValue(Codec, int)}
+     * @param valueCodec the codec to use for {@link V}
+     * @return the map codec of type {@link T} and {@link V}
+     * @param <V> the value type
+     */
     @Contract(pure = true)
     default <V> Codec<Map<T, V>> mapValue(Codec<V> valueCodec) {
         return mapValue(valueCodec, Integer.MAX_VALUE);
     }
 
+    /**
+     *
+     * @param serializers
+     * @param keyFunc
+     * @return
+     * @param <R>
+     * @param <TR>
+     */
     @Contract(pure = true)
     default <R, TR extends R> StructCodec<R> unionType(Function<T, StructCodec<TR>> serializers, Function<R, ? extends T> keyFunc) {
         return unionType("type", serializers, keyFunc);
     }
 
+    /**
+     * Creates a union type of type R
+     * <br>
+     * Useful when you have an interface of {@link T} and want a codec of {@link T}
+     * @param keyField the map key
+     * @param serializers
+     * @param keyFunc
+     * @return
+     * @param <R>
+     * @param <TR>
+     */
     @Contract(pure = true)
     default <R, TR extends R> StructCodec<R> unionType(String keyField, Function<T, StructCodec<TR>> serializers, Function<R, ? extends T> keyFunc) {
         return new CodecImpl.UnionImpl<>(keyField, this, serializers, keyFunc);
