85b6e586ace2e8b7d0e6be27849be5b02ec73e34
kermandev <46640204+kermandev@users.noreply.github.com>
2025-08-20 12:07:58 -0500

    Encoder/Decoder docs

--- src/main/java/net/minestom/server/codec/Encoder.java
+++ src/main/java/net/minestom/server/codec/Encoder.java
@@ -1,12 +1,35 @@
 package net.minestom.server.codec;
 
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.UnknownNullability;
 
-@ApiStatus.Experimental
+/**
+ * Encoders are interfaces used in {@link Codec} which purpose is to encode any value of {@link T}
+ * with a transcoder.
+ * <br>
+ * For example:
+ * <pre>{@code
+ * record Name(String imTheBoss) { }
+ * Encoder<Name> encoder = new Encoder<>() {
+ *     @Override
+ *     public <D> Result<D> encode(Transcoder<D> coder, @Nullable Name value) {
+ *         if (value == null) return new Result.Error<>("null");
+ *         return new Result.Ok<>(coder.createString(value.imTheBoss()));
+ *     }
+ * };
+ * Result<BinaryTag> result = encoder.encode(Transcoder.NBT, new Name("me")); // Result.OK(StringBinaryTag("me"))
+ * Result<BinaryTag> errorResult = encoder.encode(Transcoder.NBT, null); // Result.Error("null")
+ * }</pre>
+ * @param <T> the value type
+ */
+@FunctionalInterface
 public interface Encoder<T extends @UnknownNullability Object> {
 
+    /**
+     * Creates an empty encoder that only encodes null
+     * @return the empty encoder
+     * @param <T> the encoder type
+     */
     static <T> Encoder<T> empty() {
         return new Encoder<>() {
             @Override
@@ -16,6 +39,15 @@ public interface Encoder<T extends @UnknownNullability Object> {
         };
     }
 
+    /**
+     * Encodes a value of {@link T} using the specific {@link Transcoder}
+     * <br>
+     * The {@link Result} will be of {@link Result.Ok} or {@link Result.Error} and its typed {@link D}
+     * @param coder the transcoder to use
+     * @param value the value to encode
+     * @return the {@link Result} of the encoding with its type determined by the transcoder
+     * @param <D> The resultant type
+     */
     <D> Result<D> encode(Transcoder<D> coder, @Nullable T value);
 
 }
--- src/test/java/net/minestom/server/codec/CodecDocumentationTest.java
+++ src/test/java/net/minestom/server/codec/CodecDocumentationTest.java
@@ -1,8 +1,12 @@
 package net.minestom.server.codec;
 
 import com.google.gson.JsonElement;
+import net.kyori.adventure.nbt.BinaryTag;
+import net.kyori.adventure.nbt.EndBinaryTag;
+import net.kyori.adventure.nbt.StringBinaryTag;
 import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -27,5 +31,36 @@ public final class CodecDocumentationTest {
         MyType decoded = MyType.CODEC.decode(Transcoder.JSON, encoded).orElseThrow();
 
         Assertions.assertEquals(value, decoded);
+    }
+
+    @Test
+    public void testEncoderExample() {
+        record Name(String imTheBoss) { }
+        Encoder<Name> encoder = new Encoder<>() {
+            @Override
+            public <D> Result<D> encode(Transcoder<D> coder, @Nullable Name value) {
+                if (value == null) return new Result.Error<>("null");
+                return new Result.Ok<>(coder.createString(value.imTheBoss()));
+            }
+        };
+        Result<BinaryTag> result = encoder.encode(Transcoder.NBT, new Name("me"));
+        Result<BinaryTag> errorResult = encoder.encode(Transcoder.NBT, null);
+        Assertions.assertEquals(StringBinaryTag.stringBinaryTag("me"), result.orElseThrow());
+        CodecAssertions.assertError("null", errorResult);
+    }
+
+    @Test
+    public void testDecodingExample() {
+        record Name(String imTheBoss) { }
+        Decoder<Name> decoder = new Decoder<>() {
+            @Override
+            public <D> Result<Name> decode(Transcoder<D> coder, D value) {
+                return coder.getString(value).mapResult(Name::new);
+            }
+        };
+        Result<Name> result = decoder.decode(Transcoder.NBT, StringBinaryTag.stringBinaryTag("me"));
+        Result<Name> errorResult = decoder.decode(Transcoder.NBT, EndBinaryTag.endBinaryTag());
+        Assertions.assertEquals(new Name("me"), result.orElseThrow());
+        Assertions.assertInstanceOf(Result.Error.class, errorResult);
     }
 }
