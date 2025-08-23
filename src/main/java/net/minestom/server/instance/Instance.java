dc33bcbaa3753282f373af9300540241bc7b7abf
GreatWyrm <alecmusante@gmail.com>
2025-08-22 23:00:23 -0700

    Update chunk view distance to add 1 to the view distance to load additional chunks, add per-instance view distance

--- src/main/java/net/minestom/server/instance/Instance.java
+++ src/main/java/net/minestom/server/instance/Instance.java
@@ -119,6 +119,8 @@ public abstract class Instance implements Block.Getter, Block.Setter,
 
     private final ChunkCache blockRetriever = new ChunkCache(this, null, null);
 
+    protected int chunkViewDistance = ServerFlag.CHUNK_VIEW_DISTANCE;
+
     // the uuid of this instance
     protected UUID uuid;
 
@@ -880,6 +882,22 @@ public abstract class Instance implements Block.Getter, Block.Setter,
         float rainLevel = current.rainLevel() + (target.rainLevel() - current.rainLevel()) * (1 / (float) Math.max(1, remainingRainTransitionTicks));
         float thunderLevel = current.thunderLevel() + (target.thunderLevel() - current.thunderLevel()) * (1 / (float) Math.max(1, remainingThunderTransitionTicks));
         return new Weather(rainLevel, thunderLevel);
+    }
+
+    /**
+     * Gets the chunk view distance of this instance, which defaults to {@link ServerFlag#CHUNK_VIEW_DISTANCE}.
+     * @return The chunk view distance of this instance
+     */
+    public int viewDistance() {
+        return this.chunkViewDistance;
+    }
+
+    /**
+     * Sets the chunk view distance of this instance
+     * @param newViewDistance the new view distance
+     */
+    public void viewDistance(int newViewDistance) {
+        this.chunkViewDistance = newViewDistance;
     }
 
     /**
