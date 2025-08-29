d6f406c704f4bf176aea0ef6fba2b6096332c866
GreatWyrm <alecmusante@gmail.com>
2025-08-28 23:46:52 -0700

    Add the pack UUID to the status event

--- src/main/java/net/minestom/server/event/player/PlayerResourcePackStatusEvent.java
+++ src/main/java/net/minestom/server/event/player/PlayerResourcePackStatusEvent.java
@@ -4,6 +4,8 @@ import net.kyori.adventure.resource.ResourcePackStatus;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.trait.PlayerEvent;
 
+import java.util.UUID;
+
 /**
  * Called when a player warns the server of a resource pack status.
  */
@@ -11,10 +13,12 @@ public class PlayerResourcePackStatusEvent implements PlayerEvent {
 
     private final Player player;
     private final ResourcePackStatus status;
+    private final UUID packUUID;
 
-    public PlayerResourcePackStatusEvent(Player player, ResourcePackStatus status) {
+    public PlayerResourcePackStatusEvent(Player player, UUID packUUID, ResourcePackStatus status) {
         this.player = player;
         this.status = status;
+        this.packUUID = packUUID;
     }
 
     /**
@@ -24,6 +28,14 @@ public class PlayerResourcePackStatusEvent implements PlayerEvent {
      */
     public ResourcePackStatus getStatus() {
         return status;
+    }
+
+    /**
+     * Gets the associated pack UUID that has resolved on the client with the particular status
+     * @return the UUID of the resource pack
+     */
+    public UUID getPackUUID() {
+        return packUUID;
     }
 
     @Override
