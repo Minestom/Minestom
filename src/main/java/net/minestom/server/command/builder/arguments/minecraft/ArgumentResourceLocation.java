1ba3842bc6b6a4cd7e5b150ef8848ce22a929b4b
WithLithum <WithLithum@outlook.com>
2025-09-04 20:32:18 +0800

    Modify ArgumentResourceLocation to support Adventure Key type
    This commit modifies the ArgumentResourceLocation class to support the Key values from the Adventure library.
    
    Prior to this commit, the resource location argument implementation merely checks if there are any space characters in the input.
    
    BREAKING CHANGE: All malformed resource location values will now be rejected and ArgumentResourceLocation now returns Key from Adventure library.
    
    Signed-off-by: WithLithum <WithLithum@outlook.com>

--- src/main/java/net/minestom/server/command/builder/arguments/minecraft/ArgumentResourceLocation.java
+++ src/main/java/net/minestom/server/command/builder/arguments/minecraft/ArgumentResourceLocation.java
@@ -1,25 +1,26 @@
 package net.minestom.server.command.builder.arguments.minecraft;
 
+import net.kyori.adventure.key.KeyPattern;
 import net.minestom.server.command.ArgumentParserType;
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.arguments.Argument;
 import net.minestom.server.command.builder.exception.ArgumentSyntaxException;
-import net.minestom.server.utils.StringUtils;
+import net.kyori.adventure.key.Key;
 
-public class ArgumentResourceLocation extends Argument<String> {
+public class ArgumentResourceLocation extends Argument<Key> {
 
-    public static final int SPACE_ERROR = 1;
+    public static final int PARSE_ERROR = 1;
 
     public ArgumentResourceLocation(String id) {
         super(id);
     }
 
     @Override
-    public String parse(CommandSender sender, String input) throws ArgumentSyntaxException {
-        if (input.contains(StringUtils.SPACE))
-            throw new ArgumentSyntaxException("Resource location cannot contain space character", input, SPACE_ERROR);
+    public Key parse(CommandSender sender, @KeyPattern String input) throws ArgumentSyntaxException {
+        if (!Key.parseable(input))
+            throw new ArgumentSyntaxException("Invalid resource location", input, PARSE_ERROR);
 
-        return input;
+        return Key.key(input);
     }
 
     @Override
