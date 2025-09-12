29e76812ec2ef0567f92160c07644b2b163b9533
kermandev <46640204+kermandev@users.noreply.github.com>
2025-09-12 10:28:30 -0500

    Use trim instead of copy where plausible

--- src/main/java/net/minestom/server/utils/PacketViewableUtils.java
+++ src/main/java/net/minestom/server/utils/PacketViewableUtils.java
@@ -95,7 +95,7 @@ public final class PacketViewableUtils {
 
         private synchronized void process(Viewable viewable) {
             if (buffer.writeIndex() == 0) return;
-            final NetworkBuffer copy = buffer.copy(0, buffer.writeIndex()); // Used after release for writing (copy)
+            final NetworkBuffer copy = buffer.trim(); // Used after release for writing (copy)
             copy.readOnly();
             viewable.getViewers().forEach(player -> processPlayer(player, copy));
             this.buffer.clear();
