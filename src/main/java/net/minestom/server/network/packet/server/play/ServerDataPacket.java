dcb25453985edeae33bc04fe96cdc4c70ca482b1
Kerman <46640204+kermandev@users.noreply.github.com>
2025-09-12 19:54:37 -0500

    Resolve #2898 #2903 #2904 #2720 #2748 & more (#2909)

--- src/main/java/net/minestom/server/network/packet/server/play/ServerDataPacket.java
+++ src/main/java/net/minestom/server/network/packet/server/play/ServerDataPacket.java
@@ -8,11 +8,29 @@ import org.jetbrains.annotations.Nullable;
 
 import static net.minestom.server.network.NetworkBuffer.*;
 
-public record ServerDataPacket(Component motd, byte @Nullable [] iconBase64,
-                               boolean enforcesSecureChat) implements ServerPacket.Play {
+public record ServerDataPacket(Component motd, byte @Nullable [] iconBase64) implements ServerPacket.Play {
     public static final NetworkBuffer.Type<ServerDataPacket> SERIALIZER = NetworkBufferTemplate.template(
             COMPONENT, ServerDataPacket::motd,
             BYTE_ARRAY.optional(), ServerDataPacket::iconBase64,
-            BOOLEAN, ServerDataPacket::enforcesSecureChat,
             ServerDataPacket::new);
+
+    /**
+     * No longer exists
+     * @deprecated Use {@link ServerDataPacket(Component, byte[])}
+     * @param motd motd
+     * @param iconBase64 icon
+     * @param forRemoval enforcesSecureChat
+     */
+    @Deprecated(forRemoval = true)
+    public ServerDataPacket(Component motd, @Nullable byte[] iconBase64, boolean forRemoval) {
+        this(motd, iconBase64);
+    }
+    /**
+     * No longer exists
+     * @deprecated Use {@link ServerDataPacket(Component, byte[])}
+     */
+    @Deprecated(forRemoval = true)
+    boolean enforcesSecureChat() {
+        return false;
+    }
 }
