d32f2a768896fd9d4a4f7192f4ee705f0ca2a00a
kermandev <46640204+kermandev@users.noreply.github.com>
2025-10-30 17:40:58 -0500

    Fix MessageSignature

--- src/main/java/net/minestom/server/crypto/MessageSignature.java
+++ src/main/java/net/minestom/server/crypto/MessageSignature.java
@@ -2,9 +2,12 @@ package net.minestom.server.crypto;
 
 import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.NetworkBufferTemplate;
+import net.minestom.server.utils.validate.Check;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.UnknownNullability;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 import static net.minestom.server.network.NetworkBuffer.FixedRawBytes;
 import static net.minestom.server.network.NetworkBuffer.VAR_INT;
@@ -24,21 +27,28 @@ public record MessageSignature(byte[] signature) {
     );
 
     public record Packed(int id, @UnknownNullability MessageSignature fullSignature) {
-        private Packed(Packed packed) {
-            this(packed.id, packed.fullSignature);
+        private static final int FULL_SIGNATURE = -1;
+
+        public Packed(MessageSignature signature) {
+            this(FULL_SIGNATURE, signature);
+        }
+
+        public Packed {
+            Check.argCondition(id == FULL_SIGNATURE && fullSignature == null, "Full signature must be present");
         }
 
         public static final NetworkBuffer.Type<Packed> SERIALIZER = new NetworkBuffer.Type<>() {
             @Override
             public void write(NetworkBuffer buffer, Packed value) {
                 buffer.write(VAR_INT, value.id + 1);
-                if (value.id == 0) buffer.write(MessageSignature.SERIALIZER, value.fullSignature);
+                if (value.fullSignature != null) buffer.write(MessageSignature.SERIALIZER, value.fullSignature);
             }
 
             @Override
             public Packed read(NetworkBuffer buffer) {
                 final int id = buffer.read(VAR_INT) - 1;
-                return new Packed(id, id == -1 ? buffer.read(MessageSignature.SERIALIZER) : null);
+                return id == FULL_SIGNATURE ? new MessageSignature.Packed(buffer.read(MessageSignature.SERIALIZER))
+                        : new MessageSignature.Packed(id, null);
             }
         };
     }
