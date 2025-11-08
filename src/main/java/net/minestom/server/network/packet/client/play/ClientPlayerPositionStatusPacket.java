c32b633f7855d8db5c2961f96ad29dbce7a649e5
kermandev <46640204+kermandev@users.noreply.github.com>
2025-11-07 20:58:06 -0600

    MagicConstants for position/rotation packets

--- src/main/java/net/minestom/server/network/packet/client/play/ClientPlayerPositionStatusPacket.java
+++ src/main/java/net/minestom/server/network/packet/client/play/ClientPlayerPositionStatusPacket.java
@@ -3,19 +3,21 @@ package net.minestom.server.network.packet.client.play;
 import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.NetworkBufferTemplate;
 import net.minestom.server.network.packet.client.ClientPacket;
+import org.intellij.lang.annotations.MagicConstant;
 
 import static net.minestom.server.network.NetworkBuffer.BYTE;
-import static net.minestom.server.network.packet.client.play.ClientPlayerPositionPacket.FLAG_HORIZONTAL_COLLISION;
-import static net.minestom.server.network.packet.client.play.ClientPlayerPositionPacket.FLAG_ON_GROUND;
 
-public record ClientPlayerPositionStatusPacket(byte flags) implements ClientPacket.Play {
+public record ClientPlayerPositionStatusPacket(@MagicConstant(flagsFromClass = ClientPlayerPositionStatusPacket.class) byte flags) implements ClientPacket.Play {
+    public static final byte FLAG_ON_GROUND = 1;
+    public static final byte FLAG_HORIZONTAL_COLLISION = 1 << 1;
+
     public static final NetworkBuffer.Type<ClientPlayerPositionStatusPacket> SERIALIZER = NetworkBufferTemplate.template(
             BYTE, ClientPlayerPositionStatusPacket::flags,
             ClientPlayerPositionStatusPacket::new);
 
     public ClientPlayerPositionStatusPacket(boolean onGround, boolean horizontalCollision) {
         this((byte) ((onGround ? FLAG_ON_GROUND : 0) |
-                (byte) (horizontalCollision ? FLAG_HORIZONTAL_COLLISION : 0)));
+                (horizontalCollision ? FLAG_HORIZONTAL_COLLISION : 0)));
     }
 
     public boolean onGround() {
--- src/main/java/net/minestom/server/network/packet/client/play/ClientPlayerRotationPacket.java
+++ src/main/java/net/minestom/server/network/packet/client/play/ClientPlayerRotationPacket.java
@@ -3,13 +3,15 @@ package net.minestom.server.network.packet.client.play;
 import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.NetworkBufferTemplate;
 import net.minestom.server.network.packet.client.ClientPacket;
+import org.intellij.lang.annotations.MagicConstant;
 
 import static net.minestom.server.network.NetworkBuffer.BYTE;
 import static net.minestom.server.network.NetworkBuffer.FLOAT;
-import static net.minestom.server.network.packet.client.play.ClientPlayerPositionPacket.FLAG_HORIZONTAL_COLLISION;
-import static net.minestom.server.network.packet.client.play.ClientPlayerPositionPacket.FLAG_ON_GROUND;
 
-public record ClientPlayerRotationPacket(float yaw, float pitch, byte flags) implements ClientPacket.Play {
+public record ClientPlayerRotationPacket(float yaw, float pitch, @MagicConstant(flagsFromClass = ClientPlayerRotationPacket.class) byte flags) implements ClientPacket.Play {
+    public static final byte FLAG_ON_GROUND = 1;
+    public static final byte FLAG_HORIZONTAL_COLLISION = 1 << 1;
+
     public static final NetworkBuffer.Type<ClientPlayerRotationPacket> SERIALIZER = NetworkBufferTemplate.template(
             FLOAT, ClientPlayerRotationPacket::yaw,
             FLOAT, ClientPlayerRotationPacket::pitch,
@@ -18,7 +20,7 @@ public record ClientPlayerRotationPacket(float yaw, float pitch, byte flags) imp
 
     public ClientPlayerRotationPacket(float yaw, float pitch, boolean onGround, boolean horizontalCollision) {
         this(yaw, pitch, (byte) ((onGround ? FLAG_ON_GROUND : 0) |
-                (byte) (horizontalCollision ? FLAG_HORIZONTAL_COLLISION : 0)));
+                (horizontalCollision ? FLAG_HORIZONTAL_COLLISION : 0)));
     }
 
     public boolean onGround() {
