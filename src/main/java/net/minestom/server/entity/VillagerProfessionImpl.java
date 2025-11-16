2a8c0e72d0e6b6c8ddb8ce86be8a554fb33745a4
kermandev <46640204+kermandev@users.noreply.github.com>
2025-11-16 16:24:20 -0600

    Make VillagerProfession use its registry key

--- src/main/java/net/minestom/server/entity/VillagerProfessionImpl.java
+++ src/main/java/net/minestom/server/entity/VillagerProfessionImpl.java
@@ -1,12 +1,13 @@
 package net.minestom.server.entity;
 
+import net.minestom.server.registry.BuiltinRegistries;
 import net.minestom.server.registry.Registry;
 import net.minestom.server.registry.RegistryData;
 import net.minestom.server.registry.RegistryKey;
 import org.jetbrains.annotations.UnknownNullability;
 
 public record VillagerProfessionImpl(RegistryData.VillagerProfessionEntry registry) implements VillagerProfession {
-    static final Registry<VillagerProfession> REGISTRY = RegistryData.createStaticRegistry(RegistryKey.unsafeOf("villager_profession"),
+    static final Registry<VillagerProfession> REGISTRY = RegistryData.createStaticRegistry(BuiltinRegistries.VILLAGER_PROFESSION,
             (namespace, properties) -> new VillagerProfessionImpl(RegistryData.villagerProfession(namespace, properties)));
 
     static @UnknownNullability VillagerProfession get(RegistryKey<VillagerProfession> key) {
