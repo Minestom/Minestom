8a4ef3a25cdffde25456e6f2afa06377f82dd3c1
TropicalShadow <me@tesseract.club>
2025-11-24 23:19:10 +0000

    Update src/main/java/net/minestom/server/instance/batch/RelativeBlockBatch.java
    Co-authored-by: SorkoPiko <me@sorko.dev>

--- src/main/java/net/minestom/server/instance/batch/RelativeBlockBatch.java
+++ src/main/java/net/minestom/server/instance/batch/RelativeBlockBatch.java
@@ -123,8 +123,7 @@ public class RelativeBlockBatch implements Batch<Consumer<AbsoluteBlockBatch>> {
      * @param callback The callback to be executed when the batch is applied
      * @return The inverse of this batch, if inverse is enabled in the {@link BatchOption}
      */
-    @Nullable
-    public AbsoluteBlockBatch apply(Instance instance, int x, int y, int z, @Nullable Consumer<@Nullable AbsoluteBlockBatch> callback) {
+    public @Nullable AbsoluteBlockBatch apply(Instance instance, int x, int y, int z, @Nullable Consumer<@Nullable AbsoluteBlockBatch> callback) {
         return apply(instance, x, y, z, callback, true);
     }
 
