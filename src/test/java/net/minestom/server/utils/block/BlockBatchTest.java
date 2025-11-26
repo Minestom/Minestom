965e82c546fcb08713f7ba39587af17a777e72f2
TropicalShadow <me@tesseract.club>
2025-11-26 15:17:08 +0000

    test: simplify latch await assertions to avoid InterruptedException

--- src/test/java/net/minestom/server/utils/block/BlockBatchTest.java
+++ src/test/java/net/minestom/server/utils/block/BlockBatchTest.java
@@ -51,11 +51,7 @@ public class BlockBatchTest {
             latch.countDown();
         });
         env.tickWhile(() -> latch.getCount() > 0, Duration.ofSeconds(1));
-        try {
-            Assertions.assertTrue(latch.await(1, TimeUnit.SECONDS));
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        Assertions.assertDoesNotThrow(()->Assertions.assertTrue(latch.await(1, TimeUnit.SECONDS)));
     }
 
     @Test
@@ -72,11 +68,7 @@ public class BlockBatchTest {
             latch.countDown();
         });
         env.tickWhile(() -> latch.getCount() > 0, Duration.ofSeconds(1));
-        try {
-            Assertions.assertTrue(latch.await(1, TimeUnit.SECONDS));
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        Assertions.assertDoesNotThrow(()->Assertions.assertTrue(latch.await(1, TimeUnit.SECONDS)));
     }
 
     @Test
@@ -93,10 +85,6 @@ public class BlockBatchTest {
             latch.countDown();
         });
         env.tickWhile(() -> latch.getCount() > 0, Duration.ofSeconds(1));
-        try {
-            Assertions.assertTrue(latch.await(1, TimeUnit.SECONDS));
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        Assertions.assertDoesNotThrow(()->Assertions.assertTrue(latch.await(1, TimeUnit.SECONDS)));
     }
 }
