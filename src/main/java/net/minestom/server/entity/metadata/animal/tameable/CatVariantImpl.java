5978a61f295533faa947f03b1c8a8a278c14977a
kermandev <46640204+kermandev@users.noreply.github.com>
2026-03-08 21:17:10 -0500

    feat!: Fix existing registries

--- src/main/java/net/minestom/server/entity/metadata/animal/tameable/CatVariantImpl.java
+++ src/main/java/net/minestom/server/entity/metadata/animal/tameable/CatVariantImpl.java
@@ -3,9 +3,10 @@ package net.minestom.server.entity.metadata.animal.tameable;
 import net.kyori.adventure.key.Key;
 import net.minestom.server.utils.validate.Check;
 
-record CatVariantImpl(Key assetId) implements CatVariant {
+record CatVariantImpl(Key assetId, Key babyAssetId) implements CatVariant {
 
     public CatVariantImpl {
         Check.notNull(assetId, "assetId");
+        Check.notNull(babyAssetId, "babyAssetId");
     }
 }
