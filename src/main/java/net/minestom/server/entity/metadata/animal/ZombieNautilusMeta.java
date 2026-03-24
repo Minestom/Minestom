3780cf5e652d0fefb817693632ba2e718d83e178
ThatGravyBoat <gravy@thatgravyboat.tech>
2026-03-24 18:46:47 -0230

    fix: incorrect meta for various entities (#3063)
    * fix: shulker meta not accounting for uncolored
    
    * fix: panda meta genes being incorrectly ordered
    
    * fix: incorrect bee metadata names
    
    * fix: incorrect llama meta
    
    * fix: trader llama meta not extending llama
    
    * fix: zombie nautilus data component
    
    * fix: zombie villager meta not extending zombie and using villager meta for data field

--- src/main/java/net/minestom/server/entity/metadata/animal/ZombieNautilusMeta.java
+++ src/main/java/net/minestom/server/entity/metadata/animal/ZombieNautilusMeta.java
@@ -1,21 +1,48 @@
 package net.minestom.server.entity.metadata.animal;
 
+import net.minestom.server.component.DataComponent;
+import net.minestom.server.component.DataComponents;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.MetadataDef;
 import net.minestom.server.entity.MetadataHolder;
 import net.minestom.server.registry.RegistryKey;
+import org.jetbrains.annotations.Nullable;
 
 public class ZombieNautilusMeta extends AbstractNautilusMeta {
     public ZombieNautilusMeta(Entity entity, MetadataHolder metadata) {
         super(entity, metadata);
     }
 
+    /**
+     * @deprecated use {@link net.minestom.server.component.DataComponents#ZOMBIE_NAUTILUS_VARIANT} instead.
+     */
+    @Deprecated
     public RegistryKey<ZombieNautilusVariant> getVariant() {
         return this.metadata.get(MetadataDef.ZombieNautilus.VARIANT);
     }
 
+    /**
+     * @deprecated use {@link net.minestom.server.component.DataComponents#ZOMBIE_NAUTILUS_VARIANT} instead.
+     */
+    @Deprecated
     public void setVariant(RegistryKey<ZombieNautilusVariant> value) {
         this.metadata.set(MetadataDef.ZombieNautilus.VARIANT, value);
     }
 
+    @Override
+    @SuppressWarnings("unchecked")
+    protected <T> @Nullable T get(DataComponent<T> component) {
+        if (component == DataComponents.ZOMBIE_NAUTILUS_VARIANT) {
+            return (T) getVariant();
+        }
+        return super.get(component);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected <T> void set(DataComponent<T> component, T value) {
+        if (component == DataComponents.ZOMBIE_NAUTILUS_VARIANT) {
+            setVariant((RegistryKey<ZombieNautilusVariant>) value);
+        } else super.set(component, value);
+    }
 }
