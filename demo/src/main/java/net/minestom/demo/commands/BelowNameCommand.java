77e58a3d0261834961af70623ef0e236404e69ff
thecodertommy <193368100+thecodertommy@users.noreply.github.com>
2026-03-25 00:50:46 -0600

    fix demo again

--- demo/src/main/java/net/minestom/demo/commands/BelowNameCommand.java
+++ demo/src/main/java/net/minestom/demo/commands/BelowNameCommand.java
@@ -6,7 +6,6 @@ import net.minestom.server.command.builder.arguments.Argument;
 import net.minestom.server.command.builder.arguments.ArgumentType;
 import net.minestom.server.command.builder.arguments.minecraft.ArgumentEntity;
 import net.minestom.server.entity.Player;
-import net.minestom.server.scoreboard.BelowNameTag;
 import net.minestom.server.scoreboard.Scoreboard;
 
 import static net.minestom.server.command.builder.arguments.ArgumentType.Literal;
@@ -28,7 +27,7 @@ public class BelowNameCommand extends Command {
             if (targetPlayer == null) return;
             belowNameTag.addViewer(player);
             Integer targetValue = context.get(value);
-            belowNameTag.updateScore(targetPlayer, targetValue);
+            belowNameTag.updateScore(targetPlayer.getUsername(), targetValue);
         }, Literal("set"), target, value);
     }
 }
