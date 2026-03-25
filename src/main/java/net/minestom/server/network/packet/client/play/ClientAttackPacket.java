88e0c9c8e808c63808b57bb77c7ad77646c0c909
Kerman <46640204+kermandev@users.noreply.github.com>
2026-03-25 13:04:25 -0500

    fix: Post merge changes

--- src/main/java/net/minestom/server/network/packet/client/play/ClientAttackPacket.java
+++ src/main/java/net/minestom/server/network/packet/client/play/ClientAttackPacket.java
@@ -4,7 +4,7 @@ import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.NetworkBufferTemplate;
 import net.minestom.server.network.packet.client.ClientPacket;
 
-public record ClientAttackPacket(int targetId) implements ClientPacket {
+public record ClientAttackPacket(int targetId) implements ClientPacket.Play {
     public static final NetworkBuffer.Type<ClientAttackPacket> SERIALIZER = NetworkBufferTemplate.template(
             NetworkBuffer.VAR_INT, ClientAttackPacket::targetId,
             ClientAttackPacket::new
