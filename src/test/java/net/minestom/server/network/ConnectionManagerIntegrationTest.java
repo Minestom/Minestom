22ddc16122959fcfda51a8877d127f417d47b9d0
hapily <87914807+hapily04@users.noreply.github.com>
2026-04-01 21:35:28 -0500

    fix: findOnlinePlayer getting least similar result (#3085)

---
+++ src/test/java/net/minestom/server/network/ConnectionManagerIntegrationTest.java
@@ -0,0 +1,46 @@
+package net.minestom.server.network;
+
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import net.minestom.server.instance.Instance;
+import net.minestom.server.network.player.GameProfile;
+import net.minestom.testing.Env;
+import net.minestom.testing.EnvTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@EnvTest
+public class ConnectionManagerIntegrationTest {
+
+
+    private GameProfile[] profiles;
+
+    @BeforeEach
+    public void setup(Env env) {
+        profiles = new GameProfile[]{
+                new GameProfile(UUID.randomUUID(), "Minestom"),
+                new GameProfile(UUID.randomUUID(), "Notch")};
+    }
+
+    @Test
+    public void testPartialFind(Env env) {
+        Instance instance = env.createEmptyInstance();
+        Player minestomPlayer = env.createConnection(profiles[0]).connect(instance, Pos.ZERO);
+        ConnectionManager connectionManager = env.process().connection();
+
+        assertEquals(minestomPlayer, connectionManager.findOnlinePlayer("Mine"));
+        assertNull(connectionManager.findOnlinePlayer("No"));
+
+        Player notchPlayer = env.createConnection(profiles[1]).connect(instance, Pos.ZERO);
+
+        assertEquals(minestomPlayer, connectionManager.findOnlinePlayer("Mine"));
+        assertEquals(notchPlayer, connectionManager.findOnlinePlayer("No"));
+        assertNull(connectionManager.findOnlinePlayer("leo"));
+    }
+
+}
