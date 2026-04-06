e803727c298a8ab47fc3b72a74a133b8f16cb8ca
mworzala <mattheworzala@gmail.com>
2026-04-05 20:54:45 -0400

    chore: rebase

--- src/main/java/net/minestom/server/network/packet/client/play/ClientInteractEntityPacket.java
+++ src/main/java/net/minestom/server/network/packet/client/play/ClientInteractEntityPacket.java
@@ -8,7 +8,7 @@ import net.minestom.server.network.packet.client.ClientPacket;
 
 import static net.minestom.server.network.NetworkBuffer.*;
 
-public record ClientInteractEntityPacket(int targetId, PlayerHand hand, Vec location, boolean usingSecondaryAction) implements ClientPacket {
+public record ClientInteractEntityPacket(int targetId, PlayerHand hand, Vec location, boolean usingSecondaryAction) implements ClientPacket.Play {
     public static final NetworkBuffer.Type<ClientInteractEntityPacket> SERIALIZER = NetworkBufferTemplate.template(
             VAR_INT, ClientInteractEntityPacket::targetId,
             PlayerHand.NETWORK_TYPE, ClientInteractEntityPacket::hand,
