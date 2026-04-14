3cd6610653d110700cfe5cf57b1ac3c00ae2343b
Kerman <46640204+kermandev@users.noreply.github.com>
2026-04-14 11:36:25 -0500

    chore: Reduce diff

--- src/main/java/net/minestom/server/network/packet/client/ClientPacket.java
+++ src/main/java/net/minestom/server/network/packet/client/ClientPacket.java
@@ -6,17 +6,16 @@ package net.minestom.server.network.packet.client;
  * Packets are value-based, and should therefore not be reliant on identity.
  */
 public sealed interface ClientPacket {
-
     non-sealed interface Handshake extends ClientPacket {
-    }
-
-    non-sealed interface Configuration extends ClientPacket {
     }
 
     non-sealed interface Status extends ClientPacket {
     }
 
     non-sealed interface Login extends ClientPacket {
+    }
+
+    non-sealed interface Configuration extends ClientPacket {
     }
 
     non-sealed interface Play extends ClientPacket {
