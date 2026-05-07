da8f550a5f7b439efb086cd425da86698680fcf5
nnikitochka <asopiasoska@gmail.com>
2026-05-08 05:28:34 +0700

    тест

--- src/main/java/net/minestom/server/listener/PlayerActionListener.java
+++ src/main/java/net/minestom/server/listener/PlayerActionListener.java
@@ -71,8 +71,6 @@ public final class PlayerActionListener {
 
         // Prevent spectators and check players in adventure mode
         if (shouldPreventBreaking(player, block)) {
-            var playerTryStartDiggingEvent = new PlayerTryStartDiggingEvent(player, block, new BlockVec(blockPosition), blockFace);
-            EventDispatcher.call(playerTryStartDiggingEvent);
             return new DiggingResult(block, false);
         }
 
