57b1f58137953a147e61458bbf4981f09661b169
TheMode <themode@outlook.fr>
2026-05-11 17:15:19 +0200

    Codegen update

--- code-generators/src/main/java/net/minestom/codegen/ConstantsGenerator.java
+++ code-generators/src/main/java/net/minestom/codegen/ConstantsGenerator.java
@@ -1,34 +1,24 @@
 package net.minestom.codegen;
 
 import com.google.gson.JsonObject;
-import com.palantir.javapoet.*;
+import com.palantir.javapoet.ClassName;
+import com.palantir.javapoet.FieldSpec;
+import com.palantir.javapoet.TypeName;
+import com.palantir.javapoet.TypeSpec;
 
 import javax.lang.model.element.Modifier;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.file.Path;
-import java.util.Objects;
 
-public record ConstantsGenerator(InputStream constantsFile,
-                                 Path outputFolder) implements MinestomCodeGenerator {
-    public ConstantsGenerator {
-        Objects.requireNonNull(constantsFile, "Constants file cannot be null");
-        Objects.requireNonNull(outputFolder, "Output folder cannot be null");
-    }
+record ConstantsGenerator(Codegen codegen) {
 
-    @Override
-    public void generate() {
-        ensureDirectory(outputFolder);
-
-        final ClassName implCN = ClassName.get("net.minestom.server", "MinecraftServer");
-
-        // Important classes we use alot
-        JsonObject constants = GSON.fromJson(new InputStreamReader(constantsFile), JsonObject.class);
+    void generate() {
+        ClassName implCN = ClassName.get("net.minestom.server", "MinecraftServer");
         ClassName minecraftConstantsCN = ClassName.get("net.minestom.server", "MinecraftConstants");
+        JsonObject constants = codegen.objectResource("constants.json");
+
         TypeSpec.Builder constantsInterface = TypeSpec.interfaceBuilder(minecraftConstantsCN)
                 .addModifiers(Modifier.SEALED)
                 .addPermittedSubclass(implCN)
-                .addJavadoc(generateJavadoc(implCN));
+                .addJavadoc(codegen.constantsJavadoc(implCN));
 
         constantsInterface.addField(FieldSpec.builder(String.class, "VERSION_NAME")
                 .addModifiers(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL)
@@ -48,24 +38,18 @@ public record ConstantsGenerator(InputStream constantsFile,
         addMajorMinorField(constantsInterface, "RESOURCE_PACK_VERSION", constants.get("resourcepack").getAsString());
         addMajorMinorField(constantsInterface, "DATA_PACK_VERSION", constants.get("datapack").getAsString());
 
-        // Write files to outputFolder
-        writeFiles(JavaFile.builder("net.minestom.server", constantsInterface.build())
-                .indent("    ")
-                .skipJavaLangImports(true)
-                .build()
-        );
+        codegen.write(codegen.javaFile("net.minestom.server", constantsInterface.build()));
     }
 
     private static void addMajorMinorField(TypeSpec.Builder typeSpec, String name, String value) {
         String[] parts = value.split("\\.");
         if (parts.length != 2) throw new IllegalArgumentException("Invalid version format for " + name + ": " + value);
 
-        var majorMinorClass = ClassName.get("net.minestom.server.utils", "MajorMinorVersion");
+        ClassName majorMinorClass = ClassName.get("net.minestom.server.utils", "MajorMinorVersion");
         typeSpec.addField(FieldSpec.builder(majorMinorClass, name)
-                                            .addModifiers(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL)
-                                            .initializer("new $T($L, $L)", majorMinorClass, parts[0], parts[1])
-                                            .build()
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL)
+                .initializer("new $T($L, $L)", majorMinorClass, parts[0], parts[1])
+                .build()
         );
     }
-
 }
