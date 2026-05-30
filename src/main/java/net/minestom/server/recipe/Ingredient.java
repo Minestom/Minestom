e42c8e85437ef174b883c621efda79062efedba9
TheMode <themode@outlook.fr>
2026-05-31 01:40:00 +0200

    backward compat

--- src/main/java/net/minestom/server/recipe/Ingredient.java
+++ src/main/java/net/minestom/server/recipe/Ingredient.java
@@ -5,10 +5,27 @@ import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.NetworkBufferTemplate;
 import net.minestom.server.registry.Registries;
 import net.minestom.server.registry.RegistryTag;
+import net.minestom.server.utils.validate.Check;
+
+import java.util.List;
 
 public record Ingredient(RegistryTag<Material> tag) {
     public static final NetworkBuffer.Type<Ingredient> NETWORK_TYPE = NetworkBufferTemplate.template(
             RegistryTag.networkType(Registries::material), Ingredient::tag,
             Ingredient::new
     );
+
+    public Ingredient(Material... items) {
+        this(List.of(items));
+    }
+
+    public Ingredient(List<Material> items) {
+        this(toTag(items));
+    }
+
+    private static RegistryTag<Material> toTag(List<Material> items) {
+        Check.argCondition(items.isEmpty(), "Ingredients can't be empty");
+        Check.argCondition(items.contains(Material.AIR), "Ingredient can't contain air");
+        return RegistryTag.direct(items);
+    }
 }
