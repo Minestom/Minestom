4fe96fb4ef9396c0a4b258784e1529dda61fcf47
kermandev <46640204+kermandev@users.noreply.github.com>
2026-07-20 19:43:59 -0500

    fix: MotionBlockingHeightmap use fluid

--- src/test/java/net/minestom/server/instance/ChunkHeightmapIntegrationTest.java
+++ src/test/java/net/minestom/server/instance/ChunkHeightmapIntegrationTest.java
@@ -39,6 +39,35 @@ public class ChunkHeightmapIntegrationTest {
     }
 
     @Test
+    public void motionBlockingFluidTest(Env env) {
+        var instance = env.createFlatInstance();
+        instance.loadChunk(0, 0).join();
+        var chunk = instance.getChunk(0, 0);
+        var heightmap = chunk.motionBlockingHeightmap();
+
+        // Inherently waterlogged blocks count even without a "waterlogged" property
+        instance.setBlock(0, 45, 0, Block.KELP);
+        assertEquals(45, heightmap.getHeight(0, 0));
+
+        instance.setBlock(1, 45, 0, Block.SEAGRASS);
+        assertEquals(45, heightmap.getHeight(1, 0));
+
+        // Waterlogged state of a block without collision counts, dry state does not
+        instance.setBlock(2, 45, 0, Block.GLOW_LICHEN.withProperty("waterlogged", "true"));
+        assertEquals(45, heightmap.getHeight(2, 0));
+
+        instance.setBlock(3, 45, 0, Block.GLOW_LICHEN);
+        assertEquals(39, heightmap.getHeight(3, 0));
+
+        // Solid but not motion blocking
+        instance.setBlock(4, 45, 0, Block.COBWEB);
+        assertEquals(39, heightmap.getHeight(4, 0));
+
+        instance.setBlock(5, 45, 0, Block.BAMBOO_SAPLING);
+        assertEquals(39, heightmap.getHeight(5, 0));
+    }
+
+    @Test
     public void heightMapRemoveTest(Env env) {
         var instance = env.createFlatInstance();
         instance.loadChunk(0, 0).join();
