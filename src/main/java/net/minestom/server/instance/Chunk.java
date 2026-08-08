eb023af69bd3161833829c67a341485f92e848d0
Fisher2911 <fisher1205l@gmail.com>
2026-08-08 16:25:55 -0400

    Fix imports

--- src/main/java/net/minestom/server/instance/Chunk.java
+++ src/main/java/net/minestom/server/instance/Chunk.java
@@ -27,8 +27,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.function.BiConsumer;
-import java.util.function.BiPredicate;
 
 // TODO light data & API
 
--- src/test/java/net/minestom/server/instance/ChunkIntegrationTest.java
+++ src/test/java/net/minestom/server/instance/ChunkIntegrationTest.java
@@ -22,7 +22,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
