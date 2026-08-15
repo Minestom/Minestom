0aa6fea1c5e15ddbcc2aab261c720a068df86f30
mudkipdev <mudkip@mudkip.dev>
2026-08-15 11:36:05 -0600

    item pick up fix

--- src/main/java/net/minestom/server/entity/LivingEntity.java
+++ src/main/java/net/minestom/server/entity/LivingEntity.java
@@ -210,12 +210,16 @@ public class LivingEntity extends Entity implements EquipmentHandler {
         }
 
         // Items picking
-        if (canPickupItem() && itemPickupCooldown.isReady(time)) {
+        if (this instanceof Player player && player.getGameMode() == GameMode.SPECTATOR) {
+            return;
+        }
+
+        if (canPickupItem() && !isDead() && itemPickupCooldown.isReady(time)) {
             itemPickupCooldown.refreshLastUpdate(time);
             this.instance.getEntityTracker().nearbyEntities(position, expandedBoundingBox.width(),
                     EntityTracker.Target.ITEMS, itemEntity -> {
                         if (this instanceof Player player && !itemEntity.isViewer(player)) return;
-                        if (!itemEntity.isPickable()) return;
+                        if (!itemEntity.isPickable() || itemEntity.isRemoved()) return;
                         if (!expandedBoundingBox.intersectEntity(position, itemEntity)) return;
                         final PickupItemEvent pickupItemEvent = new PickupItemEvent(this, itemEntity);
                         EventDispatcher.callCancellable(pickupItemEvent, () -> {
--- src/main/java/net/minestom/server/entity/Player.java
+++ src/main/java/net/minestom/server/entity/Player.java
@@ -447,7 +447,7 @@ public class Player extends LivingEntity implements CommandSender, HoverEventSou
         super.update(time); // Super update (item pickup/fire management)
 
         // Experience orb pickup
-        if (experiencePickupCooldown.isReady(time)) {
+        if (gameMode != GameMode.SPECTATOR && experiencePickupCooldown.isReady(time)) {
             experiencePickupCooldown.refreshLastUpdate(time);
             this.instance.getEntityTracker().nearbyEntities(position, expandedBoundingBox.width(),
                     EntityTracker.Target.EXPERIENCE_ORBS, experienceOrb -> {
