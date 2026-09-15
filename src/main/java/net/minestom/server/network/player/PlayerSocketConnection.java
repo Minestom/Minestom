7da0b50f5bc2c9082801e3b2516a7c674d63728e
Kerman <46640204+kermandev@users.noreply.github.com>
2026-09-15 20:55:10 +0000

    fix: Player#sendPackets(Collection) generic bound should accept subtypes of SendablePacket (#3346)

--- src/main/java/net/minestom/server/network/player/PlayerSocketConnection.java
+++ src/main/java/net/minestom/server/network/player/PlayerSocketConnection.java
@@ -239,7 +239,7 @@ public class PlayerSocketConnection extends PlayerConnection {
     }
 
     @Override
-    public void sendPackets(Collection<SendablePacket> packets) {
+    public void sendPackets(Collection<? extends SendablePacket> packets) {
         for (SendablePacket packet : packets) this.packetQueue.relaxedOffer(packet);
         unlockWriteThread();
     }
